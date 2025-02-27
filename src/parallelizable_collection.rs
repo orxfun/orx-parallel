bf9976f1b421867bd3b914c5954e18f4cd47fd8a
orxfun <orx.ugur.arikan@gmail.com>
2025-02-27 14:43:04 +0100

    ParallelizableCollection trait is defined

--- src/parallelizable_collection.rs
+++ src/parallelizable_collection.rs
@@ -1,8 +1,16 @@
+use crate::{par_iter::ParEmpty, parallelizable::Parallelizable};
 use orx_concurrent_iter::ConcurrentIter;
 
 pub trait ParallelizableCollection {
     type ParItem;
 
-    type ParIter: ConcurrentIter<Item = Self::ParItem>;
-    //
+    type Parallelizable<'i>: Parallelizable<ParItem = &'i Self::ParItem>
+    where
+        Self: 'i;
+
+    fn as_parallelizable(&self) -> Self::Parallelizable<'_>;
+
+    fn par(&self) -> ParEmpty<<Self::Parallelizable<'_> as Parallelizable>::ParIter> {
+        self.as_parallelizable().par()
+    }
 }
