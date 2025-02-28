909a091664622bd83321b44fddba5de6217c4e23
Ugur-Arikan <uurfun@gmail.com>
2025-02-28 14:27:44 +0100

    vec_parallelizable_collection test is added

--- src/par_iterators/tests/parallelizable_collection.rs
+++ src/par_iterators/tests/parallelizable_collection.rs
@@ -5,3 +5,9 @@ fn take_parallelizable_collection<T>(a: impl ParallelizableCollection<ParItem = 
     let _ = a.par();
     let _ = a.into_par();
 }
+
+#[test]
+fn vec_parallelizable_collection() {
+    let vec: Vec<_> = (0..10).map(|x| x.to_string()).collect();
+    take_parallelizable_collection::<String>(vec);
+}
