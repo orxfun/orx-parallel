87ab6173d4a7678ea028bcee1ca4154cd8f4e970
Ugur-Arikan <uurfun@gmail.com>
2025-03-02 20:06:28 +0100

    ThreadRunner::complete_task is added to signal back completion of thread job

--- src/computations/runner/parallel_runner.rs
+++ src/computations/runner/parallel_runner.rs
@@ -1,8 +1,23 @@
+use orx_concurrent_iter::{ConcurrentIter, Element, Enumeration};
+
 use super::thread_runner::ThreadRunner;
 use crate::parameters::Params;
 
 pub trait ParallelRunner: Sized {
+    type SharedState;
+
     type ThreadRunner: ThreadRunner;
 
     fn new(params: Params) -> Self;
+
+    fn shared_state(&self) -> &Self::SharedState;
+
+    fn run<E, I, T>(self, iter: &I, transform: &T)
+    where
+        E: Enumeration,
+        I: ConcurrentIter<E>,
+        T: Fn(<E::Element as Element>::ElemOf<I::Item>),
+    {
+        let shared_state = self.shared_state();
+    }
 }
--- src/computations/runner/thread_runner.rs
+++ src/computations/runner/thread_runner.rs
@@ -1,18 +1,23 @@
+use super::parallel_runner::ParallelRunner;
 use crate::parameters::Params;
 use orx_concurrent_iter::{ChunkPuller, ConcurrentIter, Element, Enumeration};
 
+type SharedState<T> = <<T as ThreadRunner>::ParallelRunner as ParallelRunner>::SharedState;
+
 pub trait ThreadRunner: Sized {
-    type SharedState;
+    type ParallelRunner: ParallelRunner;
 
     fn new(params: Params) -> Self;
 
-    fn next_chunk_size<I>(&self, shared_state: &Self::SharedState, iter: &I) -> Option<usize>;
+    fn next_chunk_size<I>(&self, shared_state: &SharedState<Self>, iter: &I) -> Option<usize>;
 
     fn begin_chunk(&mut self, chunk_size: usize);
 
-    fn complete_chunk(&mut self, shared_state: &Self::SharedState, chunk_size: usize);
+    fn complete_chunk(&mut self, shared_state: &SharedState<Self>, chunk_size: usize);
 
-    fn run<E, I, T>(mut self, iter: &I, shared_state: &Self::SharedState, transform: &T)
+    fn complete_task(&mut self, shared_state: &SharedState<Self>);
+
+    fn run<E, I, T>(mut self, iter: &I, shared_state: &SharedState<Self>, transform: &T)
     where
         E: Enumeration,
         I: ConcurrentIter<E>,
@@ -28,13 +33,15 @@ pub trait ThreadRunner: Sized {
 
             self.complete_chunk(shared_state, chunk_size);
         }
+
+        self.complete_task(shared_state);
     }
 }
 
-fn transform_chunk<P, T>(puller: P, transform: T)
+fn transform_chunk<I, T>(puller: I, transform: T)
 where
-    P: Iterator,
-    T: Fn(P::Item),
+    I: Iterator,
+    T: Fn(I::Item),
 {
     for value in puller {
         transform(value);
