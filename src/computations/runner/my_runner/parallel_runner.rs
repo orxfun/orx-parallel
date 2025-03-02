de47420b3119a62a6607a397859c5cc67ee38f5f
Ugur-Arikan <uurfun@gmail.com>
2025-03-02 21:10:27 +0100

    new_shared_state is implemented for MyParallelRunner

--- src/computations/runner/my_runner/parallel_runner.rs
+++ src/computations/runner/my_runner/parallel_runner.rs
@@ -1,4 +1,6 @@
-use super::thread_runner::MyThreadRunner;
+use super::{
+    chunk_size::ResolvedChunkSize, num_threads::maximum_num_threads, thread_runner::MyThreadRunner,
+};
 use crate::{
     computations::{computation_kind::ComputationKind, runner::parallel_runner::ParallelRunner},
     parameters::Params,
@@ -18,7 +20,11 @@ impl ParallelRunner for MyParallelRunner {
         E: Enumeration,
         I: ConcurrentIter<E>,
     {
-        0.into()
+        let initial_len = iter.try_get_len();
+        let max_num_threads = maximum_num_threads(initial_len, params.num_threads);
+        let resolved_chunk_size =
+            ResolvedChunkSize::new(kind, initial_len, max_num_threads, params.chunk_size);
+        resolved_chunk_size.chunk_size().into()
     }
 
     fn do_spawn_new<E, I>(num_spawned: usize, shared_state: &Self::SharedState, iter: &I) -> bool
