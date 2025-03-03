a5fb9032c065012abf2d703fb9d1f7b47b2c0188
Ugur-Arikan <uurfun@gmail.com>
2025-03-03 14:32:12 +0100

    chunk puller is cached to improve performance

--- src/computations/runner/thread_runner.rs
+++ src/computations/runner/thread_runner.rs
@@ -20,27 +20,33 @@ pub trait ThreadRunner: Sized {
         I: ConcurrentIter<E>,
         T: Fn(<E::Element as Element>::ElemOf<I::Item>),
     {
+        let mut chunk_puller = iter.chunk_puller(0);
+        let mut item_puller = iter.item_puller();
+
         while let Some(chunk_size) = self.next_chunk_size(shared_state, iter) {
             self.begin_chunk(chunk_size);
 
             match chunk_size {
-                0 | 1 => transform_chunk(iter.item_puller(), transform),
-                c => transform_chunk(iter.chunk_puller(c).flattened(), transform),
+                0 | 1 => match item_puller.next() {
+                    Some(value) => transform(value),
+                    None => break,
+                },
+                c => {
+                    if c > chunk_puller.chunk_size() {
+                        chunk_puller = iter.chunk_puller(c);
+                    }
+
+                    let mut flattened = chunk_puller.flattened();
+                    for value in flattened.by_ref() {
+                        transform(value);
+                    }
+                    chunk_puller = flattened.into_chunk_puller();
+                }
             }
 
             self.complete_chunk(shared_state, chunk_size);
         }
 
         self.complete_task(shared_state);
-    }
-}
-
-fn transform_chunk<I, T>(puller: I, transform: T)
-where
-    I: Iterator,
-    T: Fn(I::Item),
-{
-    for value in puller {
-        transform(value);
     }
 }
