7d727652f5a2ea639c460819f41259d92d69402a
Ugur-Arikan <uurfun@gmail.com>
2025-03-08 21:58:01 +0100

    parallel_compute_in_place is implemented for map-fil-col

--- src/computations/map_filter_collect.rs
+++ src/computations/map_filter_collect.rs
@@ -2,9 +2,11 @@ use crate::{
     parameters::Params,
     runner::{ComputationKind, ParallelRunner},
 };
+use orx_concurrent_bag::ConcurrentBag;
 use orx_concurrent_iter::ConcurrentIter;
 use orx_concurrent_ordered_bag::ConcurrentOrderedBag;
 use orx_fixed_vec::IntoConcurrentPinnedVec;
+use orx_split_vec::SplitVec;
 
 pub struct MapFilterCollect<I, O, Map, Filter, P>
 where
@@ -18,7 +20,7 @@ where
     iter: I,
     map: Map,
     filter: Filter,
-    bag: ConcurrentOrderedBag<O, P>,
+    pinned: P,
 }
 
 impl<I, O, Map, Filter, P> MapFilterCollect<I, O, Map, Filter, P>
@@ -29,7 +31,31 @@ where
     Filter: Fn(&O) -> bool + Send + Sync,
     P: IntoConcurrentPinnedVec<O>,
 {
-    fn compute_in_place<R: ParallelRunner>(self) -> (usize, usize) {
-        (0, 0)
+    fn sequential_fill_bag(mut self) -> P {
+        for x in self.iter.into_seq_iter().map(self.map).filter(self.filter) {
+            self.pinned.push(x);
+        }
+        self.pinned
+    }
+
+    fn parallel_compute_in_place<R: ParallelRunner>(self) -> (usize, ConcurrentBag<O, P>) {
+        let offset = self.pinned.len();
+        let initial_len = self.iter.try_get_len();
+
+        let values: ConcurrentBag<O, P> = self.pinned.into();
+        let indices = ConcurrentOrderedBag::with_doubling_growth();
+
+        let transform = |(input_idx, value)| {
+            let value = (self.map)(value);
+            if (self.filter)(&value) {
+                let values_idx = values.push(value);
+                unsafe { indices.set_value(input_idx, values_idx) };
+            }
+        };
+
+        let runner = R::new(ComputationKind::Collect, self.params, initial_len);
+        let num_spawned = runner.run_with_idx(&self.iter, &transform);
+
+        (num_spawned, values)
     }
 }
