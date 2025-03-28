2cc4a83c308eb35517663cdc0acb3cfc95b2d3e3
orxfun <orx.ugur.arikan@gmail.com>
2025-03-28 20:24:23 +0100

    map-filter-map => map transformation is defined

--- src/computational_variants/map_filter_map.rs
+++ src/computational_variants/map_filter_map.rs
@@ -33,7 +33,7 @@ where
     F: Fn(&T) -> bool + Send + Sync,
     M2: Fn(T) -> Vo + Send + Sync,
 {
-    pub(crate) fn new(iter: I, params: Params, map1: M1, filter: F, map2: M2) -> Self {
+    pub(crate) fn new(params: Params, iter: I, map1: M1, filter: F, map2: M2) -> Self {
         Self {
             mfm: Mfm::new(params, iter, map1, filter, map2),
             phantom: PhantomData,
@@ -42,6 +42,69 @@ where
 
     fn destruct(self) -> (Params, I, M1, F, M2) {
         self.mfm.destruct()
+    }
+}
+
+impl<I, T, Vt, O, Vo, M1, F, M2, R> ParIter<R> for ParMapFilterMap<I, T, Vt, O, Vo, M1, F, M2, R>
+where
+    R: ParallelRunner,
+    I: ConcurrentIter,
+    Vt: Values<Item = T>,
+    O: Send + Sync,
+    Vo: Values<Item = O>,
+    M1: Fn(I::Item) -> Vt + Send + Sync,
+    F: Fn(&T) -> bool + Send + Sync,
+    M2: Fn(T) -> Vo + Send + Sync,
+{
+    type Item = O;
+
+    fn con_iter(&self) -> &impl ConcurrentIter {
+        self.mfm.iter()
+    }
+
+    // params transformations
+
+    fn num_threads(mut self, num_threads: impl Into<NumThreads>) -> Self {
+        self.mfm.num_threads(num_threads);
+        self
+    }
+
+    fn chunk_size(mut self, chunk_size: impl Into<ChunkSize>) -> Self {
+        self.mfm.chunk_size(chunk_size);
+        self
+    }
+
+    fn collect_ordering(mut self, collect: CollectOrdering) -> Self {
+        self.mfm.collect_ordering(collect);
+        self
+    }
+
+    fn with_runner<Q: ParallelRunner>(self) -> impl ParIter<Q, Item = Self::Item> {
+        let (params, iter, map1, filter, map2) = self.destruct();
+        ParMapFilterMap::new(params, iter, map1, filter, map2)
+    }
+
+    // computation transformations
+
+    fn map<Out, Map>(self, map3: Map) -> impl ParIter<Item = Out>
+    where
+        Out: Send + Sync,
+        Map: Fn(Self::Item) -> Out + Send + Sync + Clone,
+    {
+        let (params, iter, map1, filter, map2) = self.destruct();
+        let map23 = move |t: T| {
+            let vo = map2(t);
+            vo.map(map3.clone())
+        };
+
+        ParMapFilterMap::new(params, iter, map1, filter, map23)
+    }
+
+    fn collect_into<C>(self, output: C) -> C
+    where
+        C: ParCollectInto<Self::Item>,
+    {
+        todo!()
     }
 }
 
