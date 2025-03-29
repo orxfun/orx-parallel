1afc579b4df757cc92b1fb265fc4c2d3724c33bb
orxfun <orx.ugur.arikan@gmail.com>
2025-03-29 20:38:52 +0100

    ParXap::collect_into is implemented

--- src/computational_variants/xap.rs
+++ src/computational_variants/xap.rs
@@ -102,7 +102,6 @@ where
     where
         C: ParCollectInto<Self::Item>,
     {
-        // output.mfm_collect_into::<R, _, _, _, _, _, _>(self.x)
-        todo!()
+        output.x_collect_into::<R, _, _, _>(self.x)
     }
 }
