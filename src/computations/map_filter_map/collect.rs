a866b62b3555ae583b85befec0854e36557f9942
orxfun <orx.ugur.arikan@gmail.com>
2025-03-29 20:25:58 +0100

    code duplication fix

--- src/computations/map_filter_map/collect.rs
+++ src/computations/map_filter_map/collect.rs
@@ -1,12 +1,11 @@
 use super::mfm::Mfm;
+use crate::computations::heap_sort::heap_sort_into;
 use crate::computations::Values;
 use crate::runner::{ComputationKind, ParallelRunner, ParallelTask};
 use crate::CollectOrdering;
 use orx_concurrent_bag::ConcurrentBag;
 use orx_concurrent_iter::ConcurrentIter;
-use orx_iterable::Collection;
 use orx_pinned_vec::IntoConcurrentPinnedVec;
-use orx_priority_queue::{BinaryHeap, PriorityQueue};
 use std::marker::PhantomData;
 
 pub struct MfmCollect<I, Vt, Vo, M1, F, M2, P>
@@ -83,37 +82,8 @@ where
 
         let runner = R::new(ComputationKind::Collect, params, initial_len);
 
-        let (num_spawned, mut vectors) = runner.xfx_collect_with_idx(&iter, &map1, &filter, &map2);
-
-        let mut queue = BinaryHeap::with_capacity(vectors.len());
-        let mut indices = vec![0; vectors.len()];
-
-        for (v, vec) in vectors.iter().enumerate() {
-            if let Some(x) = vec.get(indices[v]) {
-                queue.push(v, x.0);
-            }
-        }
-        let mut curr_v = queue.pop_node();
-
-        while let Some(v) = curr_v {
-            let idx = indices[v];
-            indices[v] += 1;
-
-            curr_v = match vectors[v].get(indices[v]) {
-                Some(x) => Some(queue.push_then_pop(v, x.0).0),
-                None => queue.pop_node(),
-            };
-
-            let ptr = vectors[v].as_mut_ptr();
-            pinned_vec.push(unsafe { ptr.add(idx).read().1 });
-        }
-
-        for vec in vectors.iter_mut() {
-            // SAFETY: this prevents to drop the elements which are already moved to pinned_vec
-            // allocation within vec.capacity() will still be reclaimed; however, as uninitialized memory
-            unsafe { vec.set_len(0) };
-        }
-
+        let (num_spawned, vectors) = runner.xfx_collect_with_idx(&iter, &map1, &filter, &map2);
+        heap_sort_into(vectors, &mut pinned_vec);
         (num_spawned, pinned_vec)
     }
 }
