1b18283e7dea1baf323e18e97a6261b2ab361edb
orxfun <orx.ugur.arikan@gmail.com>
2025-03-31 00:58:28 +0200

    first is implemented for map

--- src/computations/map/first.rs
+++ src/computations/map/first.rs
@@ -1,1 +1,18 @@
+use super::m::M;
+use crate::runner::ParallelRunner;
+use orx_concurrent_iter::ConcurrentIter;
 
+impl<I, O, M1> M<I, O, M1>
+where
+    I: ConcurrentIter,
+    O: Send + Sync,
+    M1: Fn(I::Item) -> O + Send + Sync,
+{
+    pub fn first<R>(self) -> Option<O>
+    where
+        R: ParallelRunner,
+    {
+        let (_, iter, map1) = self.destruct();
+        iter.next().map(map1)
+    }
+}
