fed6ef17efab18b9b6c1f09c838c87ab76578f96
orxfun <orx.ugur.arikan@gmail.com>
2025-03-30 23:53:39 +0200

    map reduce tests added

--- src/computations/map/tests/reduce.rs
+++ src/computations/map/tests/reduce.rs
@@ -1,4 +1,8 @@
-use crate::{computations::map::m::M, runner::DefaultRunner, CollectOrdering, Params};
+use crate::{
+    computations::{map::m::M, map_self},
+    runner::DefaultRunner,
+    CollectOrdering, Params,
+};
 use orx_concurrent_iter::IntoConcurrentIter;
 use test_case::test_matrix;
 
@@ -6,6 +10,28 @@ use test_case::test_matrix;
 const N: [usize; 2] = [37, 125];
 #[cfg(not(miri))]
 const N: [usize; 2] = [1025, 4735];
+
+#[test_matrix(
+    [0, 1, N[0], N[1]],
+    [1, 2, 4],
+    [1, 64, 1024])
+]
+fn m_reduce(n: usize, nt: usize, chunk: usize) {
+    let input: Vec<_> = (0..n).map(|x| x.to_string()).collect();
+    let reduce = |x: String, y: String| match x < y {
+        true => y,
+        false => x,
+    };
+
+    let expected = input.clone().into_iter().reduce(reduce);
+
+    let params = Params::new(nt, chunk, CollectOrdering::default());
+    let iter = input.into_con_iter();
+    let m = M::new(params, iter, map_self);
+    let (_, output) = m.reduce::<DefaultRunner, _>(reduce);
+
+    assert_eq!(expected, output);
+}
 
 #[test_matrix(
     [0, 1, N[0], N[1]],
