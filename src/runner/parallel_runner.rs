92da898a4cf1d679a91268e5a5f00588bad47ba2
orxfun <orx.ugur.arikan@gmail.com>
2025-03-30 23:36:25 +0200

    x_reduce and xfx_reduce are implemented for ParallelRunnerCompute

--- src/runner/parallel_runner.rs
+++ src/runner/parallel_runner.rs
@@ -72,6 +72,8 @@ pub trait ParallelRunnerCompute: ParallelRunner {
         num_spawned
     }
 
+    // collect
+
     fn x_collect_with_idx<I, Vo, M1>(
         &self,
         iter: &I,
@@ -147,6 +149,92 @@ pub trait ParallelRunnerCompute: ParallelRunner {
         });
 
         (num_spawned, vectors)
+    }
+
+    // reduce
+
+    fn x_reduce<I, Vo, M1, X>(&self, iter: &I, map1: &M1, reduce: &X) -> (usize, Option<Vo::Item>)
+    where
+        I: ConcurrentIter,
+        Vo: Values,
+        Vo::Item: Send + Sync,
+        M1: Fn(I::Item) -> Vo + Send + Sync,
+        X: Fn(Vo::Item, Vo::Item) -> Vo::Item + Send + Sync,
+    {
+        let state = self.new_shared_state();
+        let shared_state = &state;
+
+        let mut num_spawned = 0;
+        let results = std::thread::scope(|s| {
+            let mut handles = vec![];
+
+            while self.do_spawn_new(num_spawned, shared_state, iter) {
+                num_spawned += 1;
+                handles.push(s.spawn(move || {
+                    let thread_runner = self.new_thread_runner(shared_state);
+                    thread_runner.x_reduce(iter, shared_state, map1, reduce)
+                }));
+            }
+
+            let mut results = Vec::with_capacity(handles.len());
+            for x in handles {
+                if let Some(x) = x.join().expect("failed to join the thread") {
+                    results.push(x);
+                }
+            }
+            results
+        });
+
+        let acc = results.into_iter().reduce(reduce);
+
+        (num_spawned, acc)
+    }
+
+    fn xfx_reduce<I, Vt, Vo, M1, F, M2, X>(
+        &self,
+        iter: &I,
+        map1: &M1,
+        filter: &F,
+        map2: &M2,
+        reduce: &X,
+    ) -> (usize, Option<Vo::Item>)
+    where
+        I: ConcurrentIter,
+        Vt: Values,
+        Vo: Values,
+        Vo::Item: Send + Sync,
+        M1: Fn(I::Item) -> Vt + Send + Sync,
+        F: Fn(&Vt::Item) -> bool + Send + Sync,
+        M2: Fn(Vt::Item) -> Vo + Send + Sync,
+        X: Fn(Vo::Item, Vo::Item) -> Vo::Item + Send + Sync,
+    {
+        let state = self.new_shared_state();
+        let shared_state = &state;
+
+        let mut num_spawned = 0;
+        let results = std::thread::scope(|s| {
+            let mut handles = vec![];
+
+            while self.do_spawn_new(num_spawned, shared_state, iter) {
+                num_spawned += 1;
+                handles.push(s.spawn(move || {
+                    let thread_runner = self.new_thread_runner(shared_state);
+                    thread_runner.xfx_reduce(iter, shared_state, map1, filter, map2, reduce)
+                }));
+            }
+
+            let mut results = Vec::with_capacity(handles.len());
+            for x in handles {
+                if let Some(x) = x.join().expect("failed to join the thread") {
+                    results.push(x);
+                }
+            }
+            results
+        });
+
+        let acc = results.into_iter().reduce(reduce);
+
+        (num_spawned, acc)
     }
 }
 
