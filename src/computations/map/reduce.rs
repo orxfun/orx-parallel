1eb0e43dda07f50946c8677ff5e4edaa93efea9f
orxfun <orx.ugur.arikan@gmail.com>
2025-03-30 23:44:05 +0200

    MReduce::compute is implemented

--- src/computations/map/reduce.rs
+++ src/computations/map/reduce.rs
@@ -1,5 +1,8 @@
 use super::m::M;
-use crate::runner::{ComputationKind, ParallelRunner, ParallelTaskWithIdx};
+use crate::{
+    computations::{map_self_atom, Atom},
+    runner::{ComputationKind, ParallelRunner, ParallelRunnerCompute, ParallelTaskWithIdx},
+};
 use orx_concurrent_iter::ConcurrentIter;
 use orx_concurrent_ordered_bag::ConcurrentOrderedBag;
 use orx_pinned_vec::IntoConcurrentPinnedVec;
@@ -23,16 +26,27 @@ where
     M1: Fn(I::Item) -> O + Send + Sync,
     X: Fn(O, O) -> O + Send + Sync,
 {
+    pub fn compute<R: ParallelRunner>(m: M<I, O, M1>, reduce: X) -> (usize, Option<O>) {
+        let x = Self { m, reduce };
+        let p = x.m.params();
+        match p.is_sequential() {
+            true => (0, x.sequential()),
+            false => x.parallel::<R>(),
+        }
+    }
+
     fn sequential(self) -> Option<O> {
         let (m, reduce) = (self.m, self.reduce);
         let (_, iter, map1) = m.destruct();
         iter.into_seq_iter().map(map1).reduce(reduce)
     }
 
-    fn parallel(self) -> Option<O> {
+    fn parallel<R: ParallelRunner>(self) -> (usize, Option<O>) {
         let (m, reduce) = (self.m, self.reduce);
         let (params, iter, map1) = m.destruct();
 
-        todo!()
+        let runner = R::new(ComputationKind::Reduce, params, iter.try_get_len());
+        let xap1 = |i: I::Item| Atom(map1(i));
+        runner.x_reduce(&iter, &xap1, &reduce)
     }
 }
