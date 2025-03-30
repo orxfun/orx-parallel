c05414f7cac85e42c4902b43f3d4bf2c7d5e09b8
orxfun <orx.ugur.arikan@gmail.com>
2025-03-31 01:33:08 +0200

    xfx_first is implemented for ParallelRunner

--- src/runner/parallel_runner.rs
+++ src/runner/parallel_runner.rs
@@ -239,50 +239,50 @@ pub trait ParallelRunnerCompute: ParallelRunner {
 
     // first
 
-    // fn xfx_first<I, Vt, Vo, M1, F, M2>(
-    //     &self,
-    //     iter: &I,
-    //     map1: &M1,
-    //     filter: &F,
-    //     map2: &M2,
-    // ) -> (usize, Option<Vo::Item>)
-    // where
-    //     I: ConcurrentIter,
-    //     Vt: Values,
-    //     Vo: Values,
-    //     Vo::Item: Send + Sync,
-    //     M1: Fn(I::Item) -> Vt + Send + Sync,
-    //     F: Fn(&Vt::Item) -> bool + Send + Sync,
-    //     M2: Fn(Vt::Item) -> Vo + Send + Sync,
-    // {
-    //     let state = self.new_shared_state();
-    //     let shared_state = &state;
+    fn xfx_first<I, Vt, Vo, M1, F, M2>(
+        &self,
+        iter: &I,
+        map1: &M1,
+        filter: &F,
+        map2: &M2,
+    ) -> (usize, Option<Vo::Item>)
+    where
+        I: ConcurrentIter,
+        Vt: Values,
+        Vo: Values,
+        Vo::Item: Send + Sync,
+        M1: Fn(I::Item) -> Vt + Send + Sync,
+        F: Fn(&Vt::Item) -> bool + Send + Sync,
+        M2: Fn(Vt::Item) -> Vo + Send + Sync,
+    {
+        let state = self.new_shared_state();
+        let shared_state = &state;
 
-    //     let mut num_spawned = 0;
-    //     let results = std::thread::scope(|s| {
-    //         let mut handles = vec![];
+        let mut num_spawned = 0;
+        let results = std::thread::scope(|s| {
+            let mut handles = vec![];
 
-    //         while self.do_spawn_new(num_spawned, shared_state, iter) {
-    //             num_spawned += 1;
-    //             handles.push(s.spawn(move || {
-    //                 let thread_runner = self.new_thread_runner(shared_state);
-    //                 thread_runner.xfx_first(iter, shared_state, map1, filter, map2)
-    //             }));
-    //         }
+            while self.do_spawn_new(num_spawned, shared_state, iter) {
+                num_spawned += 1;
+                handles.push(s.spawn(move || {
+                    let thread_runner = self.new_thread_runner(shared_state);
+                    thread_runner.xfx_first(iter, shared_state, map1, filter, map2)
+                }));
+            }
 
-    //         let mut results = Vec::with_capacity(handles.len());
-    //         for x in handles {
-    //             if let Some(x) = x.join().expect("failed to join the thread") {
-    //                 results.push(x);
-    //             }
-    //         }
-    //         results
-    //     });
+            let mut results: Vec<(usize, Vo::Item)> = Vec::with_capacity(handles.len());
+            for x in handles {
+                if let Some(x) = x.join().expect("failed to join the thread") {
+                    results.push(x);
+                }
+            }
+            results
+        });
 
-    //     let acc = results.into_iter().reduce(reduce);
+        let acc = results.into_iter().min_by_key(|x| x.0).map(|x| x.1);
 
-    //     (num_spawned, acc)
-    // }
+        (num_spawned, acc)
+    }
 }
 
 impl<X: ParallelRunner> ParallelRunnerCompute for X {}
