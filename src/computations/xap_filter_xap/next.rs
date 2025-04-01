fb6e6f88fd060b8600ab2b78a0e3d71a7a91f783
orxfun <orx.ugur.arikan@gmail.com>
2025-04-01 12:23:13 +0200

    next-any is implemented

--- src/computations/xap_filter_xap/next.rs
+++ src/computations/xap_filter_xap/next.rs
@@ -17,13 +17,26 @@ where
     where
         R: ParallelRunner,
     {
-        XfxNext(self).compute::<R>()
+        XfxNext {
+            xfx: self,
+            any: false,
+        }
+        .compute::<R>()
+    }
+
+    pub fn next_any<R>(self) -> (usize, Option<Vo::Item>)
+    where
+        R: ParallelRunner,
+    {
+        XfxNext {
+            xfx: self,
+            any: true,
+        }
+        .compute::<R>()
     }
 }
 
-// next
-
-struct XfxNext<I, Vt, Vo, M1, F, M2>(Xfx<I, Vt, Vo, M1, F, M2>)
+struct XfxNext<I, Vt, Vo, M1, F, M2>
 where
     I: ConcurrentIter,
     Vt: Values + Send + Sync,
@@ -31,7 +44,11 @@ where
     Vo::Item: Send + Sync,
     M1: Fn(I::Item) -> Vt + Send + Sync,
     F: Fn(&Vt::Item) -> bool + Send + Sync,
-    M2: Fn(Vt::Item) -> Vo + Send + Sync;
+    M2: Fn(Vt::Item) -> Vo + Send + Sync,
+{
+    xfx: Xfx<I, Vt, Vo, M1, F, M2>,
+    any: bool,
+}
 
 impl<I, Vt, Vo, M1, F, M2> XfxNext<I, Vt, Vo, M1, F, M2>
 where
@@ -47,14 +64,14 @@ where
     where
         R: ParallelRunner,
     {
-        match self.0.params().is_sequential() {
+        match self.xfx.params().is_sequential() {
             true => (0, self.sequential()),
             false => self.parallel::<R>(),
         }
     }
 
     fn sequential(self) -> Option<Vo::Item> {
-        let (_, iter, xap1, filter, xap2) = self.0.destruct();
+        let (_, iter, xap1, filter, xap2) = self.xfx.destruct();
 
         iter.into_seq_iter()
             .flat_map(|i| xap1(i).values().into_iter())
@@ -64,51 +81,11 @@ where
     }
 
     fn parallel<R: ParallelRunner>(self) -> (usize, Option<Vo::Item>) {
-        let (params, iter, xap1, filter, xap2) = self.0.destruct();
+        let (params, iter, xap1, filter, xap2) = self.xfx.destruct();
         let runner = R::new(ComputationKind::EarlyReturn, params, iter.try_get_len());
-        runner.xfx_next(&iter, &xap1, &filter, &xap2)
-    }
-}
-
-// next any
-
-struct XfxNextAny<I, Vt, Vo, M1, F, M2>(Xfx<I, Vt, Vo, M1, F, M2>)
-where
-    I: ConcurrentIter,
-    Vt: Values + Send + Sync,
-    Vo: Values + Send + Sync,
-    Vo::Item: Send + Sync,
-    M1: Fn(I::Item) -> Vt + Send + Sync,
-    F: Fn(&Vt::Item) -> bool + Send + Sync,
-    M2: Fn(Vt::Item) -> Vo + Send + Sync;
-
-impl<I, Vt, Vo, M1, F, M2> XfxNextAny<I, Vt, Vo, M1, F, M2>
-where
-    I: ConcurrentIter,
-    Vt: Values + Send + Sync,
-    Vo: Values + Send + Sync,
-    Vo::Item: Send + Sync,
-    M1: Fn(I::Item) -> Vt + Send + Sync,
-    F: Fn(&Vt::Item) -> bool + Send + Sync,
-    M2: Fn(Vt::Item) -> Vo + Send + Sync,
-{
-    pub fn compute<R>(self) -> (usize, Option<Vo::Item>)
-    where
-        R: ParallelRunner,
-    {
-        match self.0.params().is_sequential() {
-            true => (0, self.sequential()),
-            false => self.parallel::<R>(),
+        match self.any {
+            true => runner.xfx_next_any(&iter, &xap1, &filter, &xap2),
+            false => runner.xfx_next(&iter, &xap1, &filter, &xap2),
         }
-    }
-
-    fn sequential(self) -> Option<Vo::Item> {
-        XfxNext(self.0).sequential()
-    }
-
-    fn parallel<R: ParallelRunner>(self) -> (usize, Option<Vo::Item>) {
-        let (params, iter, xap1, filter, xap2) = self.0.destruct();
-        let runner = R::new(ComputationKind::EarlyReturn, params, iter.try_get_len());
-        runner.xfx_next(&iter, &xap1, &filter, &xap2)
     }
 }
