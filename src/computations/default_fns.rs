8910498b181bb1f31aca9e36297c2d8ed4bf4eaf
orxfun <orx.ugur.arikan@gmail.com>
2025-04-01 12:12:49 +0200

    clean up

--- src/computations/default_fns.rs
+++ src/computations/default_fns.rs
@@ -1,5 +1,4 @@
 use super::generalized_values::Atom;
-use crate::Maybe;
 
 #[inline(always)]
 pub fn map_self<T>(input: T) -> T {
@@ -14,14 +13,4 @@ pub fn map_self_atom<T>(input: T) -> Atom<T> {
 #[inline(always)]
 pub fn filter_true<T>(_: &T) -> bool {
     true
-}
-
-#[inline(always)]
-pub fn maybe_has_value<O, M: Maybe<O>>(maybe: &M) -> bool {
-    maybe.has_value()
-}
-
-#[inline(always)]
-pub fn map_maybe_value<O, M: Maybe<O>>(maybe: M) -> O {
-    maybe.unwrap()
 }
