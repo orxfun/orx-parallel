3baebc10c452420eda58d0770828f879ee308b0d
orxfun <orx.ugur.arikan@gmail.com>
2025-04-03 13:13:07 +0200

    benchmarks feature configurations set up

--- examples/benchmark_collect.rs
+++ examples/benchmark_collect.rs
@@ -1,4 +1,11 @@
-/* REQUIRES FEATURE: generic_iterator
+mod utils;
+
+#[cfg(not(feature = "generic_iterator"))]
+fn main() {
+    panic!(
+        r#"
+
+REQUIRES FEATURE: generic_iterator
 
 To view the arguments:
 cargo run --release --features generic_iterator --example benchmark_collect -- --help
@@ -9,47 +16,46 @@ cargo run --release --features generic_iterator --example benchmark_collect
 To run with desired arguments:
 cargo run --release --features generic_iterator --example benchmark_collect -- --len 123456 --num-repetitions 10
 
-Play with the transformations inside the "compute" method to test out different computations.
-Only make sure that:
-* elements of the input iterator are of "usize", and
-* elements of the output vector are of "String".
-*/
+Play with the transformations inside the compute method to test out different computations.
 
-mod utils;
-
-use clap::Parser;
-use orx_parallel::{generic_iterator::GenericIterator, IntoParIter, ParIter};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use utils::timed_collect_all;
-
-#[derive(Parser, Debug)]
-struct Args {
-    /// Number of items in the input iterator.
-    #[arg(long, default_value_t = 100000)]
-    len: usize,
-    /// Number of repetitions to measure time; total time will be reported.
-    #[arg(long, default_value_t = 100)]
-    num_repetitions: usize,
+"#
+    );
 }
 
-fn compute(
-    iter: GenericIterator<
-        usize,
-        impl Iterator<Item = usize>,
-        impl ParallelIterator<Item = usize>,
-        impl ParIter<Item = usize>,
-    >,
-) -> Vec<String> {
-    iter.map(|x| x.to_string())
-        .filter_map(|x| (!x.starts_with('1')).then_some(x))
-        .flat_map(|x| [format!("{}!", &x), x])
-        .filter(|x| !x.starts_with('2'))
-        .filter_map(|x| x.parse::<u64>().ok())
-        .map(|x| x.to_string())
-        .collect_vec()
-}
-
+#[cfg(feature = "generic_iterator")]
 fn main() {
+    use clap::Parser;
+    use orx_parallel::{generic_iterator::GenericIterator, IntoParIter, ParIter};
+    use rayon::iter::{IntoParallelIterator, ParallelIterator};
+    use utils::timed_collect_all;
+
+    #[derive(Parser, Debug)]
+    struct Args {
+        /// Number of items in the input iterator.
+        #[arg(long, default_value_t = 100000)]
+        len: usize,
+        /// Number of repetitions to measure time; total time will be reported.
+        #[arg(long, default_value_t = 100)]
+        num_repetitions: usize,
+    }
+
+    fn compute(
+        iter: GenericIterator<
+            usize,
+            impl Iterator<Item = usize>,
+            impl ParallelIterator<Item = usize>,
+            impl ParIter<Item = usize>,
+        >,
+    ) -> Vec<String> {
+        iter.map(|x| x.to_string())
+            .filter_map(|x| (!x.starts_with('1')).then_some(x))
+            .flat_map(|x| [format!("{}!", &x), x])
+            .filter(|x| !x.starts_with('2'))
+            .filter_map(|x| x.parse::<u64>().ok())
+            .map(|x| x.to_string())
+            .collect_vec()
+    }
+
     let args = Args::parse();
 
     let input = move || (0..args.len as usize).collect::<Vec<_>>();
--- examples/benchmark_reduce.rs
+++ examples/benchmark_reduce.rs
@@ -1,4 +1,11 @@
-/* REQUIRES FEATURE: generic_iterator
+mod utils;
+
+#[cfg(not(feature = "generic_iterator"))]
+fn main() {
+    panic!(
+        r#"
+
+REQUIRES FEATURE: generic_iterator
 
 To view the arguments:
 cargo run --release --features generic_iterator --example benchmark_reduce -- --help
@@ -9,51 +16,51 @@ cargo run --release --features generic_iterator --example benchmark_reduce
 To run with desired arguments:
 cargo run --release --features generic_iterator --example benchmark_reduce -- --len 123456 --num-repetitions 10
 
-Play with the transformations inside the "compute" method or with the reduction defined in "reduce" method.
-Only make sure that:
-* elements of the input iterator are of "usize".
-*/
+Play with the transformations inside the compute method to test out different computations.
 
-mod utils;
-
-use clap::Parser;
-use orx_parallel::{generic_iterator::GenericIterator, IntoParIter, ParIter};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use utils::timed_reduce_all;
-
-#[derive(Parser, Debug)]
-struct Args {
-    /// Number of items in the input iterator.
-    #[arg(long, default_value_t = 100000)]
-    len: usize,
-    /// Number of repetitions to measure time; total time will be reported.
-    #[arg(long, default_value_t = 100)]
-    num_repetitions: usize,
+"#
+    );
 }
 
-fn reduce(a: usize, b: usize) -> usize {
-    (a + b).saturating_sub(1)
-}
-
-fn compute(
-    iter: GenericIterator<
-        usize,
-        impl Iterator<Item = usize>,
-        impl ParallelIterator<Item = usize>,
-        impl ParIter<Item = usize>,
-    >,
-) -> usize {
-    iter.map(|x| x.to_string())
-        .filter_map(|x| (!x.starts_with('1')).then_some(x))
-        .flat_map(|x| [format!("{}!", &x), x])
-        .filter(|x| !x.starts_with('2'))
-        .filter_map(|x| x.parse::<u64>().ok())
-        .map(|x| x.to_string().len())
-        .reduce(reduce)
-        .unwrap_or(0)
-}
-
+#[cfg(feature = "generic_iterator")]
 fn main() {
+    use clap::Parser;
+    use orx_parallel::{generic_iterator::GenericIterator, IntoParIter, ParIter};
+    use rayon::iter::{IntoParallelIterator, ParallelIterator};
+    use utils::timed_reduce_all;
+
+    #[derive(Parser, Debug)]
+    struct Args {
+        /// Number of items in the input iterator.
+        #[arg(long, default_value_t = 100000)]
+        len: usize,
+        /// Number of repetitions to measure time; total time will be reported.
+        #[arg(long, default_value_t = 100)]
+        num_repetitions: usize,
+    }
+
+    fn reduce(a: usize, b: usize) -> usize {
+        (a + b).saturating_sub(1)
+    }
+
+    fn compute(
+        iter: GenericIterator<
+            usize,
+            impl Iterator<Item = usize>,
+            impl ParallelIterator<Item = usize>,
+            impl ParIter<Item = usize>,
+        >,
+    ) -> usize {
+        iter.map(|x| x.to_string())
+            .filter_map(|x| (!x.starts_with('1')).then_some(x))
+            .flat_map(|x| [format!("{}!", &x), x])
+            .filter(|x| !x.starts_with('2'))
+            .filter_map(|x| x.parse::<u64>().ok())
+            .map(|x| x.to_string().len())
+            .reduce(reduce)
+            .unwrap_or(0)
+    }
+
     let args = Args::parse();
 
     let input = move || (0..args.len as usize).collect::<Vec<_>>();
