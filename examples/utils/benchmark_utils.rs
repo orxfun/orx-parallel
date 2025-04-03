0eaae8e5ffa1863203666ceefbb3280ad8f32523
orxfun <orx.ugur.arikan@gmail.com>
2025-04-03 12:14:16 +0200

    timed_collect_all utility method is implemented

--- examples/utils/benchmark_utils.rs
+++ examples/utils/benchmark_utils.rs
@@ -1,8 +1,12 @@
+#![allow(dead_code)]
+
 use std::{
     fmt::Debug,
     hint::black_box,
     time::{Duration, SystemTime},
 };
+
+// reduce
 
 fn timed_reduce<F, O>(num_repetitions: usize, expected_output: O, fun: F) -> Duration
 where
@@ -36,6 +40,45 @@ pub fn timed_reduce_all<O>(
 {
     for (name, fun) in computations {
         let duration = timed_reduce(num_repetitions, expected_output, fun);
+        println!("{:>10} : {:?}", name, duration);
+    }
+}
+
+// collect
+
+fn timed_collect<F, Out, O>(num_repetitions: usize, expected_output: &[O], fun: F) -> Duration
+where
+    F: Fn() -> Out,
+    Out: IntoIterator<Item = O>,
+    O: PartialEq + Debug,
+{
+    let result = fun();
+    assert_eq!(result.into_iter().collect::<Vec<_>>(), expected_output);
+
+    // warm up
+    for _ in 0..10 {
+        let _ = black_box(fun());
+    }
+
+    // measurement
+
+    let now = SystemTime::now();
+    for _ in 0..num_repetitions {
+        let _ = black_box(fun());
+    }
+    now.elapsed().unwrap()
+}
+
+pub fn timed_collect_all<Out, O>(
+    num_repetitions: usize,
+    expected_output: &[O],
+    computations: &[(&str, Box<dyn Fn() -> Out>)],
+) where
+    Out: IntoIterator<Item = O>,
+    O: PartialEq + Debug,
+{
+    for (name, fun) in computations {
+        let duration = timed_collect(num_repetitions, expected_output, fun);
         println!("{:>10} : {:?}", name, duration);
     }
 }
