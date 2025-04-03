4f23d429c1b97f1f172bb53f22cf0f980cdd1974
orxfun <orx.ugur.arikan@gmail.com>
2025-04-03 06:56:22 +0200

    generic map is implemented

--- src/generic_iterator2/iter.rs
+++ src/generic_iterator2/iter.rs
@@ -1,4 +1,5 @@
-use crate::ParIter;
+use crate::{IntoParIter, ParIter};
+use rayon::iter::IntoParallelIterator;
 
 pub enum GenericIterator<T, S, R, O>
 where
@@ -32,4 +33,38 @@ where
     pub fn orx(iter: O) -> Self {
         Self::Orx(iter)
     }
+
+    // computation transformations
+
+    pub fn map<Out, Map>(
+        self,
+        map: Map,
+    ) -> GenericIterator<
+        Out,
+        impl Iterator<Item = Out>,
+        impl rayon::iter::ParallelIterator<Item = Out>,
+        impl ParIter<Item = Out>,
+    >
+    where
+        Out: Send + Sync,
+        Map: Fn(T) -> Out + Send + Sync + Clone,
+    {
+        match self {
+            GenericIterator::Sequential(x) => GenericIterator::Sequential(x.map(map)),
+            GenericIterator::Rayon(x) => GenericIterator::Rayon(x.map(map)),
+            GenericIterator::Orx(x) => GenericIterator::Orx(x.map(map)),
+        }
+    }
+}
+
+pub fn any_sequential<T>() -> impl Iterator<Item = T> {
+    vec![].into_iter()
+}
+
+pub fn any_rayon<T: Send + Sync>() -> impl rayon::iter::ParallelIterator<Item = T> {
+    vec![].into_par_iter()
+}
+
+pub fn any_orx<T: Send + Sync>() -> impl ParIter<Item = T> {
+    vec![].into_par()
 }
