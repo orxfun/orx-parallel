1cf6693c5719dbb8cbcb599a6b19183ed99e40df
orxfun <orx.ugur.arikan@gmail.com>
2025-04-03 06:51:46 +0200

    constructors implemented

--- src/generic_iterator2/iter.rs
+++ src/generic_iterator2/iter.rs
@@ -11,3 +11,25 @@ where
     Rayon(R),
     Orx(O),
 }
+
+impl<T, S, R, O> GenericIterator<T, S, R, O>
+where
+    T: Send + Sync,
+    S: Iterator<Item = T>,
+    R: rayon::iter::ParallelIterator<Item = T>,
+    O: ParIter<Item = T>,
+{
+    // constructors
+
+    pub fn sequential(iter: S) -> Self {
+        Self::Sequential(iter)
+    }
+
+    pub fn rayon(iter: R) -> Self {
+        Self::Rayon(iter)
+    }
+
+    pub fn orx(iter: O) -> Self {
+        Self::Orx(iter)
+    }
+}
