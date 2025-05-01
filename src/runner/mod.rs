3fd1a78f4021ef282a4cbc2989bc48963e8bf9cd
orxfun <orx.ugur.arikan@gmail.com>
2025-05-01 23:55:26 +0200

    ParallelRunner is documented

--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -10,4 +10,10 @@ pub(crate) use parallel_runner::ParallelRunnerCompute;
 pub use parallel_task::{ParallelTask, ParallelTaskWithIdx};
 pub use thread_runner::ThreadRunner;
 
+/// Default parallel runner.
+///
+/// Unless explicitly set to another parallel runner by [`with_runner`] method,
+/// parallel computations will be executed using the default parallel runner.
+///
+/// [`with_runner`]: crate::ParIter::with_runner
 pub type DefaultRunner = fixed_chunk_runner::FixedChunkRunner;
--- src/runner/parallel_runner.rs
+++ src/runner/parallel_runner.rs
@@ -6,15 +6,27 @@ use super::{
 use crate::{computations::Values, parameters::Params};
 use orx_concurrent_iter::ConcurrentIter;
 
+/// A parallel runner which is responsible for taking a computation defined as a composition
+/// of iterator methods, spawns threads, shares tasks and returns the result of the parallel
+/// execution.
 pub trait ParallelRunner: Sized + Sync {
+    /// Data shared to the thread runners.
     type SharedState: Send + Sync;
 
+    /// Thread runner that is responsible for executing the tasks allocated to a thread.
     type ThreadRunner: ThreadRunner<SharedState = Self::SharedState>;
 
+    /// Creates a new parallel runner for the given computation `kind`, parallelization `params`
+    /// and `initial_input_len`.
     fn new(kind: ComputationKind, params: Params, initial_input_len: Option<usize>) -> Self;
 
+    /// Creates an initial shared state.
     fn new_shared_state(&self) -> Self::SharedState;
 
+    /// Returns true if it is beneficial to spawn a new thread provided that:
+    ///
+    /// * `num_spawned` threads are already been spawned, and
+    /// * `shared_state` is the current parallel execution state.
     fn do_spawn_new<I>(
         &self,
         num_spawned: usize,
@@ -24,6 +36,8 @@ pub trait ParallelRunner: Sized + Sync {
     where
         I: ConcurrentIter;
 
+    /// Creates a new thread runner provided that the current parallel execution state is
+    /// `shared_state`.
     fn new_thread_runner(&self, shared_state: &Self::SharedState) -> Self::ThreadRunner;
 }
 
