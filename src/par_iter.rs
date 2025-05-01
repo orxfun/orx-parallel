cecf99cb1791b88ee38ac7d8a6dde15080b4b9f9
orxfun <orx.ugur.arikan@gmail.com>
2025-05-02 00:27:11 +0200

    par iter configurations documented

--- src/par_iter.rs
+++ src/par_iter.rs
@@ -1,5 +1,3 @@
-#![allow(missing_docs)]
-
 use crate::{
     Params,
     collect_into::ParCollectInto,
@@ -11,24 +9,173 @@ use crate::{
 use orx_concurrent_iter::ConcurrentIter;
 use std::cmp::Ordering;
 
+/// Parallel iterator.
 pub trait ParIter<R = DefaultRunner>: Sized + Send + Sync
 where
     R: ParallelRunner,
 {
+    /// Element type of the parallel iterator.
     type Item: Send + Sync;
 
+    /// Returns a reference to the input concurrent iterator.
     fn con_iter(&self) -> &impl ConcurrentIter;
 
+    /// Parameters of the parallel iterator.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use orx_parallel::*;
+    ///
+    /// let vec = vec![1, 2, 3, 4];
+    ///
+    /// assert_eq!(
+    ///     vec.par().params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Auto, CollectOrdering::Ordered)
+    /// );
+    ///
+    /// assert_eq!(
+    ///     vec.par().num_threads(1).params(),
+    ///     &Params::new(1, ChunkSize::Auto, CollectOrdering::Ordered)
+    /// );
+    ///
+    /// assert_eq!(
+    ///     vec.par().num_threads(1).chunk_size(64).params(),
+    ///     &Params::new(1, 64, CollectOrdering::Ordered)
+    /// );
+    ///
+    /// assert_eq!(
+    ///     vec.par()
+    ///         .num_threads(1)
+    ///         .chunk_size(64)
+    ///         .collect_ordering(CollectOrdering::Arbitrary)
+    ///         .params(),
+    ///     &Params::new(1, 64, CollectOrdering::Arbitrary)
+    /// );
+    /// ```
     fn params(&self) -> &Params;
 
     // params transformations
 
+    /// Sets the number of threads to be used in the parallel execution.
+    /// Integers can be used as the argument with the following mapping:
+    ///
+    /// * `0` -> `NumThreads::Auto`
+    /// * `1` -> `NumThreads::sequential()`
+    /// * `n > 0` -> `NumThreads::Max(n)`
+    ///
+    /// See [`NumThreads`] for details.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use orx_parallel::*;
+    ///
+    /// let vec = vec![1, 2, 3, 4];
+    ///
+    /// // all available threads can be used
+    /// assert_eq!(
+    ///     vec.par().params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Auto, CollectOrdering::Ordered)
+    /// );
+    ///
+    /// assert_eq!(
+    ///     vec.par().num_threads(0).params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Auto, CollectOrdering::Ordered)
+    /// );
+    ///
+    /// // maximum 4 threads can be used
+    /// assert_eq!(
+    ///     vec.par().num_threads(4).chunk_size(64).params(),
+    ///     &Params::new(4, 64, CollectOrdering::Ordered)
+    /// );
+    /// ```
     fn num_threads(self, num_threads: impl Into<NumThreads>) -> Self;
 
+    /// Sets the number of elements to be pulled from the concurrent iterator during the
+    /// parallel execution. When integers are used as argument, the following mapping applies:
+    ///
+    /// * `0` -> `ChunkSize::Auto`
+    /// * `n > 0` -> `NumThreads::Min(n)`
+    ///
+    /// See [`ChunkSize`] for details.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use orx_parallel::*;
+    /// use std::num::NonZero;
+    ///
+    /// let vec = vec![1, 2, 3, 4];
+    ///
+    /// // chunk sizes will be dynamically decided by the parallel runner
+    /// assert_eq!(
+    ///     vec.par().params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Auto, CollectOrdering::Ordered)
+    /// );
+    ///
+    /// assert_eq!(
+    ///     vec.par().chunk_size(0).params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Auto, CollectOrdering::Ordered)
+    /// );
+    ///
+    /// // minimum chunk size will be 64, but can be dynamically increased by the parallel runner
+    /// assert_eq!(
+    ///     vec.par().chunk_size(64).params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Min(NonZero::new(64).unwrap()), CollectOrdering::Ordered)
+    /// );
+    ///
+    /// // chunk size will always be 64, parallel runner cannot change
+    /// assert_eq!(
+    ///     vec.par().chunk_size(ChunkSize::Exact(NonZero::new(64).unwrap())).params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Exact(NonZero::new(64).unwrap()), CollectOrdering::Ordered)
+    /// );
+    /// ```
     fn chunk_size(self, chunk_size: impl Into<ChunkSize>) -> Self;
 
+    /// Sets the collect ordering of the parallel computation.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use orx_parallel::*;
+    ///
+    /// let vec = vec![1, 2, 3, 4];
+    ///
+    /// /// results are collected in order consistent to the input order
+    /// assert_eq!(
+    ///     vec.par().params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Auto, CollectOrdering::Ordered)
+    /// );
+    ///
+    /// assert_eq!(
+    ///     vec.par().collect_ordering(CollectOrdering::Ordered).params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Auto, CollectOrdering::Ordered)
+    /// );
+    ///
+    /// /// results might be collected in arbitrary order
+    /// assert_eq!(
+    ///     vec.par().collect_ordering(CollectOrdering::Arbitrary).params(),
+    ///     &Params::new(NumThreads::Auto, ChunkSize::Auto, CollectOrdering::Arbitrary)
+    /// );
+    /// ```
     fn collect_ordering(self, collect: CollectOrdering) -> Self;
 
+    /// Rather than the [`DefaultRunner`], uses the parallel runner `Q` which implements [`ParallelRunner`].
+    ///
+    /// # Examples
+    ///
+    /// ```ignore
+    /// use orx_parallel::*;
+    ///
+    /// let inputs = vec![1, 2, 3, 4];
+    ///
+    /// // uses the default runner
+    /// let sum = inputs.par().sum();
+    ///
+    /// // uses the custom parallel runner MyParallelRunner: ParallelRunner
+    /// let sum = inputs.par().with_runner::<MyParallelRunner>().sum();
+    /// ```
     fn with_runner<Q: ParallelRunner>(self) -> impl ParIter<Q, Item = Self::Item>;
 
     // computation transformations
@@ -238,5 +385,39 @@ where
         Predicate: Fn(&Self::Item) -> bool + Send + Sync + Clone,
     {
         self.filter(predicate).any_element()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn abc() {
+        let vec = vec![1, 2, 3, 4];
+
+        assert_eq!(
+            vec.par().params(),
+            &Params::new(NumThreads::Auto, ChunkSize::Auto, CollectOrdering::Ordered)
+        );
+
+        assert_eq!(
+            vec.par().num_threads(1).params(),
+            &Params::new(1, ChunkSize::Auto, CollectOrdering::Ordered)
+        );
+
+        assert_eq!(
+            vec.par().num_threads(1).chunk_size(64).params(),
+            &Params::new(1, 64, CollectOrdering::Ordered)
+        );
+
+        assert_eq!(
+            vec.par()
+                .num_threads(1)
+                .chunk_size(64)
+                .collect_ordering(CollectOrdering::Arbitrary)
+                .params(),
+            &Params::new(1, 64, CollectOrdering::Arbitrary)
+        );
     }
 }
