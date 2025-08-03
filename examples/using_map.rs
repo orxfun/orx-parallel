c29a5cb747d4e88dcb968f3ff47a7738b89fe7b3
orxfun <orx.ugur.arikan@gmail.com>
2025-08-03 19:56:00 +0200

    revise using_map example

--- examples/using_map.rs
+++ examples/using_map.rs
@@ -16,7 +16,19 @@ fn fibonacci(n: u64) -> u64 {
     a
 }
 
-fn par_using() -> u64 {
+fn using() -> u64 {
+    let input: Vec<u64> = (1..N).collect();
+
+    input
+        .into_par()
+        .using(|thread_idx| ChaCha20Rng::seed_from_u64(thread_idx as u64 * 10))
+        .map(|_, i| fibonacci(i) % 1000 + 1)
+        .filter(|rng: &mut ChaCha20Rng, _: &u64| rng.random_bool(0.4))
+        .map(|rng: &mut ChaCha20Rng, i: u64| rng.random_range(0..i))
+        .sum()
+}
+
+fn using_clone() -> u64 {
     let rng = ChaCha20Rng::seed_from_u64(42);
 
     let input: Vec<u64> = (1..N).collect();
@@ -30,7 +42,7 @@ fn par_using() -> u64 {
         .sum()
 }
 
-fn par_using_counting_clones() -> u64 {
+fn using_clone_while_counting_clones() -> u64 {
     static COUNTER: AtomicUsize = AtomicUsize::new(0);
     struct Rng(ChaCha20Rng);
     unsafe impl Send for Rng {}
@@ -55,17 +67,38 @@ fn par_using_counting_clones() -> u64 {
         .sum();
 
     let num_clones = COUNTER.load(Ordering::Relaxed);
-    println!("> number of clones (= used threads) = {num_clones}");
+    println!("\n> number of times RNG is cloned (= used threads) = {num_clones}");
 
     result
 }
 
 fn main() {
-    println!("\n\nparallel result:");
-    let parallel_result = par_using();
-    println!("{parallel_result}");
+    println!("\n\n\n### I. using:");
+    println!(
+        "* Allows to create the random number generator (RNG) of each thread using a closure that takes the thread index."
+    );
+    println!("* Hence, we can guarantee to use different RNGs for each thread.");
+    println!("* The following is the example 'using' transformation used in this example.");
+    println!("\n  >  par.using(|thread_idx| ChaCha20Rng::seed_from_u64(thread_idx as u64 * 10))");
+    let parallel_result = using();
+    println!("\n* result = {parallel_result}");
 
-    println!("\n\nparallel result counting clones:");
-    let parallel_result = par_using_counting_clones();
+    println!("\n\n\n### II. using_clone:");
+    println!(
+        "* With this call we create and pass in the RNG; then each thread receives a clone of it as in its initial state."
+    );
+    println!("\n  >  par.using_clone(my_rng)");
+    let parallel_result = using_clone();
+    println!("\n* result = {parallel_result}");
+
+    println!("\n\n\n### III. using_clone with a hook to count number of times the RNG is cloned:");
+    println!(
+        "* The computation is limited to 8 threads; therefore, at most, 8 clones of the RNG must be created."
+    );
+    println!(
+        "* This example demonstrates that exactly one value is created-or-cloned per thread used."
+    );
+    let parallel_result = using_clone_while_counting_clones();
     println!("{parallel_result}");
+    println!("\n* result = {parallel_result}");
 }
