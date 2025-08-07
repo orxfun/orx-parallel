52a29443092a839a2e03cc1de940eae251a867ac
orxfun <orx.ugur.arikan@gmail.com>
2025-08-07 13:39:22 +0200

    wip example

--- examples/collect_result_vec.rs
+++ examples/collect_result_vec.rs
@@ -1,20 +1,74 @@
-use orx_parallel::*;
+use orx_concurrent_bag::ConcurrentBag;
+use orx_concurrent_ordered_bag::ConcurrentOrderedBag;
+
+const N: usize = 1_000_000;
+const ERROR_AT: usize = 567_890;
 
 fn map(a: usize) -> Result<usize, String> {
     match a {
-        6 | 8 => Err(format!("no {a}")),
+        ERROR_AT => Err(format!("no {a}")),
         _ => Ok(a),
     }
 }
 
+fn seq() {
+    println!("\n\nSEQ");
+    let bag = ConcurrentBag::new();
+    let inputs = 0..N;
+    let result: Result<Vec<_>, _> = inputs
+        .map(|x| {
+            bag.push(x);
+            map(x)
+        })
+        .collect();
+    println!("result = {result:?}");
+    let touched = bag.into_inner().to_vec();
+    let num_touched = touched.len();
+    println!("num_touched = {num_touched:?}");
+}
+
+fn rayon() {
+    use rayon::iter::*;
+
+    println!("\n\nRAYON");
+    let bag = ConcurrentBag::new();
+    let inputs = 0..N;
+    let result: Result<Vec<_>, _> = inputs
+        .into_par_iter()
+        .map(|x| {
+            bag.push(x);
+            map(x)
+        })
+        .collect();
+    println!("result = {result:?}");
+    let touched = bag.into_inner().to_vec();
+    let num_touched = touched.len();
+    println!("num_touched = {num_touched:?}");
+}
+
+// fn orx() {
+//     use orx_parallel::*;
+//     println!("\n\nORX");
+//     let bag = ConcurrentBag::new();
+//     let o_bag = ConcurrentOrderedBag::new();
+//     let inputs = 0..N;
+//     let result: Result<Vec<_>, _> = inputs
+//         .par()
+//         .find(|x| {
+//             let mapped = map(*x);
+//             match mapped {
+//                 Ok(x) => o_bag.push(x),
+//                 Err(e) => {}
+//             }
+//         })
+//         .collect();
+//     println!("result = {result:?}");
+//     let touched = bag.into_inner().to_vec();
+//     let num_touched = touched.len();
+//     println!("num_touched = {num_touched:?}");
+// }
+
 fn main() {
-    let inputs = 0..10;
-    let results: Result<Vec<_>, _> = inputs.map(map).collect();
-    println!("{results:?}");
-
-    let inputs = 0..10;
-    let par = inputs.par();
-
-    let results: Result<Vec<_>, _> = inputs.map(map).collect();
-    println!("{results:?}")
+    seq();
+    rayon();
 }
