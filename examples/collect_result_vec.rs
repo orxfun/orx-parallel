6facffe260bcc3a25fc938b782afa7b0bc3f625f
orxfun <orx.ugur.arikan@gmail.com>
2025-08-07 13:25:52 +0200

    wip

--- examples/collect_result_vec.rs
+++ examples/collect_result_vec.rs
@@ -1,3 +1,5 @@
+use orx_parallel::*;
+
 fn map(a: usize) -> Result<usize, String> {
     match a {
         6 | 8 => Err(format!("no {a}")),
@@ -7,6 +9,12 @@ fn map(a: usize) -> Result<usize, String> {
 
 fn main() {
     let inputs = 0..10;
+    let results: Result<Vec<_>, _> = inputs.map(map).collect();
+    println!("{results:?}");
+
+    let inputs = 0..10;
+    let par = inputs.par();
+
     let results: Result<Vec<_>, _> = inputs.map(map).collect();
     println!("{results:?}")
 }
