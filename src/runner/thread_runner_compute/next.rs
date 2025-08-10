391cbfaa9605541c24f9870c1e494bc2b2287d51
orxfun <orx.ugur.arikan@gmail.com>
2025-08-10 17:35:31 +0200

    parallel next for x

--- src/runner/thread_runner_compute/next.rs
+++ src/runner/thread_runner_compute/next.rs
@@ -1,6 +1,68 @@
 use crate::{ThreadRunner, computations::Values};
 use orx_concurrent_iter::{ChunkPuller, ConcurrentIter};
 
+pub fn x<C, I, Vo, X1>(
+    mut runner: C,
+    iter: &I,
+    shared_state: &C::SharedState,
+    xap1: &X1,
+) -> Option<(usize, Vo::Item)>
+where
+    C: ThreadRunner,
+    I: ConcurrentIter,
+    Vo: Values,
+    X1: Fn(I::Item) -> Vo,
+{
+    let mut chunk_puller = iter.chunk_puller(0);
+    let mut item_puller = iter.item_puller_with_idx();
+
+    loop {
+        let chunk_size = runner.next_chunk_size(shared_state, iter);
+
+        runner.begin_chunk(chunk_size);
+
+        match chunk_size {
+            0 | 1 => match item_puller.next() {
+                Some((idx, i)) => {
+                    let vt = xap1(i);
+                    if let Some(first) = vt.first() {
+                        iter.skip_to_end();
+                        runner.complete_chunk(shared_state, chunk_size);
+                        runner.complete_task(shared_state);
+                        return Some((idx, first));
+                    }
+                }
+                None => break,
+            },
+            c => {
+                if c > chunk_puller.chunk_size() {
+                    chunk_puller = iter.chunk_puller(c);
+                }
+
+                match chunk_puller.pull_with_idx() {
+                    Some((idx, chunk)) => {
+                        for i in chunk {
+                            let vt = xap1(i);
+                            if let Some(first) = vt.first() {
+                                iter.skip_to_end();
+                                runner.complete_chunk(shared_state, chunk_size);
+                                runner.complete_task(shared_state);
+                                return Some((idx, first));
+                            }
+                        }
+                    }
+                    None => break,
+                }
+            }
+        }
+
+        runner.complete_chunk(shared_state, chunk_size);
+    }
+
+    runner.complete_task(shared_state);
+    None
+}
+
 pub fn xfx<C, I, Vt, Vo, M1, F, M2>(
     mut runner: C,
     iter: &I,
