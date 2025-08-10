0e853c10a81d1f89f5ce642810dca4287d875647
orxfun <orx.ugur.arikan@gmail.com>
2025-08-10 17:43:43 +0200

    thread runners for map next

--- src/runner/parallel_runner_compute/next.rs
+++ src/runner/parallel_runner_compute/next.rs
@@ -1,10 +1,52 @@
-use crate::computations::X;
+use crate::computations::{M, X};
 use crate::runner::thread_runner_compute as thread;
 use crate::{
     computations::{Values, Xfx},
     runner::ParallelRunnerCompute,
 };
 use orx_concurrent_iter::ConcurrentIter;
+
+pub fn m<C, I, O, M1>(runner: C, m: M<I, O, M1>) -> (usize, Option<O>)
+where
+    C: ParallelRunnerCompute,
+    I: ConcurrentIter,
+    O: Send,
+    M1: Fn(I::Item) -> O + Sync,
+{
+    let (_, iter, xap1) = m.destruct();
+
+    let state = runner.new_shared_state();
+    let shared_state = &state;
+
+    let mut num_spawned = 0;
+    let results = std::thread::scope(|s| {
+        let mut handles = vec![];
+
+        while runner.do_spawn_new(num_spawned, shared_state, &iter) {
+            num_spawned += 1;
+            handles.push(s.spawn(|| {
+                thread::next::m(
+                    runner.new_thread_runner(shared_state),
+                    &iter,
+                    shared_state,
+                    &xap1,
+                )
+            }))
+        }
+
+        let mut results: Vec<(usize, O)> = Vec::with_capacity(handles.len());
+        for x in handles {
+            if let Some(x) = x.join().expect("failed to join the thread") {
+                results.push(x);
+            }
+        }
+        results
+    });
+
+    let acc = results.into_iter().min_by_key(|x| x.0).map(|x| x.1);
+
+    (num_spawned, acc)
+}
 
 pub fn x<C, I, Vo, X1>(runner: C, x: X<I, Vo, X1>) -> (usize, Option<Vo::Item>)
 where
--- src/runner/parallel_runner_compute/next_any.rs
+++ src/runner/parallel_runner_compute/next_any.rs
@@ -1,10 +1,47 @@
-use crate::computations::X;
+use crate::computations::{M, X};
 use crate::runner::thread_runner_compute as thread;
 use crate::{
     computations::{Values, Xfx},
     runner::ParallelRunnerCompute,
 };
 use orx_concurrent_iter::ConcurrentIter;
+
+pub fn m<C, I, O, M1>(runner: C, m: M<I, O, M1>) -> (usize, Option<O>)
+where
+    C: ParallelRunnerCompute,
+    I: ConcurrentIter,
+    O: Send,
+    M1: Fn(I::Item) -> O + Sync,
+{
+    let (_, iter, xap1) = m.destruct();
+
+    let state = runner.new_shared_state();
+    let shared_state = &state;
+
+    let mut num_spawned = 0;
+    let result = std::thread::scope(|s| {
+        let mut handles = vec![];
+
+        while runner.do_spawn_new(num_spawned, shared_state, &iter) {
+            num_spawned += 1;
+            handles.push(s.spawn(|| {
+                thread::next_any::m(
+                    runner.new_thread_runner(shared_state),
+                    &iter,
+                    shared_state,
+                    &xap1,
+                )
+            }));
+        }
+
+        // do not wait to join other threads
+        handles
+            .into_iter()
+            .find_map(|x| x.join().expect("failed to join the thread"))
+    });
+
+    (num_spawned, result)
+}
 
 pub fn x<C, I, Vo, X1>(runner: C, x: X<I, Vo, X1>) -> (usize, Option<Vo::Item>)
 where
