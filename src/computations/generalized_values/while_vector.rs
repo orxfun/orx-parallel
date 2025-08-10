885eeab7c56d8b9566f9220b612c99a0aec2148b
orxfun <orx.ugur.arikan@gmail.com>
2025-08-10 23:01:02 +0200

    WhileVector::push_to_pinned_vec is implemented

--- src/computations/generalized_values/while_vector.rs
+++ src/computations/generalized_values/while_vector.rs
@@ -17,83 +17,89 @@ where
     }
 }
 
-// impl<I, T> Values for WhileVector<I, T>
-// where
-//     I: IntoIterator<Item = Option<T>>,
-// {
-//     type Item = T;
+impl<I, T> Values for WhileVector<I, T>
+where
+    I: IntoIterator<Item = Option<T>>,
+{
+    type Item = T;
 
-//     fn values(self) -> impl IntoIterator<Item = Self::Item> {
-//         self.0
-//             .into_iter()
-//             .take_while(|x| x.is_some())
-//             .map(|x| x.unwrap()) // SAFETY: taken while x is Some
-//     }
+    fn values(self) -> impl IntoIterator<Item = Self::Item> {
+        self.0
+            .into_iter()
+            .take_while(|x| x.is_some())
+            .map(|x| x.unwrap()) // SAFETY: taken while x is Some
+    }
 
-//     fn push_to_pinned_vec<P>(self, vector: &mut P) -> Option<usize>
-//     where
-//         P: PinnedVec<Self::Item>,
-//     {
-//         todo!()
-//     }
+    fn push_to_pinned_vec<P>(self, vector: &mut P) -> bool
+    where
+        P: PinnedVec<Self::Item>,
+    {
+        for x in self.0 {
+            match x {
+                Some(x) => vector.push(x),
+                None => return true,
+            }
+        }
+        false
+    }
 
-//     fn push_to_vec_with_idx(self, idx: usize, vec: &mut Vec<(usize, Self::Item)>) -> Option<usize> {
-//         todo!()
-//     }
+    fn push_to_vec_with_idx(self, idx: usize, vec: &mut Vec<(usize, Self::Item)>) -> Option<usize> {
+        todo!()
+    }
 
-//     fn push_to_bag<P>(self, bag: &ConcurrentBag<Self::Item, P>) -> Option<usize>
-//     where
-//         P: IntoConcurrentPinnedVec<Self::Item>,
-//         Self::Item: Send,
-//     {
-//         todo!()
-//     }
+    fn push_to_bag<P>(self, bag: &ConcurrentBag<Self::Item, P>) -> Option<usize>
+    where
+        P: IntoConcurrentPinnedVec<Self::Item>,
+        Self::Item: Send,
+    {
+        todo!()
+    }
 
-//     fn map<M, O>(self, map: M) -> impl Values<Item = O>
-//     where
-//         M: Fn(Self::Item) -> O,
-//     {
-//         todo!()
-//     }
+    fn map<M, O>(self, map: M) -> impl Values<Item = O>
+    where
+        M: Fn(Self::Item) -> O,
+    {
+        todo!()
+    }
 
-//     #[inline(always)]
-//     fn filter<F>(self, filter: F) -> impl Values<Item = Self::Item>
-//     where
-//         F: Fn(&Self::Item) -> bool,
-//     {
-//         todo!()
-//     }
+    #[inline(always)]
+    fn filter<F>(self, filter: F) -> impl Values<Item = Self::Item>
+    where
+        F: Fn(&Self::Item) -> bool,
+    {
+        todo!()
+    }
 
-//     fn flat_map<Fm, Vo>(self, flat_map: Fm) -> impl Values<Item = Vo::Item>
-//     where
-//         Vo: IntoIterator,
-//         Fm: Fn(Self::Item) -> Vo,
-//     {
-//         todo!()
-//     }
+    fn flat_map<Fm, Vo>(self, flat_map: Fm) -> impl Values<Item = Vo::Item>
+    where
+        Vo: IntoIterator,
+        Fm: Fn(Self::Item) -> Vo,
+    {
+        todo!()
+    }
 
-//     fn filter_map<Fm, O>(self, filter_map: Fm) -> impl Values<Item = O>
-//     where
-//         Fm: Fn(Self::Item) -> Option<O>,
-//     {
-//         todo!()
-//     }
+    fn filter_map<Fm, O>(self, filter_map: Fm) -> impl Values<Item = O>
+    where
+        Fm: Fn(Self::Item) -> Option<O>,
+    {
+        todo!()
+    }
 
-//     fn acc_reduce<X>(self, acc: Option<Self::Item>, reduce: X) -> (bool, Option<Self::Item>)
-//     where
-//         X: Fn(Self::Item, Self::Item) -> Self::Item,
-//     {
-//         todo!()
-//     }
+    fn acc_reduce<X>(self, acc: Option<Self::Item>, reduce: X) -> (bool, Option<Self::Item>)
+    where
+        X: Fn(Self::Item, Self::Item) -> Self::Item,
+    {
+        todo!()
+    }
 
-//     fn u_acc_reduce<U, X>(self, u: &mut U, acc: Option<Self::Item>, reduce: X) -> Option<Self::Item>
-//     where
-//         X: Fn(&mut U, Self::Item, Self::Item) -> Self::Item,
-//     {
-//         todo!()
-//     }
+    fn u_acc_reduce<U, X>(self, u: &mut U, acc: Option<Self::Item>, reduce: X) -> Option<Self::Item>
+    where
+        X: Fn(&mut U, Self::Item, Self::Item) -> Self::Item,
+    {
+        todo!()
+    }
 
-//     fn first(self) -> Option<Self::Item> {
-//         todo!()
-//     }
-// }
+    fn first(self) -> Option<Self::Item> {
+        todo!()
+    }
+}
