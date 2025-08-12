5dc513197c22599de825ad85babbc5ed230cbb6e
orxfun <orx.ugur.arikan@gmail.com>
2025-08-12 18:16:32 +0200

    collect_option_into is implemented

--- src/par_iter_option.rs
+++ src/par_iter_option.rs
@@ -1,8 +1,9 @@
 use crate::{ParCollectInto, ParIter};
+use orx_concurrent_iter::ConcurrentIter;
 use std::sync::atomic::{AtomicBool, Ordering};
 
 pub trait ParIterOption<T>: ParIter<Item = Option<T>> {
-    fn collect_option<C>(self) -> Option<C>
+    fn collect_option_into<C>(self, output: C) -> Option<C>
     where
         C: ParCollectInto<T>,
     {
@@ -16,12 +17,20 @@ pub trait ParIterOption<T>: ParIter<Item = Option<T>> {
             })
             .whilst(|x| x.is_some())
             .map(|x| x.unwrap())
-            .collect::<C>();
+            .collect_into(output);
 
         match has_none.load(Ordering::Relaxed) {
             false => Some(result),
             true => None,
         }
+    }
+
+    fn collect_option<C>(self) -> Option<C>
+    where
+        C: ParCollectInto<T>,
+    {
+        let output = C::empty(self.con_iter().try_get_len());
+        self.collect_option_into(output)
     }
 }
 
