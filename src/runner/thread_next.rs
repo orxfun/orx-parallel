beed9279d5782b0934f871b94d59372524833154
orxfun <orx.ugur.arikan@gmail.com>
2025-08-13 14:02:43 +0200

    thread-next reduction simplified

--- src/runner/thread_next.rs
+++ src/runner/thread_next.rs
@@ -5,24 +5,18 @@ pub enum ThreadNext<T> {
 }
 
 impl<T> ThreadNext<T> {
+    /// Returns the value which has the smallest idx
+    ///
     pub fn reduce(results: Vec<Self>) -> Option<(usize, T)> {
-        let mut stopped_idx = usize::MAX;
-
-        for result in &results {
-            match result {
-                Self::Stopped { idx } => stopped_idx = core::cmp::min(stopped_idx, *idx),
-                _ => {}
-            }
-        }
-
-        let mut found_idx = usize::MAX;
+        let mut idx_bound = usize::MAX;
         let mut result = None;
         for x in results {
             match x {
-                Self::Found { idx, value } if idx < stopped_idx && idx < found_idx => {
-                    found_idx = idx;
+                Self::Found { idx, value } if idx < idx_bound => {
+                    idx_bound = idx;
                     result = Some((idx, value));
                 }
+                Self::Stopped { idx } if idx < idx_bound => idx_bound = idx,
                 _ => {}
             }
         }
