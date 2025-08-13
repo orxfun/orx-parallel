c97ca4676c273c9b03361cb6c948352c856343f4
orxfun <orx.ugur.arikan@gmail.com>
2025-08-13 17:38:50 +0200

    ThreadCollect is defined

--- src/values/runner_results/push.rs
+++ src/values/runner_results/push.rs
@@ -3,3 +3,16 @@ pub enum ValuesPush<E> {
     StoppedByWhileCondition { idx: usize },
     StoppedByError { idx: usize, error: E },
 }
+
+pub enum ThreadCollect<T, E> {
+    AllCollected {
+        vec: Vec<(usize, T)>,
+    },
+    StoppedByWhileCondition {
+        vec: Vec<(usize, T)>,
+        stopped_idx: usize,
+    },
+    StoppedByError {
+        error: E,
+    },
+}
