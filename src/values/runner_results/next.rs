48ea54ae65456a597f649dcb095fe9f9262ed3c5
orxfun <orx.ugur.arikan@gmail.com>
2025-08-15 20:42:51 +0200

    NextWithIdx is defined

--- src/values/runner_results/next.rs
+++ src/values/runner_results/next.rs
@@ -9,3 +9,14 @@ pub enum Next<V: Values> {
         error: <V::Fallibility as Fallibility>::Error,
     },
 }
+
+pub struct NextWithIdx<V: Values> {
+    next: Next<V>,
+    idx: usize,
+}
+
+impl<V: Values> Next<V> {
+    pub fn with_idx(self, idx: usize) -> NextWithIdx<V> {
+        NextWithIdx { next: self, idx }
+    }
+}
