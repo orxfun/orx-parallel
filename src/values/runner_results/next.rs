8d55892b9baf390ca1e386c19cc365247ff5920f
orxfun <orx.ugur.arikan@gmail.com>
2025-08-25 17:11:42 +0200

    clean up

--- src/values/runner_results/next.rs
+++ src/values/runner_results/next.rs
@@ -24,42 +24,6 @@ pub enum NextWithIdx<V: Values> {
     },
 }
 
-impl<V: Values> NextWithIdx<V> {
-    fn found_or_stopped_idx(&self) -> Option<usize> {
-        match self {
-            Self::Found { idx, value: _ } => Some(*idx),
-            Self::StoppedByWhileCondition { idx } => Some(*idx),
-            _ => None,
-        }
-    }
-
-    pub fn into_found_value(self) -> Option<V::Item> {
-        match self {
-            Self::Found { idx: _, value } => Some(value),
-            _ => None,
-        }
-    }
-
-    /// Returns the value with the smallest found idx whose idx is less than
-    /// the smallest of the stopped indices, if any.
-    ///
-    /// Returns None if there is no found items before the process stopped.
-    pub fn reduce(results: Vec<Self>) -> Self {
-        let mut result = Self::NotFound;
-        let mut idx_bound = usize::MAX;
-        for x in results {
-            if let Some(idx) = x.found_or_stopped_idx()
-                && idx < idx_bound
-            {
-                idx_bound = idx;
-                result = x;
-            }
-        }
-
-        result
-    }
-}
-
 pub enum NextSuccess<T> {
     Found { idx: usize, value: T },
     StoppedByWhileCondition { idx: usize },
