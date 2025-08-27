058e9761aceab97660cc357f00de71be08fdc2da
orxfun <orx.ugur.arikan@gmail.com>
2025-08-27 13:51:38 +0200

    benchmark scripts updated

--- .scripts/run_benchmark.sh
+++ .scripts/run_benchmark.sh
@@ -3,6 +3,8 @@ bench=$1
 
 sed -i "s/$original_bench/$bench/g" Cargo.toml
 
-cargo bench >> benches/results/$bench.txt;
+rm -f benches/results/$bench.txt
+
+cargo bench >> benches/results/$bench.txt
 
 sed -i "s/$bench/$original_bench/g" Cargo.toml
--- .scripts/run_benchmarks.sh
+++ .scripts/run_benchmarks.sh
@@ -1,6 +1,39 @@
 # allBenches=(collect_filter sum)
 
-allBenches=(collect_filter collect_map_filter count_map_filter find_map_filter reduce_map sum_map_filter collect_filtermap collect_map_filter_hash_set drain_vec_collect_map_filter mut_for_each_iter reduce_map_filter vec_deque_collect_map_filter collect_flatmap collect_result find mut_for_each_slice vec_deque_collect_map_filter_owned collect_iter_into_par count_filtermap find_any reduce sum collect_long_chain count_flatmap find_flatmap reduce_iter_into_par sum_filtermap collect_map count_map find_iter_into_par reduce_long_chain sum_flatmap)
+allBenches=(
+    collect_filter
+    collect_filtermap
+    collect_flatmap
+    collect_iter_into_par
+    collect_long_chain
+    collect_map
+    collect_map_filter
+    collect_map_filter_hash_set
+    collect_result
+    count_filtermap
+    count_flatmap
+    count_map
+    count_map_filter
+    drain_vec_collect_map_filter
+    find
+    find_any
+    find_flatmap
+    find_iter_into_par
+    find_map_filter
+    mut_for_each_iter
+    mut_for_each_slice
+    reduce
+    reduce_iter_into_par
+    reduce_long_chain
+    reduce_map
+    reduce_map_filter
+    sum
+    sum_filtermap
+    sum_flatmap
+    sum_map_filter
+    vec_deque_collect_map_filter
+    vec_deque_collect_map_filter_owned
+)
 
 counter = 0
 
