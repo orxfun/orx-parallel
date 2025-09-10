c259936907d584bd0b2a405b891f4f2175171930
orxfun <orx.ugur.arikan@gmail.com>
2025-09-10 10:07:50 +0200

    Orchestrator::new_runner is defined

--- src/orch/orchestrator.rs
+++ src/orch/orchestrator.rs
@@ -1,5 +1,13 @@
-use crate::ParallelRunner;
+use crate::{ParallelRunner, Params, runner::ComputationKind};
 
 pub trait Orchestrator {
     type Runner: ParallelRunner;
+
+    fn new_runner(
+        kind: ComputationKind,
+        params: Params,
+        initial_input_len: Option<usize>,
+    ) -> Self::Runner {
+        <Self::Runner as ParallelRunner>::new(kind, params, initial_input_len)
+    }
 }
