257010b21f03e2656d4edbaff9a396be7ffdfe0c
orxfun <orx.ugur.arikan@gmail.com>
2025-09-10 10:22:37 +0200

    impl orchestrator for std orchestrator

--- src/orch/implementations/std_orchestrator.rs
+++ src/orch/implementations/std_orchestrator.rs
@@ -1,4 +1,4 @@
-use crate::ParallelRunner;
+use crate::{ParallelRunner, orch::Orchestrator};
 use std::marker::PhantomData;
 
 pub struct StdOrchestrator<R>
@@ -6,4 +6,11 @@ where
     R: ParallelRunner,
 {
     r: PhantomData<R>,
+}
+
+impl<R> Orchestrator for StdOrchestrator<R>
+where
+    R: ParallelRunner,
+{
+    type Runner = R;
 }
