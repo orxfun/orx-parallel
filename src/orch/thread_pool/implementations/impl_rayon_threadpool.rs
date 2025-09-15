f100124d424269440553ee5296f99944adcb1026
orxfun <orx.ugur.arikan@gmail.com>
2025-09-15 14:26:04 +0200

    ParThreadPool is implemented for all references

--- src/orch/thread_pool/implementations/impl_rayon_threadpool.rs
+++ src/orch/thread_pool/implementations/impl_rayon_threadpool.rs
@@ -26,9 +26,68 @@ impl ParThreadPool for rayon::ThreadPool {
     }
 
     fn max_num_threads(&self) -> NonZeroUsize {
-        match self.current_num_threads() {
-            0 => NonZeroUsize::new(1).expect(">0"),
-            n => NonZeroUsize::new(n).expect(">0"),
-        }
+        NonZeroUsize::new(self.current_num_threads().max(1)).expect(">0")
+    }
+}
+
+// &
+
+impl<'a> ParThreadPool for &'a rayon::ThreadPool {
+    type ScopeRef<'s, 'env, 'scope>
+        = &'s rayon::Scope<'scope>
+    where
+        'scope: 's,
+        'env: 'scope + 's;
+
+    fn run_in_scope<'s, 'env, 'scope, W>(s: &Self::ScopeRef<'s, 'env, 'scope>, work: &'env W)
+    where
+        'scope: 's,
+        'env: 'scope + 's,
+        W: Fn() + Sync + 'scope + 'env,
+    {
+        s.spawn(|_| work());
+    }
+
+    fn scoped_computation<'env, 'scope, F>(&'env mut self, f: F)
+    where
+        'env: 'scope,
+        for<'s> F: FnOnce(&'s rayon::Scope<'scope>) + Send,
+    {
+        self.scope(f)
+    }
+
+    fn max_num_threads(&self) -> NonZeroUsize {
+        NonZeroUsize::new(self.current_num_threads().max(1)).expect(">0")
+    }
+}
+
+// &mut
+
+impl<'a> ParThreadPool for &'a mut rayon::ThreadPool {
+    type ScopeRef<'s, 'env, 'scope>
+        = &'s rayon::Scope<'scope>
+    where
+        'scope: 's,
+        'env: 'scope + 's;
+
+    fn run_in_scope<'s, 'env, 'scope, W>(s: &Self::ScopeRef<'s, 'env, 'scope>, work: &'env W)
+    where
+        'scope: 's,
+        'env: 'scope + 's,
+        W: Fn() + Sync + 'scope + 'env,
+    {
+        s.spawn(|_| work());
+    }
+
+    fn scoped_computation<'env, 'scope, F>(&'env mut self, f: F)
+    where
+        'env: 'scope,
+        for<'s> F: FnOnce(&'s rayon::Scope<'scope>) + Send,
+    {
+        self.scope(f)
+    }
+
+    fn max_num_threads(&self) -> NonZeroUsize {
+        NonZeroUsize::new(self.current_num_threads().max(1)).expect(">0")
     }
 }
