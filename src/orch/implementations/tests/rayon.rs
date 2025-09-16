02348811a04036c0ff52030b836bf77416b612ec
orxfun <orx.ugur.arikan@gmail.com>
2025-09-16 09:17:08 +0200

    rayon orch tests extracted

--- src/orch/implementations/tests/rayon.rs
+++ src/orch/implementations/tests/rayon.rs
@@ -1,6 +1,5 @@
-use crate::{IntoParIter, IterationOrder, ParIter, orch::implementations::RayonOrchestrator};
-use orx_pinned_vec::PinnedVec;
-use orx_split_vec::SplitVec;
+use super::run_map;
+use crate::{IterationOrder, orch::implementations::RayonOrchestrator};
 use test_case::test_matrix;
 
 #[cfg(miri)]
@@ -15,38 +14,10 @@ const N: [usize; 2] = [1025, 4735];
     [IterationOrder::Ordered, IterationOrder::Arbitrary])
 ]
 fn pool_rayon_map(n: usize, nt: usize, chunk: usize, ordering: IterationOrder) {
-    let offset = 33;
-
-    let input: Vec<_> = (0..n).map(|x| x.to_string()).collect();
-    let map = |x: String| format!("{}!", x);
-
-    let mut output = SplitVec::with_doubling_growth_and_max_concurrent_capacity();
-    let mut expected = Vec::new();
-
-    for i in 0..offset {
-        let value = || map(i.to_string());
-        output.push(value());
-        expected.push(value());
-    }
-    expected.extend(input.clone().into_iter().map(|x| map(x)));
-
     let pool = rayon::ThreadPoolBuilder::new()
         .num_threads(nt)
         .build()
         .unwrap();
-    let mut orch: RayonOrchestrator<_> = (&pool).into();
-    let mut output = input
-        .into_par()
-        .with_runner(&mut orch)
-        .chunk_size(chunk)
-        .iteration_order(ordering)
-        .map(map)
-        .collect_into(output);
-
-    if matches!(ordering, IterationOrder::Arbitrary) {
-        expected.sort();
-        output.sort();
-    }
-
-    assert_eq!(expected, output.to_vec());
+    let orch: RayonOrchestrator<_> = (&pool).into();
+    run_map(n, chunk, ordering, orch);
 }
---
+++ src/orch/implementations/tests/utils.rs
@@ -0,0 +1,35 @@
+use crate::{IntoParIter, IterationOrder, ParIter, orch::Orchestrator};
+use orx_pinned_vec::PinnedVec;
+use orx_split_vec::SplitVec;
+
+pub fn run_map(n: usize, chunk: usize, ordering: IterationOrder, mut orch: impl Orchestrator) {
+    let offset = 33;
+
+    let input: Vec<_> = (0..n).map(|x| x.to_string()).collect();
+    let map = |x: String| format!("{}!", x);
+
+    let mut output = SplitVec::with_doubling_growth_and_max_concurrent_capacity();
+    let mut expected = Vec::new();
+
+    for i in 0..offset {
+        let value = || map(i.to_string());
+        output.push(value());
+        expected.push(value());
+    }
+    expected.extend(input.clone().into_iter().map(|x| map(x)));
+
+    let mut output = input
+        .into_par()
+        .with_runner(&mut orch)
+        .chunk_size(chunk)
+        .iteration_order(ordering)
+        .map(map)
+        .collect_into(output);
+
+    if matches!(ordering, IterationOrder::Arbitrary) {
+        expected.sort();
+        output.sort();
+    }
+
+    assert_eq!(expected, output.to_vec());
+}
