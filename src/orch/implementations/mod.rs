54d1690d2f9b9b540468026c849a15335029608d
orxfun <orx.ugur.arikan@gmail.com>
2025-09-16 08:59:16 +0200

    feature flags added for rayon

--- src/orch/implementations/mod.rs
+++ src/orch/implementations/mod.rs
@@ -1,5 +1,7 @@
 mod default_std_orchestrator;
-mod rayon_orchestrator;
-
 pub use default_std_orchestrator::DefaultStdOrchestrator;
+
+#[cfg(feature = "rayon")]
+mod rayon_orchestrator;
+#[cfg(feature = "rayon")]
 pub use rayon_orchestrator::RayonOrchestrator;
