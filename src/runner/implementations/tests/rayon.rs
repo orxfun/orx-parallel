22d20da6af3f84ae5051f8d891211653825c478e
orxfun <orx.ugur.arikan@gmail.com>
2025-09-18 07:33:09 +0200

    exclude rayon pool from miri tests

--- src/runner/implementations/tests/rayon.rs
+++ src/runner/implementations/tests/rayon.rs
@@ -7,6 +7,8 @@ const N: [usize; 2] = [37, 125];
 #[cfg(not(miri))]
 const N: [usize; 2] = [1025, 4735];
 
+// TODO: rayon pool fails the miri test (integer-to-pointer cast crossbeam-epoch-0.9.18/src/atomic.rs:204:11)
+#[cfg(not(miri))]
 #[test_matrix(
     [0, 1, N[0], N[1]],
     [1, 4],
