fc8f0aee757c9dd3f7bc18f2792da8006ec18a2a
orxfun <orx.ugur.arikan@gmail.com>
2025-09-18 23:44:23 +0200

    --all-features is added to benchmark script

--- .scripts/run_benchmark.sh
+++ .scripts/run_benchmark.sh
@@ -5,6 +5,6 @@ sed -i "s/$original_bench/$bench/g" Cargo.toml
 
 rm -f benches/results/$bench.txt
 
-cargo bench >> benches/results/$bench.txt
+cargo bench --all-features >> benches/results/$bench.txt
 
 sed -i "s/$bench/$original_bench/g" Cargo.toml
