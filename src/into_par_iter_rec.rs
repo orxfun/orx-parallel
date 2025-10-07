d41f7ab6aad0ba7b628c9fb4d70abc1cd73545ae
orxfun <orx.ugur.arikan@gmail.com>
2025-10-07 15:36:06 +0200

    define IntoParIterRec trait

---
+++ src/into_par_iter_rec.rs
@@ -0,0 +1,18 @@
+use crate::{DefaultRunner, computational_variants::Par};
+use orx_concurrent_recursive_iter::ConcurrentRecursiveIter;
+
+pub trait IntoParIterRec
+where
+    Self: IntoIterator,
+    <Self as IntoIterator>::IntoIter: ExactSizeIterator,
+    <Self as IntoIterator>::Item: Send,
+{
+    fn into_par_rec<E, I>(
+        self,
+        extend: E,
+    ) -> Par<ConcurrentRecursiveIter<<Self as IntoIterator>::Item, E, I>, DefaultRunner>
+    where
+        I: IntoIterator<Item = <Self as IntoIterator>::Item>,
+        I::IntoIter: ExactSizeIterator,
+        E: Fn(&<Self as IntoIterator>::Item) -> I + Sync;
+}
