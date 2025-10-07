78b2d7ca08acfad838ecf21da63fdea2f6d0501f
orxfun <orx.ugur.arikan@gmail.com>
2025-10-07 15:42:57 +0200

    IntoParIterRec is implemented for all sequential iterators

--- src/into_par_iter_rec.rs
+++ src/into_par_iter_rec.rs
@@ -1,10 +1,25 @@
-use crate::{DefaultRunner, computational_variants::Par};
+use crate::{DefaultRunner, Params, computational_variants::Par};
 use orx_concurrent_recursive_iter::ConcurrentRecursiveIter;
 
 pub trait IntoParIterRec
 where
     Self: IntoIterator,
-    <Self as IntoIterator>::Item: Send,
+    Self::Item: Send,
+{
+    fn into_par_rec<E, I>(
+        self,
+        extend: E,
+    ) -> Par<ConcurrentRecursiveIter<Self::Item, E, I>, DefaultRunner>
+    where
+        I: IntoIterator<Item = Self::Item>,
+        I::IntoIter: ExactSizeIterator,
+        E: Fn(&Self::Item) -> I + Sync;
+}
+
+impl<X> IntoParIterRec for X
+where
+    X: IntoIterator,
+    X::Item: Send,
 {
     fn into_par_rec<E, I>(
         self,
@@ -13,5 +28,9 @@ where
     where
         I: IntoIterator<Item = <Self as IntoIterator>::Item>,
         I::IntoIter: ExactSizeIterator,
-        E: Fn(&<Self as IntoIterator>::Item) -> I + Sync;
+        E: Fn(&<Self as IntoIterator>::Item) -> I + Sync,
+    {
+        let con_rec_iter = ConcurrentRecursiveIter::new(extend, self);
+        Par::new(DefaultRunner::default(), Params::default(), con_rec_iter)
+    }
 }
