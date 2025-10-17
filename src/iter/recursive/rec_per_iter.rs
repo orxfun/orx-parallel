cf2e979626fe97693eca664db5cc44742e000790
orxfun <orx.ugur.arikan@gmail.com>
2025-10-17 11:36:48 +0200

    update eager calls by utilizing exact length when known

--- src/iter/recursive/rec_per_iter.rs
+++ src/iter/recursive/rec_per_iter.rs
@@ -4,12 +4,13 @@ use crate::{
     generic_values::{TransformableValues, runner_results::Infallible},
 };
 use orx_concurrent_iter::{ConcurrentIter, IntoConcurrentIter, implementations::ConIterVec};
-use orx_concurrent_recursive_iter::ConcurrentRecursiveIter;
+use orx_concurrent_recursive_iter::{ConcurrentRecursiveIterCore, Size};
 
-type Rec<I, E> = ConcurrentRecursiveIter<<I as IntoIterator>::Item, E, I>;
+type Rec<S, I, E> = ConcurrentRecursiveIterCore<S, <I as IntoIterator>::Item, E, I>;
 
-impl<E, I, R> Par<Rec<I, E>, R>
+impl<S, E, I, R> Par<Rec<S, I, E>, R>
 where
+    S: Size,
     I: IntoIterator,
     I::IntoIter: ExactSizeIterator,
     I::Item: Send,
@@ -18,14 +19,15 @@ where
 {
     pub fn into_eager(self) -> Par<ConIterVec<I::Item>, R> {
         let (orchestrator, params, iter) = self.destruct();
-        let items: Vec<_> = iter.into_seq_iter().collect();
+        let items = collect_items(iter);
         let iter = items.into_con_iter();
         Par::new(orchestrator, params, iter)
     }
 }
 
-impl<E, I, R, O, M1> ParMap<Rec<I, E>, O, M1, R>
+impl<S, E, I, R, O, M1> ParMap<Rec<S, I, E>, O, M1, R>
 where
+    S: Size,
     I: IntoIterator,
     I::IntoIter: ExactSizeIterator,
     I::Item: Send,
@@ -35,14 +37,15 @@ where
 {
     pub fn into_eager(self) -> ParMap<ConIterVec<I::Item>, O, M1, R> {
         let (orchestrator, params, iter, map1) = self.destruct();
-        let items: Vec<_> = iter.into_seq_iter().collect();
+        let items = collect_items(iter);
         let iter = items.into_con_iter();
         ParMap::new(orchestrator, params, iter, map1)
     }
 }
 
-impl<E, I, R, Vo, X1> ParXap<Rec<I, E>, Vo, X1, R>
+impl<S, E, I, R, Vo, X1> ParXap<Rec<S, I, E>, Vo, X1, R>
 where
+    S: Size,
     I: IntoIterator,
     I::IntoIter: ExactSizeIterator,
     I::Item: Send,
@@ -53,8 +56,26 @@ where
 {
     pub fn into_eager(self) -> ParXap<ConIterVec<I::Item>, Vo, X1, R> {
         let (orchestrator, params, iter, xap1) = self.destruct();
-        let items: Vec<_> = iter.into_seq_iter().collect();
+        let items = collect_items(iter);
         let iter = items.into_con_iter();
         ParXap::new(orchestrator, params, iter, xap1)
+    }
+}
+
+fn collect_items<S, I, E>(iter: Rec<S, I, E>) -> Vec<I::Item>
+where
+    S: Size,
+    I: IntoIterator,
+    I::IntoIter: ExactSizeIterator,
+    I::Item: Send,
+    E: Fn(&I::Item) -> I + Sync,
+{
+    match iter.try_get_len() {
+        Some(len) => {
+            let mut items = Vec::with_capacity(len);
+            items.extend(iter.into_seq_iter());
+            items
+        }
+        None => iter.into_seq_iter().collect(),
     }
 }
