648e88b64856c38276a44920d17f6099f136ec0e
orxfun <orx.ugur.arikan@gmail.com>
2025-10-17 09:32:28 +0200

    ParMap into_eager is implemented for recursive iterators

--- src/iter/recursive/rec_per_iter.rs
+++ src/iter/recursive/rec_per_iter.rs
@@ -23,6 +23,19 @@ where
     }
 }
 
-// pub struct ParMap<I, O, M1, R = DefaultRunner>
-
-// impl<E,I,R, O,M1> ParMap<>
+impl<E, I, R, O, M1> ParMap<Rec<I, E>, O, M1, R>
+where
+    I: IntoIterator,
+    I::IntoIter: ExactSizeIterator,
+    I::Item: Send,
+    E: Fn(&I::Item) -> I + Sync,
+    R: ParallelRunner,
+    M1: Fn(I::Item) -> O + Sync,
+{
+    pub fn into_eager(self) -> ParMap<ConIterVec<I::Item>, O, M1, R> {
+        let (orchestrator, params, iter, map1) = self.destruct();
+        let items: Vec<_> = iter.into_seq_iter().collect();
+        let iter = items.into_con_iter();
+        ParMap::new(orchestrator, params, iter, map1)
+    }
+}
