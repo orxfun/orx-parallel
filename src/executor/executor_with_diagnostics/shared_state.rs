4e8d0263898b3799815b549f95091c948e26bc3b
orxfun <orx.ugur.arikan@gmail.com>
2025-10-19 18:33:14 +0200

    diagnostics shared state holds task_counts

--- src/executor/executor_with_diagnostics/shared_state.rs
+++ src/executor/executor_with_diagnostics/shared_state.rs
@@ -2,17 +2,24 @@ use orx_concurrent_bag::ConcurrentBag;
 
 pub struct SharedStateWithDiagnostics<S> {
     inner: S,
-    tasks: ConcurrentBag<usize>,
+    task_counts: ConcurrentBag<(usize, usize)>, // (thread_idx, chunk_size)
 }
 
 impl<S> SharedStateWithDiagnostics<S> {
     pub fn new(inner: S) -> Self {
         let tasks = ConcurrentBag::new();
-        Self { inner, tasks }
+        Self {
+            inner,
+            task_counts: tasks,
+        }
     }
 
     #[inline(always)]
     pub fn inner(&self) -> &S {
         &self.inner
+    }
+
+    pub fn add_task_count(&self, thread_idx: usize, chunk_size: usize) {
+        self.task_counts.push((thread_idx, chunk_size));
     }
 }
