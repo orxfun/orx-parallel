b9f22db0ccf2130ac212bd98b2be0a0bad182c9e
orxfun <orx.ugur.arikan@gmail.com>
2026-01-02 14:19:54 +0100

    copied test for using computations

--- src/using/computational_variants/mod.rs
+++ src/using/computational_variants/mod.rs
@@ -1,3 +1,13 @@
+#[cfg(test)]
+mod tests;
+
+/// A parallel iterator for which the computation either completely succeeds,
+/// or fails and **early exits** with None.
+pub mod u_fallible_option;
+/// A parallel iterator for which the computation either completely succeeds,
+/// or fails and **early exits** with an error.
+pub mod u_fallible_result;
+
 mod u_map;
 mod u_par;
 mod u_xap;
@@ -5,11 +15,3 @@ mod u_xap;
 pub use u_map::UParMap;
 pub use u_par::UPar;
 pub use u_xap::UParXap;
-
-/// A parallel iterator for which the computation either completely succeeds,
-/// or fails and **early exits** with None.
-pub mod u_fallible_option;
-
-/// A parallel iterator for which the computation either completely succeeds,
-/// or fails and **early exits** with an error.
-pub mod u_fallible_result;
---
+++ src/using/computational_variants/tests/copied.rs
@@ -0,0 +1,156 @@
+use crate::{test_utils::*, *};
+use alloc::vec::Vec;
+use std::string::{String, ToString};
+use test_case::test_matrix;
+
+fn input<O: FromIterator<usize>>(n: usize) -> O {
+    (0..n).collect()
+}
+
+fn make_u_map<I, O>(map: impl Fn(I) -> O + Clone) -> impl Fn(&mut String, I) -> O + Clone {
+    move |u: &mut String, x: I| {
+        let u = u.as_mut_str();
+        u.get_mut(0..2).unwrap().make_ascii_uppercase();
+        map(x)
+    }
+}
+
+fn make_u_filter<I>(filter: &impl Fn(&I) -> bool) -> impl Fn(&mut String, &I) -> bool + Clone {
+    |u: &mut String, x: &I| {
+        let u = u.as_mut_str();
+        u.get_mut(0..2).unwrap().make_ascii_uppercase();
+        filter(x)
+    }
+}
+
+#[test_matrix(N, NT, CHUNK)]
+fn copied_cloned_empty(n: &[usize], nt: &[usize], chunk: &[usize]) {
+    let test = |n, nt, chunk| {
+        let input: Vec<_> = input::<Vec<_>>(n);
+        let expected: usize = input.iter().copied().sum();
+        let par = || {
+            input
+                .par()
+                .num_threads(nt)
+                .chunk_size(chunk)
+                .using_clone("XyZw".to_string())
+        };
+
+        let output = par().copied().sum();
+        assert_eq!(output, expected);
+
+        let output = par().cloned().sum();
+        assert_eq!(output, expected);
+    };
+    test_n_nt_chunk(n, nt, chunk, test);
+}
+
+#[test_matrix(N, NT, CHUNK)]
+fn copied_cloned_map(n: &[usize], nt: &[usize], chunk: &[usize]) {
+    let test = |n, nt, chunk| {
+        let input = input::<Vec<_>>(n);
+        let map = |x: usize| x + 1;
+        let expected: usize = input.iter().copied().map(&map).sum();
+        let par = || {
+            input
+                .par()
+                .num_threads(nt)
+                .chunk_size(chunk)
+                .using_clone("XyZw".to_string())
+        };
+
+        let output = par().copied().map(make_u_map(map)).sum();
+        assert_eq!(output, expected);
+
+        let output = par().cloned().map(make_u_map(map)).sum();
+        assert_eq!(output, expected);
+    };
+    test_n_nt_chunk(n, nt, chunk, test);
+}
+
+#[test_matrix(N, NT, CHUNK)]
+fn copied_cloned_xap_flat_map(n: &[usize], nt: &[usize], chunk: &[usize]) {
+    let test = |n, nt, chunk| {
+        let input = input::<Vec<_>>(n);
+        let flat_map = |x: usize| [x, x + 1, x + 2];
+        let expected: usize = input.iter().copied().flat_map(&flat_map).sum();
+        let par = || {
+            input
+                .par()
+                .num_threads(nt)
+                .chunk_size(chunk)
+                .using_clone("XyZw".to_string())
+        };
+
+        let output = par().copied().flat_map(make_u_map(flat_map)).sum();
+        assert_eq!(output, expected);
+
+        let output = par().cloned().flat_map(make_u_map(flat_map)).sum();
+        assert_eq!(output, expected);
+    };
+    test_n_nt_chunk(n, nt, chunk, test);
+}
+
+#[test_matrix(N, NT, CHUNK)]
+fn copied_cloned_xap_filter_map(n: &[usize], nt: &[usize], chunk: &[usize]) {
+    let test = |n, nt, chunk| {
+        let input = input::<Vec<_>>(n);
+        let filter_map = |x: usize| (x % 3 != 0).then(|| x + 1);
+        let expected: usize = input.iter().copied().filter_map(&filter_map).sum();
+        let par = || {
+            input
+                .par()
+                .num_threads(nt)
+                .chunk_size(chunk)
+                .using_clone("XyZw".to_string())
+        };
+
+        let output = par().copied().filter_map(make_u_map(filter_map)).sum();
+        assert_eq!(output, expected);
+
+        let output = par().cloned().filter_map(make_u_map(filter_map)).sum();
+        assert_eq!(output, expected);
+    };
+    test_n_nt_chunk(n, nt, chunk, test);
+}
+
+#[test_matrix(N, NT, CHUNK)]
+fn copied_cloned_xap_filter_xap(n: &[usize], nt: &[usize], chunk: &[usize]) {
+    let test = |n, nt, chunk| {
+        let input = input::<Vec<_>>(n);
+        let filter_map = |x: usize| (x % 3 != 0).then(|| x + 1);
+        let filter = |x: &usize| x % 3 != 1;
+        let flat_map = |x: usize| [x, x + 1, x + 2];
+        let expected: usize = input
+            .iter()
+            .copied()
+            .filter_map(&filter_map)
+            .filter(&filter)
+            .flat_map(&flat_map)
+            .sum();
+        let par = || {
+            input
+                .par()
+                .num_threads(nt)
+                .chunk_size(chunk)
+                .using_clone("XyZw".to_string())
+        };
+
+        let output = par()
+            .copied()
+            .filter_map(make_u_map(filter_map))
+            .filter(make_u_filter(&filter))
+            .flat_map(make_u_map(flat_map))
+            .sum();
+        assert_eq!(output, expected);
+
+        let output = par()
+            .cloned()
+            .filter_map(make_u_map(filter_map))
+            .filter(make_u_filter(&filter))
+            .flat_map(make_u_map(flat_map))
+            .sum();
+        assert_eq!(output, expected);
+    };
+    test_n_nt_chunk(n, nt, chunk, test);
+}
