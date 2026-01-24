aaf5e6e438c2de91b6df81a57e121663c959840c
orxfun <orx.ugur.arikan@gmail.com>
2026-01-24 20:01:03 +0100

    implement uniform slicing utility function

--- src/sort/slice.rs
+++ src/sort/slice.rs
@@ -1,4 +1,6 @@
 use crate::ParThreadPool;
+use alloc::vec::Vec;
+use core::ops::Range;
 
 pub fn sort<P, T>(pool: &mut P, slice: &mut [T])
 where
@@ -6,4 +8,29 @@ where
     T: Ord,
 {
     slice.sort();
+}
+
+fn slice_ranges(len: usize, num_chunks: usize) -> Vec<Range<usize>> {
+    let num_chunks = match num_chunks < len {
+        true => len,
+        false => num_chunks,
+    };
+
+    let avg_len = len / num_chunks;
+    let lower_sum = avg_len * num_chunks;
+    let num_larger = len - lower_sum;
+
+    let mut slices = Vec::with_capacity(num_chunks);
+    let mut begin = 0;
+    let chunk_size = |c: usize| match c < num_larger {
+        true => avg_len + 1,
+        false => avg_len,
+    };
+    for c in 0..num_chunks {
+        let end = begin + chunk_size(c);
+        slices.push(begin..end);
+        begin = end;
+    }
+
+    slices
 }
