d2e8558bfac4763b4fb816c095ff1c81fd3a9308
orxfun <orx.ugur.arikan@gmail.com>
2026-01-24 19:53:37 +0100

    parametrize sort tests over thread pools

--- src/sort/tests/slice.rs
+++ src/sort/tests/slice.rs
@@ -1,7 +1,6 @@
-use crate::{
-    ParThreadPool, StdDefaultPool,
-    sort::{slice::sort, tests::utils::create_input_and_sorted},
-};
+use crate::sort::{slice::sort, tests::utils::create_input_and_sorted};
+use crate::{ParThreadPool, StdDefaultPool};
+use core::num::NonZeroUsize;
 use test_case::test_matrix;
 
 #[test_matrix(
@@ -9,7 +8,8 @@ use test_case::test_matrix;
     [0, 10000],
     [
         StdDefaultPool::default(),
-        
+        StdDefaultPool::with_max_num_threads(NonZeroUsize::new(1).unwrap()),
+        StdDefaultPool::with_max_num_threads(NonZeroUsize::new(4).unwrap()),
     ]
 )]
 fn slice_sort<P>(len: usize, number_of_swaps: usize, mut pool: P)
