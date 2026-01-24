dd5149e1442ca4b3e8f84d1f52b3e1082c55cafa
orxfun <orx.ugur.arikan@gmail.com>
2026-01-24 20:10:15 +0100

    add slice_ranges tests

--- src/sort/slice.rs
+++ src/sort/slice.rs
@@ -10,26 +10,32 @@ where
     slice.sort();
 }
 
-fn slice_ranges(len: usize, num_chunks: usize) -> Vec<Range<usize>> {
-    let num_chunks = match num_chunks < len {
+pub(super) fn slice_ranges(len: usize, num_chunks: usize) -> Vec<Range<usize>> {
+    let num_chunks = match num_chunks > len {
         true => len,
         false => num_chunks,
     };
 
-    let avg_len = len / num_chunks;
-    let lower_sum = avg_len * num_chunks;
-    let num_larger = len - lower_sum;
-
     let mut slices = Vec::with_capacity(num_chunks);
-    let mut begin = 0;
-    let chunk_size = |c: usize| match c < num_larger {
-        true => avg_len + 1,
-        false => avg_len,
-    };
-    for c in 0..num_chunks {
-        let end = begin + chunk_size(c);
-        slices.push(begin..end);
-        begin = end;
+
+    match num_chunks {
+        0 => {}
+        _ => {
+            let avg_len = len / num_chunks;
+            let lower_sum = avg_len * num_chunks;
+            let num_larger = len - lower_sum;
+
+            let mut begin = 0;
+            let chunk_size = |c: usize| match c < num_larger {
+                true => avg_len + 1,
+                false => avg_len,
+            };
+            for c in 0..num_chunks {
+                let end = begin + chunk_size(c);
+                slices.push(begin..end);
+                begin = end;
+            }
+        }
     }
 
     slices
--- src/sort/tests/slice.rs
+++ src/sort/tests/slice.rs
@@ -1,7 +1,23 @@
 use crate::sort::{slice::sort, tests::utils::create_input_and_sorted};
 use crate::{ParThreadPool, StdDefaultPool};
+use alloc::vec;
+use alloc::vec::Vec;
 use core::num::NonZeroUsize;
-use test_case::test_matrix;
+use core::ops::Range;
+use test_case::{test_case, test_matrix};
+
+#[test_case(0, 0, vec![])]
+#[test_case(0, 1, vec![])]
+#[test_case(10, 0, vec![])]
+#[test_case(15, 5, vec![0..3, 3..6, 6..9, 9..12, 12..15])]
+#[test_case(15, 4, vec![0..4, 4..8, 8..12, 12..15])]
+#[test_case(15, 2, vec![0..8, 8..15])]
+#[test_case(15, 1, vec![0..15])]
+#[test_case(15, 6, vec![0..3, 3..6, 6..9, 9..11, 11..13, 13..15])]
+#[test_case(15, 10, vec![0..2, 2..4, 4..6, 6..8, 8..10, 10..11, 11..12, 12..13, 13..14, 14..15])]
+fn slice_ranges(len: usize, num_chunks: usize, ranges: Vec<Range<usize>>) {
+    assert_eq!(crate::sort::slice::slice_ranges(len, num_chunks), ranges);
+}
 
 #[test_matrix(
     [0, 1, 1034],
