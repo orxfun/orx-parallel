77a23cb67f3fb5e16b508f795bcb5f2667b42941
orxfun <orx.ugur.arikan@gmail.com>
2026-01-25 06:54:39 +0100

    introduce SliceChunk and slice_chunks method to split the slice into smaller chunks

--- src/sort/slice.rs
+++ src/sort/slice.rs
@@ -1,6 +1,8 @@
 use crate::{ParIter, ParThreadPool, ParallelizableCollection};
+use alloc::vec;
 use alloc::vec::Vec;
 use core::{num::NonZeroUsize, ops::Range, ptr::slice_from_raw_parts_mut};
+use orx_priority_queue::BinaryHeap;
 
 pub fn sort<P, T>(pool: &mut P, num_threads: NonZeroUsize, slice: &mut [T])
 where
@@ -8,18 +10,41 @@ where
     T: Ord,
 {
     let num_chunks: usize = pool.max_num_threads().min(num_threads).into();
-    let ranges = slice_ranges(slice.len(), num_chunks);
-    let p = slice.as_mut_ptr() as usize;
-    ranges.par().for_each(|range| {
-        let p = p as *mut T;
-        let ptr = unsafe { p.add(range.start) };
-        let slice = unsafe { &mut *slice_from_raw_parts_mut(ptr, range.len()) };
-        slice.sort();
-    });
+    // let ranges = slice_ranges(slice.len(), num_chunks);
+
+    let mut buf = Vec::<T>::with_capacity(slice.len());
+    let buf_ptr = buf.as_mut_ptr();
+    unsafe { buf_ptr.copy_from_nonoverlapping(slice.as_ptr(), slice.len()) };
+
+    let chunks = slice_chunks(slice.as_mut_ptr(), slice.len(), num_chunks);
+
+    chunks.par().for_each(|chunk| chunk.as_mut_slice().sort());
+
+    // let mut queue = BinaryHeap::with_capacity(ranges.len());
+    // let mut indices = vec![0; ranges.len()];
+
+    // for (v, vec) in ranges.iter().enumerate() {
+    //     if let Some(x) = vec.get(indices[v])
+    //         && x.0 <= max_idx_or_inf
+    //     {
+    //         queue.push(v, x.0);
+    //     }
+    // }
+    // let mut curr_v = queue.pop_node();
     // slice.sort();
 }
 
-pub(super) fn slice_ranges(len: usize, num_chunks: usize) -> Vec<Range<usize>> {
+pub(super) struct SliceChunk<T>(*mut T, usize);
+unsafe impl<T> Send for SliceChunk<T> {}
+unsafe impl<T> Sync for SliceChunk<T> {}
+
+impl<T> SliceChunk<T> {
+    pub(super) fn as_mut_slice(&self) -> &mut [T] {
+        unsafe { &mut *slice_from_raw_parts_mut(self.0, self.1) }
+    }
+}
+
+pub(super) fn slice_chunks<T>(data: *mut T, len: usize, num_chunks: usize) -> Vec<SliceChunk<T>> {
     let num_chunks = match num_chunks > len {
         true => len,
         false => num_chunks,
@@ -40,8 +65,11 @@ pub(super) fn slice_ranges(len: usize, num_chunks: usize) -> Vec<Range<usize>> {
                 false => avg_len,
             };
             for c in 0..num_chunks {
-                let end = begin + chunk_size(c);
-                slices.push(begin..end);
+                let data = unsafe { data.add(begin) };
+                let chunk_size = chunk_size(c);
+                slices.push(SliceChunk(data, chunk_size));
+
+                let end = begin + chunk_size;
                 begin = end;
             }
         }
--- src/sort/tests/slice.rs
+++ src/sort/tests/slice.rs
@@ -3,20 +3,26 @@ use crate::{ParThreadPool, StdDefaultPool};
 use alloc::vec;
 use alloc::vec::Vec;
 use core::num::NonZeroUsize;
-use core::ops::Range;
 use test_case::{test_case, test_matrix};
 
 #[test_case(0, 0, vec![])]
 #[test_case(0, 1, vec![])]
 #[test_case(10, 0, vec![])]
-#[test_case(15, 5, vec![0..3, 3..6, 6..9, 9..12, 12..15])]
-#[test_case(15, 4, vec![0..4, 4..8, 8..12, 12..15])]
-#[test_case(15, 2, vec![0..8, 8..15])]
-#[test_case(15, 1, vec![0..15])]
-#[test_case(15, 6, vec![0..3, 3..6, 6..9, 9..11, 11..13, 13..15])]
-#[test_case(15, 10, vec![0..2, 2..4, 4..6, 6..8, 8..10, 10..11, 11..12, 12..13, 13..14, 14..15])]
-fn slice_ranges(len: usize, num_chunks: usize, ranges: Vec<Range<usize>>) {
-    assert_eq!(crate::sort::slice::slice_ranges(len, num_chunks), ranges);
+#[test_case(15, 5, vec![vec![0,1,2], vec![3,4,5], vec![6,7,8], vec![9,10,11], vec![12,13,14]])]
+#[test_case(15, 4, vec![vec![0,1,2,3], vec![4,5,6,7], vec![8,9,10,11], vec![12,13,14]])]
+#[test_case(15, 2, vec![vec![0,1,2,3,4,5,6,7], vec![8,9,10,11,12,13,14]])]
+#[test_case(15, 1, vec![vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]])]
+#[test_case(15, 6, vec![vec![0,1,2], vec![3,4,5], vec![6,7,8], vec![9,10], vec![11,12], vec![13,14]])]
+#[test_case(15, 10, vec![vec![0,1], vec![2,3], vec![4,5], vec![6,7], vec![8,9], vec![10], vec![11], vec![12], vec![13], vec![14]])]
+fn slice_chunks(len: usize, num_chunks: usize, chunks: Vec<Vec<usize>>) {
+    let mut v: Vec<_> = (0..len).collect();
+    assert_eq!(
+        crate::sort::slice::slice_chunks(v.as_mut_ptr(), v.len(), num_chunks)
+            .into_iter()
+            .map(|c| c.as_mut_slice().iter().copied().collect::<Vec<_>>())
+            .collect::<Vec<_>>(),
+        chunks
+    );
 }
 
 #[test_matrix(
