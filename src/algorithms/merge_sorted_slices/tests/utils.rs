b275e135f9e8b11b86d00f15bc6e6671decdbe28
orxfun <orx.ugur.arikan@gmail.com>
2026-01-30 18:12:53 +0100

    add utility test methods for merge_sorted_slices

--- src/algorithms/merge_sorted_slices/tests/utils.rs
+++ src/algorithms/merge_sorted_slices/tests/utils.rs
@@ -1,1 +1,39 @@
+use alloc::vec::Vec;
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
 
+pub enum SortKind {
+    Sorted,
+    ReverseSorted,
+    Mixed,
+}
+
+pub fn new_vec<T: Ord>(len: usize, elem: impl Fn(usize) -> T, sort_kind: SortKind) -> Vec<T> {
+    let mut vec: Vec<_> = (0..len).map(elem).collect();
+    match sort_kind {
+        SortKind::Sorted => vec.sort(),
+        SortKind::ReverseSorted => {
+            vec.sort();
+            vec.reverse();
+        }
+        SortKind::Mixed => {
+            let num_shuffles = 10 * len;
+            let mut rng = ChaCha8Rng::seed_from_u64(42);
+            for _ in 0..num_shuffles {
+                let i = rng.random_range(0..len);
+                let j = rng.random_range(0..len);
+                vec.swap(i, j);
+            }
+        }
+    }
+    vec
+}
+
+pub fn split_to_sorted_vecs<T: Ord + Clone>(vec: &[T], split_at: usize) -> (Vec<T>, Vec<T>) {
+    let (left, right) = vec.split_at(split_at);
+    let mut left: Vec<_> = left.iter().cloned().collect();
+    let mut right: Vec<_> = right.iter().cloned().collect();
+    left.sort();
+    right.sort();
+    (left, right)
+}
