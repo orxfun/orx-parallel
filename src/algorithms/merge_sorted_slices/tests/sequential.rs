7a6becda04a5f2f1a8d6c1de978fd8d09bf06a46
orxfun <orx.ugur.arikan@gmail.com>
2026-01-30 18:48:27 +0100

    fix memory leak issue in merge sorted slices tests

--- src/algorithms/merge_sorted_slices/tests/sequential.rs
+++ src/algorithms/merge_sorted_slices/tests/sequential.rs
@@ -2,7 +2,6 @@ use super::utils::{SortKind, new_vec, split_to_sorted_vecs};
 use crate::algorithms::data_structures::{Slice, SliceMut};
 use crate::algorithms::merge_sorted_slices::sequential;
 use crate::algorithms::merge_sorted_slices::tests::utils::SplitKind;
-use alloc::boxed::Box;
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 use core::fmt::Debug;
@@ -16,12 +15,8 @@ fn elem_string(x: usize) -> String {
     x.to_string()
 }
 
-fn elem_box(x: usize) -> Box<usize> {
-    Box::new(x)
-}
-
 #[test_matrix(
-    [elem_usize, elem_string, elem_box],
+    [elem_usize, elem_string],
     [0, 1, 2, 3, 37],
     [SortKind::Sorted, SortKind::ReverseSorted, SortKind::Mixed],
     [SplitKind::AllInLeft, SplitKind::AllInRight, SplitKind::OneInLeft, SplitKind::OneInRight, SplitKind::MoreInLeft, SplitKind::MoreInRight, SplitKind::Middle]
@@ -33,7 +28,7 @@ fn merge_sorted_slices_seq<T: Ord + Clone + Debug>(
     split_kind: SplitKind,
 ) {
     let input = new_vec(len, elem, sort_kind);
-    let (left, right) = split_to_sorted_vecs(&input, split_kind);
+    let (mut left, mut right) = split_to_sorted_vecs(&input, split_kind);
 
     let mut result = Vec::with_capacity(input.len());
     sequential::merge_sorted_slices(
@@ -44,9 +39,11 @@ fn merge_sorted_slices_seq<T: Ord + Clone + Debug>(
     );
 
     // all elements of left & right are moved to result
-    unsafe { result.set_len(left.len() + right.len()) };
-    core::mem::forget(left);
-    core::mem::forget(right);
+    unsafe {
+        result.set_len(left.len() + right.len());
+        left.set_len(0);
+        right.set_len(0);
+    }
 
     let mut expected = input.clone();
     expected.sort();
