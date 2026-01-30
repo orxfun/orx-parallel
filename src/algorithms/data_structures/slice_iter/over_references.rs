415dbb3ca8507fd02da694c6af12a7a9bc058085
orxfun <orx.ugur.arikan@gmail.com>
2026-01-30 14:38:58 +0100

    implement SliceIterRef

--- src/algorithms/data_structures/slice_iter/over_references.rs
+++ src/algorithms/data_structures/slice_iter/over_references.rs
@@ -5,8 +5,21 @@ pub struct SliceIterRef<'a, T: 'a>(SliceIterCore<'a, T>);
 impl<'a, T: 'a> Iterator for SliceIterRef<'a, T> {
     type Item = &'a T;
 
+    #[inline(always)]
     fn next(&mut self) -> Option<Self::Item> {
         // SAFETY: SliceIterCore::next returns a valid pointer.
         self.0.next().map(|x| unsafe { &*x })
+    }
+
+    #[inline(always)]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.0.len(), Some(self.0.len()))
+    }
+}
+
+impl<'a, T: 'a> ExactSizeIterator for SliceIterRef<'a, T> {
+    #[inline(always)]
+    fn len(&self) -> usize {
+        self.0.len()
     }
 }
