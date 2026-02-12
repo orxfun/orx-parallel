cc94f301ed19cf6ca31173577d86d15a63a0c354
orxfun <orx.ugur.arikan@gmail.com>
2026-02-12 22:48:38 +0100

    copy one side condition of parallel extend

--- src/algorithms/merge_sorted_slices/parallel.rs
+++ src/algorithms/merge_sorted_slices/parallel.rs
@@ -7,8 +7,42 @@ struct TaskData<'a, T> {
     target: Slice<'a, T>,
 }
 
-enum Task {
+impl<'a, T> Clone for TaskData<'a, T> {
+    fn clone(&self) -> Self {
+        Self {
+            left: self.left.clone(),
+            right: self.right.clone(),
+            target: self.target.clone(),
+        }
+    }
+}
+
+enum TaskKind {
     CopyOneSide,
     SequentialMerge,
     Split,
+}
+
+struct Task<'a, T> {
+    data: TaskData<'a, T>,
+    kind: TaskKind,
+}
+
+impl<'a, T> Task<'a, T> {
+    fn as_copy_one_side(&self) -> Self {
+        Self {
+            data: self.data.clone(),
+            kind: TaskKind::CopyOneSide,
+        }
+    }
+}
+
+fn extend<'a, 'b, T>(task: &'a Task<'b, T>, queue: &Queue<'b, Task<'b, T>>)
+where
+    T: Send,
+{
+    match (task.data.left.len(), task.data.right.len()) {
+        (0, _) | (_, 0) => queue.push(task.as_copy_one_side()),
+        _ => todo!(),
+    }
 }
