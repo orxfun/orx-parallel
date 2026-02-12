788e513c0b3a7da6d60232859278b282aeba453c
orxfun <orx.ugur.arikan@gmail.com>
2026-02-12 22:37:13 +0100

    define sequential task variants for parallel merge

--- src/algorithms/merge_sorted_slices/parallel.rs
+++ src/algorithms/merge_sorted_slices/parallel.rs
@@ -1,5 +1,21 @@
+use crate::algorithms::data_structures::Slice;
 use orx_concurrent_recursive_iter::{ConcurrentRecursiveIter, Queue};
 
-enum Task {
-    CopyOneSide,
+enum Task<'a, T> {
+    CopyOneSide {
+        source: Slice<'a, T>,
+        target: Slice<'a, T>,
+    },
+
+    SequentialMerge {
+        left: Slice<'a, T>,
+        right: Slice<'a, T>,
+        target: Slice<'a, T>,
+    },
+
+    Split {
+        left: Slice<'a, T>,
+        right: Slice<'a, T>,
+        target: Slice<'a, T>,
+    },
 }
