63e5bb4dc0715f0cc178f50d0f2b44c95bffc557
orxfun <orx.ugur.arikan@gmail.com>
2026-02-13 10:34:46 +0100

    handle empty left or right cases

--- src/algorithms/merge_sorted_slices/parallel.rs
+++ src/algorithms/merge_sorted_slices/parallel.rs
@@ -1,13 +1,21 @@
 use crate::algorithms::data_structures::Slice;
 use orx_concurrent_recursive_iter::{ConcurrentRecursiveIter, Queue};
 
-struct TaskData<'a, T> {
+struct Task<'a, T> {
     left: Slice<'a, T>,
     right: Slice<'a, T>,
     target: Slice<'a, T>,
 }
 
-impl<'a, T> TaskData<'a, T> {
+impl<'a, T> Task<'a, T> {
+    fn do_merge_sequentially(&self, sequential_merge_threshold: usize) -> bool {
+        self.left.len() < 2
+            || self.right.len() < 2
+            || self.left.len() + self.right.len() <= sequential_merge_threshold
+    }
+
+    // sequential runs
+
     fn copy_left_to_target(&self) {
         debug_assert_eq!(self.right.len(), 0);
         self.target.copy_from_nonoverlapping(&self.left);
@@ -19,7 +27,7 @@ impl<'a, T> TaskData<'a, T> {
     }
 }
 
-impl<'a, T> Clone for TaskData<'a, T> {
+impl<'a, T> Clone for Task<'a, T> {
     fn clone(&self) -> Self {
         Self {
             left: self.left.clone(),
@@ -29,34 +37,13 @@ impl<'a, T> Clone for TaskData<'a, T> {
     }
 }
 
-enum TaskKind {
-    CopyOneSide,
-    SequentialMerge,
-    Split,
-}
-
-struct Task<'a, T> {
-    data: TaskData<'a, T>,
-    kind: TaskKind,
-}
-
-impl<'a, T> Task<'a, T> {
-    fn into_one_side(&self) -> Self {
-        Self {
-            data: self.data.clone(),
-            kind: TaskKind::CopyOneSide,
-        }
-    }
-}
-
-fn extend<'a, 'b, T>(t: &'a Task<'b, T>, queue: &Queue<'b, Task<'b, T>>)
+fn handle<'a, 'b, T>(t: &'a Task<'b, T>, queue: &Queue<'b, Task<'b, T>>)
 where
     T: Send,
 {
-    match (t.data.left.len(), t.data.right.len()) {
-        (0, _) | (_, 0) => {
-            //
-        }
+    match (t.left.len(), t.right.len()) {
+        (0, _) => t.copy_right_to_target(),
+        (_, 0) => t.copy_left_to_target(),
         _ => {
             //
             todo!()
