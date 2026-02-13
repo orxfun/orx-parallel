a48f58c174e3b6456a66c35f4c6b9019d03f4555
orxfun <orx.ugur.arikan@gmail.com>
2026-02-13 11:14:31 +0100

    implement seq_merge_streak_binary

--- src/algorithms/merge_sorted_slices/sequential.rs
+++ src/algorithms/merge_sorted_slices/sequential.rs
@@ -1,3 +1,5 @@
+use core::cmp::Ordering;
+
 use crate::algorithms::data_structures::Slice;
 use crate::algorithms::merge_sorted_slices::params::ExpSeqMergeSortedSlicesParams;
 
@@ -24,26 +26,26 @@ fn seq_merge_streak_none<'a, T: 'a, F>(
 ) where
     F: Fn(&T, &T) -> bool,
 {
-    let mut left = left.iter_over_ptr();
-    let mut right = right.iter_over_ptr();
-    let mut dst = target.iter_as_dst();
+    let mut it_left = left.iter_over_ptr();
+    let mut it_right = right.iter_over_ptr();
+    let mut it_dst = target.iter_as_dst();
 
     loop {
         unsafe {
-            let l = left.current_unchecked();
-            let r = right.current_unchecked();
+            let l = it_left.current_unchecked();
+            let r = it_right.current_unchecked();
 
             match is_leq(l, r) {
                 true => {
-                    dst.write_one_unchecked(left.next_unchecked());
-                    if left.is_finished() {
-                        dst.write_remaining_from(&right.remaining_into_slice());
+                    it_dst.write_one_unchecked(it_left.next_unchecked());
+                    if it_left.is_finished() {
+                        it_dst.write_remaining_from(&it_right.remaining_into_slice());
                     }
                 }
                 false => {
-                    dst.write_one_unchecked(right.next_unchecked());
-                    if right.is_finished() {
-                        dst.write_remaining_from(&left.remaining_into_slice());
+                    it_dst.write_one_unchecked(it_right.next_unchecked());
+                    if it_right.is_finished() {
+                        it_dst.write_remaining_from(&it_left.remaining_into_slice());
                     }
                 }
             }
@@ -59,38 +61,105 @@ fn seq_merge_streak_linear<'a, T: 'a, F>(
 ) where
     F: Fn(&T, &T) -> bool,
 {
-    let mut left = left.iter_over_ptr();
-    let mut right = right.iter_over_ptr();
-    let mut dst = target.iter_as_dst();
+    let mut it_left = left.iter_over_ptr();
+    let mut it_right = right.iter_over_ptr();
+    let mut it_dst = target.iter_as_dst();
 
     loop {
         unsafe {
-            let l = left.current_unchecked();
-            let r = right.current_unchecked();
+            let l = it_left.current_unchecked();
+            let r = it_right.current_unchecked();
 
             match is_leq(l, r) {
                 true => {
-                    let src_begin = left.next_unchecked();
+                    let src_begin = it_left.next_unchecked();
                     let mut src_end_inclusive = src_begin;
-                    while let Some(next) = left.next_if_leq(&is_leq, r) {
+                    while let Some(next) = it_left.next_if_leq(&is_leq, r) {
                         src_end_inclusive = next;
                     }
-                    dst.write_many_unchecked(src_begin, src_end_inclusive);
 
-                    if left.is_finished() {
-                        dst.write_remaining_from(&right.remaining_into_slice());
+                    it_dst.write_many_unchecked(src_begin, src_end_inclusive);
+
+                    if it_left.is_finished() {
+                        it_dst.write_remaining_from(&it_right.remaining_into_slice());
                     }
                 }
                 false => {
-                    let src_begin = right.next_unchecked();
+                    let src_begin = it_right.next_unchecked();
                     let mut src_end_inclusive = src_begin;
-                    while let Some(next) = right.next_if_leq(&is_leq, l) {
+                    while let Some(next) = it_right.next_if_leq(&is_leq, l) {
                         src_end_inclusive = next;
                     }
-                    dst.write_many_unchecked(src_begin, src_end_inclusive);
 
-                    if right.is_finished() {
-                        dst.write_remaining_from(&left.remaining_into_slice());
+                    it_dst.write_many_unchecked(src_begin, src_end_inclusive);
+
+                    if it_right.is_finished() {
+                        it_dst.write_remaining_from(&it_left.remaining_into_slice());
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn seq_merge_streak_binary<'a, T: 'a, F>(
+    is_leq: F,
+    left: &'a Slice<'a, T>,
+    right: &'a Slice<'a, T>,
+    target: &Slice<'a, T>,
+) where
+    F: Fn(&T, &T) -> bool,
+{
+    let mut it_left = left.iter_over_ptr();
+    let mut it_right = right.iter_over_ptr();
+    let mut it_dst = target.iter_as_dst();
+
+    loop {
+        unsafe {
+            let l = it_left.current_unchecked();
+            let r = it_right.current_unchecked();
+
+            match is_leq(l, r) {
+                true => {
+                    let ptr = it_left.peek_unchecked();
+                    let remaining = left.subslice_from(ptr).as_slice();
+                    let bin_search = remaining.binary_search_by(|x| match is_leq(x, r) {
+                        true => Ordering::Less,
+                        false => Ordering::Greater,
+                    });
+                    let idx = match bin_search {
+                        Ok(idx) => idx,
+                        Err(idx) => idx,
+                    };
+                    let src_begin = it_left.peek_unchecked();
+                    let src_end_inclusive = src_begin.add(idx - 1);
+                    it_left.jump_to(src_end_inclusive);
+
+                    it_dst.write_many_unchecked(src_begin, src_end_inclusive);
+
+                    if it_left.is_finished() {
+                        it_dst.write_remaining_from(&it_right.remaining_into_slice());
+                    }
+                }
+                false => {
+                    let ptr = it_right.peek_unchecked();
+                    let remaining = right.subslice_from(ptr).as_slice();
+                    let bin_search = remaining.binary_search_by(|x| match is_leq(x, l) {
+                        true => Ordering::Less,
+                        false => Ordering::Greater,
+                    });
+                    let idx = match bin_search {
+                        Ok(idx) => idx,
+                        Err(idx) => idx,
+                    };
+                    let src_begin = it_right.peek_unchecked();
+                    let src_end_inclusive = src_begin.add(idx - 1);
+                    it_right.jump_to(src_end_inclusive);
+
+                    it_dst.write_many_unchecked(src_begin, src_end_inclusive);
+
+                    if it_right.is_finished() {
+                        it_dst.write_remaining_from(&it_left.remaining_into_slice());
                     }
                 }
             }
