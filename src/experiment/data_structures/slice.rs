72c3e703c922b659464fecb1eee525f0ad63a54c
orxfun <orx.ugur.arikan@gmail.com>
2026-02-13 14:34:14 +0100

    define Slice methods

--- src/experiment/data_structures/slice.rs
+++ src/experiment/data_structures/slice.rs
@@ -1,15 +1,7 @@
 use alloc::vec::Vec;
 use core::ptr::slice_from_raw_parts;
 
-/// # SAFETY
-///
-/// - (i) `data` is null and `len` is zero; or
-/// - (ii) `data` is non-null and points to `len` consecutive initialized values.
-#[inline(always)]
-unsafe fn new_slice<'a, T>(data: *const T, len: usize) -> *const [T] {
-    slice_from_raw_parts(data, len)
-}
-
+/// A raw slice of contiguous data.
 pub struct Slice<T>(*const [T]);
 
 impl<T> From<&[T]> for Slice<T> {
@@ -37,5 +29,25 @@ impl<T> Slice<T> {
     pub fn from_vec_capacity(vec: &Vec<T>) -> Self {
         // SAFETY: constructing from a valid vec allocation.
         unsafe { Self::new(vec.as_ptr(), vec.capacity()) }
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    /// # SAFETY
+    ///
+    /// - (i) `self` and `src` must have the same lengths.
+    /// - (ii) `self` and `src` must not be overlapping.
+    pub unsafe fn copy_from_nonoverlapping(&self, src: &Self) {
+        debug_assert_eq!(self.len(), src.len());
+
+        // SAFETY: (i) within bounds and (ii) slices do not overlap
+        let dst = self.0 as *mut T;
+        unsafe { dst.copy_from_nonoverlapping(src.0 as *const T, self.len()) };
     }
 }
