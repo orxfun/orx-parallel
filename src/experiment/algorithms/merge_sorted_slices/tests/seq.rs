df1223fa0810e4d2488255a5d0aba42e63af89ed
orxfun <orx.ugur.arikan@gmail.com>
2026-02-16 15:01:38 +0100

    miri green sequential merge

--- src/experiment/algorithms/merge_sorted_slices/tests/seq.rs
+++ src/experiment/algorithms/merge_sorted_slices/tests/seq.rs
@@ -4,11 +4,8 @@ use crate::experiment::algorithms::merge_sorted_slices::seq::{
 };
 use crate::experiment::data_structures::slice_dst::SliceDst;
 use crate::experiment::data_structures::slice_src::SliceSrc;
-use alloc::string::{String, ToString};
-use alloc::vec;
+use alloc::string::String;
 use alloc::vec::Vec;
-use core::fmt::Debug;
-use std::println;
 use test_case::test_matrix;
 
 const PARAMS: &[ParamsSeqMergeSortedSlices] = &[
@@ -41,53 +38,12 @@ const PARAMS: &[ParamsSeqMergeSortedSlices] = &[
 #[test_matrix(
     [(0, 0), (0, 5), (5, 5), (4, 20), (10, 20), (14, 20)],
     [SortKind::Sorted, SortKind::ReverseSorted, SortKind::Mixed],
-    // [PARAMS[0],PARAMS[1],PARAMS[2],PARAMS[3],PARAMS[4],PARAMS[5]])
-    [PARAMS[1]])
+    [PARAMS[0],PARAMS[1],PARAMS[2],PARAMS[3],PARAMS[4],PARAMS[5]])
 ]
 fn xyz((left_len, total_len): (usize, usize), sort: SortKind, params: ParamsSeqMergeSortedSlices) {
     let (mut expected, mut left, mut right) = sorted_slices(left_len, total_len, sort);
 
     let mut result = Vec::<String>::with_capacity(total_len);
-
-    seq_merge(
-        |a, b| a < b,
-        SliceSrc::from_slice(left.as_slice()),
-        SliceSrc::from_slice(right.as_slice()),
-        SliceDst::from_vec(&mut result),
-        params,
-    );
-
-    // all elements of left & right are moved to result
-    unsafe {
-        result.set_len(left.len() + right.len());
-        left.set_len(0);
-        right.set_len(0);
-    }
-
-    expected.sort();
-    assert_eq!(result, expected);
-}
-
-#[test]
-fn abc() {
-    let left_len = 2;
-    let total_len = 4;
-    let sort = SortKind::Mixed;
-    let params = PARAMS[4];
-
-    let (mut expected, mut left, mut right) = sorted_slices(left_len, total_len, sort);
-
-    let mut result = Vec::<String>::with_capacity(total_len);
-    // result.push("a".to_string());
-    // result.push("b".to_string());
-    // result.push("c".to_string());
-    // unsafe { result.set_len(0) };
-
-    // let dst = result.as_ptr() as *mut String;
-    // let source = right.as_ptr();
-    // unsafe { dst.copy_from_nonoverlapping(source, result.capacity()) };
-
-    // println!("src={}\ndst={}\n", source as usize, dst as usize);
 
     seq_merge(
         |a, b| a < b,
