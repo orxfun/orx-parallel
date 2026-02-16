20e07366179a5fb133221691e0589d1858fd36f4
orxfun <orx.ugur.arikan@gmail.com>
2026-02-16 17:24:53 +0100

    clean up

--- src/experiment/data_structures/slice_iter_ptr_dst.rs
+++ src/experiment/data_structures/slice_iter_ptr_dst.rs
@@ -109,37 +109,4 @@ impl<'a, T: 'a> SliceIterPtrDst<'a, T> {
         self.0.jump_to_end();
         src.jump_to_end();
     }
-
-    #[inline(always)]
-    pub unsafe fn write_one_unchecked(&mut self, src: *const T) {
-        // debug_assert!(self.0.len() > 0);
-        // let dst = unsafe { self.0.next_unchecked() };
-        // unsafe { dst.copy_from_nonoverlapping(src, 1) };
-    }
-
-    #[inline(always)]
-    pub unsafe fn write_many_unchecked(
-        &mut self,
-        src_begin: *const T,
-        src_end_inclusive: *const T,
-    ) {
-        // let count = unsafe { src_end_inclusive.offset_from(src_begin) + 1 } as usize;
-        // debug_assert!(self.0.len() >= count);
-        // let dst = unsafe { self.0.next_many_unchecked(count) };
-        // unsafe { dst.copy_from_nonoverlapping(src_begin, count) };
-    }
-
-    #[inline(always)]
-    pub unsafe fn write_remaining_from(&mut self, src: &Slice<T>) {
-        // debug_assert_eq!(src.len(), self.0.len());
-
-        // match self.0.next_remaining() {
-        //     Some((dst, len)) => {
-        //         debug_assert_eq!(src.len(), len);
-        //         unsafe { dst.copy_from_nonoverlapping(src.data(), len) };
-        //     }
-        //     None => {}
-        // }
-        // debug_assert!(self.0.is_finished());
-    }
 }
