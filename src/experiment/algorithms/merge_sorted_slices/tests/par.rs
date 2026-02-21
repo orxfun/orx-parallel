8913680f41400f9dace00b6e377d7ca126302a24
orxfun <orx.ugur.arikan@gmail.com>
2026-02-21 20:43:11 +0100

    add large data tests on parallel merge

--- src/experiment/algorithms/merge_sorted_slices/tests/par.rs
+++ src/experiment/algorithms/merge_sorted_slices/tests/par.rs
@@ -44,7 +44,7 @@ const PARAMS: &[ParamsSeqMergeSortedSlices] = &[
     [PARAMS[0],PARAMS[1],PARAMS[2],PARAMS[3],PARAMS[4],PARAMS[5]],
     [0, 1, 64])
 ]
-fn xyz(
+fn merge_sorted_slices_par(
     (left_len, total_len): (usize, usize),
     sort: SortKind,
     params: ParamsSeqMergeSortedSlices,
@@ -60,6 +60,46 @@ fn xyz(
     let (mut expected, mut left, mut right) = sorted_slices(left_len, total_len, sort);
 
     let mut result = Vec::<String>::with_capacity(total_len);
+
+    par_merge(
+        |a, b| a < b,
+        SliceSrc::from_slice(left.as_slice()),
+        SliceSrc::from_slice(right.as_slice()),
+        SliceDst::from_vec(&mut result),
+        &params,
+    );
+
+    // all elements of left & right are moved to result
+    unsafe {
+        result.set_len(left.len() + right.len());
+        left.set_len(0);
+        right.set_len(0);
+    }
+
+    expected.sort();
+    assert_eq!(result, expected);
+}
+
+#[cfg(not(miri))]
+#[test_matrix(
+    [1<<15],
+    [0, 1, 64],
+    [4, 16]
+)]
+fn merge_sorted_slices_par_large(len: usize, num_threads: usize, chunk_size: usize) {
+    let params = ParamsParMergeSortedSlices {
+        seq_params: ParamsSeqMergeSortedSlices {
+            streak_search: StreakSearch::Linear,
+            put_large_to_left: true,
+        },
+        put_large_to_left: true,
+        chunk_size,
+        num_threads,
+    };
+
+    let (mut expected, mut left, mut right) = sorted_slices(len / 2, len, SortKind::Mixed);
+
+    let mut result = Vec::<String>::with_capacity(len);
 
     par_merge(
         |a, b| a < b,
