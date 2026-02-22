4d5bdd4c1c5d1c3f23912ce4e69b6aad809c9ce4
orxfun <orx.ugur.arikan@gmail.com>
2026-02-22 19:20:34 +0100

    set up experiment feature for examples, tests and benchmarks

--- examples/par_merge_sorted.rs
+++ examples/par_merge_sorted.rs
@@ -1,96 +1,102 @@
-use clap::Parser;
-use orx_parallel::{
-    DefaultRunner,
-    experiment::{
-        algorithms::merge_sorted_slices::{
-            par::{ParamsParMergeSortedSlices, PivotSearch, par_merge},
-            seq::{ParamsSeqMergeSortedSlices, StreakSearch, seq_merge},
+#[cfg(not(feature = "experiment"))]
+fn main() {
+    panic!("REQUIRES FEATURE: experiment");
+}
+
+#[cfg(feature = "experiment")]
+fn main() {
+    use clap::Parser;
+    use orx_parallel::{
+        DefaultRunner,
+        experiment::{
+            algorithms::merge_sorted_slices::{
+                par::{ParamsParMergeSortedSlices, PivotSearch, par_merge},
+                seq::{ParamsSeqMergeSortedSlices, StreakSearch, seq_merge},
+            },
+            data_structures::{slice_dst::SliceDst, slice_src::SliceSrc},
         },
-        data_structures::{slice_dst::SliceDst, slice_src::SliceSrc},
-    },
-};
-use rand::prelude::*;
-use rand_chacha::ChaCha8Rng;
-use std::{cell::UnsafeCell, time::Instant};
+    };
+    use rand::prelude::*;
+    use rand_chacha::ChaCha8Rng;
+    use std::{cell::UnsafeCell, time::Instant};
 
-type X = usize;
+    type X = usize;
 
-fn elem(i: usize) -> X {
-    i
-}
-
-#[inline(always)]
-fn is_leq(a: &X, b: &X) -> bool {
-    a < b
-}
-
-fn new_vec<T: Ord>(len: usize, elem: impl Fn(usize) -> T) -> Vec<T> {
-    let mut vec: Vec<_> = (0..len).map(elem).collect();
-
-    let num_shuffles = 10 * len;
-    let mut rng = ChaCha8Rng::seed_from_u64(42);
-    for _ in 0..num_shuffles {
-        let i = rng.random_range(0..len);
-        let j = rng.random_range(0..len);
-        vec.swap(i, j);
+    fn elem(i: usize) -> X {
+        i
     }
-    vec
-}
 
-fn split_to_sorted_vecs<T: Ord + Clone>(vec: &[T]) -> (Vec<T>, Vec<T>) {
-    split_at(vec, vec.len() / 2)
-}
-
-fn split_at<T: Ord + Clone>(vec: &[T], split_at: usize) -> (Vec<T>, Vec<T>) {
-    let (left, right) = vec.split_at(split_at);
-    let mut left = left.to_vec();
-    let mut right = right.to_vec();
-    left.sort();
-    right.sort();
-    (left, right)
-}
-
-struct Input {
-    left: Vec<X>,
-    right: Vec<X>,
-    target: UnsafeCell<Vec<X>>,
-}
-
-impl Input {
-    fn is_target_sorted(&mut self) -> bool {
-        let target = self.target.get_mut();
-        let mut sorted = target.clone();
-        sorted.sort();
-        target == &sorted
+    #[inline(always)]
+    fn is_leq(a: &X, b: &X) -> bool {
+        a < b
     }
-}
 
-impl Drop for Input {
-    fn drop(&mut self) {
-        unsafe {
-            let target = &mut *self.target.get();
-            target.set_len(self.left.len() + self.right.len());
-            self.left.set_len(0);
-            self.right.set_len(0);
+    fn new_vec<T: Ord>(len: usize, elem: impl Fn(usize) -> T) -> Vec<T> {
+        let mut vec: Vec<_> = (0..len).map(elem).collect();
+
+        let num_shuffles = 10 * len;
+        let mut rng = ChaCha8Rng::seed_from_u64(42);
+        for _ in 0..num_shuffles {
+            let i = rng.random_range(0..len);
+            let j = rng.random_range(0..len);
+            vec.swap(i, j);
+        }
+        vec
+    }
+
+    fn split_to_sorted_vecs<T: Ord + Clone>(vec: &[T]) -> (Vec<T>, Vec<T>) {
+        split_at(vec, vec.len() / 2)
+    }
+
+    fn split_at<T: Ord + Clone>(vec: &[T], split_at: usize) -> (Vec<T>, Vec<T>) {
+        let (left, right) = vec.split_at(split_at);
+        let mut left = left.to_vec();
+        let mut right = right.to_vec();
+        left.sort();
+        right.sort();
+        (left, right)
+    }
+
+    struct Input {
+        left: Vec<X>,
+        right: Vec<X>,
+        target: UnsafeCell<Vec<X>>,
+    }
+
+    impl Input {
+        fn is_target_sorted(&mut self) -> bool {
+            let target = self.target.get_mut();
+            let mut sorted = target.clone();
+            sorted.sort();
+            target == &sorted
         }
     }
-}
 
-#[derive(Parser, Debug)]
-struct Args {
-    #[arg(long, default_value_t = false)]
-    with_diagnostics: bool,
-    #[arg(long, default_value_t = 10)]
-    min_split_len_e: usize,
-    #[arg(long, default_value_t = 8)]
-    num_threads: usize,
-    #[arg(long, default_value_t = 1024)]
-    chunk_size: usize,
-    #[arg(long, default_value_t = 23)]
-    len_e: usize,
-}
+    impl Drop for Input {
+        fn drop(&mut self) {
+            unsafe {
+                let target = &mut *self.target.get();
+                target.set_len(self.left.len() + self.right.len());
+                self.left.set_len(0);
+                self.right.set_len(0);
+            }
+        }
+    }
 
-fn main() {
+    #[derive(Parser, Debug)]
+    struct Args {
+        #[arg(long, default_value_t = false)]
+        with_diagnostics: bool,
+        #[arg(long, default_value_t = 10)]
+        min_split_len_e: usize,
+        #[arg(long, default_value_t = 8)]
+        num_threads: usize,
+        #[arg(long, default_value_t = 1024)]
+        chunk_size: usize,
+        #[arg(long, default_value_t = 23)]
+        len_e: usize,
+    }
+
     let args = Args::parse();
 
     let Args {
