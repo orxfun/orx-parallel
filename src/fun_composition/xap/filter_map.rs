cda92e513ac0fd5f03b9c8114c6f305ae5562ff7
orxfun <orx.ugur.arikan@gmail.com>
2026-02-28 21:25:24 +0100

    compose filter-map and map-filter

--- src/fun_composition/xap/filter_map.rs
+++ src/fun_composition/xap/filter_map.rs
@@ -1,4 +1,5 @@
 use crate::fun_composition::{
+    fn_filter::FilterQ,
     fn_map::{FnMapUnit, MapQ},
     xap::fn_xap::Xap,
 };
@@ -6,7 +7,7 @@ use core::marker::PhantomData;
 
 pub struct FilterMap<I, O, F, M>
 where
-    F: Fn(&I) -> bool,
+    F: FilterQ<I = I>,
     M: MapQ<I = I, O = O>,
 {
     f: F,
@@ -16,13 +17,13 @@ where
 
 impl<I, O, F, M> Xap<I> for FilterMap<I, O, F, M>
 where
-    F: Fn(&I) -> bool,
+    F: FilterQ<I = I>,
     M: MapQ<I = I, O = O>,
 {
     type O = Option<O>;
 
     fn run(&self, i: I) -> Self::O {
-        match (self.f)(&i) {
+        match self.f.run(&i) {
             true => Some(self.m.run(i)),
             false => None,
         }
@@ -31,18 +32,22 @@ where
 
 impl<I, O, F, M> FilterMap<I, O, F, M>
 where
-    F: Fn(&I) -> bool,
+    F: FilterQ<I = I>,
     M: MapQ<I = I, O = O>,
 {
+    fn new(f: F, m: M) -> Self {
+        Self {
+            f,
+            m,
+            phantom: PhantomData,
+        }
+    }
+
     pub fn map<M2, O2>(self, m2: M2) -> FilterMap<I, O2, F, M::PushBack<FnMapUnit<O, O2, M2>>>
     where
         M2: Fn(O) -> O2,
     {
         let m = self.m.push_back(FnMapUnit::new(m2));
-        FilterMap {
-            f: self.f,
-            m,
-            phantom: PhantomData,
-        }
+        FilterMap::new(self.f, m)
     }
 }
