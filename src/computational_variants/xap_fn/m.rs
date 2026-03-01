c85f3739ae38b2de48638b1b52d768407fece50f
orxfun <orx.ugur.arikan@gmail.com>
2026-03-01 18:49:58 +0100

    define map composition

--- src/computational_variants/xap_fn/m.rs
+++ src/computational_variants/xap_fn/m.rs
@@ -1,5 +1,5 @@
 use crate::{
-    computational_variants::xap_fn::{ff::FF, filter::Filter, xap::XapFn},
+    computational_variants::xap_fn::{ff::FF, filter::Filter, map::Map, mm::MM, xap::XapFn},
     generic_values::Values,
 };
 use core::marker::PhantomData;
@@ -19,5 +19,27 @@ where
     pub fn new(f: X) -> Self {
         let p = PhantomData;
         Self { f, p }
+    }
+}
+
+impl<I, O, X> Map<I, O> for M<I, O, X>
+where
+    X: Fn(I) -> O,
+{
+    #[inline(always)]
+    fn map(&self, i: I) -> O {
+        (self.f)(i)
+    }
+
+    type Compose<Y, Q>
+        = MM<I, O, X, Q, M<O, Q, Y>>
+    where
+        Y: Fn(O) -> Q;
+
+    fn compose<Y, Q>(self, y: Y) -> Self::Compose<Y, Q>
+    where
+        Y: Fn(O) -> Q,
+    {
+        MM::new(self.f, M::new(y))
     }
 }
