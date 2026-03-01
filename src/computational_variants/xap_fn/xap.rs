8d504855cbbe905c510d1623a8625aa7918f2e0b
orxfun <orx.ugur.arikan@gmail.com>
2026-03-01 17:31:05 +0100

    define filter on xap-fn

--- src/computational_variants/xap_fn/xap.rs
+++ src/computational_variants/xap_fn/xap.rs
@@ -12,4 +12,11 @@ where
     fn map<M, O>(self, map: M) -> Self::Map<M, O>
     where
         M: Fn(<Vo as Values>::Item) -> O;
+
+    type Filter<F>: XapFn<I, Vo::Filter<F>>
+    where
+        F: Fn(&<Vo as Values>::Item) -> bool;
+    fn filter<F>(self, filter: F) -> Self::Filter<F>
+    where
+        F: Fn(&<Vo as Values>::Item) -> bool;
 }
