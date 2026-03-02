ebe2f0fe7fac2fccdd0ded5774cfdde41a937998
orxfun <orx.ugur.arikan@gmail.com>
2026-03-02 13:51:38 +0100

    wip

--- src/computational_variants/xap_fn/mfmf.rs
+++ src/computational_variants/xap_fn/mfmf.rs
@@ -1,4 +1,9 @@
-use crate::computational_variants::xap_fn::{filter::Filter, map::Map, map_filter::MapFilter};
+use crate::{
+    computational_variants::xap_fn::{
+        f0::F0, filter::Filter, m::M, map::Map, map_filter::MapFilter, xap::XapFn,
+    },
+    generic_values::Values,
+};
 use core::marker::PhantomData;
 
 pub struct MFMF<I, O1, M1, F1, O2, Mf2>
@@ -25,30 +30,80 @@ where
     }
 }
 
-impl<I, O1, M1, F1, O2, Mf2> MapFilter<I, O2> for MFMF<I, O1, M1, F1, O2, Mf2>
-where
-    M1: Map<I, O1>,
-    F1: Filter<O1>,
-    Mf2: MapFilter<O1, O2>,
-{
-    fn map_filter(&self, i: I) -> Option<O2> {
-        todo!()
-    }
+// impl<I, O1, M1, F1, O2, Mf2> MapFilter<I, O2> for MFMF<I, O1, M1, F1, O2, Mf2>
+// where
+//     M1: Map<I, O1>,
+//     F1: Filter<O1>,
+//     Mf2: MapFilter<O1, O2>,
+// {
+//     fn map_filter(&self, i: I) -> Option<O2> {
+//         todo!()
+//     }
 
-    type Compose<Q, M3, F3>
-        = MFMF<I, O1, M1, F1, Q, Mf2::Compose<Q, M3, F3>>
-    where
-        M3: Map<O2, Q>,
-        F3: Filter<Q>;
+//     type Compose<Q, M3, F3>
+//         = MFMF<I, O1, M1, F1, Q, Mf2::Compose<Q, M3, F3>>
+//     where
+//         M3: Map<O2, Q>,
+//         F3: Filter<Q>;
 
-    fn compose<Q, M3, F3>(self, m: M3, f: F3) -> Self::Compose<Q, M3, F3>
-    where
-        M3: Map<O2, Q>,
-        F3: Filter<Q>,
-    {
-        MFMF::new(self.m, self.f, self.mf2.compose(m, f))
-    }
-}
+//     fn compose<Q, M3, F3>(self, m: M3, f: F3) -> Self::Compose<Q, M3, F3>
+//     where
+//         M3: Map<O2, Q>,
+//         F3: Filter<Q>,
+//     {
+//         MFMF::new(self.m, self.f, self.mf2.compose(m, f))
+//     }
+
+//     type ComposeF<F3>
+//         = MFMF<I, O1, M1, F1, O2, Mf2::ComposeF<F3>>
+//     where
+//         F3: Filter<O2>;
+
+//     fn compose_f<F3>(self, f: F3) -> Self::ComposeF<F3>
+//     where
+//         F3: Filter<O2>,
+//     {
+//         MFMF::new(self.m, self.f, self.mf2.compose_f(f))
+//     }
+// }
+
+// impl<I, O1, M1, F1, O2, Mf2> XapFn<I, Option<O2>> for MFMF<I, O1, M1, F1, O2, Mf2>
+// where
+//     M1: Map<I, O1>,
+//     F1: Filter<O1>,
+//     Mf2: MapFilter<O1, O2>,
+// {
+//     fn xap(&self, i: I) -> Option<O2> {
+//         todo!()
+//     }
+
+//     type Map<Y, Q>
+//         = MFMF<I, O1, M1, F1, Q, Mf2::Compose<Q, M<O2, Q, Y>, F0<Q>>>
+//     where
+//         Y: Fn(O2) -> Q;
+
+//     fn map<Y, Q>(self, map: Y) -> Self::Map<Y, Q>
+//     where
+//         Y: Fn(O2) -> Q,
+//     {
+//         let m = M::new(map);
+//         let f = F0::<Q>::new();
+//         let mf2 = self.mf2.compose(m, f);
+//         MFMF::new(self.m, self.f, mf2)
+//     }
+
+//     type Filter<Y>
+//         = Self
+//     where
+//         Y: Fn(&O2) -> bool;
+
+//     fn filter<Y>(self, filter: Y) -> Self::Filter<Y>
+//     where
+//         Y: Fn(&O2) -> bool,
+//     {
+//         todo!()
+//     }
+// }
 
 // use crate::{
 //     computational_variants::xap_fn::{f0::F0, map_filter::MapFilter, xap::XapFn},
