643001c7a75bffa81120d2db8309466b05f49e91
orxfun <orx.ugur.arikan@gmail.com>
2026-03-02 10:02:44 +0100

    define map-filter composition

--- src/computational_variants/xap_fn/mf.rs
+++ src/computational_variants/xap_fn/mf.rs
@@ -1,6 +1,5 @@
-use core::marker::PhantomData;
-
 use crate::computational_variants::xap_fn::{map_filter::MapFilter, mfmf::MFMF};
+use core::marker::PhantomData;
 
 pub struct MF<I, O, X, Y>
 where
@@ -30,5 +29,18 @@ where
 {
     fn map_filter(&self, i: I) -> Option<O> {
         todo!()
+    }
+
+    type Compose<O3, X3, Y3>
+        = MFMF<I, O, X, Y, O3, MF<O, O3, X3, Y3>>
+    where
+        X3: Fn(O) -> O3,
+        Y3: Fn(&O3) -> bool;
+    fn compose<O3, X3, Y3>(self, m: X3, f: Y3) -> Self::Compose<O3, X3, Y3>
+    where
+        X3: Fn(O) -> O3,
+        Y3: Fn(&O3) -> bool,
+    {
+        MFMF::new(self.m, self.f, MF::new(m, f))
     }
 }
