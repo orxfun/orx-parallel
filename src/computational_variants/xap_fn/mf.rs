4472e16348f80b890f0bc544fe52a232423e0873
orxfun <orx.ugur.arikan@gmail.com>
2026-03-02 09:44:59 +0100

    rename xap fn

--- src/computational_variants/xap_fn/mf.rs
+++ src/computational_variants/xap_fn/mf.rs
@@ -1,5 +1,7 @@
 use core::marker::PhantomData;
 
+use crate::computational_variants::xap_fn::{map_filter::MapFilter, mfmf::MFMF};
+
 pub struct MF<I, O, X, Y>
 where
     X: Fn(I) -> O,
@@ -18,5 +20,15 @@ where
     pub fn new(m: X, f: Y) -> Self {
         let p = PhantomData;
         Self { m, f, p }
+    }
+}
+
+impl<I, O, X, Y> MapFilter<I, O> for MF<I, O, X, Y>
+where
+    X: Fn(I) -> O,
+    Y: Fn(&O) -> bool,
+{
+    fn map_filter(&self, i: I) -> Option<O> {
+        todo!()
     }
 }
