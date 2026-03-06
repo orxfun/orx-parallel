0b59d27dc9a8704be21aacbcd784dc26b06bd055
orxfun <orx.ugur.arikan@gmail.com>
2026-03-06 21:48:14 +0100

    define transformation functions on xap

--- src/xap/faker.rs
+++ src/xap/faker.rs
@@ -24,24 +24,60 @@ impl<I, O> Xap for Faker<I, O> {
     where
         G: Fn(Self::O) -> Q;
 
+    fn map<Q, H>(self, h: H) -> Self::Map<Q, H>
+    where
+        H: Fn(Self::O) -> Q,
+    {
+        todo!()
+    }
+
     type Inspect<G>
         = Faker<I, O>
     where
         G: Fn(&Self::O);
+
+    fn inspect<H>(self, h: H) -> Self::Inspect<H>
+    where
+        H: Fn(&Self::O),
+    {
+        todo!()
+    }
 
     type Filter<G>
         = Faker<I, O>
     where
         G: Fn(&Self::O) -> bool;
 
+    fn filter<H>(self, h: H) -> Self::Filter<H>
+    where
+        H: Fn(&Self::O) -> bool,
+    {
+        todo!()
+    }
+
     type FilterMap<Q, G>
         = Faker<I, Q>
     where
         G: Fn(Self::O) -> Option<Q>;
+
+    fn filter_map<Q, H>(self, h: H) -> Self::FilterMap<Q, H>
+    where
+        H: Fn(Self::O) -> Option<Q>,
+    {
+        todo!()
+    }
 
     type FlatMap<V, G>
         = Faker<I, V::Item>
     where
         V: IntoIterator,
         G: Fn(Self::O) -> V;
+
+    fn flat_map<V, H>(self, h: H) -> Self::FlatMap<V, H>
+    where
+        V: IntoIterator,
+        H: Fn(Self::O) -> V,
+    {
+        todo!()
+    }
 }
--- src/xap/xap_trait.rs
+++ src/xap/xap_trait.rs
@@ -17,7 +17,15 @@ pub trait Xap {
     where
         H: Fn(Self::O) -> Q;
 
+    fn map<Q, H>(self, h: H) -> Self::Map<Q, H>
+    where
+        H: Fn(Self::O) -> Q;
+
     type Inspect<H>: Xap<I = Self::I, O = Self::O>
+    where
+        H: Fn(&Self::O);
+
+    fn inspect<H>(self, h: H) -> Self::Inspect<H>
     where
         H: Fn(&Self::O);
 
@@ -25,11 +33,24 @@ pub trait Xap {
     where
         H: Fn(&Self::O) -> bool;
 
+    fn filter<H>(self, h: H) -> Self::Filter<H>
+    where
+        H: Fn(&Self::O) -> bool;
+
     type FilterMap<Q, H>: Xap<I = Self::I, O = Q>
     where
         H: Fn(Self::O) -> Option<Q>;
 
+    fn filter_map<Q, H>(self, h: H) -> Self::FilterMap<Q, H>
+    where
+        H: Fn(Self::O) -> Option<Q>;
+
     type FlatMap<V, H>: Xap<I = Self::I, O = V::Item>
+    where
+        V: IntoIterator,
+        H: Fn(Self::O) -> V;
+
+    fn flat_map<V, H>(self, h: H) -> Self::FlatMap<V, H>
     where
         V: IntoIterator,
         H: Fn(Self::O) -> V;
