e95026976f76d1453457539c81453b6c35b1e390
orxfun <orx.ugur.arikan@gmail.com>
2026-03-06 20:49:17 +0100

    draft MightStopMap as iterator

--- src/xap/might_stop_iterators/map.rs
+++ src/xap/might_stop_iterators/map.rs
@@ -1,1 +1,24 @@
-// pub struct MightStopMap<I> {}
+use crate::xap::{
+    stopper::{MightStop, StoppedBy},
+    xap_trait::Xap,
+};
+
+pub struct MightStopMap<E, X: Xap<S = MightStop<E>>, O, F: Fn(X::O) -> O> {
+    i: <X::Values as IntoIterator>::IntoIter,
+    f: F,
+}
+
+impl<E, X: Xap<S = MightStop<E>>, O, F: Fn(X::O) -> O> MightStopMap<E, X, O, F> {
+    pub fn new(i: <X::Values as IntoIterator>::IntoIter, f: F) -> Self {
+        Self { i, f }
+    }
+}
+
+impl<E, X: Xap<S = MightStop<E>>, O, F: Fn(X::O) -> O> Iterator for MightStopMap<E, X, O, F> {
+    type Item = Result<O, StoppedBy<E>>;
+
+    #[inline(always)]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.i.next().map(|x| x.map(|y| (self.f)(y)))
+    }
+}
