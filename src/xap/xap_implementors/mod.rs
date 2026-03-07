fd039330ee16578c1649a013008da40bcfcfc1bb
orxfun <orx.ugur.arikan@gmail.com>
2026-03-07 21:40:12 +0100

    clean up

--- src/xap/xap_implementors/mod.rs
+++ src/xap/xap_implementors/mod.rs
@@ -9,7 +9,6 @@ mod id;
 mod ins;
 mod m;
 mod m0;
-mod xap_iters;
 
 pub use f::F;
 pub use f0::F0;
@@ -18,4 +17,3 @@ pub use fla_m::FlaM;
 pub use id::Id;
 pub use ins::Ins;
 pub use m::M;
-pub use xap_iters::IterF;
--- src/xap/xap_implementors/xap_iters/mod.rs
+++
@@ -1,4 +0,0 @@
-mod iter_f;
-mod map_many;
-
-pub use iter_f::IterF;
