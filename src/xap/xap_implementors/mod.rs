e4a90ff28072b0b3abb6a0df7887bcb307303584
orxfun <orx.ugur.arikan@gmail.com>
2026-03-08 20:09:37 +0100

    define transformations only with four function types

--- src/xap/xap_implementors/mod.rs
+++ src/xap/xap_implementors/mod.rs
@@ -2,15 +2,15 @@
 mod tests;
 
 mod f;
-mod f0;
+// mod f0;
 mod fil_map;
 mod fla_map;
 mod id;
 mod m;
-mod m0;
+// mod m0;
 
 pub use f::F;
-pub use f0::F0;
+// pub use f0::F0;
 pub use fil_map::FilMap;
 pub use fla_map::FlaMap;
 pub use id::Id;
