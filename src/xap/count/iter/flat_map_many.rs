c944b0b66bb2a3d5c285e68edfba39b20727e80f
orxfun <orx.ugur.arikan@gmail.com>
2026-03-13 16:24:28 +0100

    optimize flat-map fold

--- src/xap/count/iter/flat_map_many.rs
+++ src/xap/count/iter/flat_map_many.rs
@@ -1,9 +1,25 @@
 use crate::xap::fun::flat_map::FlatMap;
+use core::ops::Add;
 
 pub struct FlatMapIterMany<I: Iterator, G: FlatMap<I = I::Item>> {
     i: I,
     g: G,
     inner: Option<<G::O as IntoIterator>::IntoIter>,
+}
+
+impl<I: Iterator, G: FlatMap<I = I::Item>> FlatMapIterMany<I, G>
+where
+    <G::O as IntoIterator>::Item: Add + core::iter::Sum<<G::O as IntoIterator>::Item>,
+{
+    fn sum_fold(self) -> <G::O as IntoIterator>::Item {
+        let iter = self.i.map(|x| {
+            self.g
+                .flat_map(x)
+                .into_iter()
+                .sum::<<G::O as IntoIterator>::Item>()
+        });
+        iter.sum::<<G::O as IntoIterator>::Item>()
+    }
 }
 
 impl<I: Iterator, G: FlatMap<I = I::Item>> FlatMapIterMany<I, G> {
@@ -32,29 +48,63 @@ impl<I: Iterator, G: FlatMap<I = I::Item>> Iterator for FlatMapIterMany<I, G> {
 
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
+        todo!();
+        let (flo, fhi) = self
+            .inner
+            .as_ref()
+            .map_or((0, Some(0)), <G::O as IntoIterator>::IntoIter::size_hint);
+
+        // (flo, None)
+
         match &self.inner {
             Some(inner) => (inner.size_hint().0, None),
             None => (0, None),
         }
     }
 
+    #[inline]
     fn fold<B, F>(self, init: B, mut f: F) -> B
     where
         Self: Sized,
         F: FnMut(B, Self::Item) -> B,
     {
-        let mut value = match self.inner {
+        let acc = match self.inner {
             Some(inner) => inner.fold(init, &mut f),
             None => init,
         };
 
+        self.i.fold(acc, |acc, i| {
+            self.g.flat_map(i).into_iter().fold(acc, &mut f)
+        })
+    }
+
+    #[inline]
+    fn count(self) -> usize
+    where
+        Self: Sized,
+    {
+        let mut acc = match self.inner {
+            Some(inner) => inner.count(),
+            None => 0,
+        };
+
         for i in self.i {
             let inner = self.g.flat_map(i);
-            value = inner.into_iter().fold(value, &mut f);
+            acc += inner.into_iter().count();
         }
 
-        value
+        acc
     }
+
+    // fn flatten(self) -> core::iter::Flatten<Self>
+    // where
+    //     Self: Sized,
+    //     Self::Item: IntoIterator,
+    // {
+    //     let mut abc = [1, 2, 3].into_iter().map(|x| [x, x + 1]);
+    //     let def = abc.flatten();
+    //     todo!()
+    // }
 }
 
 #[inline(always)]
