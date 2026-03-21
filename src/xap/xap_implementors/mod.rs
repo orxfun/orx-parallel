d423abebbe793aa76bed9052d50cbb89e7d0b137
orxfun <orx.ugur.arikan@gmail.com>
2026-03-21 11:20:18 +0100

    clean up

--- src/xap/xap_implementors/mod.rs
+++ src/xap/xap_implementors/mod.rs
@@ -1,19 +1,11 @@
 #[cfg(test)]
 mod tests;
 
-// mod f;
-// mod f0;
 mod fil_map;
 mod flat_map;
-// mod flat_map0;
 mod id;
-// mod m;
 mod m2;
-// mod m0;
 
-// pub use f::F;
-// pub use f0::F0;
 pub use fil_map::FilMap;
 pub use flat_map::FlaMap;
 pub use id::Id;
-// pub use m::M;
