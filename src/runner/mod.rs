b72f0279d0051540ab47ef328519a2971fc49f7f
orxfun <orx.ugur.arikan@gmail.com>
2026-03-24 18:28:27 +0100

    flat runner design

--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -1,11 +1,2 @@
-use crate::pool::ParThreadPool;
-
-pub trait Runner {
-    type Pool: ParThreadPool;
-
-    /// Reference to the underlying thread pool.
-    fn thread_pool(&self) -> &Self::Pool;
-
-    /// Mutable reference to the underlying thread pool.
-    fn thread_pool_mut(&mut self) -> &mut Self::Pool;
-}
+mod runner;
+mod spawned;
