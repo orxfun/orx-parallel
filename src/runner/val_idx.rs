e8168104b0932eeddb80704c815c6c7746a8b08d
orxfun <orx.ugur.arikan@gmail.com>
2026-03-25 11:50:16 +0100

    complete parallel next implementation

--- src/runner/val_idx.rs
+++ src/runner/val_idx.rs
@@ -1,3 +1,5 @@
+use alloc::vec::Vec;
+
 pub struct ValIdx<T> {
     pub val: T,
     pub idx: usize,
@@ -7,5 +9,21 @@ impl<T> ValIdx<T> {
     #[inline(always)]
     pub fn new(val: T, idx: usize) -> Self {
         Self { val, idx }
+    }
+
+    pub fn find_next(results: Vec<Option<ValIdx<T>>>) -> Option<ValIdx<T>> {
+        let mut min_idx = usize::MAX;
+        let mut value = None;
+
+        for x in results {
+            if let Some(y) = x {
+                if y.idx < min_idx {
+                    min_idx = y.idx;
+                    value = Some(y);
+                }
+            }
+        }
+
+        value
     }
 }
