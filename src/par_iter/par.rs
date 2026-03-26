9c6289c134ac2d07061cb4e3452eb7ee686fd6fb
orxfun <orx.ugur.arikan@gmail.com>
2026-03-26 19:36:01 +0100

    define all par transformations with fully qualified type names

--- src/par_iter/par.rs
+++ src/par_iter/par.rs
@@ -41,4 +41,37 @@ where
         let xap = self.xap.map(h);
         Par::new(self.iter, xap, self.exe)
     }
+
+    pub fn inspect<H>(self, h: H) -> Par<I, X::Inspect<H>, R>
+    where
+        H: Fn(&X::O) + Copy + Send,
+    {
+        let xap = self.xap.inspect(h);
+        Par::new(self.iter, xap, self.exe)
+    }
+
+    pub fn filter<H>(self, h: H) -> Par<I, X::Filter<H>, R>
+    where
+        H: Fn(&X::O) -> bool + Copy + Send,
+    {
+        let xap = self.xap.filter(h);
+        Par::new(self.iter, xap, self.exe)
+    }
+
+    pub fn filter_map<Q, H>(self, h: H) -> Par<I, X::FilterMap<Q, H>, R>
+    where
+        H: Fn(X::O) -> Option<Q> + Copy + Send,
+    {
+        let xap = self.xap.filter_map(h);
+        Par::new(self.iter, xap, self.exe)
+    }
+
+    pub fn flat_map<V, H>(self, h: H) -> Par<I, X::FlatMap<V, H>, R>
+    where
+        V: IntoIterator,
+        H: Fn(X::O) -> V + Copy + Send,
+    {
+        let xap = self.xap.flat_map(h);
+        Par::new(self.iter, xap, self.exe)
+    }
 }
