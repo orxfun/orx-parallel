93f91e39d5177b593c336ad7585f44c63affad2f
orxfun <orx.ugur.arikan@gmail.com>
2026-03-26 19:44:55 +0100

    ordered first calculation is implemented

--- src/par_iter/par.rs
+++ src/par_iter/par.rs
@@ -1,3 +1,4 @@
+use crate::parameters::{IterationOrder, Params};
 use crate::runner::{DefaultRunner, ParRunner, default_runner};
 use crate::xap::{Id, Xap};
 use orx_concurrent_iter::ConcurrentIter;
@@ -8,6 +9,7 @@ pub fn par<I: ConcurrentIter>(iter: I) -> Par<I, Id<I::Item>, DefaultRunner> {
         iter,
         xap: Id::new(),
         exe: default_runner(),
+        params: Default::default(),
     }
 }
 
@@ -20,6 +22,7 @@ where
     iter: I,
     xap: X,
     exe: R,
+    params: Params,
 }
 
 impl<I, X, R> Par<I, X, R>
@@ -28,8 +31,21 @@ where
     X: Xap<I = I::Item>,
     R: ParRunner,
 {
-    fn new(iter: I, xap: X, exe: R) -> Self {
-        Self { iter, xap, exe }
+    fn new(iter: I, xap: X, exe: R, params: Params) -> Self {
+        Self {
+            iter,
+            xap,
+            exe,
+            params,
+        }
+    }
+
+    fn with_xap<Y: Xap<I = I::Item>>(self, xap: Y) -> Par<I, Y, R> {
+        Par::new(self.iter, xap, self.exe, self.params)
+    }
+
+    fn destruct(self) -> (I, X, R, Params) {
+        (self.iter, self.xap, self.exe, self.params)
     }
 
     // transformations
@@ -39,7 +55,7 @@ where
         H: Fn(X::O) -> Q + Copy + Send,
     {
         let xap = self.xap.map(h);
-        Par::new(self.iter, xap, self.exe)
+        self.with_xap(xap)
     }
 
     pub fn inspect<H>(self, h: H) -> Par<I, X::Inspect<H>, R>
@@ -47,7 +63,7 @@ where
         H: Fn(&X::O) + Copy + Send,
     {
         let xap = self.xap.inspect(h);
-        Par::new(self.iter, xap, self.exe)
+        self.with_xap(xap)
     }
 
     pub fn filter<H>(self, h: H) -> Par<I, X::Filter<H>, R>
@@ -55,7 +71,7 @@ where
         H: Fn(&X::O) -> bool + Copy + Send,
     {
         let xap = self.xap.filter(h);
-        Par::new(self.iter, xap, self.exe)
+        self.with_xap(xap)
     }
 
     pub fn filter_map<Q, H>(self, h: H) -> Par<I, X::FilterMap<Q, H>, R>
@@ -63,7 +79,7 @@ where
         H: Fn(X::O) -> Option<Q> + Copy + Send,
     {
         let xap = self.xap.filter_map(h);
-        Par::new(self.iter, xap, self.exe)
+        self.with_xap(xap)
     }
 
     pub fn flat_map<V, H>(self, h: H) -> Par<I, X::FlatMap<V, H>, R>
@@ -72,6 +88,20 @@ where
         H: Fn(X::O) -> V + Copy + Send,
     {
         let xap = self.xap.flat_map(h);
-        Par::new(self.iter, xap, self.exe)
+        self.with_xap(xap)
+    }
+
+    // compute - early exit
+
+    pub fn first(self) -> Option<X::O>
+    where
+        X::O: Send,
+    {
+        let (iter, x, mut exe, params) = self.destruct();
+        match params.iteration_order {
+            IterationOrder::Ordered => exe.next(params, iter, x),
+            IterationOrder::Arbitrary => todo!(),
+        }
+        .map(|x| x.val)
     }
 }
