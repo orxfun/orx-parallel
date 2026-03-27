4c9f0e735ab8f8f52d7c881a9376eacc801f8883
orxfun <orx.ugur.arikan@gmail.com>
2026-03-27 14:16:10 +0100

    wip

--- src/runner/thread_computations/next.rs
+++ src/runner/thread_computations/next.rs
@@ -50,3 +50,53 @@ where
 
     None
 }
+
+pub fn option_next<Q, I, X, O>(th_idx: usize, state: &Q::State, iter: &I, x: X) -> Option<ValIdx<O>>
+where
+    Q: ParRunner,
+    I: ConcurrentIter,
+    X: Xap<I = I::Item, O = Option<O>>,
+{
+    let mut chunk_puller = iter.chunk_puller(0);
+    let mut item_puller = iter.item_puller_with_idx();
+
+    loop {
+        let chunk_size = Q::next_chunk_size(state, iter.try_get_len());
+        let chunk_state = Q::begin_chunk(th_idx, chunk_size);
+
+        match chunk_size {
+            0 | 1 => match item_puller.next() {
+                Some((idx, i)) => {
+                    if let Some(val) = x.xap(i).into_iter().next() {
+                        broadcast_stop::<_, Q>(iter, state, chunk_state);
+                        // return Some(ValIdx { val, idx });
+                        todo!()
+                    }
+                }
+                None if iter.is_completed_when_none_returned() => break,
+                None => {}
+            },
+            c => {
+                if c > chunk_puller.chunk_size() {
+                    chunk_puller = iter.chunk_puller(c);
+                }
+
+                match chunk_puller.pull_with_idx() {
+                    Some((idx, chunk)) => {
+                        if let Some(val) = chunk.flat_map(|i| x.xap(i).into_iter()).next() {
+                            broadcast_stop::<_, Q>(iter, state, chunk_state);
+                            // return Some(ValIdx { val, idx });
+                            todo!()
+                        }
+                    }
+                    None if iter.is_completed_when_none_returned() => break,
+                    None => {}
+                }
+            }
+        }
+
+        Q::complete_chunk(state, chunk_state);
+    }
+
+    None
+}
