e2d8897f06753e9c15e4cda5dca48feb4674eac5
orxfun <orx.ugur.arikan@gmail.com>
2026-03-27 15:07:50 +0100

    wip

--- src/par_iter/par_opt.rs
+++ src/par_iter/par_opt.rs
@@ -12,3 +12,21 @@ where
 {
     par: Par<I, X, R>,
 }
+
+impl<I, X, O, R> ParOpt<I, X, O, R>
+where
+    I: ConcurrentIter,
+    X: Xap<I = I::Item, O = Option<O>>,
+    R: ParRunner,
+{
+    // transformations
+
+    pub fn map<Q, H>(self, h: H) -> Par<I, X::Map<Q, H>, R>
+    where
+        H: Fn(X::O) -> Q + Copy + Send,
+    {
+        todo!()
+        // let xap = self.xap.map(h);
+        // self.with_xap(xap)
+    }
+}
