40c3e227e0b33f62a2c99cf3769041800ff8edd6
orxfun <orx.ugur.arikan@gmail.com>
2026-03-27 15:00:38 +0100

    define fallible option_next_any on thread computation

--- src/runner/thread_computations/next_any.rs
+++ src/runner/thread_computations/next_any.rs
@@ -1,3 +1,4 @@
+use crate::runner::results::Opt;
 use crate::runner::{ParRunner, thread_computations::sync::broadcast_stop};
 use crate::xap::Xap;
 use orx_concurrent_iter::{ChunkPuller, ConcurrentIter};
@@ -49,7 +50,12 @@ where
     None
 }
 
-pub fn option_next_any<Q, I, X, O>(th_idx: usize, state: &Q::State, iter: &I, x: X) -> Option<O>
+pub fn option_next_any<Q, I, X, O>(
+    th_idx: usize,
+    state: &Q::State,
+    iter: &I,
+    x: X,
+) -> Option<Opt<O>>
 where
     Q: ParRunner,
     I: ConcurrentIter,
@@ -65,10 +71,9 @@ where
         match chunk_size {
             0 | 1 => match item_puller.next() {
                 Some(i) => {
-                    if let Some(val) = x.xap(i).into_iter().next() {
+                    if let Some(maybe) = x.xap(i).into_iter().next() {
                         broadcast_stop::<_, Q>(iter, state, chunk_state);
-                        // return Some(val);
-                        todo!()
+                        return Some(Opt::from_maybe(maybe));
                     }
                 }
                 None if iter.is_completed_when_none_returned() => break,
@@ -81,10 +86,9 @@ where
 
                 match chunk_puller.pull() {
                     Some(chunk) => {
-                        if let Some(val) = chunk.flat_map(|i| x.xap(i).into_iter()).next() {
+                        if let Some(maybe) = chunk.flat_map(|i| x.xap(i).into_iter()).next() {
                             broadcast_stop::<_, Q>(iter, state, chunk_state);
-                            // return Some(val);
-                            todo!()
+                            return Some(Opt::from_maybe(maybe));
                         }
                     }
                     None if iter.is_completed_when_none_returned() => break,
