671e2c75a64a28d9ccb84d71071aafe5a534ead1
orxfun <orx.ugur.arikan@gmail.com>
2026-03-28 12:59:54 +0100

    map filter reduce benchmarks

--- benches/reduce/mfm.rs
+++ benches/reduce/mfm.rs
@@ -3,25 +3,25 @@
 * light & heavy show the intensity of computation
 * eN means an input of size 2^N is used
 
-reduce_m/seq/e15_light      time:   [11.074 µs 11.161 µs 11.247 µs]
-reduce_m/rayon1/e15_light   time:   [7.0328 ms 7.1301 ms 7.2289 ms]
-reduce_m/rayon2/e15_light   time:   [2.9261 ms 3.5955 ms 4.2788 ms]
-reduce_m/orx/e15_light      time:   [1.1085 ms 1.1392 ms 1.1707 ms]
+reduce_mf/seq/e15_light     time:   [24.474 µs 24.698 µs 24.935 µs]
+reduce_mf/rayon1/e15_light  time:   [8.1422 ms 8.2820 ms 8.4060 ms]
+reduce_mf/rayon2/e15_light  time:   [7.3712 ms 7.8117 ms 8.2125 ms]
+reduce_mf/orx/e15_light     time:   [1.1941 ms 1.2191 ms 1.2482 ms]
 
-reduce_m/seq/e20_light      time:   [495.38 µs 502.32 µs 509.44 µs]
-reduce_m/rayon1/e20_light   time:   [13.399 ms 13.979 ms 14.505 ms]
-reduce_m/rayon2/e20_light   time:   [13.502 ms 14.247 ms 14.951 ms]
-reduce_m/orx/e20_light      time:   [1.8877 ms 1.9350 ms 1.9825 ms]
+reduce_mf/seq/e20_light     time:   [964.37 µs 976.02 µs 989.32 µs]
+reduce_mf/rayon1/e20_light  time:   [15.169 ms 15.627 ms 16.098 ms]
+reduce_mf/rayon2/e20_light  time:   [15.516 ms 15.902 ms 16.291 ms]
+reduce_mf/orx/e20_light     time:   [1.8821 ms 1.9049 ms 1.9285 ms]
 
-reduce_m/seq/e15_heavy      time:   [1.2836 ms 1.2937 ms 1.3049 ms]
-reduce_m/rayon1/e15_heavy   time:   [7.8716 ms 8.0846 ms 8.2734 ms]
-reduce_m/rayon2/e15_heavy   time:   [8.9437 ms 9.1293 ms 9.3129 ms]
-reduce_m/orx/e15_heavy      time:   [1.8329 ms 1.8510 ms 1.8711 ms]
+reduce_mf/seq/e15_heavy     time:   [1.2139 ms 1.2220 ms 1.2312 ms]
+reduce_mf/rayon1/e15_heavy  time:   [6.4007 ms 7.0719 ms 7.7221 ms]
+reduce_mf/rayon2/e15_heavy  time:   [9.6266 ms 9.8521 ms 10.058 ms]
+reduce_mf/orx/e15_heavy     time:   [2.0351 ms 2.0709 ms 2.1092 ms]
 
-reduce_m/seq/e20_heavy      time:   [40.288 ms 40.537 ms 40.796 ms]
-reduce_m/rayon1/e20_heavy   time:   [7.5214 ms 8.0231 ms 8.5305 ms]
-reduce_m/rayon2/e20_heavy   time:   [7.5114 ms 7.9549 ms 8.4085 ms]
-reduce_m/orx/e20_heavy      time:   [5.9074 ms 5.9774 ms 6.0528 ms]
+reduce_mf/seq/e20_heavy     time:   [40.212 ms 40.674 ms 41.176 ms]
+reduce_mf/rayon1/e20_heavy  time:   [5.6430 ms 6.0319 ms 6.4429 ms]
+reduce_mf/rayon2/e20_heavy  time:   [7.0000 ms 7.5254 ms 8.0761 ms]
+reduce_mf/orx/e20_heavy     time:   [6.0235 ms 6.0753 ms 6.1275 ms]
 
 */
 
@@ -71,6 +71,20 @@ fn m(x: &u64) -> u64 {
 
 fn f(a: &u64) -> bool {
     !(a + 7).is_multiple_of(11)
+}
+
+fn h_m2(x: &u64) -> u64 {
+    match *x {
+        999 => 999,
+        n => fibonacci(n % FIB_UPPER_BOUND) + 1000,
+    }
+}
+
+fn l_m2(x: &u64) -> u64 {
+    match *x {
+        999 => 999,
+        n => 7 * n + 1000,
+    }
 }
 
 fn seq(input: &[u64], h: bool) -> Option<u64> {
@@ -126,7 +140,7 @@ fn run(c: &mut Criterion) {
         },
     ];
 
-    let mut group = c.benchmark_group("reduce_mf");
+    let mut group = c.benchmark_group("reduce_mfm");
 
     for t in treatments {
         let name = format!(
