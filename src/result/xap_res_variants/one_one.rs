7b937188c016e9671adfd9d28fdf1db99522a551
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 11:22:11 +0200

    revise XapRes Values definition

--- src/result/xap_res_variants/one_one.rs
+++ src/result/xap_res_variants/one_one.rs
@@ -1,6 +1,35 @@
-use crate::infallible::{One, Xap};
+use crate::{
+    infallible::{One, Xap},
+    result::xap_res::XapRes,
+};
 
 pub struct XapResOneOne<M, E, X1: Xap<O = Result<M, E>, Count = One>, X2: Xap<I = M, Count = One>> {
     x1: X1,
     x2: X2,
+}
+
+impl<M, E, X1: Xap<O = Result<M, E>, Count = One>, X2: Xap<I = M, Count = One>>
+    XapResOneOne<M, E, X1, X2>
+{
+    pub fn new(x1: X1, x2: X2) -> Self {
+        Self { x1, x2 }
+    }
+}
+
+impl<M, E, X1: Xap<O = Result<M, E>, Count = One>, X2: Xap<I = M, Count = One>> XapRes
+    for XapResOneOne<M, E, X1, X2>
+{
+    type M = M;
+
+    type E = E;
+
+    type X1 = X1;
+
+    type X2 = X2;
+
+    type Values = core::iter::Empty<<X2 as Xap>::O>;
+
+    fn xap_res(&self, i: <Self::X1 as Xap>::I) -> Result<Self::Values, Self::E> {
+        todo!()
+    }
 }
