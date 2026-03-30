17467dc9b2e54d217001c7cf5896917798b9622e
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 10:03:41 +0200

    define pair for result map queue

--- src/result/fun/map/pair.rs
+++ src/result/fun/map/pair.rs
@@ -19,10 +19,25 @@ impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> MapRes for Mp<F, B> {
 
     type E = F::E;
 
+    #[inline(always)]
     fn map(&self, i: Self::I) -> Result<Self::O, Self::E> {
         match self.f.map(i) {
             Ok(x) => self.b.map(x),
             Err(e) => Err(e),
         }
+    }
+}
+
+impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> MapResQueue for Mp<F, B> {
+    type Then<Q, H>
+        = Mp<F, B::Then<Q, H>>
+    where
+        H: MapRes<E = Self::E, I = Self::O, O = Q>;
+
+    fn then<Q, H>(self, h: H) -> Self::Then<Q, H>
+    where
+        H: MapRes<E = Self::E, I = Self::O, O = Q>,
+    {
+        Mp::new(self.f, self.b.then(h))
     }
 }
--- src/result/fun/map/queue.rs
+++ src/result/fun/map/queue.rs
@@ -1,11 +1,11 @@
 use crate::result::fun::map::fn_trait::MapRes;
 
 pub trait MapResQueue: MapRes {
-    type Then<Q, H>: MapResQueue<I = Self::I, O = Q>
+    type Then<Q, H>: MapResQueue<E = Self::E, I = Self::I, O = Q>
     where
-        H: MapRes<I = Self::O, O = Q>;
+        H: MapRes<E = Self::E, I = Self::O, O = Q>;
 
     fn then<Q, H>(self, h: H) -> Self::Then<Q, H>
     where
-        H: MapRes<I = Self::O, O = Q>;
+        H: MapRes<E = Self::E, I = Self::O, O = Q>;
 }
