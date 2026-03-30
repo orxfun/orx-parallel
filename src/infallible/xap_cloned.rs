fda18bf88a26a60344fea5a6aca7a7eeeab1f679
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 16:38:21 +0200

    generic xap-cloned implementation

--- src/infallible/xap_cloned.rs
+++ src/infallible/xap_cloned.rs
@@ -1,7 +1,15 @@
-use crate::infallible::xap::Xap;
+use crate::infallible::{fun::map::FnCloned, xap::Xap};
 
 pub trait XapCloned<'a, O: 'a + Clone>: Xap<O = &'a O> {
     type Cloned: Xap<I = Self::I, O = O>;
 
     fn cloned(self) -> Self::Cloned;
+}
+
+impl<'a, O: 'a + Clone, X: Xap<O = &'a O>> XapCloned<'a, O> for X {
+    type Cloned = X::Mapped<FnCloned<'a, O>>;
+
+    fn cloned(self) -> Self::Cloned {
+        self.mapped(FnCloned::new())
+    }
 }
