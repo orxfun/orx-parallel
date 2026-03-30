10ad1266f2c8ac4a6833baf949ccc733d38bd0dc
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 10:26:43 +0200

    define MapResQueue

--- src/result/fun/map/mod.rs
+++ src/result/fun/map/mod.rs
@@ -1,1 +1,3 @@
+mod fn_impl;
 mod fn_trait;
+mod queue;
---
+++ src/result/fun/map/queue.rs
@@ -0,0 +1,11 @@
+use crate::{infallible::fun::map::Map, result::fun::map::fn_trait::MapRes};
+
+pub trait MapResQueue: MapRes {
+    type Then<Q, H>: MapResQueue<E = Self::E, I = Self::I, O = Q>
+    where
+        H: Map<I = Self::O, O = Q>;
+
+    fn then<Q, H>(self, h: H) -> Self::Then<Q, H>
+    where
+        H: Map<I = Self::O, O = Q>;
+}
