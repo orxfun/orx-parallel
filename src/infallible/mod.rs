ff13dba984b930228531abbd6c7b3bb627b8dfe3
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 16:42:08 +0200

    implement thread execution for xap

--- src/infallible/mod.rs
+++ src/infallible/mod.rs
@@ -1,5 +1,7 @@
 mod fun;
 mod size;
+mod thread_execution;
+mod val_idx;
 mod xap;
 mod xap_cloned;
 mod xap_copied;
---
+++ src/infallible/thread_execution/mod.rs
@@ -0,0 +1,8 @@
+mod next;
+mod next_any;
+mod reduce;
+mod sync;
+
+pub use next::next;
+pub use next_any::next_any;
+pub use reduce::reduce;
