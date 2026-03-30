353c43c4a15658bc962292f251b39523e8294ced
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 15:26:56 +0200

    implement id

---
+++ src/infallible/xap_variants/id.rs
@@ -0,0 +1,34 @@
+use crate::infallible::{size::One, xap::Xap};
+use core::marker::PhantomData;
+
+pub struct Id<I>(PhantomData<I>);
+
+impl<I> Clone for Id<I> {
+    fn clone(&self) -> Self {
+        Self::new()
+    }
+}
+
+impl<I> Copy for Id<I> {}
+
+unsafe impl<I> Send for Id<I> {}
+
+impl<I> Id<I> {
+    pub const fn new() -> Self {
+        Self(PhantomData)
+    }
+}
+
+impl<I> Xap for Id<I> {
+    type I = I;
+
+    type O = I;
+
+    type Size = One;
+
+    type Values = [I; 1];
+
+    fn xap(&self, i: Self::I) -> Self::Values {
+        [i]
+    }
+}
