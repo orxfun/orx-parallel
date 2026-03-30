24549ffcceb4f234742e7f639c67ea5b618a3367
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 23:28:31 +0200

    clean up

--- src/infallible/mod.rs
+++ src/infallible/mod.rs
@@ -5,10 +5,6 @@ mod par_runner;
 pub mod size;
 mod thread_execution;
 mod xap;
-mod xap_cloned;
-mod xap_copied;
 pub mod xap_variants;
 
 pub use xap::{Xap, XapBin, XapOne};
-pub use xap_cloned::XapCloned;
-pub use xap_copied::XapCopied;
