09868d8cbeb7a79956deea24df8eba72028f7ce2
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 10:07:09 +0200

    implement single queue for result map

--- src/result/fun/map/pair.rs
+++ src/result/fun/map/pair.rs
@@ -1,18 +1,18 @@
 use crate::result::fun::map::{fn_trait::MapRes, queue::MapResQueue};
 
 #[derive(Clone, Copy)]
-pub struct Mp<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> {
+pub struct ResMp<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> {
     f: F,
     b: B,
 }
 
-impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> Mp<F, B> {
+impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> ResMp<F, B> {
     pub fn new(f: F, b: B) -> Self {
         Self { f, b }
     }
 }
 
-impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> MapRes for Mp<F, B> {
+impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> MapRes for ResMp<F, B> {
     type I = F::I;
 
     type O = B::O;
@@ -28,9 +28,9 @@ impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> MapRes for Mp<F, B> {
     }
 }
 
-impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> MapResQueue for Mp<F, B> {
+impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> MapResQueue for ResMp<F, B> {
     type Then<Q, H>
-        = Mp<F, B::Then<Q, H>>
+        = ResMp<F, B::Then<Q, H>>
     where
         H: MapRes<E = Self::E, I = Self::O, O = Q>;
 
@@ -38,6 +38,6 @@ impl<F: MapRes, B: MapResQueue<I = F::O, E = F::E>> MapResQueue for Mp<F, B> {
     where
         H: MapRes<E = Self::E, I = Self::O, O = Q>,
     {
-        Mp::new(self.f, self.b.then(h))
+        ResMp::new(self.f, self.b.then(h))
     }
 }
