9f432b5bd36c4cd3b10df5ff9197173064bc80a9
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 23:11:44 +0200

    implement Val::reduce_res

--- src/result/par_runner.rs
+++ src/result/par_runner.rs
@@ -56,30 +56,32 @@ pub trait ParRunnerResult: ParRunner {
         Val::first_res(results_bag.into_inner().into_inner())
     }
 
-    //     fn reduce<I, X, F>(&mut self, params: Params, iter: I, x: X, f: F) -> Option<X::O>
-    //     where
-    //         I: ConcurrentIter,
-    //         X: XapRes<I = I::Item>,
-    //         F: Fn(X::O, X::O) -> X::O + Send + Copy,
-    //         X::O: Send,  X::E: Send,
-    //     {
-    //         let mut spawned = 0;
-    //         let (max_nt, state) = self.nt_state(params, iter.try_get_len());
-    //         let results_bag = ConcurrentBag::with_fixed_capacity(max_nt);
+    fn reduce<I, X, F>(&mut self, params: Params, iter: I, x: X, f: F) -> Result<Option<X::O>, X::E>
+    where
+        I: ConcurrentIter,
+        X: XapRes<I = I::Item>,
+        F: Fn(X::O, X::O) -> X::O + Send + Copy,
+        X::O: Send,
+        X::E: Send,
+    {
+        let mut spawned = 0;
+        let (max_nt, state) = self.nt_state(params, iter.try_get_len());
+        let results_bag = ConcurrentBag::with_fixed_capacity(max_nt);
 
-    //         let (iter, state, results, x) = (&iter, &state, &results_bag, x);
-    //         self.pool_mut().scoped_computation(move |s| {
-    //             while let Some(th_idx) = Self::do_spawn_new(spawned, state) {
-    //                 spawned += 1;
-    //                 <Self::Pool as ParThreadPool>::run_in_scope(&s, move || {
-    //                     let value = th::reduce::<Self, _, _, _>(th_idx, state, iter, x, f);
-    //                     results.push(value);
-    //                 });
-    //             }
-    //         });
+        let (iter, state, results, x) = (&iter, &state, &results_bag, x);
+        self.pool_mut().scoped_computation(move |s| {
+            while let Some(th_idx) = Self::do_spawn_new(spawned, state) {
+                spawned += 1;
+                <Self::Pool as ParThreadPool>::run_in_scope(&s, move || {
+                    let value = th::reduce::<Self, _, _, _>(th_idx, state, iter, x, f);
+                    results.push(value);
+                });
+            }
+        });
 
-    //         results_bag.into_inner().into_iter().flatten().reduce(f)
-    //     }
+        // results_bag.into_inner().into_iter().flatten().reduce(f)
+        todo!()
+    }
 }
 
 impl<R: ParRunner> ParRunnerResult for R {}
