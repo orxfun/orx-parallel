fe13253ecd334cfa0aa5a80297b32f85b995da70
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 10:52:20 +0200

    define XapRes and document

---
+++ src/result/xap_res.rs
@@ -0,0 +1,14 @@
+use crate::infallible::Xap;
+
+/// Fallible computation such that:
+///
+/// * `X1` represents the first part of the computation which exits
+///   infallible and enters fallible computation; i.e., `X1: I -> Result<O, E>`.
+/// * `X2` represents the second part of the computation which transforms
+///   the successful variant of the output `O`, while keeping the error variant
+///   as `E`; i.e., `X2: O -> Q`. This allows to work with the success path,
+///   while the error results lead to a shortcut in the same manner.
+pub struct XapRes<O, E, X1: Xap<O = Result<O, E>>, X2: Xap<I = O>> {
+    x1: X1,
+    x2: X2,
+}
