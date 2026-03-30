5f7e8fe2079cfb12c751ecd49372e077644c0e25
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 22:44:49 +0200

    instantiate ParRunnerResult

--- src/result/mod.rs
+++ src/result/mod.rs
@@ -1,3 +1,4 @@
+mod par_runner;
 mod thread_execution;
 mod xap_res;
 mod xap_res_cloned;
@@ -5,3 +6,5 @@ mod xap_res_copied;
 pub mod xap_res_variants;
 
 pub use xap_res::XapRes;
+pub use xap_res_cloned::XapResCloned;
+pub use xap_res_copied::XapResCopied;
