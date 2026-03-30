c20ead5c5d5efce0897caf815ac64ad378b6f86b
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 22:29:47 +0200

    implement fallible reduction

--- src/result/thread_execution/reduce.rs
+++ src/result/thread_execution/reduce.rs
@@ -69,56 +69,68 @@ where
             }
         }
         Q::complete_chunk(state, chunk_state);
+
+        if acc.is_some() {
+            break;
+        }
     }
 
-    // // fold over the aggregate
-    // let result = match acc {
-    //     None => None,
-    //     Some(mut acc) => {
-    //         loop {
-    //             let chunk_size = Q::next_chunk_size(state, iter.try_get_len());
-    //             let chunk_state = Q::begin_chunk(th_idx, chunk_size);
+    // fold over the aggregate
+    let result = match acc {
+        None => None,
+        Some(mut acc) => {
+            loop {
+                let chunk_size = Q::next_chunk_size(state, iter.try_get_len());
+                let chunk_state = Q::begin_chunk(th_idx, chunk_size);
 
-    //             match chunk_size {
-    //                 0 | 1 => {
-    //                     match item_puller.next() {
-    //                         Some(i) => {
-    //                             let result = x.xap(i).into_iter().reduce(&f);
-    //                             if let Some(y) = result {
-    //                                 acc = f(acc, y);
-    //                             }
-    //                         }
-    //                         // TODO: a good back-off strategy might be used, needs benchmark with ConcurrentQueue
-    //                         None if iter.is_completed_when_none_returned() => break,
-    //                         None => {}
-    //                     }
-    //                 }
-    //                 c => {
-    //                     if c > chunk_puller.chunk_size() {
-    //                         chunk_puller = iter.chunk_puller(c);
-    //                     }
+                match chunk_size {
+                    0 | 1 => {
+                        match item_puller.next() {
+                            Some(i) => {
+                                for a in x.xap_res(i).into_iter() {
+                                    acc = match a {
+                                        Ok(a) => f(acc, a),
+                                        Err(e) => {
+                                            Q::broadcast_stop(iter, state, chunk_state);
+                                            return Err(e);
+                                        }
+                                    };
+                                }
+                            }
+                            // TODO: a good back-off strategy might be used, needs benchmark with ConcurrentQueue
+                            None if iter.is_completed_when_none_returned() => break,
+                            None => {}
+                        }
+                    }
+                    c => {
+                        if c > chunk_puller.chunk_size() {
+                            chunk_puller = iter.chunk_puller(c);
+                        }
 
-    //                     match chunk_puller.pull() {
-    //                         Some(chunk) => {
-    //                             let result = chunk.flat_map(|i| x.xap(i).into_iter()).reduce(&f);
-    //                             if let Some(y) = result {
-    //                                 acc = f(acc, y);
-    //                             }
-    //                         }
-    //                         None if iter.is_completed_when_none_returned() => break,
-    //                         None => {}
-    //                     }
-    //                 }
-    //             }
+                        match chunk_puller.pull() {
+                            Some(chunk) => {
+                                for a in chunk.flat_map(|i| x.xap_res(i)) {
+                                    acc = match a {
+                                        Ok(a) => f(acc, a),
+                                        Err(e) => {
+                                            Q::broadcast_stop(iter, state, chunk_state);
+                                            return Err(e);
+                                        }
+                                    };
+                                }
+                            }
+                            None if iter.is_completed_when_none_returned() => break,
+                            None => {}
+                        }
+                    }
+                }
 
-    //             Q::complete_chunk(state, chunk_state);
-    //         }
+                Q::complete_chunk(state, chunk_state);
+            }
 
-    //         Some(acc)
-    //     }
-    // };
+            Some(acc)
+        }
+    };
 
-    // result
-
-    todo!()
+    Ok(result)
 }
