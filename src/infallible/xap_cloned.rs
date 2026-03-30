6c3a9d9ec8e7fef44e16ffe53b1c1a8ec77a0b8a
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 16:39:32 +0200

    simplify copied and cloned traits

--- src/infallible/xap_cloned.rs
+++ src/infallible/xap_cloned.rs
@@ -1,15 +1,11 @@
 use crate::infallible::{fun::map::FnCloned, xap::Xap};
 
 pub trait XapCloned<'a, O: 'a + Clone>: Xap<O = &'a O> {
-    type Cloned: Xap<I = Self::I, O = O>;
-
-    fn cloned(self) -> Self::Cloned;
+    fn cloned(self) -> Self::Mapped<FnCloned<'a, O>>;
 }
 
 impl<'a, O: 'a + Clone, X: Xap<O = &'a O>> XapCloned<'a, O> for X {
-    type Cloned = X::Mapped<FnCloned<'a, O>>;
-
-    fn cloned(self) -> Self::Cloned {
+    fn cloned(self) -> Self::Mapped<FnCloned<'a, O>> {
         self.mapped(FnCloned::new())
     }
 }
--- src/infallible/xap_copied.rs
+++ src/infallible/xap_copied.rs
@@ -1,15 +1,11 @@
 use crate::infallible::{fun::map::FnCopied, xap::Xap};
 
 pub trait XapCopied<'a, O: 'a + Copy>: Xap<O = &'a O> {
-    type Copied: Xap<I = Self::I, O = O>;
-
-    fn copied(self) -> Self::Copied;
+    fn copied(self) -> Self::Mapped<FnCopied<'a, O>>;
 }
 
 impl<'a, O: 'a + Copy, X: Xap<O = &'a O>> XapCopied<'a, O> for X {
-    type Copied = X::Mapped<FnCopied<'a, O>>;
-
-    fn copied(self) -> Self::Copied {
+    fn copied(self) -> Self::Mapped<FnCopied<'a, O>> {
         self.mapped(FnCopied::new())
     }
 }
