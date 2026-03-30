a2bdf8fc1071dc142851352aee66faa273c34123
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 15:28:18 +0200

    implement OneI

---
+++ src/infallible/xap_variants/one_i.rs
@@ -0,0 +1,36 @@
+use crate::infallible::xap::{Xap, XapOne};
+use crate::infallible::{fun::filter_map::FilterMap, size::One};
+
+pub struct OneI<X: Xap<Size = One>, G: FilterMap<I = X::O>> {
+    x: X,
+    g: G,
+}
+
+impl<X: Xap<Size = One>, G: FilterMap<I = X::O>> Clone for OneI<X, G> {
+    fn clone(&self) -> Self {
+        Self::new(self.x, self.g)
+    }
+}
+
+impl<X: Xap<Size = One>, G: FilterMap<I = X::O>> Copy for OneI<X, G> {}
+
+impl<X: Xap<Size = One>, G: FilterMap<I = X::O>> OneI<X, G> {
+    pub fn new(x: X, g: G) -> Self {
+        Self { x, g }
+    }
+}
+
+impl<X: Xap<Size = One>, G: FilterMap<I = X::O>> Xap for OneI<X, G> {
+    type I = X::I;
+
+    type O = G::O;
+
+    type Size = One;
+
+    type Values = Option<G::O>;
+
+    #[inline(always)]
+    fn xap(&self, i: Self::I) -> Self::Values {
+        self.g.filter_map(self.x.one_value(i))
+    }
+}
