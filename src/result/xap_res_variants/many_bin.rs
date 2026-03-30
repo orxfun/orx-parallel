4be3027446239755022f0e0a39f6dc4073f878e9
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 17:17:53 +0200

    implement map xap-res transformations for many

--- src/result/xap_res_variants/many_bin.rs
+++ src/result/xap_res_variants/many_bin.rs
@@ -1,10 +1,11 @@
-use crate::infallible::{Many, Xap, ZeroOne};
+use crate::infallible::size::{Bin, Many};
+use crate::infallible::xap::Xap;
 use crate::result::xap_res::XapRes;
 
 pub struct XapResManyBin<M, E, X1, X2>
 where
-    X1: Xap<O = Result<M, E>, Count = Many>,
-    X2: Xap<I = M, Count = ZeroOne>,
+    X1: Xap<O = Result<M, E>, Size = Many>,
+    X2: Xap<I = M, Size = Bin>,
 {
     x1: X1,
     x2: X2,
@@ -12,8 +13,8 @@ where
 
 impl<M, E, X1, X2> XapResManyBin<M, E, X1, X2>
 where
-    X1: Xap<O = Result<M, E>, Count = Many>,
-    X2: Xap<I = M, Count = ZeroOne>,
+    X1: Xap<O = Result<M, E>, Size = Many>,
+    X2: Xap<I = M, Size = Bin>,
 {
     pub fn new(x1: X1, x2: X2) -> Self {
         Self { x1, x2 }
@@ -22,8 +23,8 @@ where
 
 impl<M, E, X1, X2> XapRes for XapResManyBin<M, E, X1, X2>
 where
-    X1: Xap<O = Result<M, E>, Count = Many>,
-    X2: Xap<I = M, Count = ZeroOne>,
+    X1: Xap<O = Result<M, E>, Size = Many>,
+    X2: Xap<I = M, Size = Bin>,
 {
     type M = M;
 
@@ -39,6 +40,20 @@ where
         let iter = self.x1.xap(i).into_iter();
         IterResManyBin { iter, x2: self.x2 }
     }
+
+    // transformations
+
+    type Map<Q, H>
+        = XapResManyBin<M, E, X1, X2::Map<Q, H>>
+    where
+        H: Fn(<Self::X2 as Xap>::O) -> Q + Copy + Send;
+
+    fn map<Q, H>(self, h: H) -> Self::Map<Q, H>
+    where
+        H: Fn(<Self::X2 as Xap>::O) -> Q + Copy + Send,
+    {
+        XapResManyBin::new(self.x1, self.x2.map(h))
+    }
 }
 
 // iter
@@ -46,7 +61,7 @@ where
 pub struct IterResManyBin<M, E, I, X2>
 where
     I: Iterator<Item = Result<M, E>>,
-    X2: Xap<I = M, Count = ZeroOne>,
+    X2: Xap<I = M, Size = Bin>,
 {
     iter: I,
     x2: X2,
@@ -55,7 +70,7 @@ where
 impl<M, E, I, X2> Iterator for IterResManyBin<M, E, I, X2>
 where
     I: Iterator<Item = Result<M, E>>,
-    X2: Xap<I = M, Count = ZeroOne>,
+    X2: Xap<I = M, Size = Bin>,
 {
     type Item = Result<X2::O, E>;
 
