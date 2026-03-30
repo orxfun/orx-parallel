c6f10c5b5056c95c273d04a37d2aae15e8d80ac0
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 23:27:43 +0200

    implement Par::cloned

--- src/infallible/par_iter_transform.rs
+++ src/infallible/par_iter_transform.rs
@@ -1,5 +1,5 @@
 use crate::infallible::XapCopied;
-use crate::infallible::fun::FnCopied;
+use crate::infallible::fun::{FnCloned, FnCopied};
 use crate::infallible::par_iter::Par;
 use crate::infallible::par_runner::ParRunnerInfallible;
 use crate::infallible::xap::Xap;
@@ -17,5 +17,17 @@ where
     pub fn copied(self) -> Par<I, X::Mapped<FnCopied<'a, O>>, R> {
         let (iter, xap, exe, params) = self.destruct();
         Par::new(iter, xap.mapped(FnCopied::new()), exe, params)
+    }
+}
+
+impl<'a, O: Clone + 'a, I, X, R> Par<I, X, R>
+where
+    I: ConcurrentIter,
+    X: Xap<I = I::Item, O = &'a O>,
+    R: ParRunner,
+{
+    pub fn cloned(self) -> Par<I, X::Mapped<FnCloned<'a, O>>, R> {
+        let (iter, xap, exe, params) = self.destruct();
+        Par::new(iter, xap.mapped(FnCloned::new()), exe, params)
     }
 }
