3c2d27c5aaabe3ee6d43c926a3baa60fb774ea23
orxfun <orx.ugur.arikan@gmail.com>
2026-03-30 16:37:12 +0200

    generic XapCopied implementation

--- src/infallible/xap_copied.rs
+++ src/infallible/xap_copied.rs
@@ -6,26 +6,10 @@ pub trait XapCopied<'a, O: 'a + Copy>: Xap<O = &'a O> {
     fn copied(self) -> Self::Copied;
 }
 
-// impl<'a, O: 'a + Copy, X: Xap<O = &'a O>> XapCopied<'a, O> for X {
-//     type Copied = X::Map<O, FnCopied<'a, O>>;
+impl<'a, O: 'a + Copy, X: Xap<O = &'a O>> XapCopied<'a, O> for X {
+    type Copied = X::Mapped<FnCopied<'a, O>>;
 
-//     fn copied(self) -> Self::Copied {
-//         todo!()
-//     }
-// }
-
-// impl<'a, I: 'a + Clone, X: Xap, G: Map<I = X::O, O = &'a I>> XapCloned<'a, I> for M<X, G> {
-//     type Cloned = M<Self, FnCloned<'a, I>>;
-
-//     fn cloned(self) -> Self::Cloned {
-//         M::new(self, FnCloned::new())
-//     }
-// }
-
-// impl<'a, I: 'a + Copy, X: Xap, G: Map<I = X::O, O = &'a I>> XapCopied<'a, I> for M<X, G> {
-//     type Copied = M<Self, FnCopied<'a, I>>;
-
-//     fn copied(self) -> Self::Copied {
-//         M::new(self, FnCopied::new())
-//     }
-// }
+    fn copied(self) -> Self::Copied {
+        self.mapped(FnCopied::new())
+    }
+}
