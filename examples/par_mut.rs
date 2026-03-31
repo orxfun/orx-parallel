dc313206612f5a5caf3abc319377fc03b486712b
orxfun <orx.ugur.arikan@gmail.com>
2026-03-31 17:03:41 +0200

    implement par_mut example

--- examples/par_mut.rs
+++ examples/par_mut.rs
@@ -1,41 +1,45 @@
 use orx_parallel::*;
+use std::collections::HashMap;
 
 const N: usize = 1_000_000;
 
-// fn mut_slice_into_par() {
-//     // We create a mutable `slice` as our source: `&mut [T]`
-//     //
-//     // `&mut [T]` implements `IntoConcurrentIter<Item = &mut T>`
-//     // => `&mut [T]` auto-implements `IntoParIter<Item = &mut T>`
-//     // Therefore, we can call `slice.into_par()` to create a parallel
-//     // iterator yielding mutable references consuming the `slice`.
+/// We create a mutable `slice` as our source: `&mut [T]`.
+///
+/// We can call `slice.into_par()` to create a parallel iterator
+/// yielding mutable references consuming the `slice`.
+///
+/// `into_par` consumes the input source which is the mutable slice;
+/// the `vec` is not consumed. It is mutated in place.
+fn mut_slice_into_par() {
+    let mut vec: Vec<_> = (0..N).collect();
+    let slice = vec.as_mut_slice();
 
-//     let mut vec: Vec<_> = (0..N).collect();
-//     let slice = vec.as_mut_slice();
+    let par = slice.into_par(); // IntoParIter on &mut [T]
+    par.filter(|x| **x != 42).for_each(|x| *x *= 0);
 
-//     let par = slice.into_par(); // IntoParIter on &mut [T]
-//     par.filter(|x| **x != 42).for_each(|x| *x *= 0);
+    let sum = vec.par().copied().reduce(|a, b| a + b).unwrap_or(0);
+    assert_eq!(sum, 42);
 
-//     let sum = vec.par().sum();
-//     assert_eq!(sum, 42);
-// }
+    assert_eq!(vec.iter().sum::<usize>(), 42);
+}
 
-// fn vec_par_mut() {
-//     // Here, we directly use the `vec` as our source: `Vec<T>`
-//     //
-//     // `Vec<T>` implements `ConcurrentCollectionMut<Item = T>`
-//     // => `Vec<T>` auto-implements `ParallelizableCollectionMut<Item = T>`
-//     // Therefore, we can call `vec.par_mut()` to create a parallel
-//     // iterator yielding mutable references, using a mutable reference to `vec`.
+/// Here we directly call `par_mut` on the input `vec` since `Vec`
+/// implements `ParColMut`.
+///
+/// `par_mut` call is non-consuming.
+///
+/// The `vec` is not consumed. It is mutated in place.
+fn vec_par_mut() {
+    let mut vec: Vec<_> = (0..N).collect();
 
-//     let mut vec: Vec<_> = (0..N).collect();
+    let par = vec.par_mut();
+    par.filter(|x| **x != 42).for_each(|x| *x *= 0);
 
-//     let par = vec.par_mut();
-//     par.filter(|x| **x != 42).for_each(|x| *x *= 0);
+    let sum = vec.par().copied().reduce(|a, b| a + b).unwrap_or(0);
+    assert_eq!(sum, 42);
 
-//     let sum = vec.par().sum();
-//     assert_eq!(sum, 42);
-// }
+    assert_eq!(vec.iter().sum::<usize>(), 42);
+}
 
 // fn iter_mut_into_par() {
 //     // Finally, here we convert any mutable iterator into a parallel iterator.
@@ -55,4 +59,8 @@ const N: usize = 1_000_000;
 //     assert_eq!(sum, 42);
 // }
 
-fn main() {}
+fn main() {
+    mut_slice_into_par();
+    vec_par_mut();
+    // iter_mut_into_par();
+}
