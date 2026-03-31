892e1ae0c4bb0653cfe6dadf20dcc6f32f4db9f8
orxfun <orx.ugur.arikan@gmail.com>
2026-03-31 14:27:02 +0200

    redefine XapEnumerable as a marker trait

--- src/infallible/xap_enum.rs
+++ src/infallible/xap_enum.rs
@@ -1,7 +1,3 @@
 use crate::infallible::Xap;
 
-pub trait XapEnumerate: Xap {
-    type Enumerated: Xap<I = Self::I, O = (usize, Self::O), Size = Self::Size>;
-
-    fn enumerate(self) -> Self::Enumerated;
-}
+pub trait XapEnumerable: Xap {}
