495c9975d5f436dcbbe91a1f50f17ee427723df2
orxfun <orx.ugur.arikan@gmail.com>
2026-03-31 11:44:41 +0200

    implement XapOptOneMany

--- src/option/xap_opt_variants/mod.rs
+++ src/option/xap_opt_variants/mod.rs
@@ -5,7 +5,7 @@ mod many_bin;
 mod many_many;
 mod many_one;
 // mod one_bin;
-// mod one_many;
+mod one_many;
 mod one_one;
 
 pub use bin_bin::XapOptBinBin;
@@ -15,5 +15,5 @@ pub use many_bin::XapOptManyBin;
 pub use many_many::XapOptManyMany;
 pub use many_one::XapOptManyOne;
 // pub use one_bin::XapResOneBin;
-// pub use one_many::XapResOneMany;
+pub use one_many::XapOptOneMany;
 // pub use one_one::XapResOneOne;
