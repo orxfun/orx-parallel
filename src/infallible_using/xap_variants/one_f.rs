64c9dd345c5bc21843876e455228dbbeddc1a04c
orxfun <orx.ugur.arikan@gmail.com>
2026-04-03 00:14:32 +0200

    implement one f xap with using

---
+++ src/infallible_using/xap_variants/one_f.rs
@@ -0,0 +1,117 @@
+use super::fake::Fake;
+use crate::infallible::size::{Bin, Many, One, Size};
+use crate::infallible_using::fun::FilterMapU;
+use crate::infallible_using::xap::XapOne;
+use crate::infallible_using::{fun::MapU, xap::Xap};
+
+pub struct OneF<X: Xap<Size = One>, G: FilterMapU<U = X::U, I = X::O>> {
+    x: X,
+    g: G,
+}
+
+impl<X: Xap<Size = One>, G: FilterMapU<U = X::U, I = X::O>> Clone for OneF<X, G> {
+    fn clone(&self) -> Self {
+        Self::new(self.x, self.g)
+    }
+}
+
+impl<X: Xap<Size = One>, G: FilterMapU<U = X::U, I = X::O>> Copy for OneF<X, G> {}
+
+impl<X: Xap<Size = One>, G: FilterMapU<U = X::U, I = X::O>> OneF<X, G> {
+    pub fn new(x: X, g: G) -> Self {
+        Self { x, g }
+    }
+}
+
+impl<X: Xap<Size = One>, G: FilterMapU<U = X::U, I = X::O>> Xap for OneF<X, G> {
+    type I = X::I;
+
+    type O = G::O;
+
+    type Size = Bin;
+
+    type Values = Option<G::O>;
+
+    type U = X::U;
+
+    fn xap(&self, u: &mut Self::U, i: Self::I) -> Self::Values {
+        let a = self.x.one_value(u, i);
+        self.g.filter_map(u, a)
+    }
+
+    // transformations
+
+    type Map<Q, H>
+        = Fake<Self::I, Q, Self::U, Self::Size>
+    where
+        H: Fn(&mut Self::U, Self::O) -> Q + Copy + Send;
+
+    fn map<Q, H>(self, h: H) -> Self::Map<Q, H>
+    where
+        H: Fn(&mut Self::U, Self::O) -> Q + Copy + Send,
+    {
+        todo!()
+    }
+
+    type Inspect<H>
+        = Fake<Self::I, Self::O, Self::U, Self::Size>
+    where
+        H: Fn(&mut Self::U, &Self::O) + Copy + Send;
+
+    fn inspect<H>(self, h: H) -> Self::Inspect<H>
+    where
+        H: Fn(&mut Self::U, &Self::O) + Copy + Send,
+    {
+        todo!()
+    }
+
+    type Filter<H>
+        = Fake<Self::I, Self::O, Self::U, <Self::Size as Size>::ThenBin>
+    where
+        H: Fn(&mut Self::U, &Self::O) -> bool + Copy + Send;
+
+    fn filter<H>(self, h: H) -> Self::Filter<H>
+    where
+        H: Fn(&mut Self::U, &Self::O) -> bool + Copy + Send,
+    {
+        todo!()
+    }
+
+    type FilterMap<Q, H>
+        = Fake<Self::I, Q, Self::U, <Self::Size as Size>::ThenBin>
+    where
+        H: Fn(&mut Self::U, Self::O) -> Option<Q> + Copy + Send;
+
+    fn filter_map<Q, H>(self, h: H) -> Self::FilterMap<Q, H>
+    where
+        H: Fn(&mut Self::U, Self::O) -> Option<Q> + Copy + Send,
+    {
+        todo!()
+    }
+
+    type FlatMap<V, H>
+        = Fake<Self::I, V::Item, Self::U, Many>
+    where
+        V: IntoIterator,
+        H: Fn(&mut Self::U, Self::O) -> V + Copy + Send;
+
+    fn flat_map<V, H>(self, h: H) -> Self::FlatMap<V, H>
+    where
+        V: IntoIterator,
+        H: Fn(&mut Self::U, Self::O) -> V + Copy + Send,
+    {
+        todo!()
+    }
+
+    type Mapped<M>
+        = Fake<Self::I, M::O, Self::U, Self::Size>
+    where
+        M: MapU<U = Self::U, I = Self::O>;
+
+    fn mapped<M>(self, m: M) -> Self::Mapped<M>
+    where
+        M: MapU<U = Self::U, I = Self::O>,
+    {
+        todo!()
+    }
+}
