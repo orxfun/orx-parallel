a1810d9d77127fa8eabad0270fccdbccea770575
orxfun <orx.ugur.arikan@gmail.com>
2026-04-06 22:31:45 +0200

    wip using tests

---
+++ src/infallible_using/tests/bin_x.rs
@@ -0,0 +1,51 @@
+use crate::infallible::tests::utils::inputs;
+use crate::parameters::IterationOrder;
+use crate::*;
+
+const N: usize = 157;
+
+#[test]
+fn bin_x_find() {
+    let inputs = inputs(N);
+    let result = inputs
+        .into_par()
+        .filter(|x| x.len() < 4)
+        .flat_map(|x| {
+            let a = x.parse::<u64>().unwrap();
+            (0..5).map(move |i| a + i)
+        })
+        .first();
+    assert_eq!(result, Some(0));
+}
+
+#[test]
+fn bin_x_find_any() {
+    let inputs = inputs(N);
+    let result = inputs
+        .into_par()
+        .filter(|x| x.len() < 4)
+        .flat_map(|x| {
+            let a = x.parse::<u64>().unwrap();
+            (0..5).map(move |i| a + i)
+        })
+        .iteration_order(IterationOrder::Arbitrary)
+        .first();
+    assert!(result.is_some());
+}
+
+#[test]
+fn bin_x_reduce() {
+    let inputs = inputs(N);
+    let result = inputs
+        .into_par()
+        .filter(|x| x.len() < 4)
+        .flat_map(|x| {
+            let a = x.parse::<u64>().unwrap();
+            (0..5).map(move |i| a + i)
+        })
+        .reduce(|a, b| match a < b {
+            true => b,
+            false => a,
+        });
+    assert_eq!(result, Some(160));
+}
