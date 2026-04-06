8351767382bd143e70e5cf735a36256f54eccd4b
orxfun <orx.ugur.arikan@gmail.com>
2026-04-06 23:17:07 +0200

    rename kind transformations module

--- src/kind_transformations/into_par_res.rs
+++ src/kind_transformations/into_par_res.rs
@@ -1,19 +1,19 @@
 use crate::infallible::Par;
 use crate::infallible::Xap;
-use crate::into_fallible::IntoXapOpt;
-use crate::option::ParOpt;
+use crate::kind_transformations::IntoXapRes;
+use crate::result::ParRes;
 use crate::runner::ParRunner;
 use orx_concurrent_iter::ConcurrentIter;
 
-impl<O, I, X, R> Par<I, X, R>
+impl<O, E, I, X, R> Par<I, X, R>
 where
     I: ConcurrentIter,
-    X: Xap<I = I::Item, O = Option<O>> + IntoXapOpt,
+    X: Xap<I = I::Item, O = Result<O, E>> + IntoXapRes,
     R: ParRunner,
 {
-    pub fn fallible_option(self) -> ParOpt<I, X::XapOpt, R> {
+    pub fn fallible_result(self) -> ParRes<I, X::XapRes, R> {
         let (iter, xap, exe, params) = self.destruct();
         let xap = xap.into_xap_res();
-        ParOpt::new(iter, xap, exe, params)
+        ParRes::new(iter, xap, exe, params)
     }
 }
