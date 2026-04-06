5975de264a368508b990ad1acbb99b5b14ba13c3
orxfun <orx.ugur.arikan@gmail.com>
2026-04-06 23:14:44 +0200

    define using parallel iterator

--- src/infallible_using/mod.rs
+++ src/infallible_using/mod.rs
@@ -2,9 +2,13 @@
 // mod tests;
 
 pub mod fun;
+mod par_iter;
 mod par_runner;
 mod thread_execution;
 pub mod using_var;
 mod xap;
 mod xap_enum;
 pub mod xap_variants;
+
+pub use xap::Xap;
+pub use xap_enum::XapEnumByInput;
