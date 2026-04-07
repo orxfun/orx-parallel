5c3d1e1723b9a036193cdbbd642f29c8444803dc
orxfun <orx.ugur.arikan@gmail.com>
2026-04-07 11:47:05 +0200

    define infallible transformations on one

--- src/infallible/size.rs
+++ src/infallible/size.rs
@@ -16,6 +16,60 @@ pub trait Size {
     where
         X: Xap<Size = Self>,
         H: Fn(X::O) -> Q + Copy + Send;
+
+    type Inspect<X, H>: Xap<I = X::I, O = X::O, Size = Self>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) + Copy + Send;
+
+    fn inspect<X, H>(x: X, h: H) -> Self::Inspect<X, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) + Copy + Send;
+
+    type Filter<X, H>: Xap<I = X::I, O = X::O, Size = Self::ThenBin>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) -> bool + Copy + Send;
+
+    fn filter<X, H>(x: X, h: H) -> Self::Filter<X, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) -> bool + Copy + Send;
+
+    type FilterMap<X, Q, H>: Xap<I = X::I, O = Q, Size = Self::ThenBin>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(X::O) -> Option<Q> + Copy + Send;
+
+    fn filter_map<X, Q, H>(x: X, h: H) -> Self::FilterMap<X, Q, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(X::O) -> Option<Q> + Copy + Send;
+
+    type FlatMap<X, V, H>: Xap<I = X::I, O = V::Item, Size = Many>
+    where
+        X: Xap<Size = Self>,
+        V: IntoIterator,
+        H: Fn(X::O) -> V + Copy + Send;
+
+    fn flat_map<X, V, H>(x: X, h: H) -> Self::FlatMap<X, V, H>
+    where
+        X: Xap<Size = Self>,
+        V: IntoIterator,
+        H: Fn(X::O) -> V + Copy + Send;
+
+    // transformations - helper
+
+    type Mapped<X, M>: Xap<I = X::I, O = M::O, Size = Self>
+    where
+        X: Xap<Size = Self>,
+        M: Map<I = X::O>;
+
+    fn mapped<X, M>(x: X, m: M) -> Self::Mapped<X, M>
+    where
+        X: Xap<Size = Self>,
+        M: Map<I = X::O>;
 }
 
 // ONE
@@ -39,6 +93,78 @@ impl Size for One {
         H: Fn(X::O) -> Q + Copy + Send,
     {
         OneM::new(x, FnMap::new(h))
+    }
+
+    type Inspect<X, H>
+        = OneM<X, FnIns<X::O, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) + Copy + Send;
+
+    fn inspect<X, H>(x: X, h: H) -> Self::Inspect<X, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) + Copy + Send,
+    {
+        OneM::new(x, FnIns::new(h))
+    }
+
+    type Filter<X, H>
+        = OneF<X, FnFil<X::O, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) -> bool + Copy + Send;
+
+    fn filter<X, H>(x: X, h: H) -> Self::Filter<X, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) -> bool + Copy + Send,
+    {
+        OneF::new(x, FnFil::new(h))
+    }
+
+    type FilterMap<X, Q, H>
+        = OneF<X, FnFilMap<X::O, Q, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(X::O) -> Option<Q> + Copy + Send;
+
+    fn filter_map<X, Q, H>(x: X, h: H) -> Self::FilterMap<X, Q, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(X::O) -> Option<Q> + Copy + Send,
+    {
+        OneF::new(x, FnFilMap::new(h))
+    }
+
+    type FlatMap<X, V, H>
+        = OneX<X, FnFlatMap<X::O, V, H>>
+    where
+        X: Xap<Size = Self>,
+        V: IntoIterator,
+        H: Fn(X::O) -> V + Copy + Send;
+
+    fn flat_map<X, V, H>(x: X, h: H) -> Self::FlatMap<X, V, H>
+    where
+        X: Xap<Size = Self>,
+        V: IntoIterator,
+        H: Fn(X::O) -> V + Copy + Send,
+    {
+        OneX::new(x, FnFlatMap::new(h))
+    }
+
+    type Mapped<X, M>
+        = OneM<X, M>
+    where
+        X: Xap<Size = Self>,
+        M: Map<I = X::O>;
+
+    fn mapped<X, M>(x: X, m: M) -> Self::Mapped<X, M>
+    where
+        X: Xap<Size = Self>,
+        M: Map<I = X::O>,
+    {
+        OneM::new(x, m)
     }
 }
 
