afaf4ac2320cbf157aeed80b199f2214bf70bf15
orxfun <orx.ugur.arikan@gmail.com>
2026-04-07 12:00:46 +0200

    define transformation on infallible many

--- src/infallible/size.rs
+++ src/infallible/size.rs
@@ -290,4 +290,78 @@ impl Size for Many {
     {
         ManyM::new(x, FnMap::new(h))
     }
+
+    type Inspect<X, H>
+        = ManyM<X, FnIns<X::O, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) + Copy + Send;
+
+    fn inspect<X, H>(x: X, h: H) -> Self::Inspect<X, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) + Copy + Send,
+    {
+        ManyM::new(x, FnIns::new(h))
+    }
+
+    type Filter<X, H>
+        = ManyF<X, FnFil<X::O, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) -> bool + Copy + Send;
+
+    fn filter<X, H>(x: X, h: H) -> Self::Filter<X, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&X::O) -> bool + Copy + Send,
+    {
+        ManyF::new(x, FnFil::new(h))
+    }
+
+    type FilterMap<X, Q, H>
+        = ManyF<X, FnFilMap<X::O, Q, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(X::O) -> Option<Q> + Copy + Send;
+
+    fn filter_map<X, Q, H>(x: X, h: H) -> Self::FilterMap<X, Q, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(X::O) -> Option<Q> + Copy + Send,
+    {
+        ManyF::new(x, FnFilMap::new(h))
+    }
+
+    type FlatMap<X, V, H>
+        = ManyX<X, FnFlatMap<X::O, V, H>>
+    where
+        X: Xap<Size = Self>,
+        V: IntoIterator,
+        H: Fn(X::O) -> V + Copy + Send;
+
+    fn flat_map<X, V, H>(x: X, h: H) -> Self::FlatMap<X, V, H>
+    where
+        X: Xap<Size = Self>,
+        V: IntoIterator,
+        H: Fn(X::O) -> V + Copy + Send,
+    {
+        ManyX::new(x, FnFlatMap::new(h))
+    }
+
+    // transformations - helper
+
+    type Mapped<X, M>
+        = ManyM<X, M>
+    where
+        X: Xap<Size = Self>,
+        M: Map<I = X::O>;
+
+    fn mapped<X, M>(x: X, m: M) -> Self::Mapped<X, M>
+    where
+        X: Xap<Size = Self>,
+        M: Map<I = X::O>,
+    {
+        ManyM::new(x, m)
+    }
 }
