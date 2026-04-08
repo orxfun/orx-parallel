8e8ecd6f38d9c12841309db140ea4aa444baab0c
orxfun <orx.ugur.arikan@gmail.com>
2026-04-08 17:24:15 +0200

    implement optional parallel iterator tests

--- src/option/mod.rs
+++ src/option/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 mod par_iter;
 mod par_runner;
 pub mod size_pairs;
---
+++ src/option/tests/one_f.rs
@@ -0,0 +1,57 @@
+use crate::option::tests::utils::inputs;
+use crate::parameters::IterationOrder;
+use crate::*;
+use std::string::String;
+
+const N: usize = 157;
+
+#[test]
+fn one_f_find_ok() {
+    let inputs = inputs(N, None);
+    let result = inputs
+        .into_par()
+        .fallible_option()
+        .filter(|x| x.len() > 1)
+        .first();
+    assert_eq!(result, Some(Some(String::from("10"))));
+}
+
+#[test]
+fn one_f_find_any_ok() {
+    let inputs = inputs(N, None);
+    let result = inputs
+        .into_par()
+        .fallible_option()
+        .filter(|x| x.len() > 1)
+        .iteration_order(IterationOrder::Arbitrary)
+        .first();
+    assert!(result.is_some());
+}
+
+#[test]
+fn one_f_reduce_ok() {
+    let inputs = inputs(N, None);
+    let result = inputs
+        .into_par()
+        .fallible_option()
+        .filter(|x| x.len() > 1)
+        .reduce(|a, b| match a < b {
+            true => b,
+            false => a,
+        });
+    assert_eq!(result, Some(Some(String::from("99"))));
+}
+
+#[test]
+fn one_f_reduce_err() {
+    let inputs = inputs(N, Some(42));
+    let result = inputs
+        .into_par()
+        .fallible_option()
+        .filter(|x| x.len() > 1)
+        .reduce(|a, b| match a < b {
+            true => b,
+            false => a,
+        });
+    assert_eq!(result, None);
+}
