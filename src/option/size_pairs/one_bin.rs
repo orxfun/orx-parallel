031fb864fe7a9fea4611ab0c741593ac27f05cf3
orxfun <orx.ugur.arikan@gmail.com>
2026-04-08 14:54:48 +0200

    one-bin optional computation

--- src/option/size_pairs/one_bin.rs
+++ src/option/size_pairs/one_bin.rs
@@ -3,21 +3,20 @@ use crate::option::size_pairs::SizePairOpt;
 use crate::sizes::OneBin;
 
 impl SizePairOpt for OneBin {
-    type XapResResult<M, E, X1, X2>
-        = Option<Result<X2::O, E>>
+    type XapOptResult<M, X1, X2>
+        = Option<Option<X2::O>>
     where
-        X1: Xap<O = Result<M, E>, Size = Self::S1>,
+        X1: Xap<O = Option<M>, Size = Self::S1>,
         X2: Xap<I = M, Size = Self::S2>;
 
-    #[inline(always)]
-    fn xap_res<M, E, X1, X2>(x1: X1, x2: X2, i: X1::I) -> Self::XapResResult<M, E, X1, X2>
+    fn xap_opt<M, X1, X2>(x1: X1, x2: X2, i: X1::I) -> Self::XapOptResult<M, X1, X2>
     where
-        X1: Xap<O = Result<M, E>, Size = Self::S1>,
+        X1: Xap<O = Option<M>, Size = Self::S1>,
         X2: Xap<I = M, Size = Self::S2>,
     {
         match x1.one_value(i) {
-            Ok(a) => x2.bin_value(a).map(Ok),
-            Err(e) => Some(Err(e)),
+            Some(a) => x2.bin_value(a).map(Some),
+            None => Some(None),
         }
     }
 }
