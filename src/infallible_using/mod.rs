9dc82986497f31597ce78dbd391057e3e6cd2bb4
orxfun <orx.ugur.arikan@gmail.com>
2026-04-08 22:39:06 +0200

    revise SizeInf for use computation

--- src/infallible_using/mod.rs
+++ src/infallible_using/mod.rs
@@ -1,2 +1,5 @@
 pub mod fun;
 mod sizes;
+mod xap_use;
+
+pub use xap_use::Xap;
--- src/infallible_using/sizes/size_inf_use.rs
+++ src/infallible_using/sizes/size_inf_use.rs
@@ -1,72 +1,72 @@
-use crate::infallible::Xap;
-use crate::infallible::fun::*;
+use crate::infallible_using::Xap;
+use crate::infallible_using::fun::*;
 use crate::sizes::Many;
 use crate::sizes::Size;
 
-pub trait SizeInfUse: Size {
+pub trait SizeInf: Size {
     // transformations
 
-    type Map<X, Q, H>: Xap<I = X::I, O = Q, Size = Self>
+    type Map<U, X, Q, H>: Xap<I = X::I, O = Q, Size = Self>
     where
-        X: Xap<Size = Self>,
-        H: Fn(X::O) -> Q + Copy + Send;
+        X: Xap<U = U, Size = Self>,
+        H: Fn(&mut U, X::O) -> Q + Copy + Send;
 
-    fn map<X, Q, H>(x: X, h: H) -> Self::Map<X, Q, H>
+    fn map<U, X, Q, H>(x: X, h: H) -> Self::Map<U, X, Q, H>
     where
-        X: Xap<Size = Self>,
-        H: Fn(X::O) -> Q + Copy + Send;
+        X: Xap<U = U, Size = Self>,
+        H: Fn(&mut U, X::O) -> Q + Copy + Send;
 
-    type Inspect<X, H>: Xap<I = X::I, O = X::O, Size = Self>
+    type Inspect<U, X, H>: Xap<I = X::I, O = X::O, Size = Self>
     where
-        X: Xap<Size = Self>,
-        H: Fn(&X::O) + Copy + Send;
+        X: Xap<U = U, Size = Self>,
+        H: Fn(&mut U, &X::O) + Copy + Send;
 
-    fn inspect<X, H>(x: X, h: H) -> Self::Inspect<X, H>
+    fn inspect<U, X, H>(x: X, h: H) -> Self::Inspect<U, X, H>
     where
-        X: Xap<Size = Self>,
-        H: Fn(&X::O) + Copy + Send;
+        X: Xap<U = U, Size = Self>,
+        H: Fn(&mut U, &X::O) + Copy + Send;
 
-    type Filter<X, H>: Xap<I = X::I, O = X::O, Size = Self::ThenBin>
+    type Filter<U, X, H>: Xap<I = X::I, O = X::O, Size = Self::ThenBin>
     where
-        X: Xap<Size = Self>,
-        H: Fn(&X::O) -> bool + Copy + Send;
+        X: Xap<U = U, Size = Self>,
+        H: Fn(&mut U, &X::O) -> bool + Copy + Send;
 
-    fn filter<X, H>(x: X, h: H) -> Self::Filter<X, H>
+    fn filter<U, X, H>(x: X, h: H) -> Self::Filter<U, X, H>
     where
-        X: Xap<Size = Self>,
-        H: Fn(&X::O) -> bool + Copy + Send;
+        X: Xap<U = U, Size = Self>,
+        H: Fn(&mut U, &X::O) -> bool + Copy + Send;
 
-    type FilterMap<X, Q, H>: Xap<I = X::I, O = Q, Size = Self::ThenBin>
+    type FilterMap<U, X, Q, H>: Xap<I = X::I, O = Q, Size = Self::ThenBin>
     where
-        X: Xap<Size = Self>,
-        H: Fn(X::O) -> Option<Q> + Copy + Send;
+        X: Xap<U = U, Size = Self>,
+        H: Fn(&mut U, X::O) -> Option<Q> + Copy + Send;
 
-    fn filter_map<X, Q, H>(x: X, h: H) -> Self::FilterMap<X, Q, H>
+    fn filter_map<U, X, Q, H>(x: X, h: H) -> Self::FilterMap<U, X, Q, H>
     where
-        X: Xap<Size = Self>,
-        H: Fn(X::O) -> Option<Q> + Copy + Send;
+        X: Xap<U = U, Size = Self>,
+        H: Fn(&mut U, X::O) -> Option<Q> + Copy + Send;
 
-    type FlatMap<X, V, H>: Xap<I = X::I, O = V::Item, Size = Many>
+    type FlatMap<U, X, V, H>: Xap<I = X::I, O = V::Item, Size = Many>
     where
-        X: Xap<Size = Self>,
+        X: Xap<U = U, Size = Self>,
         V: IntoIterator,
-        H: Fn(X::O) -> V + Copy + Send;
+        H: Fn(&mut U, X::O) -> V + Copy + Send;
 
-    fn flat_map<X, V, H>(x: X, h: H) -> Self::FlatMap<X, V, H>
+    fn flat_map<U, X, V, H>(x: X, h: H) -> Self::FlatMap<U, X, V, H>
     where
-        X: Xap<Size = Self>,
+        X: Xap<U = U, Size = Self>,
         V: IntoIterator,
-        H: Fn(X::O) -> V + Copy + Send;
+        H: Fn(&mut U, X::O) -> V + Copy + Send;
 
     // transformations - helper
 
-    type Mapped<X, M>: Xap<I = X::I, O = M::O, Size = Self>
+    type Mapped<U, X, M>: Xap<I = X::I, O = M::O, Size = Self>
     where
-        X: Xap<Size = Self>,
-        M: Map<I = X::O>;
+        X: Xap<U = U, Size = Self>,
+        M: Map<U = U, I = X::O>;
 
-    fn mapped<X, M>(x: X, m: M) -> Self::Mapped<X, M>
+    fn mapped<U, X, M>(x: X, m: M) -> Self::Mapped<U, X, M>
     where
-        X: Xap<Size = Self>,
-        M: Map<I = X::O>;
+        X: Xap<U = U, Size = Self>,
+        M: Map<U = U, I = X::O>;
 }
