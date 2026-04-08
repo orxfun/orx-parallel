58d2152ba56c5cd4ce83311e48f144ef6449c7a6
orxfun <orx.ugur.arikan@gmail.com>
2026-04-08 18:26:33 +0200

    from-many tests for fallible parallel iterator

---
+++ src/result/tests/from_many/id.rs
@@ -0,0 +1,62 @@
+use crate::parameters::IterationOrder;
+use crate::result::tests::utils::inputs_map;
+use crate::*;
+use std::string::String;
+use std::vec;
+use std::vec::Vec;
+
+const N: usize = 157;
+
+#[test]
+fn id_find_ok() {
+    let inputs = inputs_map(N);
+    let result = inputs
+        .into_par()
+        .flat_map(|x| [x.clone(), x.clone(), x].map(Result::<_, Vec<char>>::Ok))
+        .fallible_result()
+        .first();
+    assert_eq!(result, Ok(Some(String::from("0"))));
+}
+
+#[test]
+fn id_find_any_ok() {
+    let inputs = inputs_map(N);
+    let result = inputs
+        .into_par()
+        .flat_map(|x| [x.clone(), x.clone(), x].map(Result::<_, Vec<char>>::Ok))
+        .fallible_result()
+        .iteration_order(IterationOrder::Arbitrary)
+        .first();
+    assert!(result.is_ok());
+}
+
+#[test]
+fn id_reduce_ok() {
+    let inputs = inputs_map(N);
+    let result = inputs
+        .into_par()
+        .flat_map(|x| [x.clone(), x.clone(), x].map(Result::<_, Vec<char>>::Ok))
+        .fallible_result()
+        .reduce(|a, b| match a < b {
+            true => b,
+            false => a,
+        });
+    assert_eq!(result, Ok(Some(String::from("99"))));
+}
+
+#[test]
+fn id_reduce_ok_err() {
+    let inputs = inputs_map(N);
+    let result = inputs
+        .into_par()
+        .flat_map(|x| match x.as_str() == "42" {
+            true => [x.clone(), x.clone(), x].map(Result::<_, Vec<char>>::Ok),
+            false => [Err(vec!['a']), Err(vec!['b']), Err(vec!['c'])],
+        })
+        .fallible_result()
+        .reduce(|a, b| match a < b {
+            true => b,
+            false => a,
+        });
+    assert_eq!(result, Err(vec!['a']));
+}
