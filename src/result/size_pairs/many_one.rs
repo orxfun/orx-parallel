ba876e01def973aa260a2de03a8093531b743d92
orxfun <orx.ugur.arikan@gmail.com>
2026-04-08 16:56:23 +0200

    implement size_hint, fold and count for fallible many-one computation

--- src/result/size_pairs/many_one.rs
+++ src/result/size_pairs/many_one.rs
@@ -43,6 +43,44 @@ where
     fn next(&mut self) -> Option<Self::Item> {
         self.iter.next().map(|a| a.map(|a| self.x2.one_value(a)))
     }
+
+    #[inline(always)]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+
+    fn fold<B, F>(self, init: B, mut f: F) -> B
+    where
+        Self: Sized,
+        F: FnMut(B, Self::Item) -> B,
+    {
+        let mut agg = init;
+
+        for i in self.iter {
+            match i {
+                Ok(i) => agg = f(agg, Ok(self.x2.one_value(i))),
+                Err(e) => return f(agg, Err(e)),
+            }
+        }
+
+        agg
+    }
+
+    fn count(self) -> usize
+    where
+        Self: Sized,
+    {
+        let mut count = 0;
+
+        for i in self.iter {
+            match i {
+                Ok(_) => count += 1,
+                Err(_) => return count,
+            }
+        }
+
+        count
+    }
 }
 
 impl<M, E, I, X2> FusedIterator for IterResManyOne<M, E, I, X2>
