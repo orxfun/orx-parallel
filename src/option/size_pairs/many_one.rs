1f66bbb0e58c701c6d00be456d9dd51285a47be7
orxfun <orx.ugur.arikan@gmail.com>
2026-04-08 14:45:08 +0200

    implement optional many-one computation

--- src/option/size_pairs/many_one.rs
+++ src/option/size_pairs/many_one.rs
@@ -3,40 +3,39 @@ use crate::option::size_pairs::SizePairOpt;
 use crate::sizes::{ManyOne, One};
 
 impl SizePairOpt for ManyOne {
-    type XapResResult<M, E, X1, X2>
-        = IterResManyOne<M, E, <X1::Values as IntoIterator>::IntoIter, X2>
+    type XapOptResult<M, X1, X2>
+        = IterOptManyOne<M, <X1::Values as IntoIterator>::IntoIter, X2>
     where
-        X1: Xap<O = Result<M, E>, Size = Self::S1>,
+        X1: Xap<O = Option<M>, Size = Self::S1>,
         X2: Xap<I = M, Size = Self::S2>;
 
-    #[inline(always)]
-    fn xap_res<M, E, X1, X2>(x1: X1, x2: X2, i: X1::I) -> Self::XapResResult<M, E, X1, X2>
+    fn xap_opt<M, X1, X2>(x1: X1, x2: X2, i: X1::I) -> Self::XapOptResult<M, X1, X2>
     where
-        X1: Xap<O = Result<M, E>, Size = Self::S1>,
+        X1: Xap<O = Option<M>, Size = Self::S1>,
         X2: Xap<I = M, Size = Self::S2>,
     {
         let iter = x1.xap(i).into_iter();
-        IterResManyOne { iter, x2: x2 }
+        IterOptManyOne { iter, x2: x2 }
     }
 }
 
 // iter
 
-pub struct IterResManyOne<M, E, I, X2>
+pub struct IterOptManyOne<M, I, X2>
 where
-    I: Iterator<Item = Result<M, E>>,
+    I: Iterator<Item = Option<M>>,
     X2: Xap<I = M, Size = One>,
 {
     iter: I,
     x2: X2,
 }
 
-impl<M, E, I, X2> Iterator for IterResManyOne<M, E, I, X2>
+impl<M, I, X2> Iterator for IterOptManyOne<M, I, X2>
 where
-    I: Iterator<Item = Result<M, E>>,
+    I: Iterator<Item = Option<M>>,
     X2: Xap<I = M, Size = One>,
 {
-    type Item = Result<X2::O, E>;
+    type Item = Option<X2::O>;
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
