470ea09b124910ce49d77c2f646716ff0d728c64
orxfun <orx.ugur.arikan@gmail.com>
2026-04-08 23:09:24 +0200

    implement SizeInf for Bin in use computations

--- src/infallible_using/sizes/bin.rs
+++ src/infallible_using/sizes/bin.rs
@@ -5,93 +5,93 @@ use crate::infallible_using::xap_variants::*;
 use crate::sizes::Bin;
 
 impl SizeInf for Bin {
-    // // transformations
+    // transformations
 
-    // type Map<X, Q, H>
-    //     = BinM<X, FnMap<X::O, Q, H>>
-    // where
-    //     X: Xap<Size = Self>,
-    //     H: Fn(X::O) -> Q + Copy + Send;
+    type Map<X, Q, H>
+        = BinM<X, FnMap<X::U, X::O, Q, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&mut X::U, X::O) -> Q + Copy + Send;
 
-    // fn map<X, Q, H>(x: X, h: H) -> Self::Map<X, Q, H>
-    // where
-    //     X: Xap<Size = Self>,
-    //     H: Fn(X::O) -> Q + Copy + Send,
-    // {
-    //     BinM::new(x, FnMap::new(h))
-    // }
+    fn map<X, Q, H>(x: X, h: H) -> Self::Map<X, Q, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&mut X::U, X::O) -> Q + Copy + Send,
+    {
+        BinM::new(x, FnMap::new(h))
+    }
 
-    // type Inspect<X, H>
-    //     = BinM<X, FnIns<X::O, H>>
-    // where
-    //     X: Xap<Size = Self>,
-    //     H: Fn(&X::O) + Copy + Send;
+    type Inspect<X, H>
+        = BinM<X, FnIns<X::U, X::O, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&mut X::U, &X::O) + Copy + Send;
 
-    // fn inspect<X, H>(x: X, h: H) -> Self::Inspect<X, H>
-    // where
-    //     X: Xap<Size = Self>,
-    //     H: Fn(&X::O) + Copy + Send,
-    // {
-    //     BinM::new(x, FnIns::new(h))
-    // }
+    fn inspect<X, H>(x: X, h: H) -> Self::Inspect<X, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&mut X::U, &X::O) + Copy + Send,
+    {
+        BinM::new(x, FnIns::new(h))
+    }
 
-    // type Filter<X, H>
-    //     = BinF<X, FnFil<X::O, H>>
-    // where
-    //     X: Xap<Size = Self>,
-    //     H: Fn(&X::O) -> bool + Copy + Send;
+    type Filter<X, H>
+        = BinF<X, FnFil<X::U, X::O, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&mut X::U, &X::O) -> bool + Copy + Send;
 
-    // fn filter<X, H>(x: X, h: H) -> Self::Filter<X, H>
-    // where
-    //     X: Xap<Size = Self>,
-    //     H: Fn(&X::O) -> bool + Copy + Send,
-    // {
-    //     BinF::new(x, FnFil::new(h))
-    // }
+    fn filter<X, H>(x: X, h: H) -> Self::Filter<X, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&mut X::U, &X::O) -> bool + Copy + Send,
+    {
+        BinF::new(x, FnFil::new(h))
+    }
 
-    // type FilterMap<X, Q, H>
-    //     = BinF<X, FnFilMap<X::O, Q, H>>
-    // where
-    //     X: Xap<Size = Self>,
-    //     H: Fn(X::O) -> Option<Q> + Copy + Send;
+    type FilterMap<X, Q, H>
+        = BinF<X, FnFilMap<X::U, X::O, Q, H>>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&mut X::U, X::O) -> Option<Q> + Copy + Send;
 
-    // fn filter_map<X, Q, H>(x: X, h: H) -> Self::FilterMap<X, Q, H>
-    // where
-    //     X: Xap<Size = Self>,
-    //     H: Fn(X::O) -> Option<Q> + Copy + Send,
-    // {
-    //     BinF::new(x, FnFilMap::new(h))
-    // }
+    fn filter_map<X, Q, H>(x: X, h: H) -> Self::FilterMap<X, Q, H>
+    where
+        X: Xap<Size = Self>,
+        H: Fn(&mut X::U, X::O) -> Option<Q> + Copy + Send,
+    {
+        BinF::new(x, FnFilMap::new(h))
+    }
 
-    // type FlatMap<X, V, H>
-    //     = BinX<X, FnFlatMap<X::O, V, H>>
-    // where
-    //     X: Xap<Size = Self>,
-    //     V: IntoIterator,
-    //     H: Fn(X::O) -> V + Copy + Send;
+    type FlatMap<X, V, H>
+        = BinX<X, FnFlatMap<X::U, X::O, V, H>>
+    where
+        X: Xap<Size = Self>,
+        V: IntoIterator,
+        H: Fn(&mut X::U, X::O) -> V + Copy + Send;
 
-    // fn flat_map<X, V, H>(x: X, h: H) -> Self::FlatMap<X, V, H>
-    // where
-    //     X: Xap<Size = Self>,
-    //     V: IntoIterator,
-    //     H: Fn(X::O) -> V + Copy + Send,
-    // {
-    //     BinX::new(x, FnFlatMap::new(h))
-    // }
+    fn flat_map<X, V, H>(x: X, h: H) -> Self::FlatMap<X, V, H>
+    where
+        X: Xap<Size = Self>,
+        V: IntoIterator,
+        H: Fn(&mut X::U, X::O) -> V + Copy + Send,
+    {
+        BinX::new(x, FnFlatMap::new(h))
+    }
 
-    // // transformations - helper
+    // transformations - helper
 
-    // type Mapped<X, M>
-    //     = BinM<X, M>
-    // where
-    //     X: Xap<Size = Self>,
-    //     M: Map<I = X::O>;
+    type Mapped<X, M>
+        = BinM<X, M>
+    where
+        X: Xap<Size = Self>,
+        M: Map<U = X::U, I = X::O>;
 
-    // fn mapped<X, M>(x: X, m: M) -> Self::Mapped<X, M>
-    // where
-    //     X: Xap<Size = Self>,
-    //     M: Map<I = X::O>,
-    // {
-    //     BinM::new(x, m)
-    // }
+    fn mapped<X, M>(x: X, m: M) -> Self::Mapped<X, M>
+    where
+        X: Xap<Size = Self>,
+        M: Map<U = X::U, I = X::O>,
+    {
+        BinM::new(x, m)
+    }
 }
