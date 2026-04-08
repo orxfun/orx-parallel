075b7847993d4602f986f5c1c2734a29f95bd45f
orxfun <orx.ugur.arikan@gmail.com>
2026-04-08 22:49:05 +0200

    using computation wip

--- src/infallible_using/xap_use.rs
+++ src/infallible_using/xap_use.rs
@@ -15,52 +15,52 @@ pub trait Xap: Copy + Send {
 
     fn xap(&self, u: &mut Self::U, i: Self::I) -> Self::Values;
 
-    // transformations
+    // // transformations
 
-    fn map<Q, H>(self, h: H) -> MapOf<Self, Q, H>
-    where
-        H: Fn(&mut Self::U, Self::O) -> Q + Copy + Send,
-    {
-        <Self::Size as SizeInf>::map(self, h)
-    }
+    // fn map<Q, H>(self, h: H) -> MapOf<Self, Q, H>
+    // where
+    //     H: Fn(&mut Self::U, Self::O) -> Q + Copy + Send,
+    // {
+    //     <Self::Size as SizeInf>::map(self, h)
+    // }
 
-    fn inspect<H>(self, h: H) -> InsOf<Self, H>
-    where
-        H: Fn(&mut Self::U, &Self::O) + Copy + Send,
-    {
-        <Self::Size as SizeInf>::inspect(self, h)
-    }
+    // fn inspect<H>(self, h: H) -> InsOf<Self, H>
+    // where
+    //     H: Fn(&mut Self::U, &Self::O) + Copy + Send,
+    // {
+    //     <Self::Size as SizeInf>::inspect(self, h)
+    // }
 
-    fn filter<H>(self, h: H) -> FilOf<Self, H>
-    where
-        H: Fn(&mut Self::U, &Self::O) -> bool + Copy + Send,
-    {
-        <Self::Size as SizeInf>::filter(self, h)
-    }
+    // fn filter<H>(self, h: H) -> FilOf<Self, H>
+    // where
+    //     H: Fn(&mut Self::U, &Self::O) -> bool + Copy + Send,
+    // {
+    //     <Self::Size as SizeInf>::filter(self, h)
+    // }
 
-    fn filter_map<Q, H>(self, h: H) -> FilMapOf<Self, Q, H>
-    where
-        H: Fn(&mut Self::U, Self::O) -> Option<Q> + Copy + Send,
-    {
-        <Self::Size as SizeInf>::filter_map(self, h)
-    }
+    // fn filter_map<Q, H>(self, h: H) -> FilMapOf<Self, Q, H>
+    // where
+    //     H: Fn(&mut Self::U, Self::O) -> Option<Q> + Copy + Send,
+    // {
+    //     <Self::Size as SizeInf>::filter_map(self, h)
+    // }
 
-    fn flat_map<V, H>(self, h: H) -> FlatMapOf<Self, V, H>
-    where
-        V: IntoIterator,
-        H: Fn(&mut Self::U, Self::O) -> V + Copy + Send,
-    {
-        <Self::Size as SizeInf>::flat_map(self, h)
-    }
+    // fn flat_map<V, H>(self, h: H) -> FlatMapOf<Self, V, H>
+    // where
+    //     V: IntoIterator,
+    //     H: Fn(&mut Self::U, Self::O) -> V + Copy + Send,
+    // {
+    //     <Self::Size as SizeInf>::flat_map(self, h)
+    // }
 
-    // transformations - helper
+    // // transformations - helper
 
-    fn mapped<M>(self, m: M) -> MappedOf<Self, M>
-    where
-        M: Map<U = Self::U, I = Self::O>,
-    {
-        <Self::Size as SizeInf>::mapped(self, m)
-    }
+    // fn mapped<M>(self, m: M) -> MappedOf<Self, M>
+    // where
+    //     M: Map<U = Self::U, I = Self::O>,
+    // {
+    //     <Self::Size as SizeInf>::mapped(self, m)
+    // }
 }
 
 // one
@@ -87,16 +87,16 @@ pub trait XapBin: Xap<Size = Bin> {
 
 impl<X: Xap<Size = Bin>> XapBin for X {}
 
-// helper types
+// // helper types
 
-pub type MapOf<X, Q, H> = <<X as Xap>::Size as SizeInf>::Map<<X as Xap>::U, X, Q, H>;
+// pub type MapOf<X, Q, H> = <<X as Xap>::Size as SizeInf>::Map<<X as Xap>::U, X, Q, H>;
 
-pub type InsOf<X, H> = <<X as Xap>::Size as SizeInf>::Inspect<<X as Xap>::U, X, H>;
+// pub type InsOf<X, H> = <<X as Xap>::Size as SizeInf>::Inspect<<X as Xap>::U, X, H>;
 
-pub type FilOf<X, H> = <<X as Xap>::Size as SizeInf>::Filter<<X as Xap>::U, X, H>;
+// pub type FilOf<X, H> = <<X as Xap>::Size as SizeInf>::Filter<<X as Xap>::U, X, H>;
 
-pub type FilMapOf<X, Q, H> = <<X as Xap>::Size as SizeInf>::FilterMap<<X as Xap>::U, X, Q, H>;
+// pub type FilMapOf<X, Q, H> = <<X as Xap>::Size as SizeInf>::FilterMap<<X as Xap>::U, X, Q, H>;
 
-pub type FlatMapOf<X, V, H> = <<X as Xap>::Size as SizeInf>::FlatMap<<X as Xap>::U, X, V, H>;
+// pub type FlatMapOf<X, V, H> = <<X as Xap>::Size as SizeInf>::FlatMap<<X as Xap>::U, X, V, H>;
 
-pub type MappedOf<X, M> = <<X as Xap>::Size as SizeInf>::Mapped<<X as Xap>::U, X, M>;
+// pub type MappedOf<X, M> = <<X as Xap>::Size as SizeInf>::Mapped<<X as Xap>::U, X, M>;
---
+++ src/infallible_using/xap_variants/many_x.rs
@@ -0,0 +1,123 @@
+use crate::infallible_using::Xap;
+use crate::infallible_using::fun::FlatMap;
+use crate::sizes::Many;
+
+pub struct ManyX<X: Xap<Size = Many>, G: FlatMap<U = X::U, I = X::O>> {
+    x: X,
+    g: G,
+}
+
+impl<X: Xap<Size = Many>, G: FlatMap<U = X::U, I = X::O>> Clone for ManyX<X, G> {
+    fn clone(&self) -> Self {
+        Self::new(self.x, self.g)
+    }
+}
+
+impl<X: Xap<Size = Many>, G: FlatMap<U = X::U, I = X::O>> Copy for ManyX<X, G> {}
+
+impl<X: Xap<Size = Many>, G: FlatMap<U = X::U, I = X::O>> ManyX<X, G> {
+    pub fn new(x: X, g: G) -> Self {
+        Self { x, g }
+    }
+}
+
+impl<X: Xap<Size = Many>, G: FlatMap<U = X::U, I = X::O>> Xap for ManyX<X, G> {
+    type U = X::U;
+
+    type I = X::I;
+
+    type O = <G::O as IntoIterator>::Item;
+
+    type Size = Many;
+
+    type Values = IterManyX<<X::Values as IntoIterator>::IntoIter, G>;
+
+    #[inline(always)]
+    fn xap(&self, i: Self::I) -> Self::Values {
+        let i = self.x.xap(i).into_iter();
+        let (g, inner) = (self.g, None);
+        IterManyX { i, g, inner }
+    }
+}
+
+// iter
+
+pub struct IterManyX<I, G>
+where
+    I: Iterator,
+    G: FlatMap<I = I::Item>,
+{
+    i: I,
+    g: G,
+    inner: Option<<G::O as IntoIterator>::IntoIter>,
+}
+
+impl<I, G> Iterator for IterManyX<I, G>
+where
+    I: Iterator,
+    G: FlatMap<I = I::Item>,
+{
+    type Item = <G::O as IntoIterator>::Item;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if let elt @ Some(_) = and_then_or_clear(&mut self.inner, Iterator::next) {
+                return elt;
+            }
+
+            match self.i.next() {
+                Some(i) => self.inner = Some(self.g.flat_map(i).into_iter()),
+                None => return None,
+            }
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        match &self.inner {
+            Some(inner) => (inner.size_hint().0, None),
+            None => (0, None),
+        }
+    }
+
+    #[inline]
+    fn fold<B, F>(self, init: B, mut f: F) -> B
+    where
+        Self: Sized,
+        F: FnMut(B, Self::Item) -> B,
+    {
+        let acc = match self.inner {
+            Some(inner) => inner.fold(init, &mut f),
+            None => init,
+        };
+
+        self.i.fold(acc, |acc, i| {
+            self.g.flat_map(i).into_iter().fold(acc, &mut f)
+        })
+    }
+
+    #[inline]
+    fn count(self) -> usize
+    where
+        Self: Sized,
+    {
+        let count = match self.inner {
+            Some(inner) => inner.count(),
+            None => 0,
+        };
+
+        self.i.fold(count, |count, i| {
+            count + self.g.flat_map(i).into_iter().count()
+        })
+    }
+}
+
+#[inline(always)]
+fn and_then_or_clear<T, U>(opt: &mut Option<T>, f: impl FnOnce(&mut T) -> Option<U>) -> Option<U> {
+    let x = f(opt.as_mut()?);
+    if x.is_none() {
+        *opt = None;
+    }
+    x
+}
