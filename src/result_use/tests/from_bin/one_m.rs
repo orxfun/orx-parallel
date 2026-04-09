1d7eb7e6ed6fcfe3e8cbc457b5ab6def08533dd8
orxfun <orx.ugur.arikan@gmail.com>
2026-04-09 13:49:48 +0200

    use-result test organization

---
+++ src/result_use/tests/from_bin/one_m.rs
@@ -0,0 +1,77 @@
+use crate::parameters::IterationOrder;
+use crate::result_use::tests::utils::{UseValue, inputs};
+use crate::*;
+use std::vec;
+use std::vec::Vec;
+
+const N: usize = 157;
+
+#[test]
+fn one_m_find_ok() {
+    let inputs = inputs(N);
+    let result = inputs
+        .into_par()
+        .filter_map::<Result<_, Vec<char>>, _>(|x| match x.as_str() == "7" {
+            true => None,
+            false => Some(Ok(x)),
+        })
+        .fallible_result()
+        .map(|x| x.parse::<u64>().unwrap())
+        .first();
+    assert_eq!(result, Ok(Some(0)));
+}
+
+#[test]
+fn one_m_find_any_ok() {
+    let inputs = inputs(N);
+    let result = inputs
+        .into_par()
+        .filter_map::<Result<_, Vec<char>>, _>(|x| match x.as_str() == "7" {
+            true => None,
+            false => Some(Ok(x)),
+        })
+        .fallible_result()
+        .map(|x| x.parse::<u64>().unwrap())
+        .iteration_order(IterationOrder::Arbitrary)
+        .first();
+    assert!(result.is_ok());
+}
+
+#[test]
+fn one_m_reduce_ok() {
+    let inputs = inputs(N);
+    let result = inputs
+        .into_par()
+        .filter_map::<Result<_, Vec<char>>, _>(|x| match x.as_str() == "7" {
+            true => None,
+            false => Some(Ok(x)),
+        })
+        .fallible_result()
+        .map(|x| x.parse::<u64>().unwrap())
+        .reduce(|a, b| match a < b {
+            true => b,
+            false => a,
+        });
+    assert_eq!(result, Ok(Some(156)));
+}
+
+#[test]
+fn one_m_reduce_err() {
+    let inputs = inputs(N);
+    let result = inputs
+        .into_par()
+        .filter_map::<Result<_, Vec<char>>, _>(|x| match x.as_str() == "7" {
+            true => None,
+            false => Some(match x.as_str() == "42" {
+                true => Ok(x),
+                false => Err(vec!['a']),
+            }),
+        })
+        .fallible_result()
+        .map(|x| x.parse::<u64>().unwrap())
+        .reduce(|a, b| match a < b {
+            true => b,
+            false => a,
+        });
+    assert_eq!(result, Err(vec!['a']));
+}
---
+++ src/result_use/tests/from_id/one_f.rs
@@ -0,0 +1,58 @@
+use crate::parameters::IterationOrder;
+use crate::result_use::tests::utils::{UseValue, inputs_res};
+use crate::*;
+use std::string::String;
+use std::vec;
+
+const N: usize = 157;
+
+#[test]
+fn one_f_find_ok() {
+    let inputs = inputs_res(N, None);
+    let result = inputs
+        .into_par()
+        .fallible_result()
+        .filter(|x| x.len() > 1)
+        .first();
+    assert_eq!(result, Ok(Some(String::from("10"))));
+}
+
+#[test]
+fn one_f_find_any_ok() {
+    let inputs = inputs_res(N, None);
+    let result = inputs
+        .into_par()
+        .fallible_result()
+        .filter(|x| x.len() > 1)
+        .iteration_order(IterationOrder::Arbitrary)
+        .first();
+    assert!(result.is_ok());
+}
+
+#[test]
+fn one_f_reduce_ok() {
+    let inputs = inputs_res(N, None);
+    let result = inputs
+        .into_par()
+        .fallible_result()
+        .filter(|x| x.len() > 1)
+        .reduce(|a, b| match a < b {
+            true => b,
+            false => a,
+        });
+    assert_eq!(result, Ok(Some(String::from("99"))));
+}
+
+#[test]
+fn one_f_reduce_err() {
+    let inputs = inputs_res(N, Some(42));
+    let result = inputs
+        .into_par()
+        .fallible_result()
+        .filter(|x| x.len() > 1)
+        .reduce(|a, b| match a < b {
+            true => b,
+            false => a,
+        });
+    assert_eq!(result, Err(vec!['a']));
+}
