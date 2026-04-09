5bac7cfb95c70728e44cfe2bf0114e84893b7d7f
orxfun <orx.ugur.arikan@gmail.com>
2026-04-09 17:21:20 +0200

    implement collect_into for SplitVec

--- src/infallible/collectables/split_vec.rs
+++ src/infallible/collectables/split_vec.rs
@@ -10,35 +10,62 @@ use orx_concurrent_iter::ConcurrentIter;
 use orx_fixed_vec::FixedVec;
 use orx_split_vec::{Growth, GrowthWithConstantTimeAccess, PseudoDefault, SplitVec};
 
-// impl<T, G: Growth> ColIntoInf<T> for SplitVec<T, G> {
-//     fn empty(exact_len: Option<usize>) -> Self {
-//         let mut vec = Self::pseudo_default();
-//         split_vec_reserve(&mut vec, exact_len);
-//         vec
-//     }
+impl<T, G: Growth> ColIntoInf<T> for SplitVec<T, G> {
+    fn collect_into<I, X, R>(dst: Option<Self>, par: Par<I, X, R>) -> Self
+    where
+        I: ConcurrentIter,
+        X: Xap<I = I::Item, O = T>,
+        R: ParRunner,
+        T: Send,
+    {
+        let (iter, x, mut exe, params) = par.destruct();
+        let results = exe.collect(params, iter, x);
 
-//     fn collect_into<I, X, R>(mut self, par: Par<I, X, R>) -> Self
-//     where
-//         I: ConcurrentIter,
-//         X: Xap<I = I::Item, O = T>,
-//         R: ParRunner,
-//         T: Send,
-//     {
-//         let (iter, x, mut exe, params) = par.destruct();
-//         let results = exe.collect(params, iter, x);
-//         let len: usize = results.iter().map(|x| x.len()).sum();
+        let dst = dst.unwrap_or_else(|| SplitVec::pseudo_default());
+        merge_collected_into(results, dst)
+    }
 
-//         split_vec_reserve(&mut self, Some(len));
-//         todo!()
-//     }
+    fn collect_arbitrary_into<I, X, R>(
+        dst: Option<Self>,
+        par: Par<I, X, R>,
+        exact_len: Option<usize>,
+    ) -> Self
+    where
+        I: ConcurrentIter,
+        X: Xap<I = I::Item, O = T>,
+        R: ParRunner,
+        T: Send,
+    {
+        todo!()
+    }
+    //     fn empty(exact_len: Option<usize>) -> Self {
+    //         let mut vec = Self::pseudo_default();
+    //         split_vec_reserve(&mut vec, exact_len);
+    //         vec
+    //     }
 
-//     fn collect_arbitrary_into<I, X, R>(self, par: Par<I, X, R>, exact_len: Option<usize>) -> Self
-//     where
-//         I: ConcurrentIter,
-//         X: Xap<I = I::Item, O = T>,
-//         R: ParRunner,
-//         T: Send,
-//     {
-//         todo!()
-//     }
-// }
+    //     fn collect_into<I, X, R>(mut self, par: Par<I, X, R>) -> Self
+    //     where
+    //         I: ConcurrentIter,
+    //         X: Xap<I = I::Item, O = T>,
+    //         R: ParRunner,
+    //         T: Send,
+    //     {
+    //         let (iter, x, mut exe, params) = par.destruct();
+    //         let results = exe.collect(params, iter, x);
+    //         let len: usize = results.iter().map(|x| x.len()).sum();
+
+    //         split_vec_reserve(&mut self, Some(len));
+    //         todo!()
+    //     }
+
+    //     fn collect_arbitrary_into<I, X, R>(self, par: Par<I, X, R>, exact_len: Option<usize>) -> Self
+    //     where
+    //         I: ConcurrentIter,
+    //         X: Xap<I = I::Item, O = T>,
+    //         R: ParRunner,
+    //         T: Send,
+    //     {
+    //         todo!()
+    //     }
+}
