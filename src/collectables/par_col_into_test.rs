830472fc27b06364cb05c27748ba6f7a16aec0bc
orxfun <orx.ugur.arikan@gmail.com>
2026-04-09 22:19:31 +0200

    collect tests for bin-f infallible

--- src/collectables/par_col_into_test.rs
+++ src/collectables/par_col_into_test.rs
@@ -1,34 +1,52 @@
 use crate::ParCollectInto;
 use alloc::vec::Vec;
-use orx_fixed_vec::{FixedVec, PinnedVec};
+use core::fmt::Debug;
+use orx_fixed_vec::FixedVec;
+use orx_pinned_vec::PinnedVec;
 use orx_split_vec::{GrowthWithConstantTimeAccess, SplitVec};
 
-pub trait ParCollectIntoTest<T: Clone>: ParCollectInto<T> + Clone {
-    fn expected(initial: &Self, iter: impl IntoIterator<Item = T>) -> Self;
+pub trait ParCollectIntoTest<T: Clone + PartialEq + Debug>:
+    ParCollectInto<T> + Clone + PartialEq + Debug
+{
+    fn expected(&self, iter: impl IntoIterator<Item = T>) -> Self;
+
+    fn push_back(&mut self, value: T);
 }
 
-impl<T: Clone> ParCollectIntoTest<T> for FixedVec<T> {
-    fn expected(initial: &Self, iter: impl IntoIterator<Item = T>) -> Self {
-        let mut vec = initial.clone();
-        for i in iter {
-            vec.push(i);
-        }
-        vec
+impl<T: Clone + PartialEq + Debug> ParCollectIntoTest<T> for FixedVec<T> {
+    fn expected(&self, iter: impl IntoIterator<Item = T>) -> Self {
+        let mut vec = self.clone().into_inner();
+        vec.extend(iter);
+        vec.into()
+    }
+
+    fn push_back(&mut self, value: T) {
+        self.push(value);
     }
 }
 
-impl<T: Clone, G: GrowthWithConstantTimeAccess> ParCollectIntoTest<T> for SplitVec<T, G> {
-    fn expected(initial: &Self, iter: impl IntoIterator<Item = T>) -> Self {
-        let mut vec = initial.clone();
+impl<T: Clone + PartialEq + Debug, G: GrowthWithConstantTimeAccess> ParCollectIntoTest<T>
+    for SplitVec<T, G>
+{
+    fn expected(&self, iter: impl IntoIterator<Item = T>) -> Self {
+        let mut vec = self.clone();
         vec.extend(iter);
         vec
     }
+
+    fn push_back(&mut self, value: T) {
+        self.push(value);
+    }
 }
 
-impl<T: Clone> ParCollectIntoTest<T> for Vec<T> {
-    fn expected(initial: &Self, iter: impl IntoIterator<Item = T>) -> Self {
-        let mut vec = initial.clone();
+impl<T: Clone + PartialEq + Debug> ParCollectIntoTest<T> for Vec<T> {
+    fn expected(&self, iter: impl IntoIterator<Item = T>) -> Self {
+        let mut vec = self.clone();
         vec.extend(iter);
         vec
+    }
+
+    fn push_back(&mut self, value: T) {
+        self.push(value);
     }
 }
