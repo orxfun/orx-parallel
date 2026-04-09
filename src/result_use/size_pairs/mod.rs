83f75b2508daa31b7c2d5ea9fd21df6b141f1bbf
orxfun <orx.ugur.arikan@gmail.com>
2026-04-09 11:47:34 +0200

    impelment SizePairUseRes for one-bin pair

--- src/result_use/size_pairs/mod.rs
+++ src/result_use/size_pairs/mod.rs
@@ -4,7 +4,7 @@ mod bin_one;
 mod many_bin;
 mod many_many;
 mod many_one;
-// mod one_bin;
+mod one_bin;
 // mod one_many;
 // mod one_one;
 mod size_pair_use_res;
--- src/result_use/size_pairs/one_bin.rs
+++ src/result_use/size_pairs/one_bin.rs
@@ -2,21 +2,25 @@ use crate::infallible_use::{XapBin, XapOne, XapUse};
 use crate::result_use::size_pairs::size_pair_use_res::SizePairUseRes;
 use crate::sizes::OneBin;
 
-impl SizePairRes for OneBin {
-    type XapResResult<M, E, X1, X2>
+impl SizePairUseRes for OneBin {
+    type XapUseResResult<M, E, X1, X2>
         = Option<Result<X2::O, E>>
     where
-        X1: Xap<O = Result<M, E>, Size = Self::S1>,
-        X2: Xap<I = M, Size = Self::S2>;
+        X1: XapUse<O = Result<M, E>, Size = Self::S1>,
+        X2: XapUse<U = X1::U, I = M, Size = Self::S2>;
 
-    #[inline(always)]
-    fn xap_res<M, E, X1, X2>(x1: X1, x2: X2, i: X1::I) -> Self::XapResResult<M, E, X1, X2>
+    fn xap_use_res<M, E, X1, X2>(
+        u: *mut X1::U,
+        x1: X1,
+        x2: X2,
+        i: X1::I,
+    ) -> Self::XapUseResResult<M, E, X1, X2>
     where
-        X1: Xap<O = Result<M, E>, Size = Self::S1>,
-        X2: Xap<I = M, Size = Self::S2>,
+        X1: XapUse<O = Result<M, E>, Size = Self::S1>,
+        X2: XapUse<U = X1::U, I = M, Size = Self::S2>,
     {
-        match x1.one_value(i) {
-            Ok(a) => x2.bin_value(a).map(Ok),
+        match x1.one_value(u, i) {
+            Ok(a) => x2.bin_value(u, a).map(Ok),
             Err(e) => Some(Err(e)),
         }
     }
