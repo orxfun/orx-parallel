cf0db235a63ed948733dec6c963bad3a99232d19
orxfun <orx.ugur.arikan@gmail.com>
2026-04-09 11:40:14 +0200

    implement SizePairUseRes for bin-one pair

--- src/result_use/size_pairs/bin_one.rs
+++ src/result_use/size_pairs/bin_one.rs
@@ -2,20 +2,24 @@ use crate::infallible_use::{XapBin, XapOne, XapUse};
 use crate::result_use::size_pairs::size_pair_use_res::SizePairUseRes;
 use crate::sizes::BinOne;
 
-impl SizePairRes for BinOne {
-    type XapResResult<M, E, X1, X2>
+impl SizePairUseRes for BinOne {
+    type XapUseResResult<M, E, X1, X2>
         = Option<Result<X2::O, E>>
     where
-        X1: Xap<O = Result<M, E>, Size = Self::S1>,
-        X2: Xap<I = M, Size = Self::S2>;
+        X1: XapUse<O = Result<M, E>, Size = Self::S1>,
+        X2: XapUse<U = X1::U, I = M, Size = Self::S2>;
 
-    #[inline(always)]
-    fn xap_res<M, E, X1, X2>(x1: X1, x2: X2, i: X1::I) -> Self::XapResResult<M, E, X1, X2>
+    fn xap_use_res<M, E, X1, X2>(
+        u: *mut X1::U,
+        x1: X1,
+        x2: X2,
+        i: X1::I,
+    ) -> Self::XapUseResResult<M, E, X1, X2>
     where
-        X1: Xap<O = Result<M, E>, Size = Self::S1>,
-        X2: Xap<I = M, Size = Self::S2>,
+        X1: XapUse<O = Result<M, E>, Size = Self::S1>,
+        X2: XapUse<U = X1::U, I = M, Size = Self::S2>,
     {
-        let a = x1.bin_value(i);
-        a.map(|a| a.map(|a| x2.one_value(a)))
+        let a = x1.bin_value(u, i);
+        a.map(|a| a.map(|a| x2.one_value(u, a)))
     }
 }
