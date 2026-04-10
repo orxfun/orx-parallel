d2e31acf741e6a434339dee871254b1ff1da9f05
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 13:30:28 +0200

    clean up

--- src/infallible_use/tests/bin_f.rs
+++ src/infallible_use/tests/bin_f.rs
@@ -3,9 +3,6 @@ use crate::parameters::IterationOrder;
 use crate::*;
 use std::string::String;
 
-
-
-
 const N: usize = 157;
 
 #[test]
@@ -43,28 +40,4 @@ fn bin_f_find_any() {
         .iteration_order(IterationOrder::Arbitrary)
         .first();
     assert!(result.is_some());
-}
-
-#[test]
-fn xyz() {
-    let inputs = inputs(N);
-    let result = inputs
-        .into_par()
-        .using_clone(UseValue::new(42))
-        .filter(|u, x| {
-            u.mutate();
-            x.len() > 1
-        })
-        .filter(|u, x| {
-            u.mutate();
-            x.len() < 4
-        })
-        .reduce(|u, a, b| {
-            u.mutate();
-            match a < b {
-                true => b,
-                false => a,
-            }
-        });
-    assert_eq!(result, Some(String::from("99")));
 }
