589ecc2405f38b856438933ebe93d6224257dfe3
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 10:16:15 +0200

    define parallel collect_arb for infallible

--- src/infallible/thread_execution/mod.rs
+++ src/infallible/thread_execution/mod.rs
@@ -1,12 +1,15 @@
 mod collect;
 mod collect_arb;
-mod collect_arb_over_bag;
 mod next;
 mod next_any;
 mod reduce;
 
 pub use collect::collect;
-pub use collect_arb_over_bag::collect_arb_over_bag;
+pub use collect_arb::collect_arb;
 pub use next::next;
 pub use next_any::next_any;
 pub use reduce::reduce;
+
+// Experimental
+mod collect_arb_over_bag;
+pub use collect_arb_over_bag::collect_arb_over_bag;
