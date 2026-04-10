d1b325a82fb2441c2c2456497708f4cec01ad2c4
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 22:09:43 +0200

    define SliceSrc

--- src/collectables/algorithms/merge_ordered_vectors/slice_src.rs
+++ src/collectables/algorithms/merge_ordered_vectors/slice_src.rs
@@ -1,16 +1,29 @@
-// /// A raw slice of contiguous data with initialized values.
-// ///
-// /// # SAFETY
-// ///
-// /// While constructing this slice, we must guarantee that all elements of it
-// /// are initialized since it will be used as source of values.
-// ///
-// /// This is a read-only slice.
-// /// The caller must make sure that there is no concurrent write to this slice.
-// pub struct SliceSrc<'a, T>(Slice<'a, T>);
+use super::slice::Slice;
+use crate::results::ValIdx;
 
-// impl<'a, T> Clone for SliceSrc<'a, T> {
-//     fn clone(&self) -> Self {
-//         Self(self.0.clone())
-//     }
-// }
+/// A raw slice of contiguous data with initialized values.
+///
+/// # SAFETY
+///
+/// While constructing this slice, we must guarantee that all elements of it
+/// are initialized since it will be used as source of values.
+///
+/// This is a read-only slice.
+/// The caller must make sure that there is no concurrent write to this slice.
+pub struct SliceSrc<'a, T>(Slice<'a, T>);
+
+impl<'a, T> Clone for SliceSrc<'a, T> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<'a, T> SliceSrc<'a, T> {
+    pub fn destruct(self) -> *const [ValIdx<T>] {
+        self.0.destruct()
+    }
+
+    pub fn from_slice(slice: &'a [ValIdx<T>]) -> Self {
+        Self(Slice::new(slice.as_ptr(), slice.len()))
+    }
+}
