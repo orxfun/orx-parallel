560f86091aca4755b4ef87caa8f1aec282eaa9e0
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 09:28:09 +0200

    implement collect for fallible parallel runner

--- src/result/par_runner.rs
+++ src/result/par_runner.rs
@@ -3,6 +3,7 @@ use crate::result::size_pairs::SizePairRes;
 use crate::result::thread_execution as th;
 use crate::results::{Val, ValIdx};
 use crate::{parameters::Params, pool::ParThreadPool, runner::ParRunner};
+use alloc::vec::Vec;
 use orx_concurrent_bag::ConcurrentBag;
 use orx_concurrent_iter::ConcurrentIter;
 
@@ -113,6 +114,41 @@ pub trait ParRunnerRes: ParRunner {
         });
 
         Val::reduce_res(results_bag.into_inner().into_inner(), f)
+    }
+
+    fn collect<I, M, E, X1, X2, S>(
+        &mut self,
+        sizes: S,
+        params: Params,
+        iter: I,
+        x1: X1,
+        x2: X2,
+    ) -> Vec<Result<Vec<ValIdx<X2::O>>, E>>
+    where
+        I: ConcurrentIter,
+        X1: Xap<I = I::Item, O = Result<M, E>>,
+        X2: Xap<I = M>,
+        S: SizePairRes<S1 = X1::Size, S2 = X2::Size>,
+        X2::O: Send,
+        E: Send,
+    {
+        let mut spawned = 0;
+        let (max_nt, state) = self.nt_state(params, iter.try_get_len());
+        let results_bag = ConcurrentBag::with_fixed_capacity(max_nt);
+
+        let (iter, state, results) = (&iter, &state, &results_bag);
+        self.pool_mut().scoped_computation(move |s| {
+            while let Some(th_idx) = Self::do_spawn_new(spawned, state) {
+                spawned += 1;
+                <Self::Pool as ParThreadPool>::run_in_scope(&s, move || {
+                    let vec =
+                        th::collect::<Self, _, _, _, _, _, _>(sizes, th_idx, state, iter, x1, x2);
+                    results.push(vec);
+                });
+            }
+        });
+
+        results_bag.into_inner().into_inner()
     }
 }
 
--- src/result/thread_execution/mod.rs
+++ src/result/thread_execution/mod.rs
@@ -3,6 +3,7 @@ mod next;
 mod next_any;
 mod reduce;
 
+pub use collect::collect;
 pub use next::next;
 pub use next_any::next_any;
 pub use reduce::reduce;
