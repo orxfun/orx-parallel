cd8cde838825af8efde7ed6daa2a2d5c065a3bad
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 23:10:08 +0200

    implement IntoIterPtrDst for Vec

--- src/collectables/algorithms/merge_ordered_vectors/into_iter_ptr_dst.rs
+++ src/collectables/algorithms/merge_ordered_vectors/into_iter_ptr_dst.rs
@@ -1,5 +1,4 @@
 use super::iter_ptr_dst::IterPtrDst;
-use crate::results::ValIdx;
 use alloc::vec::Vec;
 use core::marker::PhantomData;
 
@@ -12,9 +11,9 @@ pub trait IntoIterPtrDst<'a, T: 'a> {
 // vec
 
 pub struct SliceIterPtrDst<'a, T: 'a> {
-    data: *const ValIdx<T>,
-    exclusive_end: *const ValIdx<T>,
-    phantom: PhantomData<&'a ()>,
+    data: *mut T,
+    exclusive_end: *mut T,
+    p: PhantomData<&'a ()>,
 }
 
 impl<'a, T: 'a> IterPtrDst<'a, T> for SliceIterPtrDst<'a, T> {
@@ -25,6 +24,25 @@ impl<'a, T: 'a> IterPtrDst<'a, T> for SliceIterPtrDst<'a, T> {
 
     #[inline(always)]
     unsafe fn next_unchecked(&mut self) -> *mut T {
-        todo!()
+        debug_assert!(self.len() > 0);
+        let value = self.data;
+        self.data = unsafe { self.data.add(1) };
+        value
+    }
+}
+
+impl<'a, T: 'a> IntoIterPtrDst<'a, T> for &'a Vec<T> {
+    type Iter = SliceIterPtrDst<'a, T>;
+
+    fn into_iter_ptr_dst(self) -> Self::Iter {
+        let n = self.len();
+        let data = self.as_ptr() as *mut T;
+        let exclusive_end = unsafe { data.add(n) };
+        let p = PhantomData;
+        SliceIterPtrDst {
+            data,
+            exclusive_end,
+            p,
+        }
     }
 }
