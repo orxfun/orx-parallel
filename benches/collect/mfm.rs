0f1abcd88a63ef74fefccb444bf375d45ea433e4
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 12:36:34 +0200

    report benchmark results

--- benches/collect/mfm.rs
+++ benches/collect/mfm.rs
@@ -10,21 +10,7 @@
   * _vv means, results are collected into a Vec<Vec<_>>
   * _ll means, results are collected into a LinkedList<Vec<_>>
 
-col_mfm/seq/e15_light       time:   [99.763 µs 100.72 µs 101.80 µs]
-col_mfm/rayon/e15_light     time:   [18.987 ms 19.995 ms 21.084 ms]
-col_mfm/orx_ord/e15_light   time:   [2.6493 ms 2.7415 ms 2.8361 ms]
-
-col_mfm/seq/e20_light       time:   [3.8361 ms 3.9048 ms 3.9747 ms]
-col_mfm/rayon/e20_light     time:   [31.905 ms 34.226 ms 36.717 ms]
-col_mfm/orx_ord/e20_light   time:   [8.2977 ms 9.0430 ms 9.8910 ms]
-
-col_mfm/seq/e15_heavy       time:   [1.4120 ms 1.4247 ms 1.4381 ms]
-col_mfm/rayon/e15_heavy     time:   [16.218 ms 16.849 ms 17.504 ms]
-col_mfm/orx_ord/e15_heavy   time:   [3.1397 ms 3.2166 ms 3.2947 ms]
-
-col_mfm/seq/e20_heavy       time:   [49.292 ms 50.898 ms 52.593 ms]
-col_mfm/rayon/e20_heavy     time:   [40.710 ms 48.117 ms 57.391 ms]
-col_mfm/orx_ord/e20_heavy   time:   [14.721 ms 15.794 ms 17.023 ms]
+placeholder
 
 */
 
