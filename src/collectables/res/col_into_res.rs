6dc8305a74d3d1b553fe295dd835837f8b3187ba
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 09:52:13 +0200

    implement ColIntoRes for Vec

--- src/collectables/res/col_into_res.rs
+++ src/collectables/res/col_into_res.rs
@@ -3,29 +3,17 @@ use crate::result::{ParRes, SizePairRes};
 use crate::runner::ParRunner;
 use orx_concurrent_iter::ConcurrentIter;
 
-pub trait ColIntoRes<T>: Sized {
-    fn inf_col_into<I, M, E, X1, X2, S, R>(
+pub trait ColIntoRes<T, E>: Sized {
+    fn inf_col_into<I, M, X1, X2, S, R>(
         dst: Option<Self>,
         par: ParRes<I, M, E, X1, X2, S, R>,
-    ) -> Self
+    ) -> Result<Self, E>
     where
         I: ConcurrentIter,
         X1: Xap<I = I::Item, O = Result<M, E>>,
-        X2: Xap<I = M>,
+        X2: Xap<I = M, O = T>,
         S: SizePairRes<S1 = X1::Size, S2 = X2::Size>,
         R: ParRunner,
-        T: Send;
-
-    fn inf_arb_col_into<I, M, E, X1, X2, S, R>(
-        dst: Option<Self>,
-        par: ParRes<I, M, E, X1, X2, S, R>,
-        exact_len: Option<usize>,
-    ) -> Self
-    where
-        I: ConcurrentIter,
-        X1: Xap<I = I::Item, O = Result<M, E>>,
-        X2: Xap<I = M>,
-        S: SizePairRes<S1 = X1::Size, S2 = X2::Size>,
-        R: ParRunner,
-        T: Send;
+        T: Send,
+        E: Send;
 }
