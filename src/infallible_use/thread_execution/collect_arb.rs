957ba3ff2a8866bfeed8f472d74ec2cab3565f9a
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 12:40:02 +0200

    implement infallible thread collect with use var

---
+++ src/infallible_use/thread_execution/collect_arb.rs
@@ -0,0 +1,46 @@
+use crate::infallible_use::xap::XapUse;
+use crate::runner::ParRunner;
+use alloc::vec::Vec;
+use orx_concurrent_iter::{ChunkPuller, ConcurrentIter};
+
+pub fn collect_arb<Q, I, X>(th_idx: usize, state: &Q::State, iter: &I, x: X) -> Vec<X::O>
+where
+    Q: ParRunner,
+    I: ConcurrentIter,
+    X: XapUse<I = I::Item>,
+{
+    todo!()
+    // let mut collected = Vec::new();
+    // let vec = &mut collected;
+
+    // let mut chunk_puller = iter.chunk_puller(0);
+    // let mut item_puller = iter.item_puller();
+
+    // loop {
+    //     let chunk_size = Q::next_chunk_size(state, iter.try_get_len());
+    //     let chunk_state = Q::begin_chunk(th_idx, chunk_size);
+
+    //     match chunk_size {
+    //         0 | 1 => match item_puller.next() {
+    //             Some(i) => vec.extend(x.xap(i)),
+    //             None if iter.is_completed_when_none_returned() => break,
+    //             None => {}
+    //         },
+    //         c => {
+    //             if c > chunk_puller.chunk_size() {
+    //                 chunk_puller = iter.chunk_puller(c);
+    //             }
+
+    //             match chunk_puller.pull() {
+    //                 Some(chunk) => vec.extend(chunk.flat_map(|i| x.xap(i))),
+    //                 None if iter.is_completed_when_none_returned() => break,
+    //                 None => {}
+    //             }
+    //         }
+    //     }
+
+    //     Q::complete_chunk(state, chunk_state);
+    // }
+
+    // collected
+}
--- src/infallible_use/thread_execution/mod.rs
+++ src/infallible_use/thread_execution/mod.rs
@@ -1,7 +1,11 @@
+mod collect;
+mod collect_arb;
 mod next;
 mod next_any;
 mod reduce;
 
+pub use collect::collect;
+pub use collect_arb::collect_arb;
 pub use next::next;
 pub use next_any::next_any;
 pub use reduce::reduce;
