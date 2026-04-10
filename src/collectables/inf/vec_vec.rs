c395798c4b36c81618d699afd540ee76dfe34a4a
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 11:34:35 +0200

    implement ColIntoInf for Vec of Vec's

---
+++ src/collectables/inf/vec_vec.rs
@@ -0,0 +1,51 @@
+use crate::collectables::inf::ColIntoInf;
+use crate::collectables::utils::merge_ord_into;
+use crate::infallible::ParRunnerInfallible;
+use crate::infallible::{Par, Xap};
+use crate::runner::ParRunner;
+use alloc::vec;
+use alloc::vec::Vec;
+use orx_concurrent_iter::ConcurrentIter;
+use orx_fixed_vec::FixedVec;
+
+impl<T> ColIntoInf<T> for Vec<Vec<T>> {
+    fn inf_col_into<I, X, R>(dst: Option<Self>, par: Par<I, X, R>) -> Self
+    where
+        I: ConcurrentIter,
+        X: Xap<I = I::Item, O = T>,
+        R: ParRunner,
+        T: Send,
+    {
+        let (iter, x, mut exe, params) = par.destruct();
+        let results = exe.collect(params, iter, x);
+        let len: usize = results.iter().map(|x| x.len()).sum();
+
+        let mut result = Vec::new();
+        result.reserve(len);
+        let result = merge_ord_into(results, FixedVec::from(result)).into();
+
+        match dst {
+            Some(mut lst) => {
+                lst.push(result);
+                lst
+            }
+            None => vec![result],
+        }
+    }
+
+    fn inf_arb_col_into<I, X, R>(dst: Option<Self>, par: Par<I, X, R>) -> Self
+    where
+        I: ConcurrentIter,
+        X: Xap<I = I::Item, O = T>,
+        R: ParRunner,
+        T: Send,
+    {
+        let (iter, x, mut exe, params) = par.destruct();
+        let results = exe.collect_arb(params, iter, x);
+        let mut dst = dst.unwrap_or_else(|| Self::new());
+        for vec in results {
+            dst.push(vec);
+        }
+        dst
+    }
+}
