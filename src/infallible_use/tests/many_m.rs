9f3720e4589afdc48d9d5cf5208f857e585ce100
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 13:39:14 +0200

    add use collect tests

--- src/infallible_use/tests/many_m.rs
+++ src/infallible_use/tests/many_m.rs
@@ -1,7 +1,13 @@
+use crate::collectables::par_col_into_test::{ColIntoMode, ParCollectIntoTest};
 use crate::infallible_use::tests::utils::{UseValue, inputs};
 use crate::parameters::IterationOrder;
 use crate::*;
-use std::string::ToString;
+use alloc::format;
+use alloc::vec::Vec;
+use orx_fixed_vec::FixedVec;
+use orx_split_vec::SplitVec;
+use std::string::{String, ToString};
+use test_case::test_matrix;
 
 const N: usize = 157;
 
@@ -67,4 +73,56 @@ fn many_m_reduce() {
             }
         });
     assert_eq!(result, Some(160));
+}
+
+#[test_matrix(
+    [Vec::new(), SplitVec::with_doubling_growth(), SplitVec::with_linear_growth(6), FixedVec::new(40)],
+    [ColIntoMode::Col, ColIntoMode::ColIntoEmpty, ColIntoMode::ColIntoFilled(N / 5)],
+    [IterationOrder::Ordered, IterationOrder::Arbitrary]
+)]
+fn many_m_collect<C: ParCollectIntoTest<String>>(_: C, mode: ColIntoMode, order: IterationOrder) {
+    let iter = || {
+        inputs(N)
+            .into_iter()
+            .flat_map(|x| {
+                let a = x.parse::<u64>().unwrap();
+                (0..5).map(move |i| (a + i).to_string())
+            })
+            .map(|x| format!("{}0", x))
+    };
+
+    let expected = C::expected(mode, |i| i.to_string(), iter());
+
+    let result = match C::init_result(mode, |i| i.to_string()) {
+        Some(c) => inputs(N)
+            .into_par()
+            .using(|th_idx| UseValue::new(th_idx))
+            .iteration_order(order)
+            .flat_map(|u, x| {
+                u.mutate();
+                let a = x.parse::<u64>().unwrap();
+                (0..5).map(move |i| (a + i).to_string())
+            })
+            .map(|u, x| {
+                u.mutate();
+                format!("{}0", x)
+            })
+            .collect_into(c),
+        None => inputs(N)
+            .into_par()
+            .using(|th_idx| UseValue::new(th_idx))
+            .iteration_order(order)
+            .flat_map(|u, x| {
+                u.mutate();
+                let a = x.parse::<u64>().unwrap();
+                (0..5).map(move |i| (a + i).to_string())
+            })
+            .map(|u, x| {
+                u.mutate();
+                format!("{}0", x)
+            })
+            .collect(),
+    };
+
+    C::assert_eq(result, expected, order);
 }
