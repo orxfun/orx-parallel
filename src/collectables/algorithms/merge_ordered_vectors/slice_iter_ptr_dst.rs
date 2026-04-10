b7faa4696fe28a83dcacb0847fa6732237ecac21
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 22:35:13 +0200

    implement SliceIterPtrSrc::jump_to_end

--- src/collectables/algorithms/merge_ordered_vectors/slice_iter_ptr_dst.rs
+++ src/collectables/algorithms/merge_ordered_vectors/slice_iter_ptr_dst.rs
@@ -1,1 +1,20 @@
-pub trait SliceIterPtrDst<'a, T: 'a> {}
+use super::slice_iter_ptr_src::SliceIterPtrSrc;
+
+pub trait SliceIterPtrDst<'a, T: 'a> {
+    fn len(&self) -> usize;
+
+    unsafe fn write_rest_from(&mut self, src: &mut SliceIterPtrSrc<'a, T>) {
+        if let Some(src) = src.next() {
+            let count = self.len();
+
+            // SAFETY: having same lengths with src by (i), self cannot be finished
+            let dst = unsafe { self.0.next_unchecked() } as *mut T;
+
+            // SAFETY: satisfied by (ii)
+            unsafe { dst.copy_from_nonoverlapping(src, count) };
+        }
+
+        self.0.jump_to_end();
+        src.jump_to_end();
+    }
+}
