49765e049fc6f199f0fa83398f346d7fa794de97
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 22:00:16 +0200

    clean up

--- src/result/tests/from_id/many_m.rs
+++ src/result/tests/from_id/many_m.rs
@@ -2,8 +2,6 @@ use crate::collectables::par_col_into_test::{ColIntoMode, ParCollectIntoTest};
 use crate::parameters::IterationOrder;
 use crate::result::tests::utils::inputs_res;
 use crate::*;
-use orx_fixed_vec::FixedVec;
-use orx_split_vec::SplitVec;
 use std::string::ToString;
 use std::vec;
 use std::vec::Vec;
