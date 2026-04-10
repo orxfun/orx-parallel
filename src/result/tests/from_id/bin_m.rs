280b94c0db563b0d640134918cf1818fa931e17d
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 14:53:12 +0200

    implement collect tests for result optional

--- src/result/tests/from_id/bin_m.rs
+++ src/result/tests/from_id/bin_m.rs
@@ -1,7 +1,12 @@
+use crate::collectables::par_col_into_test::{ColIntoMode, ParCollectIntoTest};
 use crate::parameters::IterationOrder;
 use crate::result::tests::utils::inputs_res;
 use crate::*;
+use orx_fixed_vec::FixedVec;
+use orx_split_vec::SplitVec;
 use std::vec;
+use std::vec::Vec;
+use test_case::test_matrix;
 
 const N: usize = 157;
 
@@ -57,5 +62,70 @@ fn bin_m_reduce_err() {
             true => b,
             false => a,
         });
+    assert_eq!(result, Err(vec!['a']));
+}
+
+#[test_matrix(
+    [Vec::new(), SplitVec::with_doubling_growth(), SplitVec::with_linear_growth(6), FixedVec::new(40)],
+    [ColIntoMode::Col, ColIntoMode::ColIntoEmpty, ColIntoMode::ColIntoFilled(N / 5)],
+    [IterationOrder::Ordered, IterationOrder::Arbitrary]
+)]
+fn bin_m_collect_ok<C: ParCollectIntoTest<u64>>(_: C, mode: ColIntoMode, order: IterationOrder) {
+    let expected = C::expected(
+        mode,
+        |i| i as u64,
+        inputs_res(N, None)
+            .into_par()
+            .fallible_result()
+            .filter(|x| x.len() < 4)
+            .map(|x| x.parse::<u64>().unwrap())
+            .iteration_order(order)
+            .collect::<std::vec::Vec<_>>()
+            .unwrap(),
+    );
+
+    let result = match C::init_result(mode, |i| i as u64) {
+        Some(c) => inputs_res(N, None)
+            .into_par()
+            .fallible_result()
+            .filter(|x| x.len() < 4)
+            .map(|x| x.parse::<u64>().unwrap())
+            .iteration_order(order)
+            .collect_into(c),
+        None => inputs_res(N, None)
+            .into_par()
+            .fallible_result()
+            .filter(|x| x.len() < 4)
+            .map(|x| x.parse::<u64>().unwrap())
+            .iteration_order(order)
+            .collect(),
+    };
+
+    C::assert_eq(result.unwrap(), expected, order);
+}
+
+#[test_matrix(
+[Vec::new(), SplitVec::with_doubling_growth(), SplitVec::with_linear_growth(6), FixedVec::new(40)],
+[ColIntoMode::Col, ColIntoMode::ColIntoEmpty, ColIntoMode::ColIntoFilled(N / 5)],
+[IterationOrder::Ordered, IterationOrder::Arbitrary]
+)]
+fn bin_m_collect_err<C: ParCollectIntoTest<u64>>(_: C, mode: ColIntoMode, order: IterationOrder) {
+    let result = match C::init_result(mode, |i| i as u64) {
+        Some(c) => inputs_res(N, Some(42))
+            .into_par()
+            .fallible_result()
+            .filter(|x| x.len() < 4)
+            .map(|x| x.parse::<u64>().unwrap())
+            .iteration_order(order)
+            .collect_into(c),
+        None => inputs_res(N, Some(42))
+            .into_par()
+            .fallible_result()
+            .filter(|x| x.len() < 4)
+            .map(|x| x.parse::<u64>().unwrap())
+            .iteration_order(order)
+            .collect(),
+    };
+
     assert_eq!(result, Err(vec!['a']));
 }
