20d78cbc2844d6ebcc6dc8ec54e753213138a041
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 13:23:21 +0200

    ParCollectInto requires collection with use computations

--- src/collectables/par_col_into.rs
+++ src/collectables/par_col_into.rs
@@ -1,9 +1,20 @@
-use crate::collectables::{inf::ColIntoInf, opt::ColIntoOpt, res::ColIntoRes};
+use crate::collectables::{
+    inf::ColIntoInf, inf_use::ColIntoInfUse, opt::ColIntoOpt, opt_use::ColIntoOptUse,
+    res::ColIntoRes, res_use::ColIntoResUse,
+};
 use alloc::vec::Vec;
 use orx_fixed_vec::FixedVec;
 use orx_split_vec::{Doubling, Linear, Recursive, SplitVec};
 
-pub trait ParCollectInto<T>: ColIntoInf<T> + ColIntoRes<T> + ColIntoOpt<T> {}
+pub trait ParCollectInto<T>:
+    ColIntoInf<T>
+    + ColIntoRes<T>
+    + ColIntoOpt<T>
+    + ColIntoInfUse<T>
+    + ColIntoResUse<T>
+    + ColIntoOptUse<T>
+{
+}
 
 impl<T> ParCollectInto<T> for FixedVec<T> {}
 
