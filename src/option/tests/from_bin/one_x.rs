796620d9ad4deaf9223833afb6355bc84e7f18a9
orxfun <orx.ugur.arikan@gmail.com>
2026-04-10 14:26:56 +0200

    add option collect tests

--- src/option/tests/from_bin/one_x.rs
+++ src/option/tests/from_bin/one_x.rs
@@ -1,6 +1,11 @@
+use crate::collectables::par_col_into_test::{ColIntoMode, ParCollectIntoTest};
 use crate::option::tests::utils::inputs;
 use crate::parameters::IterationOrder;
 use crate::*;
+use alloc::vec::Vec;
+use orx_fixed_vec::FixedVec;
+use orx_split_vec::SplitVec;
+use test_case::test_matrix;
 
 const N: usize = 157;
 
@@ -86,41 +91,111 @@ fn one_x_reduce_err() {
     assert_eq!(result, None);
 }
 
-#[test]
-fn one_x_collect_ok() {
-    let inputs = inputs(N);
-    let result: Option<std::vec::Vec<u64>> = inputs
-        .into_par()
-        .filter_map(|x| match x.as_str() == "7" {
-            true => None,
-            false => Some(Some(x)),
-        })
-        .fallible_option()
-        .flat_map(|x| {
-            let a = x.parse::<u64>().unwrap();
-            (0..5).map(move |i| a + i)
-        })
-        .collect::<std::vec::Vec<_>>();
-    assert!(result.is_some());
+#[test_matrix(
+    [Vec::new(), SplitVec::with_doubling_growth(), SplitVec::with_linear_growth(6), FixedVec::new(40)],
+    [ColIntoMode::Col, ColIntoMode::ColIntoEmpty, ColIntoMode::ColIntoFilled(N / 5)],
+    [IterationOrder::Ordered, IterationOrder::Arbitrary]
+)]
+fn one_x_collect_ok<C: ParCollectIntoTest<u64>>(_: C, mode: ColIntoMode, order: IterationOrder) {
+    let expected = C::expected(
+        mode,
+        |i| i as u64,
+        inputs(N)
+            .into_par()
+            .filter_map(|x| match x.as_str() == "7" {
+                true => None,
+                false => Some(Some(x)),
+            })
+            .fallible_option()
+            .flat_map(|x| {
+                let a = x.parse::<u64>().unwrap();
+                (0..5).map(move |i| a + i)
+            })
+            .iteration_order(order)
+            .collect::<std::vec::Vec<_>>()
+            .unwrap(),
+    );
+
+    let result = match C::init_result(mode, |i| i as u64) {
+        Some(c) => inputs(N)
+            .into_par()
+            .filter_map(|x| match x.as_str() == "7" {
+                true => None,
+                false => Some(Some(x)),
+            })
+            .fallible_option()
+            .flat_map(|x| {
+                let a = x.parse::<u64>().unwrap();
+                (0..5).map(move |i| a + i)
+            })
+            .iteration_order(order)
+            .collect_into(c),
+        None => inputs(N)
+            .into_par()
+            .filter_map(|x| match x.as_str() == "7" {
+                true => None,
+                false => Some(Some(x)),
+            })
+            .fallible_option()
+            .flat_map(|x| {
+                let a = x.parse::<u64>().unwrap();
+                (0..5).map(move |i| a + i)
+            })
+            .iteration_order(order)
+            .collect(),
+    };
+
+    C::assert_eq(result.unwrap(), expected, order);
 }
 
-#[test]
-fn one_x_collect_err() {
-    let inputs = inputs(N);
-    let result: Option<std::vec::Vec<u64>> = inputs
-        .into_par()
-        .filter_map(|x| match x.as_str() == "7" {
-            true => None,
-            false => Some(match x.as_str() == "42" {
-                true => Some(x),
-                false => None,
-            }),
-        })
-        .fallible_option()
-        .flat_map(|x| {
-            let a = x.parse::<u64>().unwrap();
-            (0..5).map(move |i| a + i)
-        })
-        .collect::<std::vec::Vec<_>>();
-    assert_eq!(result, None);
+mod one_x_collect_err_matrix {
+    use super::*;
+
+    #[test_matrix(
+    [Vec::new(), SplitVec::with_doubling_growth(), SplitVec::with_linear_growth(6), FixedVec::new(40)],
+    [ColIntoMode::Col, ColIntoMode::ColIntoEmpty, ColIntoMode::ColIntoFilled(N / 5)],
+    [IterationOrder::Ordered, IterationOrder::Arbitrary]
+)]
+    fn one_x_collect_err<C: ParCollectIntoTest<u64>>(
+        _: C,
+        mode: ColIntoMode,
+        order: IterationOrder,
+    ) {
+        let result = match C::init_result(mode, |i| i as u64) {
+            Some(c) => inputs(N)
+                .into_par()
+                .filter_map(|x| match x.as_str() == "7" {
+                    true => None,
+                    false => Some(match x.as_str() == "42" {
+                        true => Some(x),
+                        false => None,
+                    }),
+                })
+                .fallible_option()
+                .flat_map(|x| {
+                    let a = x.parse::<u64>().unwrap();
+                    (0..5).map(move |i| a + i)
+                })
+                .iteration_order(order)
+                .collect_into(c),
+            None => inputs(N)
+                .into_par()
+                .filter_map(|x| match x.as_str() == "7" {
+                    true => None,
+                    false => Some(match x.as_str() == "42" {
+                        true => Some(x),
+                        false => None,
+                    }),
+                })
+                .fallible_option()
+                .flat_map(|x| {
+                    let a = x.parse::<u64>().unwrap();
+                    (0..5).map(move |i| a + i)
+                })
+                .iteration_order(order)
+                .collect(),
+        };
+
+        assert_eq!(result, None);
+    }
 }
--- src/option/tests/from_many/one_m.rs
+++ src/option/tests/from_many/one_m.rs
@@ -1,6 +1,11 @@
+use crate::collectables::par_col_into_test::{ColIntoMode, ParCollectIntoTest};
 use crate::option::tests::utils::inputs;
 use crate::parameters::IterationOrder;
 use crate::*;
+use alloc::vec::Vec;
+use orx_fixed_vec::FixedVec;
+use orx_split_vec::SplitVec;
+use test_case::test_matrix;
 
 const N: usize = 157;
 
@@ -62,29 +67,81 @@ fn one_m_reduce_err() {
     assert_eq!(result, None);
 }
 
-#[test]
-fn one_m_collect_ok() {
-    let inputs = inputs(N);
-    let result: Option<std::vec::Vec<u64>> = inputs
-        .into_par()
-        .flat_map(|x| [x.clone(), x.clone(), x].map(Some))
-        .fallible_option()
-        .map(|x| x.parse::<u64>().unwrap())
-        .collect::<std::vec::Vec<_>>();
-    assert!(result.is_some());
+#[test_matrix(
+    [Vec::new(), SplitVec::with_doubling_growth(), SplitVec::with_linear_growth(6), FixedVec::new(40)],
+    [ColIntoMode::Col, ColIntoMode::ColIntoEmpty, ColIntoMode::ColIntoFilled(N / 5)],
+    [IterationOrder::Ordered, IterationOrder::Arbitrary]
+)]
+fn one_m_collect_ok<C: ParCollectIntoTest<u64>>(_: C, mode: ColIntoMode, order: IterationOrder) {
+    let expected = C::expected(
+        mode,
+        |i| i as u64,
+        inputs(N)
+            .into_par()
+            .flat_map(|x| [x.clone(), x.clone(), x].map(Some))
+            .fallible_option()
+            .map(|x| x.parse::<u64>().unwrap())
+            .iteration_order(order)
+            .collect::<std::vec::Vec<_>>()
+            .unwrap(),
+    );
+
+    let result = match C::init_result(mode, |i| i as u64) {
+        Some(c) => inputs(N)
+            .into_par()
+            .flat_map(|x| [x.clone(), x.clone(), x].map(Some))
+            .fallible_option()
+            .map(|x| x.parse::<u64>().unwrap())
+            .iteration_order(order)
+            .collect_into(c),
+        None => inputs(N)
+            .into_par()
+            .flat_map(|x| [x.clone(), x.clone(), x].map(Some))
+            .fallible_option()
+            .map(|x| x.parse::<u64>().unwrap())
+            .iteration_order(order)
+            .collect(),
+    };
+
+    C::assert_eq(result.unwrap(), expected, order);
 }
 
-#[test]
-fn one_m_collect_err() {
-    let inputs = inputs(N);
-    let result: Option<std::vec::Vec<u64>> = inputs
-        .into_par()
-        .flat_map(|x| match x.as_str() == "42" {
-            true => [x.clone(), x.clone(), x].map(Some),
-            false => [None, None, None],
-        })
-        .fallible_option()
-        .map(|x| x.parse::<u64>().unwrap())
-        .collect::<std::vec::Vec<_>>();
-    assert_eq!(result, None);
+mod one_m_collect_err_matrix {
+    use super::*;
+
+    #[test_matrix(
+    [Vec::new(), SplitVec::with_doubling_growth(), SplitVec::with_linear_growth(6), FixedVec::new(40)],
+    [ColIntoMode::Col, ColIntoMode::ColIntoEmpty, ColIntoMode::ColIntoFilled(N / 5)],
+    [IterationOrder::Ordered, IterationOrder::Arbitrary]
+)]
+    fn one_m_collect_err<C: ParCollectIntoTest<u64>>(
+        _: C,
+        mode: ColIntoMode,
+        order: IterationOrder,
+    ) {
+        let result = match C::init_result(mode, |i| i as u64) {
+            Some(c) => inputs(N)
+                .into_par()
+                .flat_map(|x| match x.as_str() == "42" {
+                    true => [x.clone(), x.clone(), x].map(Some),
+                    false => [None, None, None],
+                })
+                .fallible_option()
+                .map(|x| x.parse::<u64>().unwrap())
+                .iteration_order(order)
+                .collect_into(c),
+            None => inputs(N)
+                .into_par()
+                .flat_map(|x| match x.as_str() == "42" {
+                    true => [x.clone(), x.clone(), x].map(Some),
+                    false => [None, None, None],
+                })
+                .fallible_option()
+                .map(|x| x.parse::<u64>().unwrap())
+                .iteration_order(order)
+                .collect(),
+        };
+
+        assert_eq!(result, None);
+    }
 }
