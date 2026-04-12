1859a83a341ccfbd2378563e07811f65b78b8eea
orxfun <orx.ugur.arikan@gmail.com>
2026-04-12 20:06:28 +0200

    implement IntoRecParIter for all iterators

--- src/into_parallel/into_rec_par_iter.rs
+++ src/into_parallel/into_rec_par_iter.rs
@@ -1,6 +1,5 @@
 use crate::infallible::{Par, xap_variants::Id};
 use crate::runner::default_runner;
-use orx_concurrent_iter::IntoConcurrentIter;
 use orx_concurrent_recursive_iter::{ConcurrentRecursiveIter, Queue};
 
 pub trait IntoRecParIter
@@ -15,4 +14,25 @@ where
     ) -> Par<ConcurrentRecursiveIter<Self::Item, F>, Id<Self::Item>>
     where
         F: Fn(&Self::Item, &Queue<Self::Item>) + Sync;
+}
+
+impl<X> IntoRecParIter for X
+where
+    X: IntoIterator,
+    X::Item: Send,
+{
+    fn into_rec_par<F>(
+        self,
+        extend: F,
+        exact_len: Option<usize>,
+    ) -> Par<ConcurrentRecursiveIter<Self::Item, F>, Id<Self::Item>>
+    where
+        F: Fn(&Self::Item, &Queue<Self::Item>) + Sync,
+    {
+        let iter = match exact_len {
+            Some(exact_len) => ConcurrentRecursiveIter::new_exact(self, extend, exact_len),
+            None => ConcurrentRecursiveIter::new(self, extend),
+        };
+        Par::new(iter, Id::new(), default_runner(), Default::default())
+    }
 }
