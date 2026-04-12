fa263993ceebbc8358b1f5df0cf64bfc116d7e0b
orxfun <orx.ugur.arikan@gmail.com>
2026-04-12 18:59:13 +0200

    define thread collect for option compute

--- src/option/thread_execution/collect.rs
+++ src/option/thread_execution/collect.rs
@@ -1,6 +1,6 @@
 use crate::option::size_pairs::SizePairOpt;
-use crate::{infallible::Xap, results::ValIdx, runner::ParRunner};
-use alloc::vec::Vec;
+use crate::results::ValsAndIdx;
+use crate::{infallible::Xap, runner::ParRunner};
 use orx_concurrent_iter::{ChunkPuller, ConcurrentIter};
 
 pub fn collect<Q, I, M, X1, X2, S>(
@@ -10,7 +10,7 @@ pub fn collect<Q, I, M, X1, X2, S>(
     iter: &I,
     x1: X1,
     x2: X2,
-) -> Option<Vec<ValIdx<X2::O>>>
+) -> Option<ValsAndIdx<X2::O>>
 where
     Q: ParRunner,
     I: ConcurrentIter,
@@ -18,8 +18,8 @@ where
     X2: Xap<I = M>,
     S: SizePairOpt<S1 = X1::Size, S2 = X2::Size>,
 {
-    let mut collected = Vec::new();
-    let vec = &mut collected;
+    let mut collected = ValsAndIdx::new();
+    let out = &mut collected;
 
     let mut chunk_puller = iter.chunk_puller(0);
     let mut item_puller = iter.item_puller_with_idx();
@@ -31,14 +31,10 @@ where
         match chunk_size {
             0 | 1 => match item_puller.next() {
                 Some((idx, i)) => {
-                    for a in S::xap_opt(x1, x2, i) {
-                        match a {
-                            Some(a) => vec.push(ValIdx::new(a, idx)),
-                            None => {
-                                Q::broadcast_stop(iter, state, chunk_state);
-                                return None;
-                            }
-                        }
+                    let failed = out.extend_opt(idx, S::xap_opt(x1, x2, i));
+                    if failed {
+                        Q::broadcast_stop(iter, state, chunk_state);
+                        return None;
                     }
                 }
                 None if iter.is_completed_when_none_returned() => break,
@@ -51,14 +47,11 @@ where
 
                 match chunk_puller.pull_with_idx() {
                     Some((idx, chunk)) => {
-                        for a in chunk.flat_map(|i| S::xap_opt(x1, x2, i)) {
-                            match a {
-                                Some(a) => vec.push(ValIdx::new(a, idx)),
-                                None => {
-                                    Q::broadcast_stop(iter, state, chunk_state);
-                                    return None;
-                                }
-                            }
+                        let values = chunk.flat_map(|i| S::xap_opt(x1, x2, i));
+                        let failed = out.extend_opt(idx, values);
+                        if failed {
+                            Q::broadcast_stop(iter, state, chunk_state);
+                            return None;
                         }
                     }
                     None if iter.is_completed_when_none_returned() => break,
