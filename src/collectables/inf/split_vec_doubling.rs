87d05be6d7fcb16f78a5c146a082b956dd6bffad
orxfun <orx.ugur.arikan@gmail.com>
2026-04-12 18:30:31 +0200

    revise ColIntoInf

--- src/collectables/inf/split_vec_doubling.rs
+++ src/collectables/inf/split_vec_doubling.rs
@@ -1,5 +1,7 @@
+use crate::collectables::alg::merge_collected::{
+    merge_arb_into_split_vec, merge_ord_into_split_vec,
+};
 use crate::collectables::inf::ColIntoInf;
-use crate::collectables::alg::merge_collected::{merge_arb_into_split_vec, merge_ord_into};
 use crate::infallible::ParRunnerInfallible;
 use crate::infallible::{Par, Xap};
 use crate::runner::ParRunner;
@@ -7,20 +9,6 @@ use orx_concurrent_iter::ConcurrentIter;
 use orx_split_vec::{Doubling, SplitVec};
 
 impl<T> ColIntoInf<T> for SplitVec<T, Doubling> {
-    fn inf_col_into<I, X, R>(dst: Option<Self>, par: Par<I, X, R>) -> Self
-    where
-        I: ConcurrentIter,
-        X: Xap<I = I::Item, O = T>,
-        R: ParRunner,
-        T: Send,
-    {
-        let (iter, x, mut exe, params) = par.destruct();
-        let results = exe.collect(params, iter, x);
-
-        let dst = dst.unwrap_or_else(|| Self::with_doubling_growth());
-        merge_ord_into(results, dst)
-    }
-
     fn inf_arb_col_into<I, X, R>(dst: Option<Self>, par: Par<I, X, R>) -> Self
     where
         I: ConcurrentIter,
@@ -40,6 +28,8 @@ impl<T> ColIntoInf<T> for SplitVec<T, Doubling> {
         R: ParRunner,
         T: Send,
     {
-        todo!()
+        let (iter, x, mut exe, params) = par.destruct();
+        let results = exe.collect_new(params, iter, x);
+        merge_ord_into_split_vec(results, dst)
     }
 }
