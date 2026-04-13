14ba38ca94c6a7133723768aac03c15ed356fe14
orxfun <orx.ugur.arikan@gmail.com>
2026-04-13 09:14:40 +0200

    init diagnostics example

---
+++ examples/diagnostics.rs
@@ -0,0 +1,27 @@
+use orx_parallel::*;
+
+fn main() {
+    let sum = (1..1000)
+        .par()
+        .map(|i| 2 * i)
+        .flat_map(|i| match i.is_multiple_of(2) {
+            true => [i, i + 1, i + 2, i + 3, i + 4],
+            false => [i, i + 1, usize::MAX, usize::MAX, usize::MAX],
+        })
+        .filter_map(|i| (i < usize::MAX).then_some(i))
+        .reduce(|a, b| a + b)
+        .unwrap();
+    println!("sum with default runner = {sum}");
+
+    let sum = (1..1000)
+        .par()
+        .map(|i| 2 * i)
+        .flat_map(|i| match i.is_multiple_of(2) {
+            true => [i, i + 1, i + 2, i + 3, i + 4],
+            false => [i, i + 1, usize::MAX, usize::MAX, usize::MAX],
+        })
+        .filter_map(|i| (i < usize::MAX).then_some(i))
+        .reduce(|a, b| a + b)
+        .unwrap();
+    println!("sum using runner with diagnostics = {sum}");
+}
