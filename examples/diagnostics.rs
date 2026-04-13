8a3659d6cfdc6f0d0d3376b4ea1afe076f678fb9
orxfun <orx.ugur.arikan@gmail.com>
2026-04-13 12:01:17 +0200

    revise diagnostics

--- examples/diagnostics.rs
+++ examples/diagnostics.rs
@@ -1,27 +1,29 @@
 use orx_parallel::*;
 
 fn main() {
-    let sum = (1..1000)
+    let n = 1_000_000;
+
+    let sum = (1..n)
         .par()
         .map(|i| 2 * i)
-        .flat_map(|i| match i.is_multiple_of(2) {
-            true => [i, i + 1, i + 2, i + 3, i + 4],
-            false => [i, i + 1, usize::MAX, usize::MAX, usize::MAX],
-        })
-        .filter_map(|i| (i < usize::MAX).then_some(i))
+        // .flat_map(|i| match i.is_multiple_of(2) {
+        //     true => [i, i + 1, i + 2, i + 3, i + 4],
+        //     false => [i, i + 1, usize::MAX, usize::MAX, usize::MAX],
+        // })
+        // .filter_map(|i| (i < usize::MAX).then_some(i))
         .reduce(|a, b| a + b)
         .unwrap();
     println!("sum with default runner = {sum}");
 
-    let sum = (1..1000)
+    let sum = (1..n)
         .par()
         .runner_with_diagnostics()
         .map(|i| 2 * i)
-        .flat_map(|i| match i.is_multiple_of(2) {
-            true => [i, i + 1, i + 2, i + 3, i + 4],
-            false => [i, i + 1, usize::MAX, usize::MAX, usize::MAX],
-        })
-        .filter_map(|i| (i < usize::MAX).then_some(i))
+        // .flat_map(|i| match i.is_multiple_of(2) {
+        //     true => [i, i + 1, i + 2, i + 3, i + 4],
+        //     false => [i, i + 1, usize::MAX, usize::MAX, usize::MAX],
+        // })
+        // .filter_map(|i| (i < usize::MAX).then_some(i))
         .reduce(|a, b| a + b)
         .unwrap();
     println!("sum using runner with diagnostics = {sum}");
