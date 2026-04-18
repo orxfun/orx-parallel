f0217b09f1e3d776c7f57d06f9fc65e287db7dd6
orxfun <orx.ugur.arikan@gmail.com>
2026-04-18 20:16:21 +0200

    add busy-idle distrubiton to diagnostics

--- examples/recursive/node.rs
+++ examples/recursive/node.rs
@@ -10,7 +10,7 @@ impl Node {
     pub fn compute(&self, amount_of_work: usize) -> u64 {
         (0..amount_of_work)
             .map(|j| {
-                let n = core::hint::black_box(40 + self.id as u64 + j as u64);
+                let n = core::hint::black_box((self.id as u64 + j as u64) % 40);
                 let mut a = 0;
                 let mut b = 1;
                 for _ in 0..n {
