fa22219cfaec2d1d2ecc3aaac0f443615d3980a9
orxfun <orx.ugur.arikan@gmail.com>
2026-04-18 21:08:54 +0200

    extend recursive example by allowing diagnostics through arguments

--- examples/recursive/main.rs
+++ examples/recursive/main.rs
@@ -19,10 +19,25 @@ mod utils;
 struct Args {
     /// Amount of work (num times Fibonacci will be repeated).
     #[arg(long, default_value_t = 1)]
-    amount_of_work: usize,
+    work: usize,
+    /// (only for orx) Number of threads (0 = auto)
+    #[arg(long, default_value_t = 0)]
+    num_threads: usize,
+    /// (only for orx) Chunk size (0 = auto)
+    #[arg(long, default_value_t = 0)]
+    chunk_size: usize,
+    /// (only for orx, try)
+    /// When true, first extends & discovers all tasks and then executes.
+    /// When false, extension and execution happen together on the fly.
+    #[arg(long, default_value_t = false)]
+    extended: bool,
+    /// When true, only orx variant will execute with defined settings reporting diagnostics.
+    /// When false, all variants will be executed and reported.
+    #[arg(long, default_value_t = false)]
+    diagnostics: bool,
 }
 
-fn seq(storage: &NodesStorage, roots: &[&Node], amount_of_work: usize) -> (u64, usize) {
+fn seq(storage: &NodesStorage, roots: &[&Node], args: &Args) -> (u64, usize) {
     let mut status = NodeStatusSeq::new(storage.all_nodes.len(), roots);
 
     let tasks: ImpVec<_> = roots.iter().copied().collect();
@@ -43,7 +58,7 @@ fn seq(storage: &NodesStorage, roots: &[&Node], amount_of_work: usize) -> (u64, 
                     }
 
                     // process
-                    let value = node.compute(amount_of_work);
+                    let value = node.compute(args.work);
                     sum += value;
                 }
             }
@@ -53,13 +68,13 @@ fn seq(storage: &NodesStorage, roots: &[&Node], amount_of_work: usize) -> (u64, 
     (sum, status.num_processed())
 }
 
-fn rayon(storage: &NodesStorage, roots: &[&Node], amount_of_work: usize) -> (u64, usize) {
+fn rayon(storage: &NodesStorage, roots: &[&Node], args: &Args) -> (u64, usize) {
     fn spawn_job<'a>(
         scope: &Scope<'a>,
         storage: &'a NodesStorage,
         status: &'a NodeStatusPar,
         node: &'a Node,
-        amount_of_work: usize,
+        args: &'a Args,
         sum: &'a AtomicU64,
     ) {
         scope.spawn(move |scope| {
@@ -67,11 +82,11 @@ fn rayon(storage: &NodesStorage, roots: &[&Node], amount_of_work: usize) -> (u64
                 for s in &node.children_symbols {
                     let child = storage.get_relevant_node(s);
                     if status.load_child(child) {
-                        spawn_job(scope, storage, status, child, amount_of_work, sum);
+                        spawn_job(scope, storage, status, child, args, sum);
                     }
                 }
 
-                let value = node.compute(amount_of_work);
+                let value = node.compute(args.work);
                 sum.fetch_add(value, Ordering::Relaxed);
             }
         });
@@ -91,19 +106,14 @@ fn rayon(storage: &NodesStorage, roots: &[&Node], amount_of_work: usize) -> (u64
 
     pool.scope(|scope| {
         for root in roots {
-            spawn_job(scope, storage, &status, root, amount_of_work, &sum);
+            spawn_job(scope, storage, &status, root, args, &sum);
         }
     });
 
     (sum.load(Ordering::Relaxed), status.num_processed())
 }
 
-pub fn orx(
-    storage: &NodesStorage,
-    roots: &[&Node],
-    amount_of_work: usize,
-    exact: bool,
-) -> (u64, usize) {
+fn orx_rec(storage: &NodesStorage, roots: &[&Node], args: &Args, exact: bool) -> (u64, usize) {
     fn get_extend<'x, 'b>(
         storage: &'x NodesStorage,
         status: &'x NodeStatusPar,
@@ -132,7 +142,43 @@ pub fn orx(
         .iter()
         .copied()
         .into_par_recursive(extend, exact_len)
-        .map(|x| x.compute(amount_of_work))
+        .map(|x| x.compute(args.work))
+        .reduce(|a, b| a + b)
+        .unwrap();
+
+    (sum, status.num_processed())
+}
+
+fn orx_extended(storage: &NodesStorage, roots: &[&Node], args: &Args, exact: bool) -> (u64, usize) {
+    fn get_extend<'x, 'b>(
+        storage: &'x NodesStorage,
+        status: &'x NodeStatusPar,
+    ) -> impl Fn(&&'b Node, &Queue<&'b Node>)
+    where
+        'x: 'b,
+    {
+        |node: &&'b Node, queue: &Queue<&'b Node>| {
+            if status.start_processing(node) {
+                for s in &node.children_symbols {
+                    let child = storage.get_relevant_node(s);
+                    if status.load_child(child) {
+                        queue.push(child);
+                    }
+                }
+            }
+        }
+    }
+
+    let status = NodeStatusPar::new(storage.all_nodes.len(), roots);
+    let extend = get_extend(storage, &status);
+
+    let exact_len = exact.then_some(storage.all_nodes.len());
+
+    let sum = roots
+        .iter()
+        .copied()
+        .extend_into_par(extend, exact_len)
+        .map(|x| x.compute(args.work))
         .reduce(|a, b| a + b)
         .unwrap();
 
@@ -140,7 +186,7 @@ pub fn orx(
 }
 
 fn main() {
-    let work = Args::parse().amount_of_work;
+    let args = Args::parse();
     let seed = 42;
 
     let log = |(sum, count): &(u64, usize)| println!("  count = {count}\n  sum = {sum}");
@@ -150,23 +196,42 @@ fn main() {
     let storage = NodesStorage::new(10_000, &mut rng);
     let roots = storage.get_roots(20, &mut rng);
 
-    _ = run("seq", || seq(&storage, &roots, work), log);
-    _ = run("orx", || orx(&storage, &roots, work, false), log);
-    _ = run("rayon", || rayon(&storage, &roots, work), log);
-    _ = run("orx_exact", || orx(&storage, &roots, work, true), log);
-
-    // _ = run(
-    //     "orx_diagnostics",
-    //     || orx_diagnostics(&storage, &roots, work, true),
-    //     log,
-    // );
+    match args.diagnostics {
+        true => {
+            _ = run(
+                "orx_diagnostics",
+                || orx_diagnostics(&storage, &roots, &args, true),
+                log,
+            );
+        }
+        false => {
+            _ = run("seq", || seq(&storage, &roots, &args), log);
+            _ = run("rayon", || rayon(&storage, &roots, &args), log);
+            _ = run("orx_rec", || orx_rec(&storage, &roots, &args, false), log);
+            _ = run(
+                "orx_rec_exact",
+                || orx_rec(&storage, &roots, &args, true),
+                log,
+            );
+            _ = run(
+                "orx_extended",
+                || orx_extended(&storage, &roots, &args, false),
+                log,
+            );
+            _ = run(
+                "orx_extended_exact",
+                || orx_extended(&storage, &roots, &args, true),
+                log,
+            );
+        }
+    }
 }
 
 #[allow(dead_code)]
 fn orx_diagnostics(
     storage: &NodesStorage,
     roots: &[&Node],
-    amount_of_work: usize,
+    args: &Args,
     exact: bool,
 ) -> (u64, usize) {
     fn get_extend<'x, 'b>(
@@ -192,15 +257,25 @@ fn orx_diagnostics(
     let extend = get_extend(storage, &status);
 
     let exact_len = exact.then_some(storage.all_nodes.len());
-    let sum = roots
-        .iter()
-        .copied()
-        .into_par_recursive(extend, exact_len)
-        .num_threads(4)
-        .map(|x| x.compute(amount_of_work))
-        .runner_with_diagnostics()
-        .reduce(|a, b| a + b)
-        .unwrap();
+    let input = roots.iter().copied();
+    let sum = match args.extended {
+        true => input
+            .extend_into_par(extend, exact_len)
+            .num_threads(args.num_threads)
+            .chunk_size(args.chunk_size)
+            .map(|x| x.compute(args.work))
+            .runner_with_diagnostics()
+            .reduce(|a, b| a + b)
+            .unwrap(),
+        false => input
+            .into_par_recursive(extend, exact_len)
+            .num_threads(args.num_threads)
+            .chunk_size(args.chunk_size)
+            .map(|x| x.compute(args.work))
+            .runner_with_diagnostics()
+            .reduce(|a, b| a + b)
+            .unwrap(),
+    };
 
     (sum, status.num_processed())
 }
