2f7d6c9242af9a3c753aa8ba30504276767ced63
orxfun <orx.ugur.arikan@gmail.com>
2026-04-18 21:51:04 +0200

    revise example computation

--- examples/recursive/node.rs
+++ examples/recursive/node.rs
@@ -8,18 +8,18 @@ pub struct Node {
 impl Node {
     /// Fibonacci as example computation on each of the node values.
     pub fn compute(&self, amount_of_work: usize) -> u64 {
-        (0..amount_of_work)
-            .map(|j| {
-                let n = core::hint::black_box((self.id as u64 + j as u64) % 40);
-                let mut a = 0;
-                let mut b = 1;
-                for _ in 0..n {
-                    let c = a + b;
-                    a = b;
-                    b = c;
-                }
-                a
-            })
-            .sum()
+        let iter = Box::new(core::hint::black_box(0..amount_of_work));
+        iter.map(|j| {
+            let n = core::hint::black_box((self.id as u64 + j as u64) % 40);
+            let mut a = 0;
+            let mut b = 1;
+            for _ in 0..n {
+                let c = core::hint::black_box(a + b);
+                a = b;
+                b = c;
+            }
+            a
+        })
+        .sum()
     }
 }
