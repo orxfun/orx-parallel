f3e96dc3cc917e33c46dc5df3ef7ab7051c1b225
orxfun <orx.ugur.arikan@gmail.com>
2026-04-19 08:10:09 +0200

    export ParIter

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,7 @@ mod runner;
 mod sizes;
 
 pub use collectables::ParCollectInto;
+pub use infallible::ParIter;
 pub use into_parallel::{
     IntoParIter, IntoParIterRecursive, IterIntoParIter, ParCol, ParColMut, ParDrain, Parallelizable,
 };
