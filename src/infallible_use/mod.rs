3f5d7d621e3b92f3b0a5640b9bcef67ca1f3acc1
orxfun <orx.ugur.arikan@gmail.com>
2026-04-20 21:45:47 +0200

    define ParUseIterCore

--- src/infallible_use/mod.rs
+++ src/infallible_use/mod.rs
@@ -4,6 +4,7 @@ mod tests;
 pub mod fun;
 mod par;
 mod par_iter;
+mod par_iter_core;
 mod par_runner;
 mod sizes;
 mod thread_execution;
@@ -14,6 +15,7 @@ pub mod xap_variants;
 
 pub use par::ParUse;
 pub use par_iter::ParUseIter;
+pub use par_iter_core::ParUseIterCore;
 pub use par_runner::ParRunnerInfallibleUse;
 pub use sizes::SizeInfUse;
 pub use use_var::{Use, UseClone, UseFun};
