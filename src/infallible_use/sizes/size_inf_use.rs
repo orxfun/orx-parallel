ffa9ee3a3b0b7e87e03a0bac2d72372284b2e715
orxfun <orx.ugur.arikan@gmail.com>
2026-04-20 22:26:20 +0200

    extend Size trait with infallible transformations

--- src/infallible_use/sizes/size_inf_use.rs
+++ src/infallible_use/sizes/size_inf_use.rs
@@ -6,53 +6,53 @@ use crate::sizes::Size;
 pub trait SizeInfUse: Size {
     // transformations
 
-    type Map<X, Q, H>: XapUse<U = X::U, I = X::I, O = Q, Size = Self>
+    type UMap<X, Q, H>: XapUse<U = X::U, I = X::I, O = Q, Size = Self>
     where
         X: XapUse<Size = Self>,
         H: Fn(&mut X::U, X::O) -> Q + Copy + Send;
 
-    fn map<X, Q, H>(x: X, h: H) -> Self::Map<X, Q, H>
+    fn u_map<X, Q, H>(x: X, h: H) -> Self::UMap<X, Q, H>
     where
         X: XapUse<Size = Self>,
         H: Fn(&mut X::U, X::O) -> Q + Copy + Send;
 
-    type Inspect<X, H>: XapUse<U = X::U, I = X::I, O = X::O, Size = Self>
+    type UInspect<X, H>: XapUse<U = X::U, I = X::I, O = X::O, Size = Self>
     where
         X: XapUse<Size = Self>,
         H: Fn(&mut X::U, &X::O) + Copy + Send;
 
-    fn inspect<X, H>(x: X, h: H) -> Self::Inspect<X, H>
+    fn u_inspect<X, H>(x: X, h: H) -> Self::UInspect<X, H>
     where
         X: XapUse<Size = Self>,
         H: Fn(&mut X::U, &X::O) + Copy + Send;
 
-    type Filter<X, H>: XapUse<U = X::U, I = X::I, O = X::O, Size = Self::ThenBin>
+    type UFilter<X, H>: XapUse<U = X::U, I = X::I, O = X::O, Size = Self::ThenBin>
     where
         X: XapUse<Size = Self>,
         H: Fn(&mut X::U, &X::O) -> bool + Copy + Send;
 
-    fn filter<X, H>(x: X, h: H) -> Self::Filter<X, H>
+    fn u_filter<X, H>(x: X, h: H) -> Self::UFilter<X, H>
     where
         X: XapUse<Size = Self>,
         H: Fn(&mut X::U, &X::O) -> bool + Copy + Send;
 
-    type FilterMap<X, Q, H>: XapUse<U = X::U, I = X::I, O = Q, Size = Self::ThenBin>
+    type UFilterMap<X, Q, H>: XapUse<U = X::U, I = X::I, O = Q, Size = Self::ThenBin>
     where
         X: XapUse<Size = Self>,
         H: Fn(&mut X::U, X::O) -> Option<Q> + Copy + Send;
 
-    fn filter_map<X, Q, H>(x: X, h: H) -> Self::FilterMap<X, Q, H>
+    fn u_filter_map<X, Q, H>(x: X, h: H) -> Self::UFilterMap<X, Q, H>
     where
         X: XapUse<Size = Self>,
         H: Fn(&mut X::U, X::O) -> Option<Q> + Copy + Send;
 
-    type FlatMap<X, V, H>: XapUse<U = X::U, I = X::I, O = V::Item, Size = Many>
+    type UFlatMap<X, V, H>: XapUse<U = X::U, I = X::I, O = V::Item, Size = Many>
     where
         X: XapUse<Size = Self>,
         V: IntoIterator,
         H: Fn(&mut X::U, X::O) -> V + Copy + Send;
 
-    fn flat_map<X, V, H>(x: X, h: H) -> Self::FlatMap<X, V, H>
+    fn u_flat_map<X, V, H>(x: X, h: H) -> Self::UFlatMap<X, V, H>
     where
         X: XapUse<Size = Self>,
         V: IntoIterator,
@@ -60,12 +60,12 @@ pub trait SizeInfUse: Size {
 
     // transformations - helper
 
-    type Mapped<X, M>: XapUse<U = X::U, I = X::I, O = M::O, Size = Self>
+    type UMapped<X, M>: XapUse<U = X::U, I = X::I, O = M::O, Size = Self>
     where
         X: XapUse<Size = Self>,
         M: Map<U = X::U, I = X::O>;
 
-    fn mapped<X, M>(x: X, m: M) -> Self::Mapped<X, M>
+    fn u_mapped<X, M>(x: X, m: M) -> Self::UMapped<X, M>
     where
         X: XapUse<Size = Self>,
         M: Map<U = X::U, I = X::O>;
