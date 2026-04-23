7f4c851ff614a6b1b67ac030b949b9d92753e4ef
orxfun <orx.ugur.arikan@gmail.com>
2026-04-23 08:52:00 +0200

    define UseBox constructors

--- src/infallible_use/use_var/use_box.rs
+++ src/infallible_use/use_var/use_box.rs
@@ -1,13 +1,26 @@
-use crate::infallible_use::Use;
+use crate::infallible_use::{Use, UseClone, UseFun};
 use alloc::boxed::Box;
 
-pub struct UseBox<U>(Box<dyn Use<Item = U>>);
+pub struct UseBox<'a, U> {
+    u: Box<dyn Use<Item = U> + 'a>,
+}
 
-impl<U> Use for UseBox<U> {
-    type Item = U;
+impl<'a, U: 'a> UseBox<'a, U> {
+    pub fn fun<F>(f: F) -> Self
+    where
+        F: Fn(usize) -> U + Sync + 'a,
+    {
+        let use_fun = UseFun::new(f);
+        let u: Box<dyn Use<Item = U> + 'a> = Box::new(use_fun);
+        Self { u }
+    }
 
-    #[inline]
-    fn create(&self, thread_idx: usize) -> Self::Item {
-        self.0.create(thread_idx)
+    pub fn clone(value: U) -> Self
+    where
+        U: Clone + Send,
+    {
+        let use_clone = UseClone::new(value);
+        let u: Box<dyn Use<Item = U> + 'a> = Box::new(use_clone);
+        Self { u }
     }
 }
