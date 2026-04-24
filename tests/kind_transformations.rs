50df5db7590a62e483d0258f81a47ea3a0349754
orxfun <orx.ugur.arikan@gmail.com>
2026-04-24 11:17:07 +0200

    document kind transformation tests

--- tests/kind_transformations.rs
+++ tests/kind_transformations.rs
@@ -1,79 +1,12 @@
+/*
+These tests make sure that the api of parallel iterator traits are
+set up properly so that the corresponding transformation and computation
+methods are available on the trait, without requiring the concrete
+iterator type implementing the trait.
+*/
+
 use orx_parallel::*;
 use std::string::{String, ToString};
-
-fn get_par(n: usize) -> impl Par<Item = String> + EnumeratePar {
-    (0..n).par().map(|x| x.to_string())
-}
-
-fn get_par_use(n: usize) -> impl ParUse<Use = String, Item = String> + EnumerateParUse {
-    (0..n)
-        .par()
-        .using_clone("abc".to_string())
-        .map(|_, x| x.to_string())
-}
-
-fn map(par: impl Par<Item = String>) -> impl Par<Item = String> {
-    par.map(|x| format!("{x}!"))
-        .num_threads(2)
-        .chunk_size(0)
-        .iteration_order(IterationOrder::Ordered)
-}
-
-fn map_opt(par: impl Par<Item = String>) -> impl ParOption<Item = String> {
-    par.map(|x| Some(format!("{x}!")))
-        .into_optional()
-        .num_threads(2)
-        .chunk_size(0)
-        .iteration_order(IterationOrder::Ordered)
-}
-
-fn map_res(par: impl Par<Item = String>) -> impl ParResult<Item = String, Error = char> {
-    par.map(|x| Ok(format!("{x}!")))
-        .into_fallible()
-        .num_threads(2)
-        .chunk_size(0)
-        .iteration_order(IterationOrder::Ordered)
-}
-
-fn count_opt(par: impl ParOption) -> Option<usize> {
-    par.num_threads(1)
-        .chunk_size(7)
-        .map(|_| 1)
-        .reduce(|a, b| a + b)
-        .map(|x| x.unwrap_or(0))
-}
-
-fn count_res(par: impl ParResult<Error = char>) -> Result<usize, char> {
-    par.num_threads(1)
-        .chunk_size(7)
-        .map(|_| 1)
-        .reduce(|a, b| a + b)
-        .map(|x| x.unwrap_or(0))
-}
-
-fn count_use(par: impl ParUse<Use = String>) -> usize {
-    par.num_threads(1)
-        .chunk_size(7)
-        .map(|_use, _| 1)
-        .reduce(|_use, a, b| a + b)
-        .unwrap_or(0)
-}
-
-fn count_use_opt(par: impl ParUseOption<Use = String>) -> Option<usize> {
-    par.num_threads(1)
-        .chunk_size(7)
-        .map(|_use, _| 1)
-        .reduce(|_use, a, b| a + b)
-        .map(|x| x.unwrap_or(0))
-}
-
-fn count_use_res(par: impl ParUseResult<Use = String, Error = char>) -> Result<usize, char> {
-    par.num_threads(1)
-        .chunk_size(7)
-        .map(|_use, _| 1)
-        .reduce(|_use, a, b| a + b)
-        .map(|x| x.unwrap_or(0))
-}
 
 #[test]
 fn kind_inf_transform_compute() {
@@ -107,18 +40,30 @@ fn kind_inf_transform_compute() {
             .iteration_order(IterationOrder::Ordered)
     }
 
+    fn filter(par: impl Par<Item = String>) -> impl Par<Item = String> {
+        par.filter(|x| x.len() > 0)
+    }
+
+    fn filter_map(par: impl Par<Item = String>) -> impl Par<Item = String> {
+        par.filter_map(Some)
+    }
+
+    fn flat_map(par: impl Par<Item = String>) -> impl Par<Item = String> {
+        par.flat_map(|x| [x])
+    }
+
     let par = get_par(42);
-    let par = map(par);
+    let par = flat_map(filter_map(filter(map(par))));
     let result = collect(par);
     assert_eq!(result.len(), 42);
 
     let par = get_par(42);
-    let par = map(par);
+    let par = flat_map(filter_map(filter(map(par))));
     let result = count(par);
     assert_eq!(result, 42);
 
     let par = get_par(42);
-    let par = map(par);
+    let par = flat_map(filter_map(filter(map(par))));
     let result = find(par);
     assert!(result.is_some());
 }
@@ -155,18 +100,36 @@ fn kind_use_inf_transform_compute() {
             .iteration_order(IterationOrder::Ordered)
     }
 
+    fn filter(
+        par: impl ParUse<Use = char, Item = String>,
+    ) -> impl ParUse<Use = char, Item = String> {
+        par.filter(|_u, x| x.len() > 0)
+    }
+
+    fn filter_map(
+        par: impl ParUse<Use = char, Item = String>,
+    ) -> impl ParUse<Use = char, Item = String> {
+        par.filter_map(|_u, x| Some(x))
+    }
+
+    fn flat_map(
+        par: impl ParUse<Use = char, Item = String>,
+    ) -> impl ParUse<Use = char, Item = String> {
+        par.flat_map(|_, x| [x])
+    }
+
     let par = get_par(42, 'x');
-    let par = map(par);
+    let par = flat_map(filter_map(filter(map(par))));
     let result = collect(par);
     assert_eq!(result.len(), 42);
 
     let par = get_par(42, 'x');
-    let par = map(par);
+    let par = flat_map(filter_map(filter(map(par))));
     let result = count(par);
     assert_eq!(result, 42);
 
     let par = get_par(42, 'x');
-    let par = map(par);
+    let par = flat_map(filter_map(filter(map(par))));
     let result = find(par);
     assert!(result.is_some());
 }
