4243c9cd495bd5ae90e3e9d67a1dd53d8a0d6f04
orxfun <orx.ugur.arikan@gmail.com>
2026-04-24 11:33:35 +0200

    implement kind_use_opt_transform_compute tests

--- tests/kind_transformations.rs
+++ tests/kind_transformations.rs
@@ -252,3 +252,73 @@ fn kind_opt_transform_compute() {
     let par = map_to_use(get_par(42));
     assert_eq!(par.first(), Some(Some(String::from("0"))));
 }
+
+#[test]
+fn kind_use_opt_transform_compute() {
+    fn get_par(n: usize) -> impl ParUseOption<Use = char, Item = String> {
+        (0..n)
+            .par()
+            .map(|x| x.to_string())
+            .map(Some)
+            .into_optional()
+            .using_clone('x')
+    }
+
+    fn collect(par: impl ParUseOption<Use = char, Item = String>) -> Option<Vec<String>> {
+        par.num_threads(3).chunk_size(1).collect()
+    }
+
+    fn count(par: impl ParUseOption<Use = char, Item = String>) -> Option<usize> {
+        par.num_threads(1)
+            .chunk_size(7)
+            .map(|_u, _| 1)
+            .reduce(|_u, a, b| a + b)
+            .map(|x| x.unwrap_or(0))
+    }
+
+    fn find(par: impl ParUseOption<Use = char, Item = String>) -> Option<Option<String>> {
+        par.filter(|_u, x| x.len() > 2)
+            .num_threads(6)
+            .chunk_size(3)
+            .first()
+    }
+
+    fn map(
+        par: impl ParUseOption<Use = char, Item = String>,
+    ) -> impl ParUseOption<Use = char, Item = String> {
+        par.map(|_u, x| format!("{x}!"))
+    }
+
+    fn filter(
+        par: impl ParUseOption<Use = char, Item = String>,
+    ) -> impl ParUseOption<Use = char, Item = String> {
+        par.filter(|_u, x| x.len() > 0)
+    }
+
+    fn filter_map(
+        par: impl ParUseOption<Use = char, Item = String>,
+    ) -> impl ParUseOption<Use = char, Item = String> {
+        par.filter_map(|_u, x| Some(x))
+    }
+
+    fn flat_map(
+        par: impl ParUseOption<Use = char, Item = String>,
+    ) -> impl ParUseOption<Use = char, Item = String> {
+        par.flat_map(|_u, x| [x])
+    }
+
+    let par = get_par(42);
+    let par = flat_map(filter_map(filter(map(par))));
+    let result = collect(par).unwrap();
+    assert_eq!(result.len(), 42);
+
+    let par = get_par(42);
+    let par = flat_map(filter_map(filter(map(par))));
+    let result = count(par).unwrap();
+    assert_eq!(result, 42);
+
+    let par = get_par(42);
+    let par = flat_map(filter_map(filter(map(par))));
+    let result = find(par).unwrap();
+    assert!(result.is_some());
+}
