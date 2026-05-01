d3fb8a087d12422c7fab7d2afd8e93bd01a2bb9e
orxfun <orx.ugur.arikan@gmail.com>
2026-05-01 18:36:36 +0200

    infallible-use collect-into is refactored to work with mutable reference

--- src/collectables/inf_use/split_vec_recursive.rs
+++ src/collectables/inf_use/split_vec_recursive.rs
@@ -1,10 +1,38 @@
-use crate::collectables::alg::merge_collected::merge_ord_into_split_vec;
+use crate::collectables::alg::merge_collected::{
+    merge_arb_into_split_vec_new, merge_ord_into_split_vec, merge_ord_into_split_vec_new,
+};
 use crate::collectables::inf_use::ColIntoInfUse;
 use crate::infallible_use::{ParRunnerInfallibleUse, ParUseCore, ParUseIter, Use, XapUse};
 use orx_concurrent_iter::ConcurrentIter;
 use orx_split_vec::{Recursive, SplitVec};
 
 impl<T> ColIntoInfUse<T> for SplitVec<T, Recursive> {
+    fn inf_use_col_into_new<U, I, X, R>(dst: &mut Self, par: ParUseIter<U, I, X, R>)
+    where
+        U: Use,
+        I: ConcurrentIter,
+        X: XapUse<U = U::Item, I = I::Item, O = T>,
+        R: ParRunnerInfallibleUse,
+        T: Send,
+    {
+        let (u, iter, x, mut exe, params) = par.destruct();
+        let results = exe.collect(params, u, iter, x);
+        merge_ord_into_split_vec_new(results, dst);
+    }
+
+    fn inf_use_arb_col_into_new<U, I, X, R>(dst: &mut Self, par: ParUseIter<U, I, X, R>)
+    where
+        U: Use,
+        I: ConcurrentIter,
+        X: XapUse<U = U::Item, I = I::Item, O = T>,
+        R: ParRunnerInfallibleUse,
+        T: Send,
+    {
+        let (u, iter, x, mut exe, params) = par.destruct();
+        let results = exe.collect_arb(params, u, iter, x);
+        merge_arb_into_split_vec_new(results, dst);
+    }
+
     fn inf_use_col_into<U, I, X, R>(dst: Option<Self>, par: ParUseIter<U, I, X, R>) -> Self
     where
         U: Use,
--- src/collectables/inf_use/vec_vec.rs
+++ src/collectables/inf_use/vec_vec.rs
@@ -1,11 +1,40 @@
-use crate::collectables::alg::merge_collected::merge_ord_into_vec;
+use crate::collectables::alg::merge_collected::{merge_ord_into_vec, merge_ord_into_vec_new};
 use crate::collectables::inf_use::ColIntoInfUse;
-use crate::infallible_use::{ParRunnerInfallibleUse, ParUseIter, ParUseCore, Use, XapUse};
+use crate::infallible_use::{ParRunnerInfallibleUse, ParUseCore, ParUseIter, Use, XapUse};
 use alloc::vec;
 use alloc::vec::Vec;
 use orx_concurrent_iter::ConcurrentIter;
 
 impl<T> ColIntoInfUse<T> for Vec<Vec<T>> {
+    fn inf_use_col_into_new<U, I, X, R>(dst: &mut Self, par: ParUseIter<U, I, X, R>)
+    where
+        U: Use,
+        I: ConcurrentIter,
+        X: XapUse<U = U::Item, I = I::Item, O = T>,
+        R: ParRunnerInfallibleUse,
+        T: Send,
+    {
+        let (u, iter, x, mut exe, params) = par.destruct();
+        let results = exe.collect(params, u, iter, x);
+
+        let mut ordered = Vec::new();
+        merge_ord_into_vec_new(results, &mut ordered);
+        dst.push(ordered);
+    }
+
+    fn inf_use_arb_col_into_new<U, I, X, R>(dst: &mut Self, par: ParUseIter<U, I, X, R>)
+    where
+        U: Use,
+        I: ConcurrentIter,
+        X: XapUse<U = U::Item, I = I::Item, O = T>,
+        R: ParRunnerInfallibleUse,
+        T: Send,
+    {
+        let (u, iter, x, mut exe, params) = par.destruct();
+        let results = exe.collect_arb(params, u, iter, x);
+        dst.extend(results);
+    }
+
     fn inf_use_col_into<U, I, X, R>(dst: Option<Self>, par: ParUseIter<U, I, X, R>) -> Self
     where
         U: Use,
