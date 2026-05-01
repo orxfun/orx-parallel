4dc6a118b5968ab94003a9a7060d2f2e4610ff16
orxfun <orx.ugur.arikan@gmail.com>
2026-05-01 19:30:30 +0200

    implement extend_par

--- src/ops/extend.rs
+++ src/ops/extend.rs
@@ -1,5 +1,10 @@
 use crate::{Par, ParCollectInto};
 
 pub trait ExtendPar<T>: ParCollectInto<T> {
-    fn extend_par(&mut self, iter: impl Par<Item = T>) {}
+    fn extend_par(&mut self, iter: impl Par<Item = T>)
+    where
+        T: Send,
+    {
+        iter.collect_into(self);
+    }
 }
