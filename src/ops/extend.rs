213a77ad17ef6d0b3d4fc876dde58117bd528855
orxfun <orx.ugur.arikan@gmail.com>
2026-05-01 19:37:25 +0200

    implement extend_par for both Par and ParUse via common InfalliblePar trait

--- src/ops/extend.rs
+++ src/ops/extend.rs
@@ -1,10 +1,10 @@
-use crate::{Par, ParCollectInto};
+use crate::{InfalliblePar, ParCollectInto};
 
 pub trait ExtendPar<T>: ParCollectInto<T> {
-    fn extend_par(&mut self, iter: impl Par<Item = T>)
+    fn extend_par(&mut self, iter: impl InfalliblePar<InfItem = T>)
     where
         T: Send,
     {
-        iter.collect_into(self);
+        iter.inf_collect_into(self);
     }
 }
