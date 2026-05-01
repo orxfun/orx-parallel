e64ad26a26e76a2862aaca7a1054bc54a41cddef
orxfun <orx.ugur.arikan@gmail.com>
2026-05-01 07:42:43 +0200

    inf tests added

--- src/infallible/tests/compute.rs
+++ src/infallible/tests/compute.rs
@@ -5,117 +5,98 @@ use alloc::vec::Vec;
 use core::sync::atomic::{AtomicUsize, Ordering};
 use std::string::{String, ToString};
 
-const N: usize = 157;
+const N: [usize; 2] = [2, 157];
 
 #[test]
 fn inf_first() {
-    let input = inputs(N);
+    for n in N {
+        let input = inputs(n);
 
-    let result = input.par().filter(|x| *x == &(N / 2).to_string()).first();
-    assert_eq!(result, Some(&(N / 2).to_string()));
+        let result = input.par().filter(|x| *x == &(n / 2).to_string()).first();
+        assert_eq!(
+            result,
+            input.iter().filter(|x| *x == &(n / 2).to_string()).next()
+        );
 
-    let result = input.par().filter(|x| x.as_str() == "x").first();
-    assert_eq!(result, None);
-
-    // empty
-    let input = inputs(0);
-    let result = input.par().filter(|x| *x == &(N / 2).to_string()).first();
-    assert_eq!(result, None);
+        let result = input.par().filter(|x| x.as_str() == "x").first();
+        assert_eq!(result, input.iter().filter(|x| x.as_str() == "x").next());
+    }
 }
 
 #[test]
 fn inf_reduce() {
-    let input = inputs(N);
+    for n in N {
+        let input = inputs(n);
 
-    let result = input.par().map(|x| x.len()).reduce(|a, b| a + b);
-    assert_eq!(result, input.iter().map(|x| x.len()).reduce(|a, b| a + b));
-
-    // empty
-    let input = inputs(0);
-    let result = input.par().map(|x| x.len()).reduce(|a, b| a + b);
-    assert_eq!(result, None);
+        let result = input.par().map(|x| x.len()).reduce(|a, b| a + b);
+        assert_eq!(result, input.iter().map(|x| x.len()).reduce(|a, b| a + b));
+    }
 }
 
 #[test]
 fn inf_collect() {
-    let input = inputs(N);
-    let result: Vec<String> = input.into_par().filter(|x| x.len() < 2).collect();
-    assert_eq!(result, (0..10).map(|x| x.to_string()).collect::<Vec<_>>());
-
-    // empty
-    let input = inputs(0);
-    let result: Vec<String> = input.into_par().filter(|x| x.len() < 2).collect();
-    assert_eq!(result, Vec::<String>::new());
+    for n in N {
+        let input = inputs(n);
+        let result: Vec<String> = input.clone().into_par().filter(|x| x.len() < 2).collect();
+        assert_eq!(
+            result,
+            input
+                .into_iter()
+                .filter(|x| x.len() < 2)
+                .collect::<Vec<_>>()
+        );
+    }
 }
 
 #[test]
 fn inf_collect_into() {
-    let result = vec!["x".to_string()];
-    let input = inputs(N);
-    let result = input
-        .into_par()
-        .filter(|x| x.len() < 2)
-        .collect_into(result);
-    let expected: Vec<_> = ["x", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        .into_iter()
-        .map(|x| x.to_string())
-        .collect();
-    assert_eq!(result, expected);
+    for n in N {
+        let input = inputs(n);
 
-    // empty
-    let result = vec!["x".to_string()];
-    let input = inputs(0);
-    let result = input
-        .into_par()
-        .filter(|x| x.len() < 2)
-        .collect_into(result);
-    assert_eq!(result, vec!["x".to_string()]);
+        let result = vec!["x".to_string()];
+        let result = input
+            .clone()
+            .into_par()
+            .filter(|x| x.len() < 2)
+            .collect_into(result);
+
+        let mut expected = vec!["x".to_string()];
+        expected.extend(input.into_iter().filter(|x| x.len() < 2));
+
+        assert_eq!(result, expected);
+    }
 }
 
 #[test]
 fn inf_all() {
-    let input = inputs(N);
+    for n in N {
+        let input = inputs(n);
 
-    let result = input.par().all(|x| x.len() > 0);
-    assert_eq!(result, true);
+        let result = input.par().all(|x| x.len() > 0);
+        assert_eq!(result, input.iter().all(|x| x.len() > 0));
 
-    let result = input.par().all(|x| x.len() == 1);
-    assert_eq!(result, false);
-
-    // empty
-    let input = inputs(0);
-
-    let result = input.par().all(|x| x.len() > 0);
-    assert_eq!(result, true);
-
-    let result = input.par().all(|x| x.len() == 1);
-    assert_eq!(result, true);
+        let result = input.par().all(|x| x.len() == 1);
+        assert_eq!(result, input.iter().all(|x| x.len() == 1));
+    }
 }
 
 #[test]
 fn inf_any() {
-    let input = inputs(N);
+    for n in N {
+        let input = inputs(n);
 
-    let result = input.par().any(|x| x.len() > 1);
-    assert_eq!(result, true);
+        let result = input.par().any(|x| x.len() > 1);
+        assert_eq!(result, input.iter().any(|x| x.len() > 1));
 
-    let result = input.par().any(|x| x.len() == 4);
-    assert_eq!(result, false);
-
-    // empty
-    let input = inputs(0);
-
-    let result = input.par().any(|x| x.len() > 1);
-    assert_eq!(result, false);
-
-    let result = input.par().any(|x| x.len() == 4);
-    assert_eq!(result, false);
+        let result = input.par().any(|x| x.len() == 4);
+        assert_eq!(result, input.iter().any(|x| x.len() == 4));
+    }
 }
 
 #[test]
 fn inf_count() {
-    for n in [0, N] {
-        let input = inputs(N);
+    for n in N {
+        let input = inputs(n);
 
         let result = input.par().filter(|x| x.len() < 2).count();
         assert_eq!(result, input.iter().filter(|x| x.len() < 2).count());
@@ -127,7 +108,7 @@ fn inf_count() {
 
 #[test]
 fn inf_find() {
-    for n in [0, N] {
+    for n in N {
         let input = inputs(n);
 
         let result = input.par().find(|x| x.len() > 1);
@@ -140,7 +121,7 @@ fn inf_find() {
 
 #[test]
 fn inf_find_any() {
-    let input = inputs(N);
+    let input = inputs(N[1]);
 
     let result = input
         .par()
@@ -165,8 +146,8 @@ fn inf_find_any() {
 
 #[test]
 fn inf_for_each() {
-    for n in [0, N] {
-        let input = inputs(N);
+    for n in N {
+        let input = inputs(n);
         let total_len = AtomicUsize::new(0);
         input
             .par()
@@ -177,9 +158,87 @@ fn inf_for_each() {
 
 #[test]
 fn inf_max() {
-    for n in [0, N] {
+    for n in N {
         let input = inputs(n);
         let result = input.par().map(|x| x.len()).max();
         assert_eq!(result, input.iter().map(|x| x.len()).max());
+    }
+}
+
+#[test]
+fn inf_max_by() {
+    for n in N {
+        let input = inputs(n);
+        let result = input.par().map(|x| x.len()).max_by(|a, b| a.cmp(&b));
+        assert_eq!(
+            result,
+            input.iter().map(|x| x.len()).max_by(|a, b| a.cmp(&b))
+        );
+    }
+}
+
+#[test]
+fn inf_max_by_key() {
+    for n in N {
+        let input = inputs(n);
+        let result = input
+            .par()
+            .max_by_key(|x| x.len() * 100 + x.parse::<usize>().unwrap());
+        assert_eq!(
+            result,
+            input
+                .iter()
+                .max_by_key(|x| x.len() * 100 + x.parse::<usize>().unwrap())
+        );
+    }
+}
+
+#[test]
+fn inf_min() {
+    for n in N {
+        let input = inputs(n);
+        let result = input.par().map(|x| x.len()).min();
+        assert_eq!(result, input.iter().map(|x| x.len()).min());
+    }
+}
+
+#[test]
+fn inf_min_by() {
+    for n in N {
+        let input = inputs(n);
+        let result = input.par().map(|x| x.len()).min_by(|a, b| a.cmp(&b));
+        assert_eq!(
+            result,
+            input.iter().map(|x| x.len()).min_by(|a, b| a.cmp(&b))
+        );
+    }
+}
+
+#[test]
+fn inf_min_by_key() {
+    for n in N {
+        let input = inputs(n);
+        let result = input
+            .par()
+            .min_by_key(|x| x.len() * 100 + x.parse::<usize>().unwrap());
+        assert_eq!(
+            result,
+            input
+                .iter()
+                .min_by_key(|x| x.len() * 100 + x.parse::<usize>().unwrap())
+        );
+    }
+}
+
+#[test]
+fn inf_sum() {
+    for n in N {
+        let input = inputs(n);
+
+        let result = input.par().filter(|x| x.len() > 1).map(|x| x.len()).sum();
+        assert_eq!(
+            result,
+            input.iter().filter(|x| x.len() > 1).map(|x| x.len()).sum()
+        );
     }
 }
