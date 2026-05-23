848f4613243ba77c0eafecb56308c3e4d819e325
orxfun <orx.ugur.arikan@gmail.com>
2026-05-23 21:05:36 +0200

    fix missing import

--- src/pool/env.rs
+++ src/pool/env.rs
@@ -2,11 +2,11 @@
 const MAX_NUM_THREADS_ENV_VARIABLE: &str = "ORX_PARALLEL_MAX_NUM_THREADS";
 
 #[cfg(feature = "std")]
-pub fn max_num_threads_by_env_variable() -> Option<NonZeroUsize> {
+pub fn max_num_threads_by_env_variable() -> Option<core::num::NonZeroUsize> {
     match std::env::var(MAX_NUM_THREADS_ENV_VARIABLE) {
         Ok(s) => match s.parse::<usize>() {
-            Ok(x) => NonZeroUsize::new(x), // None if 0; Some(x) if x is set to a positive bound
-            Err(_e) => None,               // not a number, ignored assuming no bound
+            Ok(x) => core::num::NonZeroUsize::new(x), // None if 0; Some(x) if x is set to a positive bound
+            Err(_e) => None,                          // not a number, ignored assuming no bound
         },
         Err(_e) => None, // not set, no bound
     }
