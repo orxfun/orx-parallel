7df9573315a0b42c72dcadba943659feb10bf294
orxfun <orx.ugur.arikan@gmail.com>
2026-05-23 07:06:13 +0200

    update recursive concurrent parallel iterator to v3

--- src/into_parallel/into_par_iter_rec.rs
+++ src/into_parallel/into_par_iter_rec.rs
@@ -1,18 +1,19 @@
 use crate::infallible::{ParIter, xap_variants::Id};
 use crate::runner::default_runner;
-use orx_concurrent_recursive_iter::{ConcurrentRecursiveIter, Queue};
+use orx_concurrent_recursive_iter::ConcurrentRecursiveIter;
 
 pub trait IntoParIterRecursive
 where
     Self: IntoIterator,
     Self::Item: Send,
 {
-    fn into_par_recursive<F>(
+    fn into_par_recursive<I, F>(
         self,
         extend: F,
-    ) -> ParIter<ConcurrentRecursiveIter<Self::Item, F>, Id<Self::Item>>
+    ) -> ParIter<ConcurrentRecursiveIter<I, F>, Id<Self::Item>>
     where
-        F: Fn(&Self::Item, &Queue<Self::Item>) + Sync;
+        I: IntoIterator<Item = Self::Item>,
+        F: Fn(&Self::Item) -> I + Send + Sync;
 }
 
 impl<X> IntoParIterRecursive for X
@@ -20,14 +21,15 @@ where
     X: IntoIterator,
     X::Item: Send,
 {
-    fn into_par_recursive<F>(
+    fn into_par_recursive<I, F>(
         self,
         extend: F,
-    ) -> ParIter<ConcurrentRecursiveIter<Self::Item, F>, Id<Self::Item>>
+    ) -> ParIter<ConcurrentRecursiveIter<I, F>, Id<Self::Item>>
     where
-        F: Fn(&Self::Item, &Queue<Self::Item>) + Sync,
+        I: IntoIterator<Item = Self::Item>,
+        F: Fn(&Self::Item) -> I + Send + Sync,
     {
-        let iter = ConcurrentRecursiveIter::new(self, extend);
+        let iter = ConcurrentRecursiveIter::new(self, extend, None, None);
         ParIter::new(iter, Id::new(), default_runner(), Default::default())
     }
 }
