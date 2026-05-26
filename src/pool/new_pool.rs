02a87e6e017d1d2b1760be3dda42f6cadaf7a368
orxfun <orx.ugur.arikan@gmail.com>
2026-05-26 10:43:26 +0200

    define rayon pool creator

--- src/pool/new_pool.rs
+++ src/pool/new_pool.rs
@@ -8,4 +8,27 @@ impl Pool {
     pub fn once(num_threads: impl Into<NumThreads>) -> OncePool {
         OncePool::new(num_threads)
     }
+
+    /// Creates a rayon [`ThreadPool`].
+    ///
+    /// When `num_threads` is set to `n > 0` or `NumThreads::Max(n)`, then the resulting thread pools
+    /// are guaranteed to start at most this number of threads.
+    ///
+    /// When `num_threads` is 0 or `NumThreads::Auto`, then the Rayon runtime will select the number
+    /// of threads automatically. At present, this is based on the RAYON_NUM_THREADS environment variable
+    /// (if set), or the number of logical CPUs (otherwise). In the future, however, the default behavior
+    /// may change to dynamically add or remove threads as needed.
+    ///
+    /// [`ThreadPool`]: https://docs.rs/rayon-core/latest/rayon_core/struct.ThreadPool.html
+    #[cfg(feature = "rayon-core")]
+    pub fn rayon(num_threads: impl Into<NumThreads>) -> rayon_core::ThreadPool {
+        let num_threads = match num_threads.into() {
+            NumThreads::Auto => 0,
+            NumThreads::Max(nt) => nt.into(),
+        };
+        rayon_core::ThreadPoolBuilder::new()
+            .num_threads(num_threads)
+            .build()
+            .expect("failed to build rayon thread pool")
+    }
 }
