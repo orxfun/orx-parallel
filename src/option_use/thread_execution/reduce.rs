70dc5c36130a28ece90001fb6cb28166d7428678
orxfun <orx.ugur.arikan@gmail.com>
2026-05-27 14:20:02 +0200

    all option-use thread executions use thread index aware pullers

--- src/option_use/thread_execution/reduce.rs
+++ src/option_use/thread_execution/reduce.rs
@@ -25,8 +25,7 @@ where
     let mut u = u.create(th_idx);
     let u = &mut u as *mut U::Item;
 
-    let mut chunk_puller = iter.chunk_puller(0);
-    let mut item_puller = iter.item_puller();
+    let mut chunk_puller = iter.chunk_puller_by(0, th_idx);
 
     let mut acc = None;
 
@@ -37,7 +36,7 @@ where
 
         match chunk_size {
             0 | 1 => {
-                match item_puller.next() {
+                match iter.next_by(th_idx) {
                     Some(i) => {
                         for a in S::xap_use_opt(u, x1, x2, i) {
                             acc = match (a, acc.is_some()) {
@@ -56,9 +55,7 @@ where
                 }
             }
             c => {
-                if c > chunk_puller.chunk_size() {
-                    chunk_puller = iter.chunk_puller(c);
-                }
+                chunk_puller.resize_for_chunk_size(c);
 
                 match chunk_puller.pull() {
                     Some(chunk) => {
@@ -95,7 +92,7 @@ where
 
                 match chunk_size {
                     0 | 1 => {
-                        match item_puller.next() {
+                        match iter.next_by(th_idx) {
                             Some(i) => {
                                 for a in S::xap_use_opt(u, x1, x2, i) {
                                     acc = match a {
@@ -113,9 +110,7 @@ where
                         }
                     }
                     c => {
-                        if c > chunk_puller.chunk_size() {
-                            chunk_puller = iter.chunk_puller(c);
-                        }
+                        chunk_puller.resize_for_chunk_size(c);
 
                         match chunk_puller.pull() {
                             Some(chunk) => {
