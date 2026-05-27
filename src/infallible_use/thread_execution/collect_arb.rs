f57b43c87f2fd39e2d62b7697d75e6d10a627dff
orxfun <orx.ugur.arikan@gmail.com>
2026-05-27 14:11:42 +0200

    all infallible-use thread executions use thread index aware pullers

--- src/infallible_use/thread_execution/collect_arb.rs
+++ src/infallible_use/thread_execution/collect_arb.rs
@@ -16,23 +16,20 @@ where
     let mut u = u.create(th_idx);
     let u = &mut u as *mut U::Item;
 
-    let mut chunk_puller = iter.chunk_puller(0);
-    let mut item_puller = iter.item_puller();
+    let mut chunk_puller = iter.chunk_puller_by(0, th_idx);
 
     loop {
         let chunk_size = Q::next_chunk_size(state, iter.size_hint());
         let chunk_state = Q::begin_chunk(th_idx, chunk_size);
 
         match chunk_size {
-            0 | 1 => match item_puller.next() {
+            0 | 1 => match iter.next_by(th_idx) {
                 Some(i) => vec.extend(x.xap_use(u, i)),
                 None if iter.is_completed_when_none_returned() => break,
                 None => {}
             },
             c => {
-                if c > chunk_puller.chunk_size() {
-                    chunk_puller = iter.chunk_puller(c);
-                }
+                chunk_puller.resize_for_chunk_size(c);
 
                 match chunk_puller.pull() {
                     Some(chunk) => vec.extend(chunk.flat_map(|i| x.xap_use(u, i))),
--- src/infallible_use/thread_execution/next.rs
+++ src/infallible_use/thread_execution/next.rs
@@ -18,15 +18,14 @@ where
 {
     let mut u = u.create(th_idx);
     let u = &mut u as *mut U::Item;
-    let mut chunk_puller = iter.chunk_puller(0);
-    let mut item_puller = iter.item_puller_with_idx();
+    let mut chunk_puller = iter.chunk_puller_by(0, th_idx);
 
     loop {
         let chunk_size = Q::next_chunk_size(state, iter.size_hint());
         let chunk_state = Q::begin_chunk(th_idx, chunk_size);
 
         match chunk_size {
-            0 | 1 => match item_puller.next() {
+            0 | 1 => match iter.next_with_idx_by(th_idx) {
                 Some((idx, i)) => {
                     if let Some(val) = x.xap_use(u, i).into_iter().next() {
                         Q::broadcast_stop(iter, state, chunk_state);
@@ -37,9 +36,7 @@ where
                 None => {}
             },
             c => {
-                if c > chunk_puller.chunk_size() {
-                    chunk_puller = iter.chunk_puller(c);
-                }
+                chunk_puller.resize_for_chunk_size(c);
 
                 match chunk_puller.pull_with_idx() {
                     Some((idx, chunk)) => {
