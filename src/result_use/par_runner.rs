32784da994fb61ca3164ddd793e33a277af86163
orxfun <orx.ugur.arikan@gmail.com>
2026-05-29 21:50:52 +0200

    implement fold for result-use parallel runner

--- src/result_use/par_runner.rs
+++ src/result_use/par_runner.rs
@@ -137,6 +137,60 @@ pub trait ParRunnerUseRes: ParRunner {
         })
     }
 
+    fn fold<U, I, M, E, X1, X2, S, F>(
+        &mut self,
+        sizes: S,
+        params: Params,
+        u: U,
+        iter: I,
+        x1: X1,
+        x2: X2,
+        f: F,
+    ) -> Result<Vec<U::Item>, E>
+    where
+        U: Use,
+        I: ConcurrentIter,
+        X1: XapUse<U = U::Item, I = I::Item, O = Result<M, E>>,
+        X2: XapUse<U = U::Item, I = M, O = ()>,
+        S: SizePair<S1 = X1::Size, S2 = X2::Size>,
+        F: Fn(&mut U::Item, X2::O, X2::O) -> X2::O + Send + Copy,
+        U::Item: Send,
+        E: Send,
+    {
+        let mut spawned = 0;
+        let (max_nt, state) = self.nt_state(params, iter.size_hint());
+        let results_bag = ConcurrentBag::with_fixed_capacity(max_nt);
+
+        {
+            let (iter, st, results, u) = (&iter, &state, &results_bag, &u);
+            self.pool_mut().scoped_computation(move |s| {
+                while let Some(th_idx) = Self::do_spawn_new(spawned, st) {
+                    spawned += 1;
+                    <Self::Pool as ParThreadPool>::run_in_scope(&s, move || {
+                        Self::begin_thread(st, th_idx);
+                        let value = th::reduce_get_u::<Self, _, _, _, _, _, _, _, _>(
+                            sizes, u, th_idx, st, iter, x1, x2, f,
+                        );
+                        results.push(value);
+                        Self::complete_thread(st, th_idx);
+                    });
+                }
+            });
+        }
+
+        Self::complete_computation(state);
+
+        let results = results_bag.into_inner().into_inner();
+        let mut success = Vec::with_capacity(results.len());
+        for result in results {
+            match result {
+                Ok((_, x)) => success.push(x),
+                Err(e) => return Err(e),
+            }
+        }
+        Ok(success)
+    }
+
     fn collect<U, I, M, E, X1, X2, S>(
         &mut self,
         sizes: S,
