9adc9c7f18cc68dfe958d9abc663f32d5dfd4d69
orxfun <orx.ugur.arikan@gmail.com>
2026-05-29 21:38:55 +0200

    implement reduce_get_u for result-use thread computation

--- src/result_use/thread_execution/reduce.rs
+++ src/result_use/thread_execution/reduce.rs
@@ -3,7 +3,7 @@ use crate::runner::ParRunner;
 use crate::sizes::SizePair;
 use orx_concurrent_iter::{ChunkPuller, ConcurrentIter};
 
-pub fn reduce<Q, U, I, M, E, X1, X2, S, F>(
+pub fn reduce_get_u<Q, U, I, M, E, X1, X2, S, F>(
     _: S,
     u: &U,
     th_idx: usize,
@@ -12,7 +12,7 @@ pub fn reduce<Q, U, I, M, E, X1, X2, S, F>(
     x1: X1,
     x2: X2,
     f: F,
-) -> Result<Option<X2::O>, E>
+) -> (Result<Option<X2::O>, E>, U::Item)
 where
     Q: ParRunner,
     U: Use,
@@ -22,8 +22,8 @@ where
     S: SizePair<S1 = X1::Size, S2 = X2::Size>,
     F: Fn(&mut U::Item, X2::O, X2::O) -> X2::O,
 {
-    let mut u = u.create(th_idx);
-    let u = &mut u as *mut U::Item;
+    let mut use_var = u.create(th_idx);
+    let u = &mut use_var as *mut U::Item;
 
     let mut chunk_puller = iter.chunk_puller_by(0, th_idx);
 
@@ -44,7 +44,7 @@ where
                                 (Ok(a), false) => Some(a),
                                 (Err(e), _) => {
                                     Q::broadcast_stop(iter, state, chunk_state);
-                                    return Err(e);
+                                    return (Err(e), use_var);
                                 }
                             };
                         }
@@ -65,7 +65,7 @@ where
                                 (Ok(a), false) => Some(a),
                                 (Err(e), _) => {
                                     Q::broadcast_stop(iter, state, chunk_state);
-                                    return Err(e);
+                                    return (Err(e), use_var);
                                 }
                             };
                         }
@@ -99,7 +99,7 @@ where
                                         Ok(a) => f(unsafe { &mut *u }, acc, a),
                                         Err(e) => {
                                             Q::broadcast_stop(iter, state, chunk_state);
-                                            return Err(e);
+                                            return (Err(e), use_var);
                                         }
                                     };
                                 }
@@ -120,7 +120,7 @@ where
                                         Ok(a) => f(unsafe { &mut *u2 }, acc, a),
                                         Err(e) => {
                                             Q::broadcast_stop(iter, state, chunk_state);
-                                            return Err(e);
+                                            return (Err(e), use_var);
                                         }
                                     };
                                 }
@@ -138,5 +138,27 @@ where
         }
     };
 
-    Ok(result)
+    (Ok(result), use_var)
+}
+
+pub fn reduce<Q, U, I, M, E, X1, X2, S, F>(
+    s: S,
+    u: &U,
+    th_idx: usize,
+    state: &Q::State,
+    iter: &I,
+    x1: X1,
+    x2: X2,
+    f: F,
+) -> Result<Option<X2::O>, E>
+where
+    Q: ParRunner,
+    U: Use,
+    I: ConcurrentIter,
+    X1: XapUse<U = U::Item, I = I::Item, O = Result<M, E>>,
+    X2: XapUse<U = U::Item, I = M>,
+    S: SizePair<S1 = X1::Size, S2 = X2::Size>,
+    F: Fn(&mut U::Item, X2::O, X2::O) -> X2::O,
+{
+    reduce_get_u::<Q, U, I, M, E, X1, X2, S, F>(s, u, th_idx, state, iter, x1, x2, f).0
 }
