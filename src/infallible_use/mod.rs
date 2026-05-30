af39f605b53465c21cca1a1d71452f0562ee4012
orxfun <orx.ugur.arikan@gmail.com>
2026-05-30 08:01:53 +0200

    wip using vec

--- src/infallible_use/mod.rs
+++ src/infallible_use/mod.rs
@@ -18,7 +18,7 @@ pub use par_core::ParUseCore;
 pub use par_enum::EnumerateParUse;
 pub use par_iter::ParUseIter;
 pub use par_runner::ParRunnerInfallibleUse;
-pub use use_var::{Use, UseClone, UseFun};
+pub use use_var::{Use, UseClone, UseFun, UseVec};
 pub use xap::{
     FilMapOf, FilOf, FlatMapOf, FlattenOf, InsOf, MapOf, MappedOf, XapUse, XapUseBin, XapUseOne,
 };
