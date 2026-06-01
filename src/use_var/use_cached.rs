1ffd3a4a4c3770b76b503c1ffe298e551df64976
orxfun <orx.ugur.arikan@gmail.com>
2026-06-01 14:29:54 +0200

    use cached

--- src/use_var/use_cached.rs
+++ src/use_var/use_cached.rs
@@ -1,20 +1,52 @@
 use crate::use_var::Use;
-use alloc::vec::Vec;
 use orx_concurrent_ordered_bag::ConcurrentOrderedBag;
+use orx_self_or::SoM;
 
-pub struct UseCached<U: Use> {
-    u: U,
-    cache: ConcurrentOrderedBag<U::Item>,
+pub struct UseCached<'a, U: Use> {
+    u: &'a mut U,
+    cache: ConcurrentOrderedBag<Ptr<U::Item>>,
 }
 
-impl<U: Use> UseCached<U> {
-    pub fn new(u: U) -> Self {
+impl<'a, U: Use> UseCached<'a, U> {
+    pub fn new(u: &'a mut U) -> Self {
         let cache = ConcurrentOrderedBag::new();
         Self { u, cache }
     }
+}
 
-    pub fn into_vec(self) -> Vec<U::Item> {
-        let vec = unsafe { self.cache.into_inner().unwrap_only_if_counts_match() };
-        vec.into_iter().collect()
+impl<'a, U: Use> Use for UseCached<'a, U> {
+    type Item = U::Item;
+
+    type ItemBorrow<'i>
+        = &'i mut U::Item
+    where
+        Self: 'i;
+
+    fn init_get(&self, thread_idx: usize) -> Self::ItemBorrow<'_> {
+        let mut use_var = self.u.init_get(thread_idx);
+        let ptr = use_var.get_mut() as *mut U::Item;
+        unsafe { self.cache.set_value(thread_idx, Ptr(ptr)) };
+
+        // let idx = self.cache.push((thread_idx, use_var));
+        // SAFETY: it is safe to access to the index as it is
+        // pushed / initialized just above. Further, `get` will
+        // be called exactly once by the corresponding thread,
+        // and hence, there will be no race condition.
+        unsafe { &mut *ptr }
+    }
+
+    fn get(&mut self, thread_idx: usize) -> Self::ItemBorrow<'_> {
+        assert!(self.cache.len() > 0);
+        let ptr = unsafe { &*self.cache.ptr_mut(thread_idx) }.0;
+        unsafe { &mut *ptr }
+    }
+
+    fn max_threads(&self) -> Option<usize> {
+        self.u.max_threads()
     }
 }
+
+#[derive(Clone)]
+struct Ptr<T>(*mut T);
+
+unsafe impl<T> Send for Ptr<T> {}
