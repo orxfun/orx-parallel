114cd6b03e81be583cea90d094a941a7da2630d3
orxfun <orx.ugur.arikan@gmail.com>
2026-06-01 14:40:40 +0200

    wip

--- src/use_var/use.rs
+++ src/use_var/use.rs
@@ -13,33 +13,3 @@ pub trait Use: Sync {
 
     fn max_threads(&self) -> Option<usize>;
 }
-
-// pub struct UsePair<U, V>
-// where
-//     U: Use,
-//     V: Use,
-// {
-//     u: U,
-//     v: V,
-// }
-
-// impl<U: Use, V: Use> Use for UsePair<U, V> {
-//     type Item = (U::Item, V::Item);
-
-//     type ItemBorrow<'i>
-//         = (U::ItemBorrow<'i>, V::ItemBorrow<'i>)
-//     where
-//         Self: 'i;
-
-//     fn init_get(&self, thread_idx: usize) -> Self::ItemBorrow<'_> {
-//         todo!()
-//     }
-
-//     fn get(&mut self, thread_idx: usize) -> Self::ItemBorrow<'_> {
-//         todo!()
-//     }
-
-//     fn max_threads(&self) -> Option<usize> {
-//         todo!()
-//     }
-// }
