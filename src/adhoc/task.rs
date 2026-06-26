210c2d49d4ca1039ffdae5d89355d14112025fd9
orxfun <orx.ugur.arikan@gmail.com>
2026-06-26 16:58:16 +0200

    wip

--- src/adhoc/task.rs
+++ src/adhoc/task.rs
@@ -1,44 +1,63 @@
+use crate::ParThreadPool;
+
 pub trait Task {}
 
 orx_meta::define_queue!(
+    // lt => [];
+    generics => [P:ParThreadPool];
     elements => [Task];
     queue => [Q; Qs, Qm];
+    // queue_of => qof;
+    // builder => Billy;
 );
 
-impl<F: Task> Task for Qs<F> {}
+impl<P: ParThreadPool, F: Task> Task for Qs<P, F> {}
 
-impl<F: Task, B: Q> Task for Qm<F, B> {}
+impl<P: ParThreadPool, F: Task, B: Q<P>> Task for Qm<P, F, B> {}
 
-pub struct TaskOf<O, F>
-where
-    F: FnOnce() -> O,
-{
-    f: F,
+pub struct TaskOf<O> {
     output: Option<O>,
 }
 
-impl<O, F: FnOnce() -> O> TaskOf<O, F> {
-    pub fn new(f: F) -> Self {
-        Self { f, output: None }
-    }
-}
-
-impl<O, F: FnOnce() -> O> Task for TaskOf<O, F> {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::*;
-
-    #[test]
-    fn abc() {
-        let q = Qm::new(TaskOf::new(|| 12))
-            .push(TaskOf::new(|| 'x'))
-            .push(TaskOf::new(|| true));
-
-        let mut pool = Pool::once(4);
-        pool.scoped_computation(|s| {
-            //
+impl<O> TaskOf<O> {
+    pub fn load<'s, 'env, 'scope, F, P: ParThreadPool>(
+        s: &P::ScopeRef<'s, 'env, 'scope>,
+        f: F,
+    ) -> Self
+    where
+        F: FnOnce() -> O + Send,
+    {
+        let x = Self { output: None };
+        P::run_in_scope(s, move || {
+            let result = f();
         });
+        x
     }
 }
+
+// impl<O, F: FnOnce() -> O> Task for TaskOf<O, F> {}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use crate::*;
+
+//     #[test]
+//     fn abc() {
+//         let q = Qm::new(TaskOf::new(|| 12))
+//             .push(TaskOf::new(|| 'x'))
+//             .push(TaskOf::new(|| true));
+
+//         let mut pool = Pool::once(4);
+//         xyz(&mut pool);
+//     }
+
+//     fn xyz<P: ParThreadPool>(pool: &mut P) {
+//         pool.scoped_computation(|s| {
+//             P::run_in_scope(&s, || {
+//                 //
+//                 let a = 12;
+//             });
+//         });
+//     }
+// }
