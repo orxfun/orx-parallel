879b941ab3c2d4dfd9068aeba17449cb84ff3014
orxfun <orx.ugur.arikan@gmail.com>
2026-06-30 10:19:30 +0200

    initiate tsp example without using

---
+++ examples/use_demo_tsp/main.rs
@@ -0,0 +1,17 @@
+mod locations;
+mod par_with_use;
+mod par_without_use;
+
+use crate::{locations::location_for, par_without_use::run_search_parallel};
+
+fn main() {
+    let iterations = 10000;
+    let threads = 4;
+    let num_cities = 5;
+    let seed = 42;
+
+    let locations: Vec<_> = (0..num_cities).map(location_for).collect();
+
+    let x = run_search_parallel(&locations, iterations, seed, threads);
+    dbg!(x);
+}
