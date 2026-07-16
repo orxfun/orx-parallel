3d568e2829006ca0c745cb79c0b4be060712c88a
orxfun <orx.ugur.arikan@gmail.com>
2026-07-16 14:52:43 +0200

    react example

---
+++ examples/wasm/tsp/react/web/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [react()],
+    envPrefix: ["VITE_", "ORX_PARALLEL_"],
+    server: {
+        headers: {
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Cross-Origin-Embedder-Policy": "require-corp"
+        }
+    },
+    worker: {
+        format: "es"
+    }
+});
