c42ed032bf2faa9e09dfeef414c2f5fc5e7199b5
orxfun <orx.ugur.arikan@gmail.com>
2026-07-17 16:46:29 +0200

    revise vanilla tests

--- examples/wasm/tsp/vanilla/crate/src/lib.rs
+++ examples/wasm/tsp/vanilla/crate/src/lib.rs
@@ -1,8 +1,6 @@
 mod computation;
 mod locations;
+mod wasm_bindings;
 
 pub use computation::{run_search_parallel, run_search_sequential};
 pub use locations::create_locations;
-
-#[cfg(target_feature = "atomics")]
-mod wasm_bindings;
