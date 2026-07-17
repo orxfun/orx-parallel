0ebc373d332a0ab3e83aed58f6e6df68c3483ebf
orxfun <orx.ugur.arikan@gmail.com>
2026-07-17 09:24:23 +0200

    init yew example

---
+++ examples/wasm/tsp/yew/web/src/main.ts
@@ -0,0 +1,13 @@
+import init, { start_app } from "../pkg/orx_parallel_wasm_tsp_yew.js";
+import "./search-runner.ts";
+import "../styles.css";
+import hljs from "highlight.js/lib/core";
+import rust from "highlight.js/lib/languages/rust";
+import "highlight.js/styles/github-dark.css";
+
+hljs.registerLanguage("rust", rust);
+
+void init().then(() => {
+    start_app();
+    hljs.highlightAll();
+});
