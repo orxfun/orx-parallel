3985a98cd1f8c9b88b2e3051ff9e88149daf914d
orxfun <orx.ugur.arikan@gmail.com>
2026-07-20 13:20:18 +0200

    wire main.ts

---
+++ examples/wasm/tsp/vanilla/ui/src/main.ts
@@ -0,0 +1,3 @@
+import init, {
+    locations,
+} from "../pkg/orx_parallel_wasm_tsp_vanilla.js";
---
+++ examples/wasm/tsp/vanilla/ui/src/search-worker.ts
@@ -0,0 +1,5 @@
+// import init, {
+//     init_parallel_runtime,
+//     run_best_tour_par,
+//     run_best_tour_seq
+// } from "../pkg/orx_parallel_wasm_tsp_vanilla.js";
