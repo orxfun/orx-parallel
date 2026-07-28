730f9ab7eb19755566cc2d0b90b4e7d27644507d
orxfun <orx.ugur.arikan@gmail.com>
2026-07-28 13:16:56 +0200

    make adaptive chunk runner the default when std is available

--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -7,7 +7,11 @@ pub use par_runner::ParRunner;
 
 // default
 
+#[cfg(not(feature = "std"))]
 pub type DefaultRunner = runner_variants::FixedChunkRunner<crate::pool::DefaultPool>;
+
+#[cfg(feature = "std")]
+pub type DefaultRunner = runner_variants::AdaptiveChunkRunner<crate::pool::DefaultPool>;
 
 pub fn default_runner() -> DefaultRunner {
     DefaultRunner::new(Default::default())
