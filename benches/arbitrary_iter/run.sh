e0ae9b401f78160d13cfc8ad0009285ae3485cc8
orxfun <orx.ugur.arikan@gmail.com>
2026-07-29 17:19:47 +0200

    remove individual run.sh files

--- benches/arbitrary_iter/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-mkdir -p benches/arbitrary_iter/results
-
-cargo bench --bench arbitrary-iter-map-set-processing
-cp target/criterion/arbitrary_iter_map_set_processing/summary_arbitrary_iter_map_set_processing.csv benches/arbitrary_iter/results/map_set_processing.csv
--- benches/stateful_using/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-mkdir -p benches/stateful_using/results
-
-cargo bench --bench stateful-using-monte-carlo-batch
-cp target/criterion/stateful_using_monte_carlo_batch/summary_stateful_using_monte_carlo_batch.csv benches/stateful_using/results/monte_carlo_batch.csv
