cfd85f9c62d8e66b4ee6e9e88f9403e8e53f9e24
orxfun <orx.ugur.arikan@gmail.com>
2026-07-29 15:23:43 +0200

    update recursive run script to store the csv files

--- benches/recursive/run.sh
+++ benches/recursive/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+mkdir -p benches/recursive/results
+
 cargo bench --bench recursive-file-system
+cp target/criterion/recursive_tree_traversal/summary_recursive_file_system.csv benches/recursive/results/file_system.csv
+
 cargo bench --bench recursive-tree-traversal
+cp target/criterion/recursive_tree_traversal/summary_recursive_tree_traversal.csv benches/recursive/results/tree_traversal.csv
