437d9edd8890505d0f2fa3da79da41a8e4abaeaf
orxfun <orx.ugur.arikan@gmail.com>
2026-07-31 20:02:10 +0200

    doc

--- src/infallible/xap_enum.rs
+++ src/infallible/xap_enum.rs
@@ -1,7 +1,10 @@
 use crate::infallible::Xap;
 
+/// Enumerable extension of [Xap].
 pub trait XapEnumByInput: Xap {
+    /// Type of the enumerated computation, which also implements [Xap].
     type Enumerated: Xap<I = (usize, Self::I), O = (usize, Self::O), Size = Self::Size>;
 
+    /// Transforms the computation to enumerated one.
     fn enumerate(self) -> Self::Enumerated;
 }
