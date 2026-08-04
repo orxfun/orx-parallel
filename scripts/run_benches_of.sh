94fe9630757b1e0c01719f0ba7144b0b3fe70fab
orxfun <orx.ugur.arikan@gmail.com>
2026-08-04 21:16:39 +0200

    change results directory

--- scripts/run_benches_of.sh
+++ scripts/run_benches_of.sh
@@ -12,7 +12,7 @@ fi
 
 CATEGORY="$1"
 BENCH_DIR="benches/$CATEGORY"
-RESULTS_DIR="benches/results/$CATEGORY"
+RESULTS_DIR="docs/bench-ui/results/$CATEGORY"
 
 # Validate that the category directory exists
 if [[ ! -d "$BENCH_DIR" ]]; then
