6041ce00499466c7f4fba17c42f2ca1c095903ea
orxfun <orx.ugur.arikan@gmail.com>
2026-08-04 20:35:23 +0200

    revise benches-ui documentation

--- benches/xap_pll/mmm.rs
+++ benches/xap_pll/mmm.rs
@@ -214,7 +214,7 @@ fn run(c: &mut Criterion) {
     let treatments: Vec<_> = len.into_iter().map(|n| InputVariant { n }).collect();
     let variants = vec![Method::Iter, Method::XapPll];
 
-    Bench.bench(c, "xap_p_mmm", &treatments, &variants);
+    Bench.bench(c, "xap_pll_mmm", &treatments, &variants);
 }
 
 criterion_group!(benches, run);
