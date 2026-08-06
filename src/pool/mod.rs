4cf2e95a093e12704c3159c782ade8b1ffd93c2d
orxfun <orx.ugur.arikan@gmail.com>
2026-08-06 22:36:31 +0200

    define persistent global pool

--- src/pool/mod.rs
+++ src/pool/mod.rs
@@ -13,8 +13,6 @@ compile_error!(
     "Features 'wasm' and 'wasm_experimental' are mutually exclusive on wasm32; enable only one wasm backend feature."
 );
 
-pub use new_pool::Pool;
-
 pub use par_thread_pool::ParThreadPool;
 #[cfg(feature = "std")]
 pub use pool_impl::BasicPool;
@@ -35,22 +33,5 @@ pub use pool_impl::wasm_web_runtime_info;
 #[cfg(all(feature = "wasm", target_arch = "wasm32", target_feature = "atomics"))]
 pub use pool_impl::wasm_web_start_worker;
 
-#[cfg(all(feature = "std", feature = "wasm_experimental", target_arch = "wasm32"))]
-pub type DefaultPool = pool_impl::WasmWebPoolExp;
-#[cfg(all(
-    feature = "std",
-    feature = "wasm",
-    target_arch = "wasm32",
-    not(feature = "wasm_experimental")
-))]
-pub type DefaultPool = pool_impl::WasmWebPool;
-#[cfg(all(
-    feature = "std",
-    not(any(
-        all(feature = "wasm", target_arch = "wasm32"),
-        all(feature = "wasm_experimental", target_arch = "wasm32")
-    ))
-))]
-pub type DefaultPool = pool_impl::OncePool;
-#[cfg(not(feature = "std"))]
-pub type DefaultPool = pool_impl::SequentialPool;
+pub use global_pool::{DefaultPool, get_global_pool};
+pub use new_pool::Pool;
