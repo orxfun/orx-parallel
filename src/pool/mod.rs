673b08ec11ac2df98883b395066424a83e783bfc
orxfun <orx.ugur.arikan@gmail.com>
2026-08-06 22:43:19 +0200

    revise pool exports

--- src/pool/mod.rs
+++ src/pool/mod.rs
@@ -4,34 +4,84 @@ mod new_pool;
 mod par_thread_pool;
 mod pool_impl;
 
-#[cfg(all(
-    feature = "wasm",
-    feature = "wasm_experimental",
-    target_arch = "wasm32"
-))]
-compile_error!(
-    "Features 'wasm' and 'wasm_experimental' are mutually exclusive on wasm32; enable only one wasm backend feature."
-);
-
-pub use par_thread_pool::ParThreadPool;
-#[cfg(feature = "std")]
-pub use pool_impl::BasicPool;
-#[cfg(all(feature = "wasm", target_arch = "wasm32"))]
-pub use pool_impl::WasmWebPool;
-#[cfg(all(feature = "wasm_experimental", target_arch = "wasm32"))]
+// 1. wasm-experimental
+#[cfg(all(feature = "std", feature = "wasm_experimental", target_arch = "wasm32"))]
 pub use pool_impl::WasmWebPoolExp;
-#[cfg(all(feature = "wasm", target_arch = "wasm32", target_feature = "atomics"))]
-pub use pool_impl::init_thread_pool;
+
 #[cfg(all(
+    feature = "std",
     feature = "wasm_experimental",
     target_arch = "wasm32",
     target_feature = "atomics"
 ))]
 pub use pool_impl::init_thread_pool;
-#[cfg(all(feature = "wasm", target_arch = "wasm32", target_feature = "atomics"))]
+
+// 2. wasm
+#[cfg(all(
+    feature = "std",
+    feature = "wasm",
+    target_arch = "wasm32",
+    not(feature = "wasm_experimental"),
+))]
+pub use pool_impl::WasmWebPool;
+
+#[cfg(all(
+    feature = "std",
+    feature = "wasm",
+    target_arch = "wasm32",
+    not(feature = "wasm_experimental"),
+    target_feature = "atomics",
+))]
+pub use pool_impl::init_thread_pool;
+
+#[cfg(all(
+    feature = "std",
+    feature = "wasm",
+    target_arch = "wasm32",
+    not(feature = "wasm_experimental"),
+    target_feature = "atomics",
+))]
 pub use pool_impl::wasm_web_runtime_info;
-#[cfg(all(feature = "wasm", target_arch = "wasm32", target_feature = "atomics"))]
+
+#[cfg(all(
+    feature = "std",
+    feature = "wasm",
+    target_arch = "wasm32",
+    not(feature = "wasm_experimental"),
+    target_feature = "atomics",
+))]
 pub use pool_impl::wasm_web_start_worker;
+
+// 4. basic
+#[cfg(all(
+    feature = "std",
+    feature = "persistent_pool",
+    not(feature = "persistent_pool_rayon"),
+    not(feature = "wasm"),
+    not(feature = "wasm_experimental"),
+))]
+pub use pool_impl::BasicPool;
+
+// 5. once
+#[cfg(all(
+    feature = "std",
+    not(feature = "persistent_pool"),
+    not(feature = "persistent_pool_rayon"),
+    not(feature = "wasm"),
+    not(feature = "wasm_experimental"),
+))]
+pub use pool_impl::OncePool;
+
+// 6. sequential
+#[cfg(all(
+    not(feature = "std"),
+    not(feature = "persistent_pool"),
+    not(feature = "persistent_pool_rayon"),
+    not(feature = "wasm"),
+    not(feature = "wasm_experimental"),
+))]
+pub use pool_impl::SequentialPool;
 
 pub use global_pool::{DefaultPool, get_global_pool};
 pub use new_pool::Pool;
+pub use par_thread_pool::ParThreadPool;
