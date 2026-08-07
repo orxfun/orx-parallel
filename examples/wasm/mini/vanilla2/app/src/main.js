7a5f7fa4ec7df3174be0329faf358dc371597432
orxfun <orx.ugur.arikan@gmail.com>
2026-08-08 00:18:18 +0200

    update vanilla2 demo

--- examples/wasm/mini/vanilla2/app/src/main.js
+++ examples/wasm/mini/vanilla2/app/src/main.js
@@ -3,12 +3,15 @@ const threadsEl = document.getElementById("num_threads");
 const runEl = document.getElementById("run");
 const resultEl = document.getElementById("result");
 
-const threads = Number(threadsEl.value);
+// Set to `N > 0` to limit all computations by `worker` to at most `N` threads.
+// Set to `0` to create the pool with all available threads.
+const threadsInPool = 32;
+
 const worker = new Worker(new URL("./worker.js", import.meta.url), {
     type: "module"
 });
 
-worker.postMessage({ type: "init", threads });
+worker.postMessage({ type: "init", threads: threadsInPool });
 
 worker.addEventListener("message", (event) => {
     const data = event.data;
@@ -21,7 +24,11 @@ worker.addEventListener("message", (event) => {
 });
 
 runEl.addEventListener("click", () => {
+    // this particular computation will use:
+    // * `min(threadsInPool, threadsForComputation)` threads when `threadsForComputation > 0`,
+    // * `threadsInPool` threads when `threadsForComputation == 0`.
+    const threadsForComputation = Number(threadsEl.value);
     const input = Number(inputEl.value);
     resultEl.textContent = '-';
-    worker.postMessage({ type: "compute", input });
+    worker.postMessage({ type: "compute", input, threadsForComputation });
 });
--- examples/wasm/mini/vanilla2/app/src/worker.js
+++ examples/wasm/mini/vanilla2/app/src/worker.js
@@ -1,17 +1,14 @@
 import init, { init_parallel_runtime, compute } from "../pkg/wasm_bindings.js";
 
-let threads = 0;
-
 self.addEventListener("message", async (event) => {
     try {
         const data = event.data;
 
         if (data.type === "init") {
-            threads = data.threads;
             await init();
-            await init_parallel_runtime(threads);
+            await init_parallel_runtime(data.threadsInPool);
         } else if (data.type === "compute") {
-            const result = compute(data.input, threads);
+            const result = compute(data.input, data.threadsForComputation);
             self.postMessage({ type: "ok", result });
         }
     } catch (err) {
