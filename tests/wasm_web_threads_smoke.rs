83b911d2594274cb194da4215c97b9b85cca160a
orxfun <orx.ugur.arikan@gmail.com>
2026-08-08 15:31:43 +0200

    fix wasm tests

--- tests/wasm_web_threads_smoke.rs
+++ tests/wasm_web_threads_smoke.rs
@@ -9,9 +9,7 @@ wasm_bindgen_test_configure!(run_in_browser);
 #[wasm_bindgen_test]
 #[should_panic(expected = "not initialized")]
 fn wasm_web_pool_panics_without_init() {
-    let pool = Pool::wasm_web(2);
-
-    let _: Vec<usize> = (0..32).into_par().pool(pool).collect();
+    let _: Vec<usize> = (0..32).into_par().collect();
 }
 
 #[wasm_bindgen_test(async)]
@@ -20,8 +18,7 @@ async fn wasm_web_pool_runs_after_init() {
         .await
         .expect("init_thread_pool should resolve");
 
-    let pool = Pool::wasm_web(2);
-    let values: Vec<usize> = (0..100).into_par().pool(pool).collect();
+    let values: Vec<usize> = (0..100).into_par().collect();
     let sum: usize = values.into_iter().sum();
 
     assert_eq!(sum, (0..100usize).sum::<usize>());
