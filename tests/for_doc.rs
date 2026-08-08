6d5299fb504087c9ba9f1c0967e68930fa6a1c5e
orxfun <orx.ugur.arikan@gmail.com>
2026-08-08 21:03:15 +0200

    clean up

--- tests/for_doc.rs
+++ tests/for_doc.rs
@@ -1,18 +1,2 @@
 #[test]
-fn for_doc() {
-    use orx_parallel::*;
-
-    struct ThreadData {
-        sum: usize,
-    }
-
-    // define how to create thread-local variables
-    let mut data = UseVec::new(|_th_idx| ThreadData { sum: 0 });
-
-    (0..100_000)
-        .into_par() // ← mutably lend it to parallel iterator
-        .use_vec(&mut data)
-        .for_each(|d, x| d.sum += x); // ← d: &mut ThreadData
-
-    let results: Vec<ThreadData> = data.into_vec(); // ← get created vars back
-}
+fn for_doc() {}
