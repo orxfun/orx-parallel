8dc1210cd898026cda8d08b3ebe1f3eaa2c01c92
orxfun <orx.ugur.arikan@gmail.com>
2026-08-13 15:49:43 +0200

    collect benchmarks

---
+++ benchmarks/collect/filtermap/src/main.rs
@@ -0,0 +1,19 @@
+mod alg;
+mod exp;
+mod input;
+
+use crate::{alg::Method, exp::Exp, input::InputVariant};
+use bench_helper::{BenchArgs, runner};
+use clap::Parser;
+
+fn main() {
+    let args = BenchArgs::parse();
+
+    let ns = [16, 20];
+    let heavy = [true, false];
+
+    let combine_heavy = |n| heavy.map(|heavy| InputVariant { n, heavy });
+    let input_variants: Vec<_> = ns.into_iter().flat_map(combine_heavy).collect();
+
+    runner::run(&args, Exp, &input_variants, &Method::get());
+}
