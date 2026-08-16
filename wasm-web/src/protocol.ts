fd538d5822249a8ace287cbb5a5b77f03d7313b8
orxfun <orx.ugur.arikan@gmail.com>
2026-08-16 19:41:27 +0200

    initiate wasm-web and ParallelWorker

---
+++ wasm-web/src/protocol.ts
@@ -0,0 +1,24 @@
+export type WorkerRequest =
+    | {
+        type: "init";
+        bindingsUrl: string;
+        threads: number;
+        methods: string[];
+    }
+    | {
+        type: "call";
+        id: number;
+        method: string;
+        args: unknown[];
+    };
+
+export type WorkerResponse =
+    | { type: "ready" }
+    | { type: "result"; id: number; value: unknown }
+    | { type: "error"; id?: number; message: string };
+
+export type WasmBindings = {
+    default: (options?: unknown) => Promise<unknown>;
+    init_parallel_runtime: (threads: number) => Promise<unknown>;
+    [method: string]: unknown;
+};
