624af2e852ffff966dbf77db18069b385fc1cfaf
orxfun <orx.ugur.arikan@gmail.com>
2026-08-16 08:49:28 +0200

    revise genetic algorithm

--- benchmarks/algorithms/genetic_alg/src/input.rs
+++ benchmarks/algorithms/genetic_alg/src/input.rs
@@ -3,17 +3,19 @@ use orx_criterion::Factors;
 pub struct InputVariant {
     pub num_items: usize,
     pub population_size: usize,
+    pub heavy: bool,
 }
 
 impl Factors for InputVariant {
     fn factor_names() -> Vec<&'static str> {
-        vec!["items", "population"]
+        vec!["items", "population", "heavy"]
     }
 
     fn factor_levels(&self) -> Vec<String> {
         vec![
             format!("{}", self.num_items),
             format!("{}", self.population_size),
+            format!("{}", self.heavy),
         ]
     }
 }
--- benchmarks/algorithms/genetic_alg/src/main.rs
+++ benchmarks/algorithms/genetic_alg/src/main.rs
@@ -10,20 +10,22 @@ fn main() {
     let args = BenchArgs::parse();
 
     let num_items = [50, 100];
-    let num_items = [20];
-    let population_sizes = [100, 200];
+    let population_sizes = [10_000, 50_000];
+    let heavy = [false, true];
 
-    let combine_populations = |items| {
-        population_sizes.map(|pop| InputVariant {
-            num_items: items,
-            population_size: pop,
+    let for_items_pop = |num_items, population_size| {
+        heavy.map(|heavy| InputVariant {
+            num_items,
+            population_size,
+            heavy,
         })
     };
-
-    let input_variants: Vec<_> = num_items
-        .into_iter()
-        .flat_map(combine_populations)
-        .collect();
+    let for_items = |items| {
+        population_sizes
+            .into_iter()
+            .flat_map(move |pop| for_items_pop(items, pop))
+    };
+    let input_variants: Vec<_> = num_items.into_iter().flat_map(for_items).collect();
 
     bench_helper::runner::run(&args, Exp, &input_variants, &Method::get());
 }
