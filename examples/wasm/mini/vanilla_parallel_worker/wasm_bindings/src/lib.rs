fc3f4f57df1a2ee71cf95ddbbc927660cc161caa
orxfun <orx.ugur.arikan@gmail.com>
2026-08-18 08:30:57 +0200

    add a secondary computation

--- examples/wasm/mini/vanilla_parallel_worker/wasm_bindings/src/lib.rs
+++ examples/wasm/mini/vanilla_parallel_worker/wasm_bindings/src/lib.rs
@@ -13,3 +13,8 @@ pub fn init_parallel_runtime(num_threads: u32) -> js_sys::Promise {
 pub fn compute(input: u32, num_threads: u32) -> u64 {
     computation::compute(input as usize, num_threads as usize)
 }
+
+#[wasm_bindgen]
+pub fn compute_chunks(input: u32, num_threads: u32, chunk_size: u32) -> u64 {
+    computation::compute_chunks(input as usize, num_threads as usize, chunk_size as usize)
+}
