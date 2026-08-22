972e218079596177676dc67f4dbe61c83cdfa76c
orxfun <orx.ugur.arikan@gmail.com>
2026-08-22 20:28:56 +0200

    revise pool initiation of wasm

--- examples/wasm/tsp/leptos/app/src/main.ts
+++ examples/wasm/tsp/leptos/app/src/main.ts
@@ -7,9 +7,17 @@ import "highlight.js/styles/github-dark.css";
 
 hljs.registerLanguage("rust", rust);
 
+// 0 means "use all available threads"; passed to createSearchWorker and used to size the threads input
+const num_threads = 0;
+const MAX_THREADS = 32;
+
+function threadsCap(): number {
+    return num_threads > 0 ? num_threads : (navigator.hardwareConcurrency || MAX_THREADS);
+}
+
 void init().then(async () => {
-    await createSearchWorker(0);
-    start_app();
+    await createSearchWorker(num_threads);
+    start_app(threadsCap());
     hljs.highlightAll();
     window.addEventListener("beforeunload", terminateSearchWorker, { once: true });
 });
