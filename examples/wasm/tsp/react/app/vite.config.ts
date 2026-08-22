158eeaf85c9d1760219f2ac9986fa0db70911d76
orxfun <orx.ugur.arikan@gmail.com>
2026-08-22 19:58:23 +0200

    revise react example

--- examples/wasm/tsp/react/app/vite.config.ts
+++ examples/wasm/tsp/react/app/vite.config.ts
@@ -1,33 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { orxParallelWasm } from "orx-parallel-web/vite";
+import { orxParallelWasm } from "orx-parallel-wasm/vite";
 
 export default defineConfig({
     plugins: [
         react(),
         orxParallelWasm({
-            bindings: "../wasm_bindings",
-            outDir: "./pkg",
-            schemas: {
-                run_search: {
-                    args: [
-                        { type: "number" },
-                        { type: "bigint" },
-                        { type: "number" },
-                        { type: "number" },
-                        {
-                            type: "array",
-                            items: {
-                                type: "object",
-                                properties: {
-                                    x: { type: "number" },
-                                    y: { type: "number" }
-                                }
-                            }
-                        }
-                    ]
-                }
-            }
+            bindings: ["../wasm_bindings"],
         })
     ],
     base: "./",
