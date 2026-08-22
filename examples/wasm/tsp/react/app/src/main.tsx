68e1e2272372493c586a80b74746658716040251
orxfun <orx.ugur.arikan@gmail.com>
2026-08-22 19:58:23 +0200

    revise react example

--- examples/wasm/tsp/react/app/src/main.tsx
+++ examples/wasm/tsp/react/app/src/main.tsx
@@ -8,11 +8,14 @@ import init from "../pkg/wasm_bindings.js";
 import { App } from "./App";
 import { createSearchWorker } from "./search-runner";
 
+// 0 means "use all available threads"; passed to createSearchWorker and used to size the threads input
+const num_threads = 16;
+
 hljs.registerLanguage("rust", rust);
 
 async function bootstrap() {
     await init();
-    const searchWorker = await createSearchWorker(0);
+    const searchWorker = await createSearchWorker(num_threads);
 
     const rootNode = document.getElementById("root");
     if (!rootNode) {
