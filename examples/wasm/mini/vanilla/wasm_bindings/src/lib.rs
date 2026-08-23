83d79549580ba76b7b9da4cd8ed0578fac64b93a
orxfun <orx.ugur.arikan@gmail.com>
2026-08-23 20:57:39 +0200

    update wasm runtime initialization

--- examples/wasm/mini/vanilla/wasm_bindings/src/lib.rs
+++ examples/wasm/mini/vanilla/wasm_bindings/src/lib.rs
@@ -1,15 +1,6 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn init_parallel_runtime(num_threads: u32) -> js_sys::Promise {
-    #[cfg(target_feature = "atomics")]
-    return orx_parallel::init_thread_pool(num_threads as usize);
-
-    #[cfg(not(target_feature = "atomics"))]
-    panic!("init_parallel_runtime requires a wasm target with atomics and shared memory enabled")
-}
-
-#[wasm_bindgen]
 pub fn calculate_fibonacci(workload: u32, num_threads: u32) -> u64 {
     computation::calculate_fibonacci(workload as usize, num_threads as usize)
 }
