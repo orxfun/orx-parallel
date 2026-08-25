ff24fefdaa9abfef1cdde337635261b7f4ba4802
orxfun <orx.ugur.arikan@gmail.com>
2026-08-25 11:31:12 +0200

    wip

--- benchmarks/recursive/tree_search/src/main.rs
+++ benchmarks/recursive/tree_search/src/main.rs
@@ -9,16 +9,16 @@ use clap::Parser;
 fn main() {
     let args = BenchArgs::parse();
     let input_variants = [
-        InputVariant {
-            depth: 6,
-            fan_out: 8,
-            threshold: 5_000,
-        },
-        InputVariant {
-            depth: 8,
-            fan_out: 6,
-            threshold: 20_000,
-        },
+        // InputVariant {
+        //     depth: 6,
+        //     fan_out: 8,
+        //     threshold: 5_000,
+        // },
+        // InputVariant {
+        //     depth: 8,
+        //     fan_out: 6,
+        //     threshold: 20_000,
+        // },
         InputVariant {
             depth: 8,
             fan_out: 8,
