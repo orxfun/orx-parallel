8c6abfac79d0490f0d2d69bbdf2bdd236d2d235f
orxfun <orx.ugur.arikan@gmail.com>
2026-08-25 22:33:06 +0200

    wip

---
+++ src/recursive/infallible/par_iter.rs
@@ -0,0 +1,59 @@
+use crate::common_par_traits::ParInfCommon;
+use crate::infallible::{ParCore, ParIter, Xap};
+use crate::parameters::{ChunkSize, IterationOrder, NumThreads, Params};
+use crate::pool::ParThreadPool;
+use crate::runner::{DefaultRunner, ParRunner};
+use crate::{Par, ParCollectInto};
+use orx_concurrent_iter::ConcurrentIter;
+
+/// Parallel iterator.
+pub struct ParIterRecursive<I, X, C, E, R = DefaultRunner>
+where
+    I: IntoIterator,
+    X: Xap<I = I::Item>,
+    R: ParRunner,
+    C: IntoIterator<Item = X::I>,
+    E: Fn(&X::I) -> C + Send + Sync,
+{
+    iter: I,
+    xap: X,
+    exe: R,
+    params: Params,
+    extend: E,
+}
+
+impl<I, X, C, E, R> ParIterRecursive<I, X, C, E, R>
+where
+    I: IntoIterator,
+    X: Xap<I = I::Item>,
+    R: ParRunner,
+    C: IntoIterator<Item = X::I>,
+    E: Fn(&X::I) -> C + Send + Sync,
+{
+    //
+}
+
+impl<I, X, C, E, R> ParCore for ParIterRecursive<I, X, C, E, R>
+where
+    I: IntoIterator,
+    X: Xap<I = I::Item>,
+    R: ParRunner,
+    C: IntoIterator<Item = X::I>,
+    E: Fn(&X::I) -> C + Send + Sync,
+{
+    type Item = X::O;
+
+    type Runner = R;
+
+    type Input = I;
+
+    type Xap = X;
+
+    fn destruct(self) -> (Self::Input, Self::Xap, Self::Runner, Params) {
+        (self.iter, self.xap, self.exe, self.params)
+    }
+
+    fn get_runner(&self) -> &Self::Runner {
+        &self.exe
+    }
+}
