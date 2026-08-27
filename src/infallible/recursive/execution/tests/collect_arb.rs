f53e7d8544c824b7ef61aca0b5cd59f649613630
orxfun <orx.ugur.arikan@gmail.com>
2026-08-27 10:51:05 +0200

    improve into_outer ergonomy

--- src/infallible/recursive/execution/tests/collect_arb.rs
+++ src/infallible/recursive/execution/tests/collect_arb.rs
@@ -32,31 +32,3 @@ fn recursive_collect_arb(depth: usize, fan_out: usize) {
     assert_eq!(result.len(), expected.len());
     assert_eq!(result, expected);
 }
-
-#[test]
-fn abc() {
-    let depth = 3;
-    let fan_out = 2;
-
-    let mut rng = ChaCha8Rng::seed_from_u64(42);
-    let tree = Node::build_tree(depth, fan_out, &mut rng);
-
-    let xap = Id::<&Node>::new()
-        .map(|x| x.value)
-        .filter(|x| !x.is_multiple_of(7));
-
-    let runner = default_runner();
-    let params = Params::default();
-
-    let mut expected: Vec<_> = flatten([&tree], &|x: &&Node| &x.children)
-        .into_iter()
-        .flat_map(|x| xap.xap(x))
-        .collect();
-    expected.sort();
-
-    let mut result = collect_arb(runner, params, [&tree], xap, |x| &x.children);
-    result.sort();
-
-    assert_eq!(result.len(), expected.len());
-    assert_eq!(result, expected);
-}
