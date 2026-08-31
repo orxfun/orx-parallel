395023307f13e549a364b25de04b41e92a22a799
orxfun <orx.ugur.arikan@gmail.com>
2026-08-31 10:15:00 +0200

    merge_arb_into_vec is made generic

--- src/collectables/alg/merge_collected/merge_arbitrary_into.rs
+++ src/collectables/alg/merge_collected/merge_arbitrary_into.rs
@@ -1,15 +1,19 @@
+use crate::collectables::Collectable;
 use alloc::vec::Vec;
 use orx_split_vec::{Growth, SplitVec};
 
-pub fn merge_arb_into_vec<T>(results: Vec<Vec<T>>, dst: &mut Vec<T>) {
-    let total_len: usize = results.iter().map(|x| x.len()).sum();
-    dst.reserve(total_len);
+pub fn merge_arb_into_split_vec<T, G: Growth>(results: Vec<Vec<T>>, dst: &mut SplitVec<T, G>) {
     for vec in results {
         dst.extend(vec);
     }
 }
 
-pub fn merge_arb_into_split_vec<T, G: Growth>(results: Vec<Vec<T>>, dst: &mut SplitVec<T, G>) {
+pub fn merge_arb_into_vec<T, D>(results: Vec<D>, dst: &mut Vec<T>)
+where
+    D: Collectable<T>,
+{
+    let total_len: usize = results.iter().map(|x| x.len()).sum();
+    dst.reserve(total_len);
     for vec in results {
         dst.extend(vec);
     }
--- src/collectables/collectable.rs
+++ src/collectables/collectable.rs
@@ -1,5 +1,17 @@
-pub trait Collectable<O>: Extend<O> + Default + FromIterator<O> + Send {}
+pub trait Collectable<O>:
+    Extend<O> + Default + FromIterator<O> + IntoIterator<Item = O> + Send
+{
+    fn len(&self) -> usize;
+}
 
-impl<O: Send> Collectable<O> for alloc::vec::Vec<O> {}
+impl<O: Send> Collectable<O> for alloc::vec::Vec<O> {
+    fn len(&self) -> usize {
+        alloc::vec::Vec::len(self)
+    }
+}
 
-impl<O: Send + Ord> Collectable<O> for alloc::collections::BTreeSet<O> {}
+impl<O: Send + Ord> Collectable<O> for alloc::collections::BTreeSet<O> {
+    fn len(&self) -> usize {
+        alloc::collections::BTreeSet::len(self)
+    }
+}
