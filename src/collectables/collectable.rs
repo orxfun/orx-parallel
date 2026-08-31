07940fb57cf56a17248bc1957fcb00b46c81791d
orxfun <orx.ugur.arikan@gmail.com>
2026-08-31 10:08:14 +0200

    BTreeSet also implements collectable

--- src/collectables/collectable.rs
+++ src/collectables/collectable.rs
@@ -1,3 +1,5 @@
 pub trait Collectable<O>: Extend<O> + Default + FromIterator<O> + Send {}
 
 impl<O: Send> Collectable<O> for alloc::vec::Vec<O> {}
+
+impl<O: Send + Ord> Collectable<O> for alloc::collections::BTreeSet<O> {}
