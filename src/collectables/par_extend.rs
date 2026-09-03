ce4b2301d84a7f47308dc1e7c0992bfb21876304
orxfun <orx.ugur.arikan@gmail.com>
2026-09-03 21:38:15 +0200

    document par-extend

--- src/collectables/par_extend.rs
+++ src/collectables/par_extend.rs
@@ -1,6 +1,26 @@
 use crate::{Par, collectables::ParExtendCore};
 
+/// Extends an existing collection with items produced by a parallel iterator.
+///
+/// Implemented for collection types supported by [`ParExtendCore`].
 pub trait ParExtend<T>: ParExtendCore<T> {
+    /// Collects items from `iter` into this collection.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use orx_parallel::{IntoParIter, ParExtend};
+    ///
+    /// let mut values = vec![0];
+    /// values.par_extend((1..=4).into_par());
+    /// assert_eq!(values, vec![0, 1, 2, 3, 4]);
+    ///
+    /// use std::collections::HashSet;
+    ///
+    /// let mut values = HashSet::from([0]);
+    /// values.par_extend((1..=4).into_par());
+    /// assert_eq!(values, HashSet::from([0, 1, 2, 3, 4]));
+    /// ```
     fn par_extend(&mut self, iter: impl Par<Item = T>)
     where
         T: Send;
