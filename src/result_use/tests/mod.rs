6bb2e51ba9e334d7fc5e9dcf2f8bc0c2e09c5971
orxfun <orx.ugur.arikan@gmail.com>
2026-09-04 10:33:17 +0200

    organize test modules

--- src/result_use/tests/mod.rs
+++ src/result_use/tests/mod.rs
@@ -1,6 +1,14 @@
+#[cfg(feature = "long-tests")]
 mod compute;
+#[cfg(feature = "long-tests")]
 mod from_bin;
+#[cfg(feature = "long-tests")]
 mod from_id;
+#[cfg(feature = "long-tests")]
 mod from_many;
+#[cfg(feature = "long-tests")]
 mod from_map;
+#[cfg(feature = "long-tests")]
 mod utils;
+
+mod size_hint;
