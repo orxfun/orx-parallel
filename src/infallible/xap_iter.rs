95ebbf26581c8262c88100bd316fd2a66cf6b963
orxfun <orx.ugur.arikan@gmail.com>
2026-09-04 11:11:58 +0200

    init XapIter

--- src/infallible/xap_iter.rs
+++ src/infallible/xap_iter.rs
@@ -9,6 +9,16 @@ where
     xap: X,
 }
 
+impl<I, X> XapIter<I, X>
+where
+    I: Iterator,
+    X: Xap<I = I::Item>,
+{
+    pub fn new(iter: I, xap: X) -> Self {
+        Self { iter, xap }
+    }
+}
+
 impl<I, X> Iterator for XapIter<I, X>
 where
     I: Iterator,
