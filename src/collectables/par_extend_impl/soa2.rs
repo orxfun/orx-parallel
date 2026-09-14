0fc0f2c726231a73af073723367539ca607cd54d
orxfun <orx.ugur.arikan@gmail.com>
2026-09-14 12:06:22 +0200

    define Soa2 initialization

--- src/collectables/par_extend_impl/soa2.rs
+++ src/collectables/par_extend_impl/soa2.rs
@@ -6,6 +6,22 @@ pub struct Soa2<T1, T2> {
     v2: Vec<T2>,
 }
 
+impl<T1, T2> Soa2<T1, T2> {
+    pub fn new() -> Self {
+        Self {
+            v1: Vec::new(),
+            v2: Vec::new(),
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            v1: Vec::with_capacity(capacity),
+            v2: Vec::with_capacity(capacity),
+        }
+    }
+}
+
 impl<T1, T2> Extend<(T1, T2)> for Soa2<T1, T2> {
     fn extend<I: IntoIterator<Item = (T1, T2)>>(&mut self, iter: I) {
         for (i1, i2) in iter {
@@ -15,71 +31,71 @@ impl<T1, T2> Extend<(T1, T2)> for Soa2<T1, T2> {
     }
 }
 
-// impl<T1: Send, T2: Send> ParExtendCore<(T1, T2)> for Soa2<T1, T2> {
-//     type ThreadValues = Self;
+impl<T1: Send, T2: Send> ParExtendCore<(T1, T2)> for Soa2<T1, T2> {
+    type ThreadValues = Self;
 
-//     type OrderedThreadValues = ColAndPos<Self>;
+    type OrderedThreadValues = ColAndPos<Self>;
 
-//     fn new_thread_values() -> Self::ThreadValues {
-//         todo!()
-//     }
+    fn new_thread_values() -> Self::ThreadValues {
+        todo!()
+    }
 
-//     fn new_ordered_thread_values() -> Self::OrderedThreadValues {
-//         todo!()
-//     }
+    fn new_ordered_thread_values() -> Self::OrderedThreadValues {
+        todo!()
+    }
 
-//     fn add_thread_value(collected: &mut Self::ThreadValues, value: (T1, T2)) {
-//         todo!()
-//     }
+    fn add_thread_value(collected: &mut Self::ThreadValues, value: (T1, T2)) {
+        todo!()
+    }
 
-//     fn add_thread_values(
-//         collected: &mut Self::ThreadValues,
-//         values: impl IntoIterator<Item = (T1, T2)>,
-//     ) {
-//         todo!()
-//     }
+    fn add_thread_values(
+        collected: &mut Self::ThreadValues,
+        values: impl IntoIterator<Item = (T1, T2)>,
+    ) {
+        todo!()
+    }
 
-//     fn add_ordered_thread_value(
-//         collected: &mut Self::OrderedThreadValues,
-//         idx: usize,
-//         value: (T1, T2),
-//     ) {
-//         todo!()
-//     }
+    fn add_ordered_thread_value(
+        collected: &mut Self::OrderedThreadValues,
+        idx: usize,
+        value: (T1, T2),
+    ) {
+        todo!()
+    }
 
-//     fn add_ordered_thread_values(
-//         collected: &mut Self::OrderedThreadValues,
-//         idx: usize,
-//         values: impl IntoIterator<Item = (T1, T2)>,
-//     ) {
-//         todo!()
-//     }
+    fn add_ordered_thread_values(
+        collected: &mut Self::OrderedThreadValues,
+        idx: usize,
+        values: impl IntoIterator<Item = (T1, T2)>,
+    ) {
+        todo!()
+    }
 
-//     fn add_ordered_thread_optionals(
-//         collected: &mut Self::OrderedThreadValues,
-//         idx: usize,
-//         values: impl IntoIterator<Item = Option<(T1, T2)>>,
-//     ) -> Option<()> {
-//         todo!()
-//     }
+    fn add_ordered_thread_optionals(
+        collected: &mut Self::OrderedThreadValues,
+        idx: usize,
+        values: impl IntoIterator<Item = Option<(T1, T2)>>,
+    ) -> Option<()> {
+        todo!()
+    }
 
-//     fn add_ordered_thread_fallibles<E>(
-//         collected: &mut Self::OrderedThreadValues,
-//         idx: usize,
-//         values: impl IntoIterator<Item = Result<(T1, T2), E>>,
-//     ) -> Result<(), E> {
-//         todo!()
-//     }
+    fn add_ordered_thread_fallibles<E>(
+        collected: &mut Self::OrderedThreadValues,
+        idx: usize,
+        values: impl IntoIterator<Item = Result<(T1, T2), E>>,
+    ) -> Result<(), E> {
+        todo!()
+    }
 
-//     fn add_one(&mut self, value: (T1, T2)) {
-//         todo!()
-//     }
+    fn add_one(&mut self, value: (T1, T2)) {
+        todo!()
+    }
 
-//     fn extend_merge_infallibles(&mut self, thread_results: Vec<Self::ThreadValues>) {
-//         todo!()
-//     }
+    fn extend_merge_infallibles(&mut self, thread_results: Vec<Self::ThreadValues>) {
+        todo!()
+    }
 
-//     fn extend_merge_ordered_infallibles(&mut self, thread_results: Vec<Self::OrderedThreadValues>) {
-//         todo!()
-//     }
-// }
+    fn extend_merge_ordered_infallibles(&mut self, thread_results: Vec<Self::OrderedThreadValues>) {
+        todo!()
+    }
+}
